bacebcd5a9d2a54dcc9b730d978f639abbea3405
Jose Daniel Lara <jdlara@berkeley.edy>
2018-05-31 16:06:09 -0700

    add simulation models

---
+++ src/simulation_models/day_ahead.jl
@@ -0,0 +1,6 @@
+
+function DayAhead(Data)
+
+    #return a power simulation model loaded with a power model
+
+end
---
+++ src/simulation_models/real_time.jl
@@ -0,0 +1,6 @@
+
+function RealTime(Data)
+
+    #return a power simulation model loaded with a power model
+
+end
