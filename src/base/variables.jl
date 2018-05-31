ccad3fe6b0ca02f5e23e13ab18d60b4bdf72f205
Jose Daniel Lara <jdlara@berkeley.edy>
2018-05-31 16:02:31 -0700

    remove variables.jl

--- src/base/variables.jl
+++
@@ -1,5 +0,0 @@
-# To Do
-# 1. Eliminate P_g Variables that are not needed in a smart way. 
-# 2. Create Variables for ac_pf network model.
-# 3. Add limits to the constraints when creating the variable
-
