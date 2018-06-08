aed109fcc2ba85c5fa4bf5eff04eed0800a29bed
Jose Daniel Lara <jdlara@berkeley.edy>
2018-06-07 22:55:12 -0400

    Variable creation for different devices.

--- src/device_models.jl/electric_loads.jl
+++ src/device_models.jl/electric_loads.jl
@@ -1,8 +1,6 @@
-
-
-function LoadVariables(m::JuMP.Model, devices::Array{T,1}, T) where T <: InterruptibleLoad
-    on_set = [d.name for d in devices if d.status == true]
+function LoadVariables(m::JuMP.Model, devices::Array{T,1}, T) where T <: ElectricLoad
+    on_set = [d.name for d in devices if d.status == true && !isa(d, StaticLoad)]
     t = 1:T
     @variable(m::JuMP.Model, P_cl[on_set,t]) # Power output of generators
-    return true    
+    return P_cl    
 end
--- src/device_models.jl/thermal_generation.jl
+++ src/device_models.jl/thermal_generation.jl
@@ -1,10 +1,12 @@
 
+# TODO: Change the types in T from generator to Thermal 
+
 """
 This function add the variables for power generation output to the model
 """
-function GenerationVariables(m::JuMP.Model, devices::Array{T,1}, T) where T <: Thermal 
+function GenerationVariables(m::JuMP.Model, devices::Array{T,1}, Time) where T <: Generator 
     on_set = [d.name for d in devices if d.status == true]
-    t = 1:T
+    t = 1:Time
     @variable(m::JuMP.Model, P_th[on_set,t]) # Power output of generators
     return true    
 end
@@ -12,34 +14,49 @@ end
 """
 This function add the variables for power generation commitment to the model
 """
-function CommitmentVariables(m::JuMP.Model, devices::Array{T,1}, T) where T <: Thermal 
+function CommitmentVariables(m::JuMP.Model, devices::Array{T,1}, Time) where T <: Generator 
     on_set = [d.name for d in devices if d.status == true]
-    t = 1:T
-    @variable(m::JuMP.Model, on_th[on_set,t]) # Power output of generators
-    @variable(m::JuMP.Model, start_th[on_set,t]) # Power output of generators
-    @variable(m::JuMP.Model, stop_th[on_set,t]) # Power output of generators
+    t = 1:Time
+    @variable(m::JuMP.Model, on_th[on_set,t], Bin) # Power output of generators
+    @variable(m::JuMP.Model, start_th[on_set,t], Bin) # Power output of generators
+    @variable(m::JuMP.Model, stop_th[on_set,t], Bin) # Power output of generators
     return true    
 end
 
+"""
+This function adds the power limits of generators when there are no CommitmentVariables
+"""
+function PowerConstraints(m::JuMP.Model, P_th, devices::Array{T,1}, Time) where T <: Generator 
 
-function powerconstraints(P_th::JuMP.JuMPArray{JuMP.Variable}, devices::Array{T,1}, T) where T <: Thermal 
-
+    (length(P_th.indexsets[2]) != Time) ? error("Length of time dimension inconsistent"): true
     for (ix, name) in enumerate(P_th.indexsets[1])
-        if name == devices[ix].name
-            powerconstraints(EconomicDispatch, P_g[name,:], generators5[ix])
-        
-        else
-            error("Bus name in Array and variable do not match")
-        end
+            if name == devices[ix].name
+                for t in P_th.indexsets[2]
+                    @constraint(m, P_th[name, t] >= devices[ix].tech.realpowerlimits.min)
+                    @constraint(m, P_th[name, t] <= devices[ix].tech.realpowerlimits.max)
+                end    
+            else
+                error("Bus name in Array and variable do not match")
+            end
     end
-    
-    end
-function RampConstraints(m::JuMP.Model, P_g::JuMP.JuMPArray{JuMP.Variable}, source::ThermalGen)
-
-
+    return true
 end
 
-function TimeConstraints(m::JuMP.Model, UC_g::JuMP.JuMPArray{JuMP.Variable}, source::Thermalgen) 
+"""
+This function adds the power limits of generators when there are CommitmentVariables
+"""
+function PowerConstraints(m::JuMP.Model, P_th, on_th, devices::Array{T,1}, Time) where T <: Generator 
 
-
+    (length(P_th.indexsets[2]) != Time) ? error("Length of time dimension inconsistent"): true
+    for (ix, name) in enumerate(P_th.indexsets[1])
+            if name == devices[ix].name
+                for t in P_th.indexsets[2]
+                    @constraint(m, P_th[name, t] >= devices[ix].tech.realpowerlimits.min*on_th[name,t])
+                    @constraint(m, P_th[name, t] <= devices[ix].tech.realpowerlimits.max*on_th[name,t])
+                end    
+            else
+                error("Bus name in Array and variable do not match")
+            end
+    end
+    return true
 end
