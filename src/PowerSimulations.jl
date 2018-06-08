cb98f3015b3cc99c064117ea09bae9672d6d06cf
Jose Daniel Lara <jdlara@berkeley.edy>
2018-06-07 22:55:59 -0400

    Changes to PowerSimulations.jl

--- src/PowerSimulations.jl
+++ src/PowerSimulations.jl
@@ -8,11 +8,15 @@ using Clp
 using Cbc
 using Compat 
 
+
+include("device_models/thermal_generation")
+
+#=
 include("core_abstract/dynamic_model.jl")
 include("core_abstract/abstract_model.jl")
 include("base/network_constructors.jl")
 include("base/simulation_constructors.jl")
 include("base/solve_model.jl")
-
+=#
 
 end # module
