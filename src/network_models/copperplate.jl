e8ad636389ef1eb659653db5cdf400f6a74b98ee
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-20 15:57:35 -0600

    Update copper plate network model

--- src/network_models/copperplate.jl
+++ src/network_models/copperplate.jl
@@ -2,7 +2,7 @@ function CopperPlateNetwork(m::JuMP.Model, NetInjection::Array{JuMP.AffExpr}, tp
     @constraintref cpn[1:tp]
     for t in 1:tp
         # TODO: Check is sum() is the best way to do this. Update in JuMP 0.19 to append!()
-        cpn[t] = @constraint(m, sum(Nets[:,t]) == 0)
+        cpn[t] = @constraint(m, sum(NetInjection[:,t]) == 0)
     end
 
     return true
