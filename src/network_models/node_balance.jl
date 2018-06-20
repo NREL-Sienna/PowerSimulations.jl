d25980b93757c915050c7f683502c16ea4cf4bce
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-20 15:17:49 -0600

    Create Injection Expressions

--- src/network_models/node_balance.jl
+++ src/network_models/node_balance.jl
@@ -1,55 +1,136 @@
-function BranchFlowVariables(m::JuMP.Model, devices::Array{T,1}, time_steps) where T <: Branch
-    on_set = [d.name for d in devices if d.available == true]
-    t = 1:time_steps
-    @variable(m, fbr[on_set,t])
-    return fbr
+function NetInjectIterate!(NetInjection::Array{JuMP.AffExpr}, buses::Array{Bus}, variable::PowerVariable, tp::Int, device::Array{T}) where T <: PowerSystems.ThermalGen
+
+        for (idx, b) in enumerate(buses)
+
+            for t = 1:tp
+
+                set = [d.name for d in device if d.bus == b]
+
+                isempty(set) ? break : total = sum(variable[i,t] for i in set)
+
+                isassigned(NetInjection, idx,t) ? append!(NetInjection[idx,t], total): NetInjection[idx,t] = total;
+
+            end
+        end
+
+    return NetInjection
+
 end
 
-function CopperPlateNetwork(m::JuMP.Model, sys, flows, tp)
-    devices = sys.network.branches
-    @constraintref cpn[1:tp]
-    on_set = [d.name for d in devices if d.available == true]
-    for t in 1:tp
-        cpn[t] = @constraint(m, sum(flows[i, t] for i in on_set) == 0)
-    end
+function NetInjectIterate!(NetInjection::Array{JuMP.AffExpr}, buses::Array{Bus}, variable::PowerVariable, tp::Int, device::Array{T}) where T <: PowerSystems.RenewableGen
 
-    return true
+        for (idx, b) in enumerate(buses)
+
+            for t = 1:tp
+
+                devices = [d for d in device if d.bus == b]
+
+                if isempty(devices)
+                    break
+                else
+                    set = [d.name for d in devices if d.bus == b && !isa(d, PowerSystems.RenewableFix)]
+                    isempty(set)? total = 0.0 : total = sum(variable[i,t] for i in set)
+                    total = total + sum([d.tech.installedcapacity*d.scalingfactor.values[t] for d in devices])
+
+                end
+
+                isassigned(NetInjection, idx,t) ? append!(NetInjection[idx,t], total): NetInjection[idx,t] = total;
+
+            end
+        end
+
+    return NetInjection
+
 end
 
-function KCLBalance(m, sys, fbr, pth, pcl, tp)
-    @constraintref kcl[1:length(sys.buses), 1:tp]
+function NetInjectIterate!(NetInjection::Array{JuMP.AffExpr}, buses::Array{Bus}, variable::PowerVariable, tp::Int, device::Array{T}) where T <: PowerSystems.HydroGen
 
-    for (idx, b) in enumerate(sys.buses)
-        # Branch Flows
-        branches_from = [br.name for br in sys.network.branches if br.connectionpoints.from == b]
-        branches_to = [br.name for br in sys.network.branches if br.connectionpoints.to == b]
-        thermal = [th.name for th in sys.generators["Thermal"] if th.bus == b]
-        loads = [cl.name for cl in sys.loads if cl.bus == b && !isa(cl, PowerSystems.StaticLoad)]
+        for (idx, b) in enumerate(buses)
+
+            for t = 1:tp
+
+                devices = [d for d in device if d.bus == b]
+
+                if isempty(devices)
+                    break
+                else
+                    set = [d.name for d in devices if d.bus == b && !isa(d, PowerSystems.HydroFix)]
+                    isempty(set)? total = 0.0 : total = sum(variable[i,t] for i in set)
+                    total = total + sum([d.tech.installedcapacity*d.scalingfactor.values[t] for d in devices])
+
+                end
+
+                isassigned(NetInjection, idx,t) ? append!(NetInjection[idx,t], total): NetInjection[idx,t] = total;
+
+            end
+        end
+
+    return NetInjection
+end
+
+function NetInjectIterate!(NetInjection::Array{JuMP.AffExpr}, buses::Array{Bus}, variable::PowerVariable, tp::Int, device::Array{T}) where T <: PowerSystems.ElectricLoad
+
+    for (idx, b) in enumerate(buses)
 
         for t = 1:tp
 
-            # TODO: check for islanded buses
-            isempty(branches_from) ? total_flows_from = 0.0 : total_flows_from = sum(fbr[i,t] for i in branches_from)
-            isempty(branches_to) ? total_flows_to = 0.0 : total_flows_to = sum(fbr[i,t] for i in branches_to)
+            devices = [d for d in device if d.bus == b]
 
-            isempty(thermal) ? total_th = 0.0 : total_th = sum(pth[i,t] for i in thermal)
+            if isempty(devices)
+                break
+            else
+                set = [d.name for d in devices if d.bus == b && !isa(d, PowerSystems.StaticLoad)]
+                isempty(set)? total = 0.0 : total = sum(variable[i,t] for i in set)
+                total = total + sum([d.maxrealpower*d.scalingfactor.values[t] for d in devices])
 
-            # isempty(renewable) ? total_re = 0.0 : total_re = sum(P_re[i,t] for i in renewable)
-            # isempty(storage) ? total_st = 0.0 : total_st = sum(Pin[i,t] - Pout[i,t] for i in storage)
-            # isempty(hydro) ? total_hy = 0.0 : total_hy = sum(Phy[i,t] for i in hydro)
+            end
 
-            isempty(loads) ? total_cl = 0.0 : total_cl = sum(pcl[i,t] for i in loads)
-
-            staticload = [sl.maxrealpower*sl.scalingfactor.values[t] for sl in sys.loads if sl.bus == b]
-
-            isempty(staticload) ? total_staticload = 0.0 : total_staticload = sum(staticload)
-            # FixedRenewables = sum([sl.maxrealpower*sl.scalingfactor.values[t] for sl in sys.loads if sl.bus == b && isa(sl, StaticLoad)])
-
-            kcl[idx, t] = @constraint(m, total_flows_to - total_flows_from + total_th + total_cl - total_staticload == 0)
+            isassigned(NetInjection, idx,t) ? append!(NetInjection[idx,t], total): NetInjection[idx,t] = total;
 
         end
-
     end
+
+    return NetInjection
 
 end
 
+function NetInjectIterate!(NetInjection::Array{JuMP.AffExpr}, buses::Array{Bus}, variable_in::PowerVariable, variable_out::PowerVariable, tp::Int, device::Array{T}) where T <: PowerSystems.Storage
+
+        for (idx, b) in enumerate(buses)
+
+            for t = 1:tp
+
+                set = [d.name for d in device if d.bus == b]
+
+                #Detects if there is a device connected at the node, if not, breaks the time loop and goes to the next bus.
+                isempty(set) ? break : total = sum(variable_in[i,t] - variable_out[i,t] for i in set)
+
+                isassigned(NetInjection, idx,t) ? append!(NetInjection[idx,t], total): NetInjection[idx,t] = total;
+
+            end
+        end
+
+    return NetInjection
+
+end
+
+
+
+function InjectionExpressions(m::JuMP.Model, sys::PowerSystems.PowerSystem; var_th=nothing, var_re=nothing, phy=nothing, var_cl=nothing, var_in=nothing, var_out=nothing)
+
+    NetInjection =  Array{JuMP.AffExpr}(length(sys.buses), sys.timesteps)
+
+    # TODO: Iterate over generator types in PowerSystems.Generators to enable any type of possivle future generation types
+
+    !isa(sys.generators.thermal,Void)? NetInjection = NetInjectIterate!(NetInjection, sys.buses, var_th, sys.timesteps, sys.generators.thermal): true
+
+    !isa(sys.generators.renewable,Void)? NetInjection = NetInjectIterate!(NetInjection, sys.buses, var_re, sys.timesteps, sys.generators.renewable): true
+
+    !isa(sys.generators.hydro,Void)? NetInjection= NetInjectIterate!(NetInjection, sys.buses, phy, sys.timesteps, sys.generators.hydro): true
+
+    !isa(sys.storage,Void)? NetInjection = NetInjectIterate!(NetInjection, sys.buses, var_in, var_out, sys.timesteps, sys.storage) : true
+
+    NetInjection = NetInjectIterate!(NetInjection, sys.buses, var_cl, sys.timesteps, sys.loads)
+
+    return NetInjection
+end
