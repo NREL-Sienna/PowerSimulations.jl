2d52182768d9c101285372189668a3dac03612fc
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-20 15:17:33 -0600

    Re-define constructors

--- src/base/simulation_constructors.jl
+++ src/base/simulation_constructors.jl
@@ -7,13 +7,6 @@ The constructor takes de information from the AbstractPowerModel to build the va
 
 function construct_model(model::AbstractPowerModel, data)
 
-    #Get all on devices in the system
-    t = 1:PowerSystem.timesteps
-    g_on_set = [g.name for g in PowerSystem.generators if g.available == true]
-    e_on_set = [e.name for e in PowerSystem.storage if e.available == true]
-    cl_on_set = [cl.name for cl in PowerSystem.loads if (cl.available == true && typeof(cl) != StaticLoad)]
-
-
     add_variables(m::JuMP.Model, data)
     power_limits(m::JuMP.Model, data)
     network(m::JuMP.Model, data)
