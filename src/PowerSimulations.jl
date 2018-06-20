8e54dd23c73fa6a7f8f9e44c978d4df7bd9a0c36
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-20 15:18:58 -0600

    update PowerSimulations.jl to include new file structure

--- src/PowerSimulations.jl
+++ src/PowerSimulations.jl
@@ -9,15 +9,24 @@ using Compat
 #include("core/models.jl")
 #include("core/simulations.jl")
 
+const PowerVariable = JuMP.JuMPArray{JuMP.Variable,2,Tuple{Array{String,1},UnitRange{Int64}}}
+
+#Device Modeling components
 include("device_models/renewable_generation.jl")
 include("device_models/thermal_generation.jl")
 include("device_models/storage.jl")
 include("device_models/hydro_generation.jl")
 include("device_models/electric_loads.jl")
+include("device_models/branches.jl")
 
+#Network related components
 include("network_models/node_balance.jl")
 
-include("cost_functions/variable_cost.jl")
-include("power_models/economic_dispatch.jl")
+#Cost Components
+include("cost_functions/thermalgenvariable_cost.jl")
+include("cost_functions/thermalgencommitment_cost.jl")
+include("cost_functions/renewablegencurtailment_cost.jl")
+include("cost_functions/controlableload_cost.jl")
+#include("power_models/economic_dispatch.jl")
 
 end # module
