f9ad8ef5241a3777cc87bafc884e8c8a305b57e5
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-22 10:29:44 -0600

    add branch limit function template

--- src/device_models/branches.jl
+++ src/device_models/branches.jl
@@ -4,3 +4,22 @@ function BranchFlowVariables(m::JuMP.Model, devices::Array{T,1}, time_steps) whe
     @variable(m, fbr[on_set,t])
     return fbr
 end
+
+function FlowConstraints(m::JuMP.Model, fbr::PowerVariable, devices::Array{T,1}, time_periods::Int) where T <: ThermalGen
+    (length(pth.indexsets[2]) != time_periods) ? error("Length of time dimension inconsistent"): true
+    # TODO: @constraintref dissapears in JuMP 0.19. A new syntax goes here.
+    # JuMP.JuMPArray(Array{ConstraintRef}(JuMP.size(x)), x.indexsets[1], x.indexsets[2])
+    @constraintref Pmaxth[1:length(pth.indexsets[1]),1:length(pth.indexsets[2])]
+    @constraintref Pminth[1:length(pth.indexsets[1]),1:length(pth.indexsets[2])]
+    for (ix, name) in enumerate(pth.indexsets[1])
+            if name == devices[ix].name
+                for t in pth.indexsets[2]
+                    Pminth[ix, t] = @constraint(m, pth[name, t] >= devices[ix].tech.realpowerlimits.min)
+                    Pmaxth[ix, t] = @constraint(m, pth[name, t] <= devices[ix].tech.realpowerlimits.max)
+                end
+            else
+                error("Bus name in Array and variable do not match")
+            end
+    end
+    return true
+end
