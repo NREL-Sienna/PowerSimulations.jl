f5bcdeb4e26b39f0476de0b289475708ebc1f16f
Jose Daniel Lara <jdlara@berkeley.edu>
2018-06-27 23:36:55 -0600

    change tp to time_periods

--- src/network_models/copperplate_balance.jl
+++ src/network_models/copperplate_balance.jl
@@ -1,6 +1,7 @@
-function CopperPlateBalance(m::JuMP.Model, NetInjection::Array{JuMP.AffExpr}, tp::Int)
-    @constraintref cpn[1:tp]
-    for t in 1:tp
+function CopperPlateBalance(m::JuMP.Model, NetInjection::Array{JuMP.AffExpr}, time_periods::Int)
+    @constraintref cpn[1:time_periods]
+
+    for t in 1:time_periods
         # TODO: Check is sum() is the best way to do this. Update in JuMP 0.19 to append!()
         cpn[t] = @constraint(m, sum(NetInjection[:,t]) == 0)
     end
