c3f5e5cd39ac5c35dc3e4d02845d47e888f057b0
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-11 17:00:03 -0600

    add tests for cost constructors

---
+++ test/costfunction_testing.jl
@@ -0,0 +1,37 @@
+using PowerSystems
+using PowerSimulations
+using JuMP
+
+include(string(homedir(),"/.julia/v0.6/PowerSystems/data/data_5bus.jl"))
+
+battery = [GenericBattery(name = "Bat",
+                status = true,
+                energy = 10.0,
+                realpower = 10.0,
+                capacity = @NT(min = 0.0, max = 10.0,),
+                inputrealpowerlimit = 10.0,
+                outputrealpowerlimit = 10.0,
+                efficiency = @NT(in = 0.90, out = 0.80),
+                )];
+
+generators_hg = [
+    HydroFix("HydroFix",true,nodes5[2],
+        TechHydro(60.0, 15.0, @NT(min = 0.0, max = 60.0), nothing, nothing, nothing, nothing),
+        TimeSeries.TimeArray(DayAhead,solar_ts_DA)
+    ),
+    HydroCurtailment("HydroCurtailment",true,nodes5[3],
+        TechHydro(60.0, 10.0, @NT(min = 0.0, max = 60.0), nothing, nothing, @NT(up = 10.0, down = 10.0), nothing),
+        1000.0,TimeSeries.TimeArray(DayAhead,wind_ts_DA) )
+]
+
+sys5b = PowerSystem(nodes5, append!(generators5, generators_hg), loads5_DA, branches5, battery, 230.0, 1000.0)
+
+m=Model()
+pcl_vars = PowerSimulations.loadvariables(m, sys5b.loads, sys5b.time_periods)
+pcl = [d for d in sys5b.loads if isa(d, PowerSystems.InterruptibleLoad)]
+
+tl = PowerSimulations.variablecost(pcl_vars, pcl)
+
+pre_vars = PowerSimulations.generationvariables(m, sys5b.generators.renewable, sys5b.time_periods)
+pre = [d for d in sys5b.generators.renewable if !isa(d, PowerSystems.RenewableFix)]
+tre = PowerSimulations.variablecost(pre_vars, pre)
