4a99c59bff461218ea5fca449a3b1f9785943f0b
Sourabh Dalvi <sourabh.dalvi@nrel.gov>
2018-07-12 00:42:03 -0600

    5bus PWL cost function implementation( needs improvements )

--- src/cost_functions/thermalgenvariable_cost.jl
+++ src/cost_functions/thermalgenvariable_cost.jl
@@ -1,17 +1,23 @@
-function variablecostgen(pth::JuMP.JuMPArray{JuMP.Variable}, generators::Array{T}) where T <: PowerSystems.ThermalGen
+function variablecostgen(m::JuMP.Model, pth::JuMP.JuMPArray{JuMP.Variable}, generators::Array{T}) where T <: PowerSystems.ThermalGen
 
     cost = 0.0;
-
+    linear, pwl, func = PowerSimulations.cost_filter(pth, generators)
     for (ix, name) in enumerate(pth.indexsets[1])
         if name == generators[ix].name
-            for time in pth.indexsets[2]
-                cost = cost + gencost(pth[string(name),time], generators[ix].econ.variablecost)
+            if (name in linear) | (name in func)
+                for time in pth.indexsets[2]
+                    cost = cost + PowerSimulations.gencost(pth[string(name),time], generators[ix].econ.variablecost)
+                end
+            elseif name in pwl
+                cost = cost + gencost(m, pth[string(name),pth.indexsets[2]], generators[ix].econ.variablecost)
+            else
+                warn("Cost type unknown")
             end
         else
             error("Bus name in Array and variable do not match")
         end
     end
-
+    
     return cost
 
 end
@@ -24,4 +30,38 @@ end
 function gencost(X::JuMP.Variable, cost_component::Function)
 
     return cost_component(X)
+end
+
+function cost_filter(pth::JuMP.JuMPArray{JuMP.Variable},generators::Array{T}) where T <: PowerSystems.ThermalGen
+    linear = []
+    pwl = []
+    func= []
+    for (ix, name) in enumerate(pth.indexsets[1])
+        if name == generators[ix].name
+            var_c = generators[ix].econ.variablecost
+            isa(var_c, Real) ? push!(linear,name) : isa(var_c, Function) ? push!(func,name) : isa(var_c, Array) ? push!(pwl,name) : warn("Cost type unknown")
+        end
+    end
+    return linear, pwl, func
+end
+
+function gencost(m::JuMP.Model, pth::Array{JuMP.Variable,1},  cost_component::Array)
+    cost_p =  [i for (ix,i) in enumerate(cost_component) if iseven(ix)]
+    power_p = [i for (ix,i) in enumerate(cost_component) if isodd(ix)]
+    time = 1:Int(length(pth))
+    n = 1:Int((length(cost_component)/2)-1)
+    @variable(m, pwl[time,n])
+    @constraintref pwl_gen[1:length(time)]
+    @constraintref pwl_limit[1:length(time),n]     
+    for t in time                        
+        for i in n
+            pwl_limit[t,n] = @constraint(m, pwl[t,i] <= power_p[i+1]-power_p[i] )
+        end
+    end
+    for t in time
+        pwl_gen[t] = @constraint(m, sum([pwl[t, i] for i in n ])== pth[t])
+    end
+    var = [pwl[i,j] for j in n for i in time] 
+    cost = JuMP.AffExpr(var,repeat(cost_p[2:end]./power_p[2:end],inner=length(time)),0.0)
+    return cost
 end
