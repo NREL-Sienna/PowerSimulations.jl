97661c9463fd1df9cba0831f4a5ad8ae9fc5efb0
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-07-13 15:38:45 -0600

    Rename AbstractPowerModel to AbstractPowerSimulationModel

--- src/power_models/economic_dispatch.jl
+++ src/power_models/economic_dispatch.jl
@@ -1,5 +1,5 @@
 """
-This function populates a particular AbstractPowerModel and populates the fields according to the
+This function populates a particular AbstractPowerSimulationModel and populates the fields according to the
     data in the system and stablishes the device models included which effectively are calls to the construction
     functions.
 """
