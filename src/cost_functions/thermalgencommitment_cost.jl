1757a2367b0fb707b6e68d9d925568ea7f3333a6
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-12 18:20:02 -0600

    Correct commitment cost model

--- src/cost_functions/thermalgencommitment_cost.jl
+++ src/cost_functions/thermalgencommitment_cost.jl
@@ -1,31 +1,27 @@
-function commitmentcost(start::JuMP.JuMPArray{JuMP.Variable}, stop::JuMP.JuMPArray{JuMP.Variable}, status::JuMP.JuMPArray{JuMP.Variable}, generators::Array{ThermalGen})
+function commitmentcost(m::JuMP.Model, start::PowerVariable, stop::PowerVariable, status::PowerVariable, generators::Array{T}) where T <: ThermalGen
 
     cost = JuMP.AffExpr()
 
     for (ix, name) in enumerate(status.indexsets[1])
         if name == generators[ix].name
-            for time in status.indexsets[2]
 
                 if generators[ix].econ.startupcost > 0
 
-                    append!(cost,cost(start[string(name),time], generators[ix].econ.startupcost))
+                    append!(cost,sum(start[string(name),:]*generators[ix].econ.startupcost))
 
                 end
 
                 if generators[ix].econ.shutdncost > 0
 
-                    append!(cost,cost(stop[string(name),time], generators[ix].econ.shutdncost))
+                    append!(cost,sum(stop[name,:]*generators[ix].econ.shutdncost))
 
                 end
 
                 if generators[ix].econ.fixedcost > 0
 
-                    append!(cost,cost(status[string(name),time], generators[ix].econ.fixedcost))
+                    append!(cost,sum(status[name,:]*generators[ix].econ.fixedcost))
 
                 end
-
-
-            end
         else
             error("Bus name in Array and variable do not match")
         end
