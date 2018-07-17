35b581f686558030145633c3c3694b2a8a395be4
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-17 11:36:29 -0600

    Test to build and solve a ED problem

--- test/buildEC_testing.jl
+++ test/buildEC_testing.jl
@@ -1,36 +1,36 @@
 using PowerSystems
+using PowerSimulations
 using JuMP
+using Clp
 
 include(string(homedir(),"/.julia/v0.6/PowerSystems/data/data_5bus.jl"))
 
-battery = [GenericBattery(name = "Bat",
-                status = true,
-                energy = 10.0,
-                realpower = 10.0,
-                capacity = @NT(min = 0.0, max = 10.0,),
-                inputrealpowerlimit = 10.0,
-                outputrealpowerlimit = 10.0,
-                efficiency = @NT(in = 0.90, out = 0.80),
-                )];
+sys5b = PowerSystem(nodes5, generators5, loads5_DA, branches5, nothing, 230.0, 1000.0)
 
-generators_hg = [
-    HydroFix("HydroFix",true,nodes5[2],
-        TechHydro(60.0, 15.0, @NT(min = 0.0, max = 60.0), nothing, nothing, nothing, nothing),
-        TimeSeries.TimeArray(DayAhead,solar_ts_DA)
-    ),
-    HydroCurtailment("HydroCurtailment",true,nodes5[3],
-        TechHydro(60.0, 10.0, @NT(min = 0.0, max = 60.0), nothing, nothing, @NT(up = 10.0, down = 10.0), nothing),
-        1000.0,TimeSeries.TimeArray(DayAhead,wind_ts_DA) )
-]
+m=JuMP.Model()
+DevicesNetInjection =  Array{JuMP.GenericAffExpr{Float64,JuMP.Variable},2}(length(sys5b.buses), sys5b.time_periods)
 
-sys5b = PowerSystem(nodes5, append!(generators5, generators_hg), loads5_DA, branches5, battery, 230.0, 1000.0)
+#Thermal Generator Models
+pth, IArray = PowerSimulations.generationvariables(m, DevicesNetInjection,  sys5b.generators.thermal, sys5b.time_periods);
+m = PowerSimulations.powerconstraints(m, pth, sys5b.generators.thermal, sys5b.time_periods)
+pre_set = [d for d in sys5b.generators.renewable if !isa(d, RenewableFix)]
+pre, IArray = PowerSimulations.generationvariables(m, DevicesNetInjection, pre_set, sys5b.time_periods)
+m = PowerSimulations.powerconstraints(m, pre, pre_set, sys5b.time_periods)
+test_cl = [d for d in sys5b.loads if !isa(d, PowerSystems.StaticLoad)] # Filter StaticLoads Out
+pcl, IArray = PowerSimulations.loadvariables(m, DevicesNetInjection, test_cl, sys5b.time_periods);
+m = PowerSimulations.powerconstraints(m, pcl, test_cl, sys5b.time_periods)
 
-m=Model()
+#Injection Array
+TsNets = PowerSimulations.tsinjectionbalance(sys5b)
+#CopperPlate Network test
+m = PowerSimulations.copperplatebalance(m, IArray, TsNets, sys5b.time_periods);
 
-pth = PowerSimulations.generationvariables(m, sys5b.generators.thermal, sys5b.time_periods)
-pre = PowerSimulations.generationvariables(m, sys5b.generators.renewable, sys5b.time_periods)
-Pin, Pout = PowerSimulations.generationvariables(m, sys5b.storage, sys5b.time_periods)
-phg = PowerSimulations.generationvariables(m, generators_hg, sys5b.time_periods)
-fl = PowerSimulations.branchflowvariables(m, sys5b.branches, sys5b.time_periods)
-pcl = PowerSimulations.loadvariables(m, sys5b.loads, sys5b.time_periods)
+#Cost Components
+tl = PowerSimulations.variablecost(m, pcl, test_cl);
+tre = PowerSimulations.variablecost(m, pre, pre_set)
+tth = PowerSimulations.variablecost(m, pth, sys5b.generators.thermal);
 
+#objective
+@objective(m, Min, tl+tre+tth);
+
+m = PowerSimulations.solvepsmodel(m);
