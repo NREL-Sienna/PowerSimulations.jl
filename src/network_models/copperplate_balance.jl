5422a63b1f7bd97b6de8072c572fcdc04971e920
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-17 10:58:31 -0600

    add remove undef to copper plate

--- src/network_models/copperplate_balance.jl
+++ src/network_models/copperplate_balance.jl
@@ -1,5 +1,6 @@
 function copperplatebalance(m::JuMP.Model, DeviceNetInjection::A, TsInjectionBalance:: Array{Float64}, time_periods::Int64) where A <: PowerExpressionArray
 
+    DeviceNetInjection = remove_undef!(DeviceNetInjection)
     TsInjectionBalance = sum(TsInjectionBalance, 1)
 
     @constraintref cpn[1:time_periods]
