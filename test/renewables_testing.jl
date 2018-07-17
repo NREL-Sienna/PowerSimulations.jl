594f8883cf5e3bf3e6d3627eb23678dc2d5c43fa
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-17 11:36:08 -0600

    Update tests of Load and Renewables

--- test/renewables_testing.jl
+++ test/renewables_testing.jl
@@ -1,4 +1,5 @@
 using PowerSystems
+using PowerSimulations
 using JuMP
 
 include(string(homedir(),"/.julia/v0.6/PowerSystems/data/data_5bus.jl"))
@@ -9,5 +10,6 @@ DevicesNetInjection =  Array{JuMP.GenericAffExpr{Float64,JuMP.Variable},2}(lengt
 
 pre_set = [d for d in sys5.generators.renewable if !isa(d, RenewableFix)]
 pre, IArray = PowerSimulations.generationvariables(m, DevicesNetInjection, pre_set, sys5.time_periods)
+m = PowerSimulations.powerconstraints(m, pre, pre_set, sys5.time_periods)
 
 true
