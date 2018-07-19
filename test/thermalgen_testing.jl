b4cc8be74c5bddee6e1e5de53db276911853a623
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-19 15:24:09 -0600

    update for new constraint building functions

--- test/thermalgen_testing.jl
+++ test/thermalgen_testing.jl
@@ -1,9 +1,10 @@
 using PowerSystems
+using PowerSystems
 using JuMP
 using Compat
 
 include(string(homedir(),"/.julia/v0.6/PowerSystems/data/data_5bus.jl"))
-sys5 = PowerSystem(nodes5, generators5, loads5_DA, branches5, nothing,  1000.0)
+sys5 = PowerSystem(nodes5, generators5, loads5_DA, branches5, nothing, 100.0)
 
 m = JuMP.Model()
 devices_netinjection =  Array{JuMP.GenericAffExpr{Float64,JuMP.Variable},2}(length(sys5.buses), sys5.time_periods)
@@ -11,7 +12,7 @@ devices_netinjection =  Array{JuMP.GenericAffExpr{Float64,JuMP.Variable},2}(leng
 #Test ED
 pth, inyection_array = PowerSimulations.generationvariables(m, devices_netinjection,   sys5.generators.thermal, sys5.time_periods);
 PowerSimulations.powerconstraints(m, pth, sys5.generators.thermal, sys5.time_periods)
-PowerSimulations.rampconstraints(m, pth, sys5.generators.thermal, sys5.time_periods) : true
+PowerSimulations.rampconstraints(m, pth, sys5.generators.thermal, sys5.time_periods)
 
 
 #Test UC
@@ -19,8 +20,8 @@ m=Model()
 pth, inyection_array = PowerSimulations.generationvariables(m, devices_netinjection,   sys5.generators.thermal, sys5.time_periods);
 on_thermal, start_thermal, stop_thermal = PowerSimulations.commitmentvariables(m, sys5.generators.thermal, sys5.time_periods)
 PowerSimulations.powerconstraints(m, pth, on_thermal, sys5.generators.thermal, sys5.time_periods)
-PowerSimulations.rampConstraints(m, pth, on_thermal, sys5.generators.thermal, sys5.time_periods)
+PowerSimulations.rampconstraints(m, pth, on_thermal, sys5.generators.thermal, sys5.time_periods)
 PowerSimulations.commitmentconstraints(m, on_thermal, start_thermal, stop_thermal, sys5.generators.thermal, sys5.time_periods)
-PowerSimulations.timeconstraints(m, on_thermal, start_thermal, stop_thermal, sys5.generators.thermal, sys5.time_periods) : true
+PowerSimulations.timeconstraints(m, on_thermal, start_thermal, stop_thermal, sys5.generators.thermal, sys5.time_periods)
 
 true
