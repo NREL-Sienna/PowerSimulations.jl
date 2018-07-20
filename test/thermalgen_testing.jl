4a72e3a83dd4530bb0e65a95a22e795f23878137
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-19 19:37:57 -0600

    update testing

--- test/thermalgen_testing.jl
+++ test/thermalgen_testing.jl
@@ -1,5 +1,5 @@
 using PowerSystems
-using PowerSystems
+using PowerSimulations
 using JuMP
 using Compat
 
@@ -9,19 +9,13 @@ sys5 = PowerSystem(nodes5, generators5, loads5_DA, branches5, nothing, 100.0)
 m = JuMP.Model()
 devices_netinjection =  Array{JuMP.GenericAffExpr{Float64,JuMP.Variable},2}(length(sys5.buses), sys5.time_periods)
 
-#Test ED
-pth, inyection_array = PowerSimulations.generationvariables(m, devices_netinjection,   sys5.generators.thermal, sys5.time_periods);
-PowerSimulations.powerconstraints(m, pth, sys5.generators.thermal, sys5.time_periods)
-PowerSimulations.rampconstraints(m, pth, sys5.generators.thermal, sys5.time_periods)
+create_constraints(Thermal, m, devices_netinjection, sys5, [rampconstraints])
 
 
 #Test UC
 m=Model()
-pth, inyection_array = PowerSimulations.generationvariables(m, devices_netinjection,   sys5.generators.thermal, sys5.time_periods);
-on_thermal, start_thermal, stop_thermal = PowerSimulations.commitmentvariables(m, sys5.generators.thermal, sys5.time_periods)
-PowerSimulations.powerconstraints(m, pth, on_thermal, sys5.generators.thermal, sys5.time_periods)
-PowerSimulations.rampconstraints(m, pth, on_thermal, sys5.generators.thermal, sys5.time_periods)
-PowerSimulations.commitmentconstraints(m, on_thermal, start_thermal, stop_thermal, sys5.generators.thermal, sys5.time_periods)
-PowerSimulations.timeconstraints(m, on_thermal, start_thermal, stop_thermal, sys5.generators.thermal, sys5.time_periods)
+devices_netinjection =  Array{JuMP.GenericAffExpr{Float64,JuMP.Variable},2}(length(sys5.buses), sys5.time_periods)
+create_constraints(Thermal, m, devices_netinjection, sys5, [commitmentconstraints, rampconstraints])
+
 
 true
