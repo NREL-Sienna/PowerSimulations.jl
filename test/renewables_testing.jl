7cc184d7cf522a4dbb8271e5c6c5d13d17d4a3a3
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-19 20:39:55 -0600

    Update testing

--- test/renewables_testing.jl
+++ test/renewables_testing.jl
@@ -1,6 +1,7 @@
 using PowerSystems
 using PowerSimulations
 using JuMP
+using Compat
 
 include(string(homedir(),"/.julia/v0.6/PowerSystems/data/data_5bus.jl"))
 sys5 = PowerSystem(nodes5, generators5, loads5_DA, branches5, nothing,  1000.0)
@@ -8,8 +9,6 @@ sys5 = PowerSystem(nodes5, generators5, loads5_DA, branches5, nothing,  1000.0)
 m = JuMP.Model()
 devices_netinjection =  Array{JuMP.GenericAffExpr{Float64,JuMP.Variable},2}(length(sys5.buses), sys5.time_periods)
 
-pre_set = [d for d in sys5.generators.renewable if !isa(d, RenewableFix)]
-pre, inyection_array = PowerSimulations.generationvariables(m, devices_netinjection,  pre_set, sys5.time_periods)
-m = PowerSimulations.powerconstraints(m, pre, pre_set, sys5.time_periods)
+constructdevice(Renewable, m, devices_netinjection, sys5, [curtailconstraints])
 
 true
--- test/thermalgen_testing.jl
+++ test/thermalgen_testing.jl
@@ -9,13 +9,13 @@ sys5 = PowerSystem(nodes5, generators5, loads5_DA, branches5, nothing, 100.0)
 m = JuMP.Model()
 devices_netinjection =  Array{JuMP.GenericAffExpr{Float64,JuMP.Variable},2}(length(sys5.buses), sys5.time_periods)
 
-create_constraints(Thermal, m, devices_netinjection, sys5, [rampconstraints])
+constructdevice(Thermal, m, devices_netinjection, sys5, [rampconstraints])
 
 
 #Test UC
 m=Model()
 devices_netinjection =  Array{JuMP.GenericAffExpr{Float64,JuMP.Variable},2}(length(sys5.buses), sys5.time_periods)
-create_constraints(Thermal, m, devices_netinjection, sys5, [commitmentconstraints, rampconstraints])
+constructdevice(Thermal, m, devices_netinjection, sys5, [commitmentconstraints, rampconstraints])
 
 
 true
