da1723ca7abd5061ae078408c55060238ee7ff6b
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-26 11:57:21 -0600

    update network constructors to new format

--- src/base/model_constructors.jl
+++ src/base/model_constructors.jl
@@ -1,34 +1,33 @@
 function buildmodel!(sys::PowerSystems.PowerSystem, model::PowerSimulationsModel)
 
     PSModel = JuMP.Model()
+    # TODO: this syntax might change with JuMP 0.19
     devices_netinjection =  Array{JuMP.GenericAffExpr{Float64,JuMP.Variable},2}(length(sys.buses), sys.time_periods)
 
+    #Knowing the network model => create reactive power constraints or not.
+    network_model = model.transmission.category
+
     for category in model.generation
-        model.psmodel = constructdevice(category.device, model.psmodel, devices_netinjection, sys, category.constraints)
+        model.psmodel = constructdevice(category.device, network_model, model.psmodel, devices_netinjection, sys, category.constraints)
     end
 
     for category in model.demand
-        model.psmodel = constructdevice(category.device, model.psmodel, devices_netinjection, sys, category.constraints)
+        model.psmodel = constructdevice(category.device, network_model, model.psmodel, devices_netinjection, sys, category.constraints)
     end
 
     for category in model.storage
-        model.psmodel = constructdevice(category.device, model.psmodel, devices_netinjection, sys, category.constraints)
-    end
-
-    for category in model.branches
-        model.psmodel = constructdevice(category.service, model.psmodel, devices_netinjection, sys, category.constraints)
-    end
-
-    for category in model.services
-        model.psmodel = constructdevice(category.service, model.psmodel, devices_netinjection, sys, category.constraints)
+        model.psmodel = constructdevice(category.device, network_model, model.psmodel, devices_netinjection, sys, category.constraints)
     end
 
     #Get Arrary with NetInjections from TimeSeries fields
     timeseries_nets = PowerSimulations.timeseries_netinjection(sys)
 
     #This function hasn't been created yet
-    model.psmodel = constructnetwork(model.transmission, model.psmodel, timeseries_nets, devices_netinjection, sys)
+    model.psmodel = constructnetwork(model.transmission, model.branches, model.psmodel, timeseries_nets, devices_netinjection, sys)
 
+    for category in model.services
+        model.psmodel = constructservice(category.service, model.psmodel, devices_netinjection, sys, category.constraints)
+    end
 
     #This function hasn't been created ye
     for caterogy in model.cost
