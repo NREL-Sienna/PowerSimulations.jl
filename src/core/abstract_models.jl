4d122ac86a531bbcef3926098f4fe4e16e996fd5
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-26 11:55:21 -0600

    change naming in abstract models

--- src/core/abstract_models.jl
+++ src/core/abstract_models.jl
@@ -1,27 +1,29 @@
 export PowerSimulationsModel
 export PowerResults
 
-abstract type AbstractPowerSimulationType end
+abstract type AbstractOperationsModel end
 
-abstract type EconomicDispatch <: AbstractPowerSimulationType end
+abstract type EconomicDispatch <: AbstractOperationsModel end
 
-abstract type UnitCommitment <: AbstractPowerSimulationType end
+abstract type UnitCommitment <: AbstractOperationsModel end
 
-mutable struct PowerOperationsModel{T<:AbstractPowerSimulationType, F <: Array{<:Function}}
+mutable struct PowerModel{T<:AbstractOperationsModel, F <: Array{<:Function}}
+    psmodel:: T
     generation::Array{@NT(device::DataType,constraints::F)}
     demand::Array{@NT(device::DataType,constraints::F)}
     storage::Array{@NT(device::DataType,constraints::F)}
+    transmission::@NT(device::DataType,constraints::F)
     branches::Array{@NT(device::DataType,constraints::F)}
     services::Array{@NT(device::DataType,constraints::F)}
-    transmission::DataType
     cost::Array{@NT(device::DataType,components::F)}
     system::PowerSystems.PowerSystem
-    psmodel::JuMP.Model
+    model::JuMP.Model
     dynamics::Bool
 end
 
-mutable struct PowerSimulationsModel{T<:AbstractPowerSimulationType}
-    model::PowerOperationsModel{T}
+mutable struct PowerSimulationsModel{T<:AbstractOperationsModel}
+    name::String
+    model::PowersModel{T}
     periods::Int64
     resolution::Int64
     date_from::DateTime
@@ -29,8 +31,7 @@ mutable struct PowerSimulationsModel{T<:AbstractPowerSimulationType}
     lookahead_periods::Int64
     lookahead_resolution::Int64
     dynamic_analysis::Bool
-    forecast::Any #Need to define this properly
-    #A constructor here has to return the model based on the data, the time is AbstractModel
+    timeseries::Any
 end
 
  mutable struct PowerResults
