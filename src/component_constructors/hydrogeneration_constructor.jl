785565bfb7c4654d552388f84afa864ba2d3fe15
Barrows <clayton.barrows@nrel.gov>
2018-07-30 16:02:05 -0600

    adding a hydrogeneration constructor

---
+++ src/component_constructors/hydrogeneration_constructor.jl
@@ -0,0 +1,27 @@
+struct Hydro end
+
+const curtailconstraints = PowerSimulations.powerconstraints
+
+function constructdevice(category::Type{Hydro}, transmission::Type{CopperPlate}, m::JuMP.Model, devices_netinjection::T, sys::PowerSystems.PowerSystem, constraints::Array{<:Function}=[powerconstraints]) where T <: PowerExpressionArray
+
+    devices = [d for d in sys.generators.hydro if (d.available == true && !isa(d, HydroFix))]
+
+    if !isempty(devices)
+
+        pre, devices_netinjection = activepowervariables(m, devices_netinjection, devices, sys.time_periods)
+
+        for c in constraints
+
+            m = c(m, devices, sys.time_periods)
+
+        end
+
+    else
+
+        warn("Hydro dispatch variables created without constraints, the problem might be unbounded")
+
+    end
+
+    return m, devices_netinjection
+
+end
