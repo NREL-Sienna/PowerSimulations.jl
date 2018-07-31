2267887690fc703440d56a293b8c86f96f17c768
Jose Daniel Lara <jdlara@berkeley.edu>
2018-07-31 17:30:01 -0600

    update thermal generation constructor for JuMP v0.19

--- src/component_constructors/thermalgeneration_constructor.jl
+++ src/component_constructors/thermalgeneration_constructor.jl
@@ -1,6 +1,6 @@
 struct Thermal end
 
-function dispatch(m::JuMP.Model, devices_netinjection::T, network::N, sys::PowerSystems.PowerSystem, constraints::Array{<:Function}) where {T <: PowerExpressionArray, N <: NetworkType}
+function dispatch(m::JuMP.Model, network::Type{N}, devices_netinjection::T, sys::PowerSystems.PowerSystem, constraints::Array{<:Function}) where {T <: JumpExpressionMatrix, N <: RealNetwork}
 
     pth, inyection_array = activepowervariables(m, devices_netinjection, sys.generators.thermal, sys.time_periods);
 
@@ -14,7 +14,7 @@ function dispatch(m::JuMP.Model, devices_netinjection::T, network::N, sys::Power
 
 end
 
-function commitment(m::JuMP.Model, devices_netinjection::T, sys::PowerSystems.PowerSystem, constraints::Array{<:Function}) where T <: PowerExpressionArray
+function commitment(m::JuMP.Model, network::Type{N}, devices_netinjection::T, sys::PowerSystems.PowerSystem, constraints::Array{<:Function}) where {T <: JumpExpressionMatrix, N <: RealNetwork}
 
     pth, inyection_array = activepowervariables(m, devices_netinjection, sys.generators.thermal, sys.time_periods);
 
@@ -32,15 +32,15 @@ function commitment(m::JuMP.Model, devices_netinjection::T, sys::PowerSystems.Po
 
 end
 
-function constructdevice(category::Type{Thermal}, transmission::N, m::JuMP.Model, devices_netinjection::T, sys::PowerSystems.PowerSystem, constraints::Array{<:Function}=[powerconstraints]) where {T <: PowerExpressionArray, N <: RealNetwork}
+function constructdevice!(category::Type{Thermal}, network::Type{N}, m::JuMP.Model, devices_netinjection::T, sys::PowerSystems.PowerSystem, constraints::Array{<:Function}=[powerconstraints]) where {T <: JumpExpressionMatrix, N <: NetworkType}
 
     if commitmentconstraints in constraints
 
-        m, devices_netinjection = commitment(m, devices_netinjection, sys, constraints)
+        m, devices_netinjection = commitment(m, network, devices_netinjection, sys, constraints)
 
     else
 
-        m, devices_netinjection = dispatch(m, devices_netinjection, sys, constraints)
+        m, devices_netinjection = dispatch(m, network, devices_netinjection, sys, constraints)
 
     end
 
