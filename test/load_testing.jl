d0d9863e03f898d94c7a2e730c2a1668eadf2340
Barrows <clayton.barrows@nrel.gov>
2018-08-25 14:18:46 -0600

    load testing working

--- test/load_testing.jl
+++ test/load_testing.jl
@@ -7,12 +7,12 @@ sys5 = PowerSystem(nodes5, generators5, loads5_DA, branches5, nothing,  1000.0)
 
 m = Model()
 
-devices_netinjection =  JumpAffineExpressionArray(length(sys5.buses), sys5.time_periods)
+devices_netinjection =  PowerSimulations.JumpAffineExpressionArray(length(sys5.buses), sys5.time_periods)
 
 test_cl = [d for d in sys5.loads if !isa(d, PowerSystems.StaticLoad)] # Filter StaticLoads Out
 
 pcl, inyection_array = PowerSimulations.loadvariables(m, devices_netinjection,  test_cl, sys5.time_periods);
-m = PowerSimulations.powerconstraints(m, pcl, test_cl, sys5.time_periods)
+m = PowerSimulations.powerconstraints(m, test_cl, sys5.time_periods)
 
 
 true
