9b72551a80f16f74edd0db39d26c12f974d80976
Barrows <clayton.barrows@nrel.gov>
2018-08-25 13:47:49 -0600

    re-enabling network_testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using PowerSimulations
 using Base.Test
 
 # write your own tests here
-#@test include("network_testing.jl")
+@test include("network_testing.jl")
 @test include("thermalgen_testing.jl")
 @test include("renewables_testing.jl")
 #@test include("hydro_testing.jl")
