c1257a95ed33cc5c45838a3e783db902564588f0
Barrows <clayton.barrows@nrel.gov>
2018-08-25 13:50:05 -0600

    hydro testing working

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Base.Test
 @test include("network_testing.jl")
 @test include("thermalgen_testing.jl")
 @test include("renewables_testing.jl")
-#@test include("hydro_testing.jl")
+@test include("hydro_testing.jl")
 #@test include("storage_testing.jl")
 #@test include("load_testing.jl")
 #@test include("buildED_CN_testing.jl")
