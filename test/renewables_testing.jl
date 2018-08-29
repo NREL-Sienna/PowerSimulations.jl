ea42958a77308324cd3d842516dfda1a4e13ee43
Jose Daniel Lara <jdlara@berkeley.edu>
2018-08-28 22:08:28 -0700

    Update file loading of 5bus.jl for testing

--- test/renewables_testing.jl
+++ test/renewables_testing.jl
@@ -1,9 +1,11 @@
 using PowerSystems
 using PowerSimulations
 using JuMP
-using Compat
 
-include(string(homedir(),"/.julia/v0.6/PowerSystems/data/data_5bus.jl"))
+base_dir = string(dirname(dirname(pathof(PowerSystems))))
+println(joinpath(base_dir,"data/data_5bus.jl"))
+include(joinpath(base_dir,"data/data_5bus.jl"))
+
 sys5 = PowerSystem(nodes5, generators5, loads5_DA, branches5, nothing,  1000.0)
 
 m = JuMP.Model()
