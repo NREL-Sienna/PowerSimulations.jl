d836671335847024d9b3fcd9318afb5d723d35aa
Jose Daniel Lara <jdlara@berkeley.edu>
2018-09-11 22:56:09 -0700

    update testing for thermal generator constructors

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,19 @@
 using PowerSimulations
-using Base.Test
+using Test
 
-#=
+
 @testset "Device Constructors" begin
-    @test include("thermalgen_testing.jl")
-    @test include("renewables_testing.jl")
-    @test include("hydro_testing.jl")
-    @test include("storage_testing.jl")
-    @test include("load_testing.jl")
-    #@test include("network_testing.jl")
+    include("thermalgen_testing.jl")
+    #include("renewables_testing.jl")
+    #include("hydro_testing.jl")
+    #include("storage_testing.jl")
+    #include("load_testing.jl")
+    #include("network_testing.jl")
 end
 
-
+#=
 @testset "Model Constructors" begin
-    @test include("buildED_CN_testing.jl")
-    @test include("buildED_NB_testing.jl")
+    include("buildED_CN_testing.jl")
+    include("buildED_NB_testing.jl")
 end
 =#
