e34115fd37a3d7ca2b1ef87f8075e68707a0ce90
Jose Daniel Lara <jdlara@berkeley.edu>
2018-09-22 00:17:54 -0700

    Remove use of AxisArrys temporarily WIP

--- src/device_models/branches/ptdf_model.jl
+++ src/device_models/branches/ptdf_model.jl
@@ -3,14 +3,17 @@ function dc_networkflow(m::JuMP.Model, netinjection::BalanceNamedTuple, PTDF::PT
     fbr = m[:fbr]
     name_index = m[:fbr].axes[1]
     time_index = m[:fbr].axes[2]
+    nodes = PTDF.axes[2].val
+    node_set = 1:length(nodes)
 
     ts_flow = gemm('N', 'N', PTDF.data, netinjection.timeseries_active)
 
     branchflow = JuMP.JuMPArray(Array{ConstraintRef}(undef,length(name_index), time_index), name_index, time_index)
 
+    #TODO: Make consistent with the use of AxisArrays. This syntax doesn't exploit it properly
     for t in time_index
-        for branch in name_index
-            branchflow[branch,t] = @constraint(m, fbr[branch,t] == sum(netinjection.var_active[i,t]*PTDF[branch,i] for i in PTDF.axes[1].val) + ts_flow[branch,t])
+        for (ix,branch) in enumerate(name_index)
+            branchflow[branch,t] = @constraint(m, fbr[branch,t] == sum(netinjection.var_active[i,t]*PTDF.data[ix,i] for i in node_set) + ts_flow[ix,t])
         end
     end
 
