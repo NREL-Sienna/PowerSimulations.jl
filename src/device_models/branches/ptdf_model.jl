b7ac7e92f820ac627ad931e89ffccc82b147c5b4
Jose Daniel Lara <jdlara@berkeley.edu>
2018-09-22 00:12:39 -0700

    Add to PTDF WIP

--- src/device_models/branches/ptdf_model.jl
+++ src/device_models/branches/ptdf_model.jl
@@ -4,13 +4,13 @@ function dc_networkflow(m::JuMP.Model, netinjection::BalanceNamedTuple, PTDF::PT
     name_index = m[:fbr].axes[1]
     time_index = m[:fbr].axes[2]
 
-    #Add Multiplication of PTDF*Time_series Injections
+    ts_flow = gemm('N', 'N', PTDF.data, netinjection.timeseries_active)
 
     branchflow = JuMP.JuMPArray(Array{ConstraintRef}(undef,length(name_index), time_index), name_index, time_index)
 
     for t in time_index
         for branch in name_index
-            branchflow[branch,t] = @constraint(m, sum(netinjection.var_active[i,t]*PTDF[branch,i] for i in PTDF.axes[1].val) == net_load[b,t])
+            branchflow[branch,t] = @constraint(m, fbr[branch,t] == sum(netinjection.var_active[i,t]*PTDF[branch,i] for i in PTDF.axes[1].val) + ts_flow[branch,t])
         end
     end
 
