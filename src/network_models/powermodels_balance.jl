432aca0e22666b458ed32a765a4a65ad8cc6d2a6
ccoffrin <carleton@coffrin.com>
2018-10-16 23:13:39 -0600

    fix powermodel building bugs

--- src/network_models/powermodels_balance.jl
+++ src/network_models/powermodels_balance.jl
@@ -1,3 +1,10 @@
+#################################################################################
+# Questions
+#
+# - why do exported functions (e.g. ids, var, con, ref) need pacakge qualification?
+# - why do non-qualified exported functions (e.g. ids, var) still throw warnings?
+#
+#
 
 
 #################################################################################
@@ -17,15 +24,15 @@ function post_nip(pm::PM.GenericPowerModel)
 
     PM.constraint_voltage(pm)
 
-    for i in ids(pm, :ref_buses)
+    for i in PM.ids(pm, :ref_buses)
         PM.constraint_theta_ref(pm, i)
     end
 
-    for i in ids(pm, :bus)
+    for i in PM.ids(pm, :bus)
        	constraint_kcl_ni(pm, i)
     end
 
-    for i in ids(pm, :branch)
+    for i in PM.ids(pm, :branch)
         PM.constraint_ohms_yt_from(pm, i)
         PM.constraint_ohms_yt_to(pm, i)
 
@@ -35,7 +42,7 @@ function post_nip(pm::PM.GenericPowerModel)
         PM.constraint_thermal_limit_to(pm, i)
     end
 
-    for i in ids(pm, :dcline)
+    for i in PM.ids(pm, :dcline)
         PM.constraint_dcline(pm, i)
     end
 end
@@ -52,49 +59,49 @@ end
 
 ""
 function variable_active_net_injection(pm::PM.GenericPowerModel; nw::Int=pm.cnw, cnd::Int=pm.ccnd)
-    var(pm, nw, cnd)[:pni] = @variable(pm.model,
-        [i in ids(pm, nw, :bus)], base_name="$(nw)_$(cnd)_pin",
+    PM.var(pm, nw, cnd)[:pni] = @variable(pm.model,
+        [i in PM.ids(pm, nw, :bus)], base_name="$(nw)_$(cnd)_pin",
         start = 0.0
     )
 end
 
 ""
 function variable_reactive_net_injection(pm::PM.GenericPowerModel; nw::Int=pm.cnw, cnd::Int=pm.ccnd)
-    var(pm, nw, cnd)[:qni] = @variable(pm.model,
-        [i in ids(pm, nw, :bus)], base_name="$(nw)_$(cnd)_qin",
+    PM.var(pm, nw, cnd)[:qni] = @variable(pm.model,
+        [i in PM.ids(pm, nw, :bus)], base_name="$(nw)_$(cnd)_qin",
         start = 0.0
     )
 end
 
 
 ""
-function constraint_kcl_shunt_ni(pm::PM.GenericPowerModel, i::Int; nw::Int=pm.cnw, cnd::Int=pm.ccnd)
-    if !haskey(con(pm, nw, cnd), :kcl_p)
-        con(pm, nw, cnd)[:kcl_p] = Dict{Int,ConstraintRef}()
+function constraint_kcl_ni(pm::PM.GenericPowerModel, i::Int; nw::Int=pm.cnw, cnd::Int=pm.ccnd)
+    if !haskey(PM.con(pm, nw, cnd), :kcl_p)
+        PM.con(pm, nw, cnd)[:kcl_p] = Dict{Int,ConstraintRef}()
     end
-    if !haskey(con(pm, nw, cnd), :kcl_q)
-        con(pm, nw, cnd)[:kcl_q] = Dict{Int,ConstraintRef}()
+    if !haskey(PM.con(pm, nw, cnd), :kcl_q)
+        PM.con(pm, nw, cnd)[:kcl_q] = Dict{Int,ConstraintRef}()
     end
 
-    bus = ref(pm, nw, :bus, i)
-    bus_arcs = ref(pm, nw, :bus_arcs, i)
-    bus_arcs_dc = ref(pm, nw, :bus_arcs_dc, i)
+    bus = PM.ref(pm, nw, :bus, i)
+    bus_arcs = PM.ref(pm, nw, :bus_arcs, i)
+    bus_arcs_dc = PM.ref(pm, nw, :bus_arcs_dc, i)
 
-    constraint_kcl_shunt_ni(pm, nw, cnd, i, bus_arcs, bus_arcs_dc)
+    constraint_kcl_ni(pm, nw, cnd, i, bus_arcs, bus_arcs_dc)
 end
 
 
 ""
 function constraint_kcl_ni(pm::PM.GenericPowerModel, n::Int, c::Int, i::Int, bus_arcs, bus_arcs_dc)
-    p = var(pm, n, c, :p)
-    q = var(pm, n, c, :q)
-    pni = var(pm, n, c, :pni)
-    qni = var(pm, n, c, :qni)
-    p_dc = var(pm, n, c, :p_dc)
-    q_dc = var(pm, n, c, :q_dc)
+    p = PM.var(pm, n, c, :p)
+    q = PM.var(pm, n, c, :q)
+    pni = PM.var(pm, n, c, :pni, i)
+    qni = PM.var(pm, n, c, :qni, i)
+    p_dc = PM.var(pm, n, c, :p_dc)
+    q_dc = PM.var(pm, n, c, :q_dc)
 
-    con(pm, n, c, :kcl_p)[i] = @constraint(pm.model, sum(p[a] for a in bus_arcs) + sum(p_dc[a_dc] for a_dc in bus_arcs_dc) == pni)
-    con(pm, n, c, :kcl_q)[i] = @constraint(pm.model, sum(q[a] for a in bus_arcs) + sum(q_dc[a_dc] for a_dc in bus_arcs_dc) == qni)
+    PM.con(pm, n, c, :kcl_p)[i] = @constraint(pm.model, sum(p[a] for a in bus_arcs) + sum(p_dc[a_dc] for a_dc in bus_arcs_dc) == pni)
+    PM.con(pm, n, c, :kcl_q)[i] = @constraint(pm.model, sum(q[a] for a in bus_arcs) + sum(q_dc[a_dc] for a_dc in bus_arcs_dc) == qni)
 end
 
 
@@ -104,11 +111,11 @@ end
 
 "active power only models ignore reactive power flows"
 function constraint_kcl_ni(pm::PM.GenericPowerModel{T}, n::Int, c::Int, i::Int, bus_arcs, bus_arcs_dc) where T <: PM.AbstractDCPForm
-    p = var(pm, n, c, :p)
-    pni = var(pm, n, c, :pni)
-    p_dc = var(pm, n, c, :p_dc)
+    p = PM.var(pm, n, c, :p)
+    pni = PM.var(pm, n, c, :pni, i)
+    p_dc = PM.var(pm, n, c, :p_dc)
 
-    con(pm, n, c, :kcl_p)[i] = @constraint(pm.model, sum(p[a] for a in bus_arcs) + sum(p_dc[a_dc] for a_dc in bus_arcs_dc) == pni)
+    PM.con(pm, n, c, :kcl_p)[i] = @constraint(pm.model, sum(p[a] for a in bus_arcs) + sum(p_dc[a_dc] for a_dc in bus_arcs_dc) == pni)
 end
 
 
