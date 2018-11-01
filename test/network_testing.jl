7d5277502f699798d1fc76b7bddf65dbad2e9257
Jose Daniel Lara <jdlara@berkeley.edu>
2018-11-01 14:02:37 -0700

    add test for PM.QCWRForm

--- test/network_testing.jl
+++ test/network_testing.jl
@@ -74,6 +74,19 @@ finally end
     isapprox(JuMP.objective_value(m), 3400, atol = 1000)
 true finally end
 
+@test try
+    @info "testing ACP- QCWForm 5-bus"
+    Net = PM.QCWRForm
+    m = Model(ipopt_optimizer);
+    netinjection = PS.instantiate_network(Net, sys5);
+    PS.constructdevice!(m, netinjection, ThermalGen, PS.ThermalDispatch, Net, sys5);
+    PS.constructnetwork!(m, [(device=Line, formulation=PS.PiLine)], netinjection, Net, sys5)
+    @objective(m, Min, m.obj_dict[:objective_function])
+    JuMP.optimize!(m)
+    isapprox(JuMP.objective_value(m), 3400, atol = 1000)
+true finally end
+
+
 
 include(joinpath(base_dir,"data/data_14bus_pu.jl"))
 sys14 = PowerSystem(nodes14, generators14, loads14, branches14, nothing,  100.0);
@@ -133,6 +146,18 @@ finally end
 @test try
     @info "testing ACP-OPF 14-bus"
     Net = PS.StandardAC
+    m = Model(ipopt_optimizer);
+    netinjection = PS.instantiate_network(Net, sys14);
+    PS.constructdevice!(m, netinjection, ThermalGen, PS.ThermalDispatch, Net, sys14);
+    PS.constructnetwork!(m, [(device=Line, formulation=PS.PiLine)], netinjection, Net, sys14)
+    @objective(m, Min, m.obj_dict[:objective_function])
+    JuMP.optimize!(m)
+    isapprox(JuMP.objective_value(m), 1200, atol = 1000)
+true finally end
+
+@test try
+    @info "testing ACP-QCWForm 14-bus"
+    Net = PM.QCWRForm
     m = Model(ipopt_optimizer);
     netinjection = PS.instantiate_network(Net, sys14);
     PS.constructdevice!(m, netinjection, ThermalGen, PS.ThermalDispatch, Net, sys14);
