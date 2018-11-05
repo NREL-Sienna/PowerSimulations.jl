78e850d1144b4fa32ebee902107b22ac118f936b
Barrows <clayton.barrows@nrel.gov>
2018-11-04 17:21:19 -0700

    replacing pwl variable bounds

--- src/device_models/common/variable_cost.jl
+++ src/device_models/common/variable_cost.jl
@@ -22,11 +22,10 @@ end
 
 function pwlgencost(m::JuMP.Model, variable::VariableRef, cost_component::Array{Tuple{Float64, Float64}})
 
-    pwlvars = @variable(m, [i = 1:(length(cost_component)-1)], base_name = "pwl_{$(variable)}")
+    pwlvars = @variable(m, [i = 1:(length(cost_component)-1)], base_name = "pwl_{$(variable)}", lower_bound = 0.0, upper_bound = (cost_component[i+1][1] - cost_component[i][1]))
 
     for (ix, pwlvar) in enumerate(pwlvars)
         c = @constraint(m, pwlvar <= cost_component[ix + 1][2])
-        c = @constraint(m, pwlvar >= 0)
     end
     c = @constraint(m, variable == sum(pwlvars[ix] for (ix, pwlvar) in enumerate(pwlvars)))
 
