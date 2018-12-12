8af7cabebe75d1599276e3be3fafc001564779db
Jose Daniel Lara <jdlara@berkeley.edu>
2018-12-11 23:24:42 -0800

    make thermal variable anonymous

--- src/device_models/thermal_generation/thermal_variables.jl
+++ src/device_models/thermal_generation/thermal_variables.jl
@@ -3,42 +3,40 @@
 """
 This function add the variables for power generation output to the model
 """
-function activepowervariables(m::JuMP.Model, devices::Array{T,1}, time_periods::Int64) where {A <: JumpExpressionMatrix, T <: PowerSystems.ThermalGen}
+function activepowervariables(ps_m::canonical_model, devices::Array{T,1}, time_periods::Int64) where {T <: PowerSystems.ThermalGen}
 
     on_set = [d.name for d in devices if d.available == true]
 
     t = 1:time_periods
 
-    p_th = @variable(m, p_th[on_set,t], start = 0.0) # Power output of generators
+    ps_m.variables["P_th"] = @variable(ps_m.JuMPmodel, [on_set,t], start = 0.0, base_name="Pth", container = DenseAxisArray) # Power output of generators
 
-    return p_th
 end
 
 """
 This function add the variables for power generation output to the model
 """
-function reactivepowervariables(m::JuMP.Model, devices::Array{T,1}, time_periods::Int64) where {A <: JumpExpressionMatrix, T <: PowerSystems.ThermalGen}
+function reactivepowervariables(ps_m::canonical_model, devices::Array{T,1}, time_periods::Int64) where {T <: PowerSystems.ThermalGen}
 
     on_set = [d.name for d in devices if d.available == true]
 
     t = 1:time_periods
 
-    q_th = @variable(m, q_th[on_set,t], start = 0.0) # Power output of generators
+    ps_m.variables["Q_th"] = @variable(ps_m.JuMPmodel, [on_set,t], start = 0.0, base_name="Qth", container = DenseAxisArray) # Power output of generators
 
-    return q_th
 end
 
 """
 This function add the variables for power generation commitment to the model
 """
-function commitmentvariables(m::JuMP.Model, devices::Array{T,1}, time_periods::Int64) where T <: PowerSystems.ThermalGen
+function commitmentvariables(ps_m::canonical_model, devices::Array{T,1}, time_periods::Int64) where {T <: PowerSystems.ThermalGen}
 
     on_set = [d.name for d in devices if d.available == true]
 
     t = 1:time_periods
 
-    @variable(m, on_th[on_set,t], Bin) # Power output of generators
-    @variable(m, start_th[on_set,t], Bin) # Power output of generators
-    @variable(m, stop_th[on_set,t], Bin) # Power output of generators
+    ps_m.variables["on_th"]  = @variable(ps_m.JuMPmodel, [on_set,t], Bin, container = DenseAxisArray) # Power output of generators
+    ps_m.variables["start_th"]  = @variable(ps_m.JuMPmodel, [on_set,t], Bin, container = DenseAxisArray) # Power output of generators
+    ps_m.variables["stop_th"]  = @variable(ps_m.JuMPmodel, [on_set,t], Bin, container = DenseAxisArray) # Power output of generators
 
 end
