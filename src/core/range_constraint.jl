33a19e9fda3e9439f0b1f8956cbb40f1dd9a8067
Jose Daniel Lara <jdlara@berkeley.edu>
2018-12-13 15:37:15 -0800

    Improve application of range constraints

--- src/core/range_constraint.jl
+++ src/core/range_constraint.jl
@@ -1,23 +1,10 @@
-function activepower_range(ps_m::canonical_model, devices::Array{T,1}, time_range::UnitRange{Int64}, cons_name::String, var_name::String) where {T <: PowerSystems.PowerSystemDevice}
+function device_range(ps_m::canonical_model, range_data::Array{Tuple{String,NamedTuple{(:min, :max),Tuple{Float64,Float64}}},1}, time_range::UnitRange{Int64}, cons_name::String, var_name::String)
 
-    ps_m.constraints["$(cons_name)"] = JuMP.Containers.DenseAxisArray{ConstraintRef}(undef, [d.name for d in devices], time_range)
+    ps_m.constraints["$(cons_name)"] = JuMP.Containers.DenseAxisArray{ConstraintRef}(undef, [r[1] for r in range_data], time_range)
 
-    for t in time_range, d in devices
+    for t in time_range, r in range_data
 
-            ps_m.constraints["$(cons_name)"][d.name, t] = @constraint(ps_m.JuMPmodel, d.tech.activepowerlimits.min <= ps_m.variables["$(var_name)"][d.name, t] <= d.tech.activepowerlimits.max)
-
-    end
-
-end
-
-
-function reactivepower_range(ps_m::canonical_model, devices::Array{T,1}, time_range::UnitRange{Int64}, cons_name::String, var_name::String, nomin::Bool) where {T <: PowerSystems.PowerSystemDevice}
-
-    ps_m.constraints["$(cons_name)"] = JuMP.Containers.DenseAxisArray{ConstraintRef}(undef, [d.name for d in devices], time_range)
-
-    for t in time_range, d in devices
-
-            ps_m.constraints["$(cons_name)"][d.name, t] = @constraint(ps_m.JuMPmodel, d.tech.reactivepowerlimits.min <= ps_m.variables["$(var_name)"][d.name, t] <= d.tech.reactivepowerlimits.max)
+            ps_m.constraints["$(cons_name)"][r[1], t] = @constraint(ps_m.JuMPmodel, r[2].min <= ps_m.variables["$(var_name)"][r[1], t] <= r[2].max)
 
     end
 
