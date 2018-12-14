97bd15abdf4adf77fc86d69c4da9d3be29658323
Jose Daniel Lara <jdlara@berkeley.edu>
2018-12-14 14:21:25 -0800

    add note about semicontinous constraints.

--- src/core/range_constraint.jl
+++ src/core/range_constraint.jl
@@ -12,6 +12,7 @@ end
 
 function device_semicontinuousrange(ps_m::canonical_model, range_data::Array{Tuple{String,NamedTuple{(:min, :max),Tuple{Float64,Float64}}},1}, time_range::UnitRange{Int64}, cons_name::String, var_name::String)
 
+    #MOI has a semicontinous set, but after some tests is not clear most MILP solvers support it. In the future this can be updated
     ps_m.constraints["$(cons_name)"] = JuMP.Containers.DenseAxisArray{ConstraintRef}(undef, [r[1] for r in range_data], time_range)
 
     for t in time_range, r in range_data
