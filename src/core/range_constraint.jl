d7beeaa937a90cde5587e1c9957cc66bbbffbfb0
Jose Daniel Lara <jdlara@berkeley.edu>
2018-12-14 14:20:07 -0800

    add function for semicontinous range constraint

--- src/core/range_constraint.jl
+++ src/core/range_constraint.jl
@@ -9,3 +9,15 @@ function device_range(ps_m::canonical_model, range_data::Array{Tuple{String,Name
     end
 
 end
+
+function device_semicontinuousrange(ps_m::canonical_model, range_data::Array{Tuple{String,NamedTuple{(:min, :max),Tuple{Float64,Float64}}},1}, time_range::UnitRange{Int64}, cons_name::String, var_name::String)
+
+    ps_m.constraints["$(cons_name)"] = JuMP.Containers.DenseAxisArray{ConstraintRef}(undef, [r[1] for r in range_data], time_range)
+
+    for t in time_range, r in range_data
+
+            ps_m.constraints["$(cons_name)"][r[1], t] = @constraint(ps_m.JuMPmodel, r[2].min*r[3] <= ps_m.variables["$(var_name)"][r[1], t] <= r[2].max*r[3])
+
+    end
+
+end
