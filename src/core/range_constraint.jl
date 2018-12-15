8738540259aa821d0288bc8b9cafa1a6336ac7cb
Jose Daniel Lara <jdlara@berkeley.edu>
2018-12-14 16:26:51 -0800

    add semicontinous constraints

--- src/core/range_constraint.jl
+++ src/core/range_constraint.jl
@@ -10,15 +10,26 @@ function device_range(ps_m::canonical_model, range_data::Array{Tuple{String,Name
 
 end
 
-function device_semicontinuousrange(ps_m::canonical_model, range_data::Array{Tuple{String,NamedTuple{(:min, :max),Tuple{Float64,Float64}}},1}, time_range::UnitRange{Int64}, cons_name::String, var_name::String)
+function device_semicontinuousrange(ps_m::canonical_model, scrange_data::Array{Tuple{String,NamedTuple{(:min, :max),Tuple{Float64,Float64}}},1}, time_range::UnitRange{Int64}, cons_name::String, var_name::String, binvar_name::String)
 
     #MOI has a semicontinous set, but after some tests is not clear most MILP solvers support it. In the future this can be updated
-    ps_m.constraints["$(cons_name)"] = JuMP.Containers.DenseAxisArray{ConstraintRef}(undef, [r[1] for r in range_data], time_range)
+    set_name = [r[1] for r in scrange_data]
+    ps_m.constraints["$(cons_name)_ub"] = JuMP.Containers.DenseAxisArray{ConstraintRef}(undef, set_name, time_range)
+    ps_m.constraints["$(cons_name)_lb"] = JuMP.Containers.DenseAxisArray{ConstraintRef}(undef, set_name, time_range)
 
-    for t in time_range, r in range_data
+    for t in time_range, r in scrange_data
 
-            ps_m.constraints["$(cons_name)"][r[1], t] = @constraint(ps_m.JuMPmodel, r[2].min*r[3] <= ps_m.variables["$(var_name)"][r[1], t] <= r[2].max*r[3])
+            if r[2].min == 0.0
+
+                ps_m.constraints["$(cons_name)_ub"][r[1], t] = @constraint(ps_m.JuMPmodel, ps_m.variables["$(var_name)"][r[1], t] <= r[2].max*ps_m.variables["$(binvar_name)"][r[1], t])
+                ps_m.constraints["$(cons_name)_lb"][r[1], t] = @constraint(ps_m.JuMPmodel, ps_m.variables["$(var_name)"][r[1], t] >= 0.0)
+
+            else
+
+                ps_m.constraints["$(cons_name)_ub"][r[1], t] = @constraint(ps_m.JuMPmodel, ps_m.variables["$(var_name)"][r[1], t] <= r[2].max*ps_m.variables["$(binvar_name)"][r[1], t])
+                ps_m.constraints["$(cons_name)_lb"][r[1], t] = @constraint(ps_m.JuMPmodel, ps_m.variables["$(var_name)"][r[1], t] >= r[2].min*ps_m.variables["$(binvar_name)"][r[1], t])
+
+            end
 
     end
-
 end
