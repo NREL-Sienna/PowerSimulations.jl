4ca8d6127a71b22b71969d4881ec93f8a4ad48d0
Jose Daniel Lara <jdlara@berkeley.edu>
2018-12-14 16:51:03 -0800

    add output constraints to storage WIP, requires review of PS data model

---
+++ src/device_models/storage/output_constraints.jl
@@ -0,0 +1,22 @@
+function activepower(ps_m::canonical_model, devices::Array{S,1}, device_formulation::Type{D}, system_formulation::Type{S}, time_range::UnitRange{Int64}) where {S <: PowerSystems.Storage, D <: AbstractStorageForm, S <: PM.AbstractPowerFormulation}
+
+    range_data_in = [(s.name, s.inputactivepowerlimits) for s in devices]
+
+    range_data_out = [(s.name, s.tech.outputactivepowerlimits) for s in devices]
+
+    device_range(ps_m, range_data_in, time_range, "storage_inputpower_range", "Psin")
+
+    device_range(ps_m, range_data_in, time_range, "storage_outputpower_range", "Psout")
+
+end
+
+"""
+This function adds the reactive  power limits of generators when there are CommitmentVariables
+"""
+function reactivepower(ps_m::canonical_model, devices::Array{S,1}, device_formulation::Type{D}, system_formulation::Type{S}, time_range::UnitRange{Int64}) where {S <: PowerSystems.Storage, D <: AbstractStorageForm, S <: AbstractACPowerModel}
+
+    range_data = [(g.name, g.reactivepowerlimits) for g in devices]
+
+    device_range(ps_m, range_data , time_range, "thermal_reactive_range", "Qst")
+
+end
