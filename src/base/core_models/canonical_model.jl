7b4b62cc6b9c42a48110b9fa36b76b8f771c4c1f
Jose Daniel Lara <jdlara@berkeley.edu>
2019-01-22 15:10:19 -0800

    update canonical model

--- src/base/core_models/canonical_model.jl
+++ src/base/core_models/canonical_model.jl
@@ -2,6 +2,7 @@ mutable struct CanonicalModel
     JuMPmodel::JuMP.AbstractModel
     variables::Dict{String, JuMP.Containers.DenseAxisArray}
     constraints::Dict{String, JuMP.Containers.DenseAxisArray}
+    cost_function::Union{Nothing,JuMP.AbstractJuMPScalar}
     expressions::Dict{String, JumpAffineExpressionArray}
     pm_model::Dict
 end
