d3d6fc568d6c7476d9ce7a9bb8ad9c8ec629cecb
Jose Daniel Lara <jdlara@berkeley.edu>
2019-01-29 17:32:34 -0800

    Fix typos

--- src/device_models/renewable_generation/injection_expression.jl
+++ src/device_models/renewable_generation/injection_expression.jl
@@ -2,9 +2,9 @@ function nodal_expression(ps_m::CanonicalModel, devices::Array{R,1}, system_form
 
     for t in time_range, d in devices
 
-        _add_to_expression(ps_m.expressions["var_active"], d.bus.number, t, d.tech.installedcapacity * values(fs.scalingfactor)[t]) 
+        _add_to_expression!(ps_m.expressions["var_active"], d.bus.number, t, d.tech.installedcapacity * values(d.scalingfactor)[t]) 
 
-        _add_to_expression(ps_m.expressions["var_reactive"], d.bus.number, t, d.tech.installedcapacity * values(fs.scalingfactor)[t]*sin(acost(d.tech.powerfactor))) 
+        _add_to_expression!(ps_m.expressions["var_reactive"], d.bus.number, t, d.tech.installedcapacity * values(d.scalingfactor)[t]*sin(acost(d.tech.powerfactor))) 
 
     end
 
@@ -15,7 +15,7 @@ function nodal_expression(ps_m::CanonicalModel, devices::Array{R,1}, system_form
 
     for t in time_range, d in devices
 
-        _add_to_expression(ps_m.expressions["var_active"], d.bus.number, t, d.tech.installedcapacity * values(fs.scalingfactor)[t]) 
+        _add_to_expression!(ps_m.expressions["var_active"], d.bus.number, t, d.tech.installedcapacity * values(d.scalingfactor)[t]) 
 
     end
 
