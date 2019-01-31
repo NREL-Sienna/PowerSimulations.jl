ca3298be2d4b3d5f4d1707b7b273c0fdbc536f88
Jose Daniel Lara <jdlara@berkeley.edu>
2019-01-30 20:54:11 -0800

    comment out test until there is data for it

--- test/RenewableGenerationConstructors_testing.jl
+++ test/RenewableGenerationConstructors_testing.jl
@@ -9,16 +9,16 @@
     PSI.constructdevice!(ps_model, PSY.RenewableGen, PSI.RenewableFullDispatch, PM.DCPlosslessForm, sys5b);
     true finally end
     
-    @test_skip try 
-    ps_model = PSI.CanonicalModel(Model(),
-                                  Dict{String, JuMP.Containers.DenseAxisArray{JuMP.VariableRef}}(),
-                                  Dict{String, JuMP.Containers.DenseAxisArray}(),
-                                  nothing,
-                                  Dict{String, PSI.JumpAffineExpressionArray}("var_active" => PSI.JumpAffineExpressionArray(undef, 14, 24),
-                                                                             "var_reactive" => PSI.JumpAffineExpressionArray(undef, 14, 24)),
-                                  Dict());
-    PSI.constructdevice!(ps_model, PSY.RenewableGen, PSI.RenewableFullDispatch, PM.StandardACPForm, sys5b); 
-    true finally end
+    #@test try 
+    #ps_model = PSI.CanonicalModel(Model(),
+    #                              Dict{String, JuMP.Containers.DenseAxisArray{JuMP.VariableRef}}(),
+    #                              Dict{String, JuMP.Containers.DenseAxisArray}(),
+    #                              nothing,
+    #                              Dict{String, PSI.JumpAffineExpressionArray}("var_active" => PSI.JumpAffineExpressionArray(undef, 14, 24),
+    #                                                                         "var_reactive" => PSI.JumpAffineExpressionArray(undef, 14, 24)),
+    #                              Dict());
+    #PSI.constructdevice!(ps_model, PSY.RenewableGen, PSI.RenewableFullDispatch, PM.StandardACPForm, sys5b); 
+    #true finally end
     
 @test try 
     ps_model = PSI.CanonicalModel(Model(GLPK_optimizer),
