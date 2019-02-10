6d7146eb74ce38a7b6e25efbbd069380748be62c
Jose Daniel Lara <jdlara@berkeley.edu>
2019-02-10 15:31:54 -0800

    change expr_name to expression for consistency

--- src/network_models/copperplate_model.jl
+++ src/network_models/copperplate_model.jl
@@ -1,11 +1,11 @@
-function copper_plate(ps_m::CanonicalModel, expr_name::String, bus_count::Int64, time_range::UnitRange{Int64})
+function copper_plate(ps_m::CanonicalModel, expression::String, bus_count::Int64, time_range::UnitRange{Int64})
 
-    devices_netinjection = _remove_undef!(ps_m.expressions["$(expr_name)"])
+    devices_netinjection = _remove_undef!(ps_m.expressions["$(expression)"])
 
     ps_m.constraints["CopperPlateBalance"] = JuMP.Containers.DenseAxisArray{JuMP.ConstraintRef}(undef, time_range)
 
     for t in time_range
-        ps_m.constraints["CopperPlateBalance"][t] = JuMP.@constraint(ps_m.JuMPmodel, sum(ps_m.expressions["$(expr_name)"][1:bus_count,t]) == 0)
+        ps_m.constraints["CopperPlateBalance"][t] = JuMP.@constraint(ps_m.JuMPmodel, sum(ps_m.expressions["$(expression)"][1:bus_count,t]) == 0)
     end
 
 end
