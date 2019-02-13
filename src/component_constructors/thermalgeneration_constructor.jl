b8ef58df8eb620c2b05ca7df10d16cce9a29cf16
Jose Daniel Lara <jdlara@berkeley.edu>
2019-02-12 16:33:41 -0800

    add initial conditions passing in Thermal Gen constructor WIP

--- src/component_constructors/thermalgeneration_constructor.jl
+++ src/component_constructors/thermalgeneration_constructor.jl
@@ -3,6 +3,31 @@ This function creates the model for a full themal dispatch formulation depending
 """
 function constructdevice!(ps_m::CanonicalModel, category::Type{T}, category_formulation::Type{D}, system_formulation::Type{S}, sys::PSY.PowerSystem; kwargs...) where {T <: PSY.ThermalGen, D <: AbstractThermalFormulation, S <: PM.AbstractPowerFormulation}
 
+    #wrangle initial_conditions
+    if :initial_conditions in keys(kwargs)
+
+        initial_conditions = kwargs[:initial_conditions]
+
+    else
+
+        initial_conditions = [(g.name, Float64(g.tech.activepower > 0.0)) for g in sys.generators.thermal]
+
+    end
+
+     #=
+            if :initialonduration in keys(args)
+            initialonduration = args[:initialonduration]
+        else
+            initialonduration = Dict(zip(name_index,ones(Float64,length(devices))*9999))
+        end
+
+        if :initialoffduration in keys(args)
+            initialoffduration = args[:initialoffduration]
+        else
+            initialoffduration = Dict(zip(name_index,ones(Float64,length(devices))*9999))
+        end
+    =#
+
     #Defining this outside in order to enable time slicing later
     time_range = 1:sys.time_periods
 
@@ -18,42 +43,13 @@ function constructdevice!(ps_m::CanonicalModel, category::Type{T}, category_form
 
     reactivepower_constraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, time_range)
 
-    if :initial_conditions in keys(kwargs)
-
-        initial_conditions = kwargs[:initial_conditions]
-
-    else
-
-        initial_conditions = [(g.name, Float64(g.tech.activepower > 0.0)) for g in sys.generators.thermal]
-
-    end
-
-    commitment_constraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, time_range, initial_conditions)
-
+    commitment_constraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, time_range, status_initial_conditions)
    
-   
-    ramp_constraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, time_range, initial_conditions)
+    ramp_constraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, time_range, ramp_initial_conditions)
 
-    time_constraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, time_range, initial_conditions)
+    time_constraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, time_range, time_initial_conditions)
 
-    #devices = [d for d in devices if !isa(d.tech.timelimits, Nothing)]
-    #=
-            if :initialonduration in keys(args)
-            initialonduration = args[:initialonduration]
-        else
-            initialonduration = Dict(zip(name_index,ones(Float64,length(devices))*9999))
-        end
-
-        if :initialoffduration in keys(args)
-            initialoffduration = args[:initialoffduration]
-        else
-            initialoffduration = Dict(zip(name_index,ones(Float64,length(devices))*9999))
-        end
-    =#
-    #timeconstraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, sys.time_periods; kwargs...)
- 
-
-    #rating constraints 
+    #TODO: rate constraints 
 
     #Cost Function
 
@@ -61,41 +57,6 @@ function constructdevice!(ps_m::CanonicalModel, category::Type{T}, category_form
 
 end
 
-
-function constructdevice!(ps_m::CanonicalModel, category::Type{T}, category_formulation::Type{D}, system_formulation::Type{S}, sys::PSY.PowerSystem; kwargs...) where {T <: PSY.ThermalGen, D <: AbstractThermalFormulation, S <: PM.AbstractActivePowerFormulation}
-
-    #Defining this outside in order to enable time slicing later
-    time_range = 1:sys.time_periods
-
-    #Variables
-    activepower_variables(ps_m, sys.generators.thermal, time_range);
-
-    commitment_variables(ps_m, sys.generators.thermal, time_range)
-
-    #Constraints
-    activepower_constraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, time_range)
-
-    if :initial_conditions in keys(kwargs)
-
-        initial_conditions = kwargs[:initial_conditions]
-
-    else
-
-        initial_conditions = [(g.name, Float64(g.tech.activepower > 0.0)) for g in sys.generators.thermal]
-
-    end
-
-    commitment_constraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, time_range, initial_conditions)
-
-    #devices = [d for d in devices if !isa(d.tech.ramplimits,Nothing)]
-    #rampconstraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, sys.time_periods; kwargs...)
-
-    #timeconstraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, sys.time_periods; kwargs...)
-
-    #Cost Function
-    cost_function(ps_m, sys.generators.thermal, category_formulation, system_formulation)
-
-end
 
 function constructdevice!(ps_m::CanonicalModel, category::Type{T}, category_formulation::Type{PSI.ThermalDispatch}, system_formulation::Type{S}, sys::PSY.PowerSystem; kwargs...) where {T<: PSY.ThermalGen, S <: PM.AbstractPowerFormulation}
 
@@ -112,7 +73,7 @@ function constructdevice!(ps_m::CanonicalModel, category::Type{T}, category_form
 
     reactivepower_constraints(ps_m, sys.generators.thermal, category_formulation, system_formulation, time_range)
 
-    #rating constraints 
+    #TODO: rate constraints 
 
     #Cost Function
     cost_function(ps_m, sys.generators.thermal, category_formulation, system_formulation)
