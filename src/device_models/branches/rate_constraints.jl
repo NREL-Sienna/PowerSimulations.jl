59774b64f357500006079d91d7efcc808643ab86
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2019-02-28 15:57:32 -0700

    Adding Flow Constraints

--- src/device_models/branches/rate_constraints.jl
+++ src/device_models/branches/rate_constraints.jl
@@ -1,8 +1,7 @@
 function rate_constraints(ps_m::CanonicalModel, devices::Array{Br,1}, device_formulation::Type{PSI.StandardPTDFModel}, system_formulation::Type{S}, time_range::UnitRange{Int64}) where {H <: PSY.HydroGen, D <: AbstractHydroDispatchForm, S <: PM.AbstractPowerFormulation}
+    rate_data = [(h.name, (min = -1*h.rate, max = h.rate) for h in devices]
 
-    rate_data = [(h.name, h.tech.activepowerlimits) for h in devices]
-
-    device_range(ps_m, range_data, time_range, "hydro_active_range", "Phy")
+    device_range(ps_m, range_data, time_range, "dc_rate_const", "Fbr")
 
 end
 
@@ -11,7 +10,7 @@ device_formulation::Type{PSI.StandardPTDFModel}, system_formulation::Type{S},
 time_range::UnitRange{Int64}) where {H <: PSY.HydroGen, D <: AbstractHydroDispatchForm, S <:
 PM.AbstractActivePowerFormulation}
 
-    rate_data = [(h.name, h.tech.activepowerlimits) for h in devices]
+    rate_data = [(h.name, h.rate) for h in devices]
 
     norm_two_constraint(ps_m, range_data, time_range, "hydro_active_range", "Phy")
 
