812d6942e35260576ecdfadf459c039549fb800d
Jose Daniel Lara <jdlara@berkeley.edu>
2019-02-28 12:08:09 -0700

    Make Operational model inner constructors

--- src/abstract_models/operation_model.jl
+++ src/abstract_models/operation_model.jl
@@ -15,123 +15,123 @@ mutable struct PowerOperationModel{M <: AbstractOperationsModel,
     services::Dict{String, DataType}
     system::PSY.PowerSystem
     canonical_model::PSI.CanonicalModel
+
+
+    function PowerOperationModel(op_model::Type{M},
+                                transmission::Type{T},
+                                devices::Dict{String, DeviceModel},
+                                branches::Dict{String, DeviceModel},
+                                services::Dict{String, DataType},
+                                system::PSY.PowerSystem,
+                                optimizer::JuMP.OptimizerFactory=empty_optimizer; kwargs...) where {M <: AbstractOperationsModel,
+                                                                                                    T <: PM.AbstractPowerFormulation}
+
+        bus_count = length(system.buses)
+
+        ps_model = CanonicalModel(JuMP.Model(optimizer),
+                                Dict{String, JuMP.Containers.DenseAxisArray}(),
+                                Dict{String, JuMP.Containers.DenseAxisArray}(),
+                                nothing,
+                                Dict{String, PSI.JumpAffineExpressionArray}("var_active" => PSI.JumpAffineExpressionArray(undef, bus_count, system.time_periods),
+                                                                            "var_reactive" => PSI.JumpAffineExpressionArray(undef, bus_count, system.time_periods)),
+                                Dict{String,Any}(),
+                                nothing);
+
+        new{M, T}(op_model,
+                transmission,
+                devices,
+                branches,
+                services,
+                system,
+                ps_model)
+
+
+    end
+
+    function PowerOperationModel(op_model::Type{M},
+                                transmission::Type{T},
+                                devices::Dict{String, DeviceModel},
+                                branches::Dict{String, DeviceModel},
+                                services::Dict{String, DataType},
+                                system::PSY.PowerSystem,
+                                optimizer::JuMP.OptimizerFactory=empty_optimizer; kwargs...) where {M <: AbstractOperationsModel,
+                                                                                                    T <: PM.AbstractActivePowerFormulation}
+
+        bus_count = length(system.buses)
+
+        ps_model = CanonicalModel(JuMP.Model(optimizer),
+                                Dict{String, JuMP.Containers.DenseAxisArray}(),
+                                Dict{String, JuMP.Containers.DenseAxisArray}(),
+                                nothing,
+                                Dict{String, PSI.JumpAffineExpressionArray}("var_active" => PSI.JumpAffineExpressionArray(undef, bus_count, system.time_periods)),
+                                Dict{String,Any}(),
+                                nothing);
+
+        new{M, T}(op_model,
+                transmission,
+                devices,
+                branches,
+                services,
+                system,
+                ps_model)
+
+    end
+
+    function PowerOperationModel(op_model::Type{M},
+                                transmission::Type{StandardPTDFModel},
+                                devices::Dict{String, DeviceModel},
+                                branches::Dict{String, DeviceModel},
+                                services::Dict{String, DataType},
+                                system::PSY.PowerSystem,
+                                optimizer::JuMP.OptimizerFactory=empty_optimizer; kwargs...) where {M <: AbstractOperationsModel}
+
+        bus_count = length(system.buses)
+
+        ps_model = CanonicalModel(JuMP.Model(optimizer),
+            Dict{String, JuMP.Containers.DenseAxisArray}(),
+            Dict{String, JuMP.Containers.DenseAxisArray}(),
+            nothing,
+            Dict{String, PSI.JumpAffineExpressionArray}("var_active" => PSI.JumpAffineExpressionArray(undef, bus_count, system.time_periods)),
+            Dict{String,Any}(),
+            nothing);
+
+            new{M, StandardPTDFModel}(op_model,
+                                            transmission,
+                                            devices,
+                                            branches,
+                                            services,
+                                            system,
+                                            ps_model)
+
+    end
+
+    function PowerOperationModel(op_model::Type{M},
+                                transmission::Type{CopperPlatePowerModel},
+                                devices::Dict{String, DeviceModel},
+                                branches::Dict{String, DeviceModel},
+                                services::Dict{String, DataType},
+                                system::PSY.PowerSystem,
+                                optimizer::JuMP.OptimizerFactory=empty_optimizer; kwargs...) where {M <: AbstractOperationsModel}
+
+        bus_count = length(system.buses)
+
+        ps_model = CanonicalModel(JuMP.Model(optimizer),
+                                Dict{String, JuMP.Containers.DenseAxisArray}(),
+                                Dict{String, JuMP.Containers.DenseAxisArray}(),
+                                nothing,
+                                Dict{String, PSI.JumpAffineExpressionArray}("var_active" => PSI.JumpAffineExpressionArray(undef, bus_count, system.time_periods)),
+                                Dict{String,Any}(),
+                                nothing);
+
+        new{M, CopperPlatePowerModel}(op_model,
+                                            transmission,
+                                            devices,
+                                            branches,
+                                            services,
+                                            system,
+                                            ps_model)
+
+    end
+
+
 end
-
-function PowerOperationModel(op_model::Type{M},
-                             transmission::Type{T},
-                             devices::Dict{String, DeviceModel},
-                             branches::Dict{String, DeviceModel},
-                             services::Dict{String, DataType},
-                             system::PSY.PowerSystem,
-                             optimizer::JuMP.OptimizerFactory=empty_optimizer; kwargs...) where {M <: AbstractOperationsModel,
-                                                                                                   T <: PM.AbstractPowerFormulation}
-
-    bus_count = length(system.buses)
-
-    ps_model = CanonicalModel(JuMP.Model(optimizer),
-                              Dict{String, JuMP.Containers.DenseAxisArray}(),
-                              Dict{String, JuMP.Containers.DenseAxisArray}(),
-                              nothing,
-                              Dict{String, PSI.JumpAffineExpressionArray}("var_active" => PSI.JumpAffineExpressionArray(undef, bus_count, system.time_periods),
-                                                                          "var_reactive" => PSI.JumpAffineExpressionArray(undef, bus_count, system.time_periods)),
-                              Dict{String,Any}(),
-                              nothing);
-
-    return PowerOperationModel(op_model,
-                               transmission,
-                               devices,
-                               branches,
-                               services,
-                               system,
-                               ps_model)
-
-
-end
-
-function PowerOperationModel(op_model::Type{M},
-                             transmission::Type{T},
-                             devices::Dict{String, DeviceModel},
-                             branches::Dict{String, DeviceModel},
-                             services::Dict{String, DataType},
-                             system::PSY.PowerSystem,
-                             optimizer::JuMP.OptimizerFactory=empty_optimizer; kwargs...) where {M <: AbstractOperationsModel,
-                                                                    T <: PM.AbstractActivePowerFormulation}
-
-    bus_count = length(system.buses)
-
-    ps_model = CanonicalModel(JuMP.Model(optimizer),
-                              Dict{String, JuMP.Containers.DenseAxisArray}(),
-                              Dict{String, JuMP.Containers.DenseAxisArray}(),
-                              nothing,
-                              Dict{String, PSI.JumpAffineExpressionArray}("var_active" => PSI.JumpAffineExpressionArray(undef, bus_count, system.time_periods)),
-                              Dict{String,Any}(),
-                              nothing);
-
-        return PowerOperationModel(op_model,
-                                  transmission,
-                                  devices,
-                                  branches,
-                                  services,
-                                  system,
-                                  ps_model)
-
-end
-
-function PowerOperationModel(op_model::Type{M},
-                             transmission::Type{StandardPTDFModel},
-                             devices::Dict{String, DeviceModel},
-                             branches::Dict{String, DeviceModel},
-                             services::Dict{String, DataType},
-                             system::PSY.PowerSystem,
-                             optimizer::JuMP.OptimizerFactory=empty_optimizer; kwargs...) where {M <: AbstractOperationsModel}
-
-    bus_count = length(system.buses)
-
-    ps_model = CanonicalModel(JuMP.Model(optimizer),
-        Dict{String, JuMP.Containers.DenseAxisArray}(),
-        Dict{String, JuMP.Containers.DenseAxisArray}(),
-        nothing,
-        Dict{String, PSI.JumpAffineExpressionArray}("var_active" => PSI.JumpAffineExpressionArray(undef, bus_count, system.time_periods)),
-        Dict{String,Any}(),
-        nothing);
-
-    return PowerOperationModel(op_model,
-                               transmission,
-                               devices,
-                               branches,
-                               services,
-                               system,
-                               ps_model)
-
-end
-
-function PowerOperationModel(op_model::Type{M},
-                             transmission::Type{CopperPlatePowerModel},
-                             devices::Dict{String, DeviceModel},
-                             branches::Dict{String, DeviceModel},
-                             services::Dict{String, DataType},
-                             system::PSY.PowerSystem,
-                             optimizer::JuMP.OptimizerFactory=empty_optimizer; kwargs...) where {M <: AbstractOperationsModel}
-
-    bus_count = length(system.buses)
-
-    ps_model = CanonicalModel(JuMP.Model(optimizer),
-                              Dict{String, JuMP.Containers.DenseAxisArray}(),
-                              Dict{String, JuMP.Containers.DenseAxisArray}(),
-                              nothing,
-                              Dict{String, PSI.JumpAffineExpressionArray}("var_active" => PSI.JumpAffineExpressionArray(undef, bus_count, system.time_periods)),
-                              Dict{String,Any}(),
-                              nothing);
-
-     return PowerOperationModel(op_model,
-                                transmission,
-                                devices,
-                                branches,
-                                services,
-                                system,
-                                ps_model)
-
-
-end
-
-
