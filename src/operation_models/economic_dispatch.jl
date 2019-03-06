4b9ba91f796921dce1113c2d3e94d59cb5c74c56
Jose Daniel Lara <jdlara@berkeley.edu>
2019-03-06 10:51:51 -0800

    add SCED Model

--- src/operation_models/economic_dispatch.jl
+++ src/operation_models/economic_dispatch.jl
@@ -1,4 +1,5 @@
 struct EconomicDispatch <: AbstractOperationsModel end
+struct SCEconomicDispatch <: AbstractOperationsModel end
 
 function EconomicDispatch(system::PSY.PowerSystem, transmission::Type{S}; optimizer::Union{Nothing,JuMP.OptimizerFactory}=nothing, kwargs...) where {S <: PM.AbstractPowerFormulation}
 
@@ -16,4 +17,31 @@ function EconomicDispatch(system::PSY.PowerSystem, transmission::Type{S}; optimi
                                     services,                                
                                     system,
                                     optimizer = optimizer; kwargs...)
+end
+
+function SCEconomicDispatch(system::PSY.PowerSystem; optimizer::Union{Nothing,JuMP.OptimizerFactory}=nothing, kwargs...)
+    
+    if :PTDF in keys(kwargs)
+
+        PTDF = kwargs[:PTDF]
+
+    else
+        @info "PTDF matrix not provided. It will be constructed using PowerSystems.PTDF"
+        PTDF, A = PowerSystems.buildptdf(system.branches, system.buses);
+    end
+    
+    devices = Dict{String, PSI.DeviceModel}("ThermalGenerators" => PSI.DeviceModel(PSY.ThermalGen, PSI.ThermalDispatch),
+    "RenewableGenerators" => PSI.DeviceModel(PSY.RenewableGen, PSI.RenewableFullDispatch),
+    "Loads" => PSI.DeviceModel(PSY.PowerLoad, PSI.StaticPowerLoad))
+
+    branches = Dict{String, PSI.DeviceModel}("Lines" => PSI.DeviceModel(PSY.Branch, PSI.SeriesLine))                                             
+    services = Dict{String, PSI.ServiceModel}("Reserves" => PSI.ServiceModel(PSY.Reserve, PSI.AbstractReservesForm))
+
+    op_model = PowerOperationModel(EconomicDispatch,
+                                    StandardPTDFForm, 
+                                    devices, 
+                                    branches, 
+                                    services,                                
+                                    system,
+                                    optimizer = optimizer; PTDF = PTDF, kwargs...)
 end
