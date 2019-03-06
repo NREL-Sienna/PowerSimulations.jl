ea28061bf4afa2bde11e05c782bf8862ba904c45
Jose Daniel Lara <jdlara@berkeley.edu>
2019-03-06 10:34:28 -0800

    add Unit Commitment

--- src/operation_models/unit_commitment.jl
+++ src/operation_models/unit_commitment.jl
@@ -1,1 +1,19 @@
-abstract type UnitCommitment <: AbstractOperationsModel end
+struct UnitCommitment <: AbstractOperationsModel end
+
+function UnitCommitment(system::PSY.PowerSystem, transmission::Type{S}; optimizer::Union{Nothing,JuMP.OptimizerFactory}=nothing, kwargs...) where {S <: PM.AbstractPowerFormulation}
+
+    devices = Dict{String, PSI.DeviceModel}("Generators" => PSI.DeviceModel(PSY.ThermalGen, PSI.ThermalUnitCommitment),
+                                            "RenewableGenerators" => PSI.DeviceModel(PSY.RenewableGen, PSI.RenewableFullDispatch),
+                                            "Loads" => PSI.DeviceModel(PSY.PowerLoad, PSI.StaticPowerLoad))
+
+    branches = Dict{String, PSI.DeviceModel}("Lines" => PSI.DeviceModel(PSY.Branch, PSI.SeriesLine))                                             
+    services = Dict{String, PSI.ServiceModel}("Reserves" => PSI.ServiceModel(PSY.Reserve, PSI.AbstractReservesForm))
+
+    op_model = PowerOperationModel(UnitCommitment,
+                                   transmission, 
+                                    devices, 
+                                    branches, 
+                                    services,                                
+                                    system;
+                                    optimizer = optimizer, kwargs...)
+end
