33acae630f1f2f7939aeb58ae0514eb2ace0995d
Jose Daniel Lara <jdlara@berkeley.edu>
2019-03-06 11:16:32 -0800

    add OPF Operational Problem

---
+++ src/operation_models/opf.jl
@@ -0,0 +1,19 @@
+struct OptimalPowerFlow <: AbstractOperationsModel end
+
+function OptimalPowerFlow(system::PSY.PowerSystem, transmission::Type{S}; optimizer::Union{Nothing,JuMP.OptimizerFactory}=nothing, kwargs...) where {S <: PM.AbstractPowerFormulation}
+
+    devices = Dict{String, PSI.DeviceModel}("ThermalGenerators" => PSI.DeviceModel(PSY.ThermalGen, PSI.ThermalDispatch),
+                                            "RenewableGenerators" => PSI.DeviceModel(PSY.RenewableGen, PSI.RenewableConstantPowerFactor),
+                                            "Loads" => PSI.DeviceModel(PSY.PowerLoad, PSI.StaticPowerLoad))
+
+    branches = Dict{String, PSI.DeviceModel}("Lines" => PSI.DeviceModel(PSY.Branch, PSI.SeriesLine))                                             
+    services = Dict{String, PSI.ServiceModel}("Reserves" => PSI.ServiceModel(PSY.Reserve, PSI.AbstractReservesForm))
+
+    op_model = PowerOperationModel(OptimalPowerFlow ,
+                                   transmission, 
+                                    devices, 
+                                    branches, 
+                                    services,                                
+                                    system,
+                                    optimizer = optimizer; kwargs...)
+end
