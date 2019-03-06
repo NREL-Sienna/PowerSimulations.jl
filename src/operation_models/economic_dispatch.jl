730e0f189254bcd24ea13ebe4f8efd6052d79bf7
Jose Daniel Lara <jdlara@berkeley.edu>
2019-03-06 10:34:18 -0800

    add Economic Dispatch

--- src/operation_models/economic_dispatch.jl
+++ src/operation_models/economic_dispatch.jl
@@ -1,1 +1,19 @@
-abstract type EconomicDispatch <: AbstractOperationsModel end
+struct EconomicDispatch <: AbstractOperationsModel end
+
+function EconomicDispatch(system::PSY.PowerSystem, transmission::Type{S}; optimizer::Union{Nothing,JuMP.OptimizerFactory}=nothing, kwargs...) where {S <: PM.AbstractPowerFormulation}
+
+    devices = Dict{String, PSI.DeviceModel}("ThermalGenerators" => PSI.DeviceModel(PSY.ThermalGen, PSI.ThermalDispatch),
+                                            "RenewableGenerators" => PSI.DeviceModel(PSY.RenewableGen, PSI.RenewableFullDispatch),
+                                            "Loads" => PSI.DeviceModel(PSY.PowerLoad, PSI.StaticPowerLoad))
+
+    branches = Dict{String, PSI.DeviceModel}("Lines" => PSI.DeviceModel(PSY.Branch, PSI.SeriesLine))                                             
+    services = Dict{String, PSI.ServiceModel}("Reserves" => PSI.ServiceModel(PSY.Reserve, PSI.AbstractReservesForm))
+
+    op_model = PowerOperationModel(EconomicDispatch,
+                                   transmission, 
+                                    devices, 
+                                    branches, 
+                                    services,                                
+                                    system,
+                                    optimizer = optimizer; kwargs...)
+end
