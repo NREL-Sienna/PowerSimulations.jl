c2f13ffa6e5b02e8fd2edfcded6c5b09b14c6384
Jose Daniel Lara <jdlara@berkeley.edu>
2019-03-08 10:36:13 -0800

    change return of  build_op_model!

--- src/core/operations_constructor.jl
+++ src/core/operations_constructor.jl
@@ -88,11 +88,12 @@ function build_op_model!(transmission::Type{T},
 end            
 
 function build_op_model!(op_model; optimizer::Union{Nothing,JuMP.OptimizerFactory}=nothing, kwargs...)
-    return build_op_model!(op_model.transmission, 
-                          op_model.devices,
-                          op_model.branches,
-                          op_model.services,
-                          op_model.system,
-                          optimizer;
-                          kwargs...)
+       op_model.canonical_nodel = build_op_model!(op_model.transmission, 
+                                                    op_model.devices,
+                                                    op_model.branches,
+                                                    op_model.services,
+                                                    op_model.system,
+                                                    optimizer;
+                                                    kwargs...)
+        return nothing
 end  
