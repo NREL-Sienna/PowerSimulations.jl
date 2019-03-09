65d55ae581c4799cee7f920457e4d16aaaa6c1c5
Jose Daniel Lara <jdlara@berkeley.edu>
2019-03-08 17:58:52 -0800

    add More Tests for Op Model Solving

--- test/operation_model_solve.jl
+++ test/operation_model_solve.jl
@@ -1,46 +1,37 @@
 
+
+@testset "Solving ED Models" begin
+    ED = PSI.EconomicDispatch(sys5b_uc, PSI.CopperPlatePowerModel; optimizer = GLPK_optimizer);
+    res_5 = solve_op_model!(ED)
+    @test isapprox(res_5.total_cost[:ED], 2400, atol = 1000)
+    ED = PSI.EconomicDispatch(sys14, PSI.CopperPlatePowerModel; optimizer = ipopt_optimizer);
+    res_14 = solve_op_model!(ED)
+    @test isapprox(res_14.total_cost[:ED], 1200, atol = 1000)
+end
+
+@testset "Solving ED with PTDF Models" begin
+    PTDF5,  = PowerSystems.buildptdf(branches5, nodes5)
+    ED = PSI.EconomicDispatch(sys5b_uc, PSI.StandardPTDFForm; PTDF = PTDF5, optimizer = GLPK_optimizer);
+    res_5 = solve_op_model!(ED)
+    @test isapprox(res_5.total_cost[:ED], 3400, atol = 1000)
+    PTDF14,  = PSY.buildptdf(sys14.branches, sys14.buses)
+    ED = PSI.EconomicDispatch(sys14, PSI.StandardPTDFForm; PTDF = PTDF14, optimizer = ipopt_optimizer);
+    res_14 = solve_op_model!(ED)
+    @test_skip isapprox(res_14.total_cost[:ED], 1200, atol = 1000)
+end
+
+
+
+@testset "testing AngleDC-OPF 5-bus" begin
+    ED = PSI.EconomicDispatch(sys5b_uc,PM.DCPlosslessForm; optimizer = GLPK_optimizer);
+    res_5= solve_op_model!(ED)
+    @test isapprox(res_5.total_cost[:ED], 3400, atol = 1000)
+    ED = PSI.EconomicDispatch(sys14,PM.DCPlosslessForm; optimizer = ipopt_optimizer);
+    res_14 = solve_op_model!(ED)
+    @test isapprox(res_14.total_cost[:ED], 1200, atol = 1000)
+end
+
 #=
-##################
-These tests still need to be rewritten for the new infrastructure in PowerSimulations
-##################
-
-@test try
-    @info "testing net flow"
-    Net = PSI.StandardNetFlow
-    m = Model(ipopt_optimizer);
-    netinjection = PSI.instantiate_network(Net, sys5);
-    PSI.construct_device!(m, netinjection, ThermalGen, PSI.ThermalDispatch, Net, sys5);
-    PSI.construct_network!(m, [(device=Line, formulation=PSI.PiLine)], netinjection, Net, sys5)
-    JuMP.@objective(m, Min, m.obj_dict[:objective_function])
-    JuMP.optimize!(m)
-    isapprox(JuMP.objective_value(m), 3400, atol = 1000)
-finally end
-
-@test try
-    @info "testing PTDF 5-bus"
-    Net = PSI.StandardPTDFForm
-    m = Model(ipopt_optimizer);
-    ptdf,  A = PSY.buildptdf(sys5.branches, sys5.buses)
-    netinjection = PSI.instantiate_network(Net, sys5);
-    PSI.construct_device!(m, netinjection, ThermalGen, PSI.ThermalDispatch, Net, sys5);
-    PSI.construct_network!(m, [(device=Line, formulation=PSI.PiLine)], netinjection, Net, sys5, PTDF = ptdf)
-    JuMP.@objective(m, Min, m.obj_dict[:objective_function])
-    JuMP.optimize!(m)
-    isapprox(JuMP.objective_value(m), 3400, atol = 1000)
-finally end
-
-@test try
-    @info "testing AngleDC-OPF 5-bus"
-    Net = PM.DCPlosslessForm
-    m = Model(ipopt_optimizer);
-    netinjection = PSI.instantiate_network(Net, sys5);
-    PSI.construct_device!(m, netinjection, ThermalGen, PSI.ThermalDispatch, Net, sys5);
-    PSI.construct_network!(m, [(device=Line, formulation=PSI.PiLine)], netinjection, Net, sys5)
-    JuMP.@objective(m, Min, m.obj_dict[:objective_function])
-    JuMP.optimize!(m)
-    isapprox(JuMP.objective_value(m), 3400, atol = 1000)
-finally end
-
 @test try
     @info "testing ACP-OPF 5-bus"
     Net = PM.StandardACPForm
@@ -64,51 +55,6 @@ true finally end
     JuMP.optimize!(m)
     isapprox(JuMP.objective_value(m), 3400, atol = 1000)
 true finally end
-
-
-
-include(joinpath(base_dir,"data/data_14bus_pu.jl"))
-sys14 = PowerSystem(nodes14, generators14, loads14, branches14, nothing,  100.0);
-
-
-@test try
-    @info "testing copper plate 14-bus"
-    Net = PSI.CopperPlatePowerModel
-    m = Model(ipopt_optimizer);
-    netinjection = PSI.instantiate_network(Net, sys14);
-    PSI.construct_device!(m, netinjection, ThermalGen, PSI.ThermalDispatch, Net, sys14);
-    PSI.construct_network!(m, [(device=Line, formulation=PSI.PiLine)], netinjection, Net, sys14)
-    JuMP.@objective(m, Min, m.obj_dict[:objective_function])
-    JuMP.optimize!(m)
-    isapprox(JuMP.objective_value(m), 1200, atol = 1000)
-finally end
-
-
-@test try
-    @info "testing net 14-bus"
-    Net = PSI.StandardNetFlow
-    m = Model(ipopt_optimizer);
-    netinjection = PSI.instantiate_network(Net, sys14);
-    PSI.construct_device!(m, netinjection, ThermalGen, PSI.ThermalDispatch, Net, sys14);
-    PSI.construct_network!(m, [(device=Line, formulation=PSI.PiLine)], netinjection, Net, sys14)
-    JuMP.@objective(m, Min, m.obj_dict[:objective_function])
-    JuMP.optimize!(m)
-    isapprox(JuMP.objective_value(m), 1200, atol = 1000)
-finally end
-
-
-@test_skip try
-    @info "testing PTDF 14-bus"
-    Net = PSI.StandardPTDFForm
-    m = Model(ipopt_optimizer);
-    ptdf,  A = PSY.buildptdf(sys14.branches, sys14.buses)
-    netinjection = PSI.instantiate_network(Net, sys14);
-    PSI.construct_device!(m, netinjection, ThermalGen, PSI.ThermalDispatch, Net, sys14);
-    PSI.construct_network!(m, [(device=Line, formulation=PSI.PiLine)], netinjection, Net, sys14, PTDF = ptdf)
-    JuMP.@objective(m, Min, m.obj_dict[:objective_function])
-    JuMP.optimize!(m)
-    isapprox(JuMP.objective_value(m), 1200, atol = 1000)
-finally end
 
 @test try
     @info "testing AngleDC-OPF 14-bus"
