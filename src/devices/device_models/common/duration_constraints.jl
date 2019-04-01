bf2e07526bf243afeedd616f99fa43bb1e726d35
Jose Daniel Lara <jdlara@berkeley.edu>
2019-04-01 13:26:31 -0700

    limit scope of duration constraints

--- src/devices/device_models/common/duration_constraints.jl
+++ src/devices/device_models/common/duration_constraints.jl
@@ -4,8 +4,8 @@ This formulation of the duration constraints, adds over the start times looking 
 """
 function device_duration_retrospective(ps_m::CanonicalModel,
                                         duration_data::Tuple{Vector{String},Vector{UpDown}},
-                                        initial_duration_on::Vector{InitialCondition},
-                                        initial_duration_off::Vector{InitialCondition},
+                                        initial_duration_on::Vector{InitialCondition{Float64}},
+                                        initial_duration_off::Vector{InitialCondition{Float64}},
                                         time_range::UnitRange{Int64},
                                         cons_name::Symbol,
                                         var_names::Tuple{Symbol,Symbol,Symbol})
@@ -23,13 +23,13 @@ function device_duration_retrospective(ps_m::CanonicalModel,
                 if t - duration_data[2][ix].up >= 1
                     tst = duration_data[2][ix].up
                 else
-                    tst = max(1.0, duration_data[2][ix].up - JuMP.value(initial_duration_on[ix].value))
+                    tst = max(1.0, duration_data[2][ix].up - initial_duration_on[ix].value)
                 end
 
                 if t - duration_data[2][ix].down >= 1
                     tsd = duration_data[2][ix].down
                 else
-                    tsd = max(1.0, duration_data[2][ix].down - JuMP.value(initial_duration_off[ix].value))
+                    tsd = max(1.0, duration_data[2][ix].down - initial_duration_off[ix].value)
                 end
 
                 ps_m.constraints[name_up][name, t] = JuMP.@constraint(ps_m.JuMPmodel, sum([ps_m.variables[var_names[2]][name,i] for i in ((t - tst + 1) :t) if i > 0 ]) <= ps_m.variables[var_names[1]][name,t])
