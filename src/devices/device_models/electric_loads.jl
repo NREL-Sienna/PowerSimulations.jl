ec041c0cdea3a73cb1db9cbb2f0aac5ded1341ba
Jose Daniel Lara <jdlara@berkeley.edu>
2019-04-02 09:40:53 -0700

    add optionl to renewables

--- src/devices/device_models/electric_loads.jl
+++ src/devices/device_models/electric_loads.jl
@@ -6,31 +6,17 @@ struct StaticPowerLoad <: AbstractLoadFormulation end
 
 struct InterruptiblePowerLoad <: AbstractControllablePowerLoadForm end
 
-function _get_time_series(device::Array{T}) where {T <: PSY.ElectricLoad}
-
-    names = Vector{String}(undef, length(device))
-    series = Vector{Vector{Float64}}(undef, length(device)) 
-
-    for (ix,d) in enumerate(devices) 
-        names[ix] = d.name
-        series[ix] = d.maxactivepower * values(d.scalingfactor)
-    end
-
-    return names, series
-
-end
-
 # load variables
 
-function activepower_variables(ps_m::CanonicalModel, 
-                               devices::Array{L,1}, 
+function activepower_variables(ps_m::CanonicalModel,
+                               devices::Array{L,1},
                                time_range::UnitRange{Int64}) where {L <: PSY.ElectricLoad}
 
-    add_variable(ps_m, 
-                 devices, 
-                 time_range, 
-                 :Pel, 
-                 false, 
+    add_variable(ps_m,
+                 devices,
+                 time_range,
+                 :Pel,
+                 false,
                  :var_active, -1)
 
     return
@@ -38,18 +24,32 @@ function activepower_variables(ps_m::CanonicalModel,
 end
 
 
-function reactivepower_variables(ps_m::CanonicalModel, 
-                                 devices::Array{L,1}, 
+function reactivepower_variables(ps_m::CanonicalModel,
+                                 devices::Array{L,1},
                                  time_range::UnitRange{Int64}) where {L <: PSY.ElectricLoad}
 
-    add_variable(ps_m, 
-                 devices, 
-                 time_range, 
-                 :Qel, 
-                 false, 
+    add_variable(ps_m,
+                 devices,
+                 time_range,
+                 :Qel,
+                 false,
                  :var_reactive, -1)
 
     return
+
+end
+
+function _get_time_series(device::Array{T}) where {T <: PSY.ElectricLoad}
+
+    names = Vector{String}(undef, length(device))
+    series = Vector{Vector{Float64}}(undef, length(device))
+
+    for (ix,d) in enumerate(devices)
+        names[ix] = d.name
+        series[ix] = d.maxactivepower * values(d.scalingfactor)
+    end
+
+    return names, series
 
 end
 
@@ -60,21 +60,21 @@ function activepower_constraints(ps_m::CanonicalModel,
                                  devices::Array{L,1},
                                  device_formulation::Type{InterruptiblePowerLoad},
                                  system_formulation::Type{S},
-                                 time_range::UnitRange{Int64}; 
+                                 time_range::UnitRange{Int64},
                                  parameters::Bool = true) where {L <: PSY.ElectricLoad,
                                                                       S <: PM.AbstractPowerFormulation}
     if parameters
-        device_timeseries_param_ub(ps_m, 
-                                   _get_time_series(devices), 
-                                   time_range, 
-                                   :load_active_ub, 
-                                   Symbol("Pel_$(eltype(devices))"), 
+        device_timeseries_param_ub(ps_m,
+                                   _get_time_series(devices),
+                                   time_range,
+                                   :load_active_ub,
+                                   Symbol("Pel_$(eltype(devices))"),
                                    :Pel)
     else
-        device_timeseries_ub(ps_m, 
-                             _get_time_series(devices), 
-                             time_range, 
-                             :load_active_ub, 
+        device_timeseries_ub(ps_m,
+                             _get_time_series(devices),
+                             time_range,
+                             :load_active_ub,
                              :Pel)
     end
 
@@ -97,7 +97,7 @@ function reactivepower_constraints(ps_m::CanonicalModel,
 
     for t in time_range, d in devices
             #Estimate PF from the load data. TODO: create a power factor field in PowerSystems
-            ps_m.constraints[:load_reactive_ub][d.name, t] = JuMP.@constraint(ps_m.JuMPmodel,  
+            ps_m.constraints[:load_reactive_ub][d.name, t] = JuMP.@constraint(ps_m.JuMPmodel,
                                                              ps_m.variables[:Qel][d.name, t] == ps_m.variables[:Pel][d.name, t] * sin(atan((d.maxreactivepower/d.maxactivepower))))
     end
 
@@ -108,14 +108,14 @@ end
 
 # controllable load cost
 
-function cost_function(ps_m::CanonicalModel, 
-                       devices::Array{PSY.InterruptibleLoad,1}, 
-                       device_formulation::Type{D}, 
-                       system_formulation::Type{S}) where {D <: InterruptiblePowerLoad, 
+function cost_function(ps_m::CanonicalModel,
+                       devices::Array{PSY.InterruptibleLoad,1},
+                       device_formulation::Type{D},
+                       system_formulation::Type{S}) where {D <: InterruptiblePowerLoad,
                                                            S <: PM.AbstractPowerFormulation}
 
-    add_to_cost(ps_m, devices, 
-                      :Pel, 
+    add_to_cost(ps_m, devices,
+                      :Pel,
                       :sheddingcost)
 
     return
@@ -125,34 +125,34 @@ end
 
 # Injection Expression with parameters
 
-function nodal_expression(ps_m::CanonicalModel, 
-                          devices::Array{L,1}, 
-                          system_formulation::Type{S}, 
+function nodal_expression(ps_m::CanonicalModel,
+                          devices::Array{L,1},
+                          system_formulation::Type{S},
                           time_range::UnitRange{Int64},
-                          parameters::Bool = true) where {L <: PSY.ElectricLoad, 
+                          parameters::Bool = true) where {L <: PSY.ElectricLoad,
                                                                S <: PM.AbstractPowerFormulation}
 
-        if parameters 
+        if parameters
             nodal_expression_param(ps_m, fixed_resources, system_formulation, time_range)
-        else 
+        else
             nodal_expression_fixed(ps_m, fixed_resources, system_formulation, time_range)
         end
-    
-    return    
 
-end                                                               
+    return
 
-function nodal_expression_param(ps_m::CanonicalModel, 
-                                devices::Array{L,1}, 
-                                system_formulation::Type{S}, 
-                                time_range::UnitRange{Int64}) where {L <: PSY.ElectricLoad, 
+end
+
+function nodal_expression_param(ps_m::CanonicalModel,
+                                devices::Array{L,1},
+                                system_formulation::Type{S},
+                                time_range::UnitRange{Int64}) where {L <: PSY.ElectricLoad,
                                                                      S <: PM.AbstractPowerFormulation}
 
     ts_data_active = Vector{Tuple{String,Int64,Vector{Float64}}}(undef, length(devices))
     ts_data_reactive = Vector{Tuple{String,Int64,Vector{Float64}}}(undef, length(devices))
 
     for (ix,d) in enumerate(devices)
-        ts_data_active[ix] = (d.name, d.bus.number, -1*d.maxactivepower * values(d.scalingfactor)) 
+        ts_data_active[ix] = (d.name, d.bus.number, -1*d.maxactivepower * values(d.scalingfactor))
         ts_data_reactive[ix] = (d.name, d.bus.number, -1*d.maxreactivepower * values(d.scalingfactor))
     end
 
@@ -161,10 +161,10 @@ function nodal_expression_param(ps_m::CanonicalModel,
 
 end
 
-function nodal_expression_param(ps_m::CanonicalModel, 
-                                devices::Array{L,1}, 
-                                system_formulation::Type{S}, 
-                                time_range::UnitRange{Int64}) where {L <: PSY.ElectricLoad, 
+function nodal_expression_param(ps_m::CanonicalModel,
+                                devices::Array{L,1},
+                                system_formulation::Type{S},
+                                time_range::UnitRange{Int64}) where {L <: PSY.ElectricLoad,
                                                                      S <: PM.AbstractActivePowerFormulation}
 
     ts_data = [(d.name, d.bus.number, -1*d.maxactivepower * values(d.scalingfactor)) for d in devices]
@@ -177,10 +177,10 @@ end
 
 # Injection Expression with fixed values
 
-function nodal_expression_fixed(ps_m::CanonicalModel, 
-                                devices::Array{L,1}, 
-                                system_formulation::Type{S}, 
-                                time_range::UnitRange{Int64}) where {L <: PSY.ElectricLoad, 
+function nodal_expression_fixed(ps_m::CanonicalModel,
+                                devices::Array{L,1},
+                                system_formulation::Type{S},
+                                time_range::UnitRange{Int64}) where {L <: PSY.ElectricLoad,
                                                                      S <: PM.AbstractPowerFormulation}
 
     for t in time_range, d in devices
@@ -192,10 +192,10 @@ function nodal_expression_fixed(ps_m::CanonicalModel,
 end
 
 
-function nodal_expression_fixed(ps_m::CanonicalModel, 
-                                devices::Array{L,1}, 
-                                system_formulation::Type{S}, 
-                                time_range::UnitRange{Int64}) where {L <: PSY.ElectricLoad, 
+function nodal_expression_fixed(ps_m::CanonicalModel,
+                                devices::Array{L,1},
+                                system_formulation::Type{S},
+                                time_range::UnitRange{Int64}) where {L <: PSY.ElectricLoad,
                                                                      S <: PM.AbstractActivePowerFormulation}
 
     for t in time_range, d in devices
