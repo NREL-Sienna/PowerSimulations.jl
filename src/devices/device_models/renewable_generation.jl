0e66d935781d328e8b420849efbafb65ff4d9a92
Jose Daniel Lara <jdlara@berkeley.edu>
2019-04-02 09:40:53 -0700

    add optionl to renewables

--- src/devices/device_models/renewable_generation.jl
+++ src/devices/device_models/renewable_generation.jl
@@ -26,19 +26,45 @@ function reactivepower_variables(ps_m::CanonicalModel, devices::Array{R,1}, time
 
 end
 
+function _get_time_series(device::Array{T}) where {T <: PSY.RenewableGen}
+
+    names = Vector{String}(undef, length(device))
+    series = Vector{Vector{Float64}}(undef, length(device))
+
+    for (ix,d) in enumerate(devices)
+        names[ix] = d.name
+        series[ix] = values(r.scalingfactor)*r.tech.installedcapacity
+    end
+
+    return names, series
+
+end
+
 # output constraints
 
 function activepower_constraints(ps_m::CanonicalModel,
                                  devices::Array{R,1},
                                  device_formulation::Type{D},
                                  system_formulation::Type{S},
-                                 time_range::UnitRange{Int64}) where {R <: PSY.RenewableGen,
+                                 time_range::UnitRange{Int64},
+                                 parameters::Bool = true) where {R <: PSY.RenewableGen,
                                                                       D <: AbstractRenewableDispatchForm,
                                                                       S <: PM.AbstractPowerFormulation}
 
-    ts_data = [(r.name, values(r.scalingfactor)*r.tech.installedcapacity) for r in devices]
-
-    device_timeseries_param_ub(ps_m, ts_data , time_range, :renewable_active_ub, Symbol("Pre_$(eltype(devices))"), :Pre)
+    if parameters
+        device_timeseries_param_ub(ps_m,
+                                   _get_time_series(devices),
+                                   time_range,
+                                   :renewable_active_ub,
+                                   Symbol("Pre_$(eltype(devices))"),
+                                   :Pre)
+    else
+        device_timeseries_ub(ps_m,
+                            _get_time_series(devices),
+                            time_range,
+                            :renewable_active_ub,
+                            :Pre)
+    end
 
     return
 
@@ -82,17 +108,70 @@ function reactivepower_constraints(ps_m::CanonicalModel,
 
 end
 
+# Injection Expression with parameters
 
-# injection expression
+function nodal_expression(ps_m::CanonicalModel,
+                            devices::Array{R,1},
+                            system_formulation::Type{S},
+                            time_range::UnitRange{Int64},
+                            parameters::Bool = true) where {R <: PSY.RenewableGen,
+                                                            S <: PM.AbstractPowerFormulation}
 
-function nodal_expression(ps_m::CanonicalModel, devices::Array{R,1}, system_formulation::Type{S}, time_range::UnitRange{Int64}) where {R <: PSY.RenewableGen, S <: PM.AbstractPowerFormulation}
+    if parameters
+        nodal_expression_param(ps_m, fixed_resources, system_formulation, time_range)
+    else
+        nodal_expression_fixed(ps_m, fixed_resources, system_formulation, time_range)
+    end
+
+return
+
+end
+
+function nodal_expression_param(ps_m::CanonicalModel,
+                                devices::Array{R,1},
+                                system_formulation::Type{S},
+                                time_range::UnitRange{Int64}) where {R <: PSY.RenewableGen,
+                                                                     S <: PM.AbstractPowerFormulation}
+
+    ts_data_active = Vector{Tuple{String,Int64,Vector{Float64}}}(undef, length(devices))
+    ts_data_reactive = Vector{Tuple{String,Int64,Vector{Float64}}}(undef, length(devices))
+
+    for (ix,d) in enumerate(devices)
+        ts_data_active[ix] = (d.name, d.bus.number, -1*d.maxactivepower * values(d.scalingfactor))
+        ts_data_reactive[ix] = (d.name, d.bus.number, -1*d.maxreactivepower * values(d.scalingfactor))
+    end
+
+    add_parameters(ps_m, ts_data_active, time_range, Symbol("Pel_$(eltype(devices))"), :var_active)
+    add_parameters(ps_m, ts_data_reactive, time_range, Symbol("Qel_$(eltype(devices))"), :var_reactive)
+
+end
+
+function nodal_expression_param(ps_m::CanonicalModel,
+                                devices::Array{R,1},
+                                system_formulation::Type{S},
+                                time_range::UnitRange{Int64}) where {R <: PSY.RenewableGen,
+                                                                     S <: PM.AbstractPowerFormulation}
+
+    ts_data = [(d.name, d.bus.number, -1*d.maxactivepower * values(d.scalingfactor)) for d in devices]
+
+    add_parameters(ps_m, ts_data, time_range, Symbol("Pre_$(eltype(devices))"), :var_active)
+
+return
+
+end
+
+
+# injection expression with fixed values
+
+function nodal_expression_fixed(ps_m::CanonicalModel,
+                                devices::Array{R,1},
+                                system_formulation::Type{S},
+                                time_range::UnitRange{Int64}) where {R <: PSY.RenewableGen,
+                                                                     S <: PM.AbstractPowerFormulation}
 
     for t in time_range, d in devices
-
         _add_to_expression!(ps_m.expressions[:var_active], d.bus.number, t, d.tech.installedcapacity * values(d.scalingfactor)[t])
-
         _add_to_expression!(ps_m.expressions[:var_reactive], d.bus.number, t, d.tech.installedcapacity * values(d.scalingfactor)[t]*sin(acos(d.tech.powerfactor)))
-
     end
 
     return
@@ -100,12 +179,14 @@ function nodal_expression(ps_m::CanonicalModel, devices::Array{R,1}, system_form
 end
 
 
-function nodal_expression(ps_m::CanonicalModel, devices::Array{R,1}, system_formulation::Type{S}, time_range::UnitRange{Int64}) where {R <: PSY.RenewableGen, S <: PM.AbstractActivePowerFormulation}
+function nodal_expression_fixed(ps_m::CanonicalModel,
+                                devices::Array{R,1},
+                                system_formulation::Type{S},
+                                time_range::UnitRange{Int64}) where {R <: PSY.RenewableGen,
+                                                                     S <: PM.AbstractActivePowerFormulation}
 
     for t in time_range, d in devices
-
         _add_to_expression!(ps_m.expressions[:var_active], d.bus.number, t, d.tech.installedcapacity * values(d.scalingfactor)[t])
-
     end
 
     return
@@ -115,7 +196,11 @@ end
 
 # renewable generation cost
 
-function cost_function(ps_m::CanonicalModel, devices::Array{PSY.RenewableCurtailment,1}, device_formulation::Type{D}, system_formulation::Type{S}) where {D <: AbstractRenewableDispatchForm, S <: PM.AbstractPowerFormulation}
+function cost_function(ps_m::CanonicalModel,
+                       devices::Array{PSY.RenewableCurtailment,1},
+                       device_formulation::Type{D},
+                       system_formulation::Type{S}) where {D <: AbstractRenewableDispatchForm,
+                                                           S <: PM.AbstractPowerFormulation}
 
     add_to_cost(ps_m, devices, :Pre, :curtailpenalty, -1)
 
