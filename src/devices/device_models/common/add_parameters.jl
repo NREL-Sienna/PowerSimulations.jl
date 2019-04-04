3e5bd5474ae3b13a7cef675361779ce789a5e2ed
Jose Daniel Lara <jdlara@berkeley.edu>
2019-04-03 23:26:11 -0700

    Whitespace fix

--- src/devices/device_models/common/add_parameters.jl
+++ src/devices/device_models/common/add_parameters.jl
@@ -11,6 +11,8 @@ function add_parameters(ps_m::CanonicalModel,
         ps_m.parameters[param_name].data[idx] = PJ.Parameter(ps_m.JuMPmodel,data[idx])
     end
 
+    return
+
 end
 
 function add_parameters(ps_m::CanonicalModel,
@@ -22,11 +24,8 @@ function add_parameters(ps_m::CanonicalModel,
     ps_m.parameters[param_name] = JuMP.Containers.DenseAxisArray{PJ.Parameter}(undef, [r[1] for r in ts_data], time_range)
 
     for t in time_range, r in ts_data
-
-        ps_m.parameters[param_name][r[1], t] = PJ.Parameter(ps_m.JuMPmodel, r[3][t]); 
-        
-        _add_to_expression!(ps_m.expressions[expression], r[2], t, ps_m.parameters[param_name][r[1], t])  
-        
+        ps_m.parameters[param_name][r[1], t] = PJ.Parameter(ps_m.JuMPmodel, r[3][t]);
+        _add_to_expression!(ps_m.expressions[expression], r[2], t, ps_m.parameters[param_name][r[1], t])
     end
 
     return
