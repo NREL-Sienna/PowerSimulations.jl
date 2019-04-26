dacf83082a9bdba6f2987650ec119a51df6f4973
Jose Daniel Lara <jdlara@berkeley.edu>
2019-04-26 14:07:11 -0700

    Compatibility Updates

--- test/get_test_data.jl
+++ test/get_test_data.jl
@@ -5,13 +5,13 @@ include(joinpath(base_dir,"data/data_5bus_pu.jl"));
 bus_numbers = [b.number for b in nodes5]
 
 include(joinpath(base_dir,"data/data_14bus_pu.jl"))
-sys14 = PSY.System(nodes14, generators14, loads14, branches14, nothing,  100.0, nothing, nothing, nothing);
+#sys14 = PSY.System(nodes14, generators14, loads14, branches14, nothing,  100.0, nothing, nothing, nothing);
 
 DATA_DIR = joinpath(base_dir, "data")
 RTS_GMLC_DIR = joinpath(DATA_DIR, "RTS_GMLC")
 cdm_dict = PSY.csv2ps_dict(RTS_GMLC_DIR, 100.0)
-sys_rts = PSY.System(cdm_dict);
+#sys_rts = PSY.System(cdm_dict);
 
-sys5b = PSY.System(nodes5, vcat(generators5,renewables), loads5_DA, branches5, nothing,  100.0, nothing, nothing, nothing);
-sys5b_uc = PSY.System(nodes5, generators5_uc, loads5_DA, branches5, nothing,  100.0, nothing, nothing, nothing);
-sys5b_storage = PSY.System(nodes5, vcat(generators5_uc), loads5_DA, branches5, battery,  100.0, nothing, nothing, nothing);
+#sys5b = PSY.System(nodes5, vcat(generators5,renewables), loads5_DA, branches5, nothing,  100.0, nothing, nothing, nothing);
+#sys5b_uc = PSY.System(nodes5, generators5_uc, loads5_DA, branches5, nothing,  100.0, nothing, nothing, nothing);
+#sys5b_storage = PSY.System(nodes5, vcat(generators5_uc), loads5_DA, branches5, battery,  100.0, nothing, nothing, nothing);
