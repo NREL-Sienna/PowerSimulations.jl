c9decabdf26e4317c47621bb236ed78687e6b19c
Jose Daniel Lara <jdlara@berkeley.edu>
2019-05-07 12:37:16 -0700

    change constructor_device! interface

--- src/core/device_constructor.jl
+++ src/core/device_constructor.jl
@@ -1,17 +1,17 @@
 function construct_device!(ps_m::CanonicalModel,
                             device_model::DeviceModel,
                             system_formulation::Type{S},
-                            system::PSY.System,
+                            system::PSY.ConcreteSystem,
                             time_range::UnitRange{Int64};
                             kwargs...) where {S <: PM.AbstractPowerFormulation}
 
-    construct_device!(ps_m,
-                      device_model.device,
-                      device_model.formulation,
-                      system_formulation,
-                      system,
-                      time_range;
-                      kwargs...)
+    _internal_device_constructor!(ps_m,
+                                  device_model.device,
+                                  device_model.formulation,
+                                  system_formulation,
+                                  system,
+                                  time_range;
+                                  kwargs...)
 
     return
 
