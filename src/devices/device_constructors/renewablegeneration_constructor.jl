08bc6a4de1c0c963a7471918789dbe224549b7e0
Jose Daniel Lara <jdlara@berkeley.edu>
2019-05-06 18:10:03 -0700

    WIP renewable constructors update

--- src/devices/device_constructors/renewablegeneration_constructor.jl
+++ src/devices/device_constructors/renewablegeneration_constructor.jl
@@ -2,43 +2,37 @@ function construct_device!(ps_m::CanonicalModel,
                            device::Type{R},
                            device_formulation::Type{D},
                            system_formulation::Type{S},
-                           sys::PSY.System,
+                           sys::PSY.ConcreteSystem,
                            time_range::UnitRange{Int64};
                            kwargs...) where {R <: PSY.RenewableGen,
                                              D <: AbstractRenewableDispatchForm,
                                              S <: PM.AbstractPowerFormulation}
 
-    if !isa(sys.generators.renewable, Nothing)
+    devices = collect(PSY.get_components(device, sys))
+
+    isconcretetype(device) ? true : true
+
+    if !isempty(devices)
 
         parameters = get(kwargs, :parameters, true)
 
-        fixed_resources = [fs for fs in sys.generators.renewable if isa(fs,PSY.RenewableFix)]
-
-        controllable_resources = [fs for fs in sys.generators.renewable if !isa(fs,PSY.RenewableFix)]
-
-        if !isempty(controllable_resources)
+        if !isempty(devices)
 
             #Variables
-            activepower_variables(ps_m, controllable_resources, time_range);
+            activepower_variables(ps_m, devices, time_range);
 
-            reactivepower_variables(ps_m, controllable_resources, time_range);
+            reactivepower_variables(ps_m, devices, time_range);
 
             #Constraints
-            activepower_constraints(ps_m, controllable_resources, device_formulation, system_formulation, time_range, parameters)
+            activepower_constraints(ps_m, devices, device_formulation, system_formulation, time_range, parameters)
 
-            reactivepower_constraints(ps_m, controllable_resources, device_formulation, system_formulation, time_range)
+            reactivepower_constraints(ps_m, devices, device_formulation, system_formulation, time_range)
 
             #Cost Function
-            cost_function(ps_m, controllable_resources, device_formulation, system_formulation)
+            cost_function(ps_m, devices, device_formulation, system_formulation)
 
         else
             @warn("The Data Doesn't Contain Controllable Renewable Resources, Consider Changing the Device Formulation to RenewableFixed")
-        end
-
-        #add to expression
-
-        if !isempty(fixed_resources)
-            nodal_expression(ps_m, fixed_resources, system_formulation, time_range, parameters)
         end
 
     end
@@ -51,15 +45,15 @@ function construct_device!(ps_m::CanonicalModel,
                            device::Type{R},
                            device_formulation::Type{D},
                            system_formulation::Type{S},
-                           sys::PSY.System,
+                           sys::PSY.ConcreteSystem,
                            time_range::UnitRange{Int64};
                            kwargs...) where {R <: PSY.RenewableGen,
                                              D <: AbstractRenewableDispatchForm,
                                              S <: PM.AbstractActivePowerFormulation}
 
+    devices = collect(PSY.get_components(device, sys))
 
-
-    if !isa(sys.generators.renewable, Nothing)
+    if !isempty(devices)
 
         parameters = get(kwargs, :parameters, true)
 
@@ -83,12 +77,6 @@ function construct_device!(ps_m::CanonicalModel,
 
         end
 
-        #add to expression
-
-        if !isempty(fixed_resources)
-            nodal_expression(ps_m, fixed_resources, system_formulation, time_range, parameters)
-        end
-
     end
 
     return
@@ -99,13 +87,14 @@ function construct_device!(ps_m::CanonicalModel,
                            device::Type{R},
                            device_formulation::Type{PSI.RenewableFixed},
                            system_formulation::Type{S},
-                           sys::PSY.System,
+                           sys::PSY.ConcreteSystem,
                            time_range::UnitRange{Int64};
                            kwargs...) where {R <: PSY.RenewableGen,
                                              S <: PM.AbstractPowerFormulation}
 
+    devices = collect(PSY.get_components(device, sys))
 
-    if !isa(sys.generators.renewable, Nothing)
+    if !isempty(devices)
 
         parameters = get(kwargs, :parameters, true)
 
@@ -115,3 +104,25 @@ function construct_device!(ps_m::CanonicalModel,
     return
 
 end
+
+function construct_device!(ps_m::CanonicalModel,
+                            device::Type{PSY.RenewableFix},
+                            device_formulation::Type{D},
+                            system_formulation::Type{S},
+                            sys::PSY.ConcreteSystem,
+                            time_range::UnitRange{Int64};
+                            kwargs...) where {D <: PSI.AbstractRenewableFormulation,
+                                              S <: PM.AbstractPowerFormulation}
+
+    if device_formulation != RenewableFixed
+        @warn("The Formulation $(D) onky applied to Controllable Renewable Resources, \n Consider Changing the Device Formulation to RenewableFixed")                                              
+    end
+
+    construct_device!(ps_m, 
+                        device,
+                        device_formulation,
+                        PSI.RenewableFixed,
+                        sys,
+                        time_range; kwargs...)
+
+end                      
