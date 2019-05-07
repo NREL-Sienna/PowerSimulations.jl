169b667086b27d250d56d90cc435878a206a04b1
Jose Daniel Lara <jdlara@berkeley.edu>
2019-05-06 18:09:51 -0700

    create new case of data

--- test/get_test_data.jl
+++ test/get_test_data.jl
@@ -11,10 +11,17 @@ DATA_DIR = joinpath(base_dir, "data")
 RTS_GMLC_DIR = joinpath(DATA_DIR, "RTS_GMLC")
 cdm_dict = PSY.csv2ps_dict(RTS_GMLC_DIR, 100.0);
 
-
+#Base Systems
 sys5 = PSY.System(nodes5, thermal_generators5, loads5, branches5, nothing,  100.0, forecasts5, nothing, nothing);
 sys14 = PSY.System(nodes14, thermal_generators14, loads14, branches14, nothing,  100.0, forecasts14, nothing, nothing);
-sys_rts = PSY.System(cdm_dict);
 c_sys5 = PSY.ConcreteSystem(sys5)
 c_sys14 = PSY.ConcreteSystem(sys14)
+
+#System with Renewable Energy
+sys5_re = PSY.System(nodes5, vcat(thermal_generators5, renewable_generators5), loads5, branches5, nothing,  100.0, forecasts5, nothing, nothing);
+c_sys5_re = PSY.ConcreteSystem(sys5)
+
+# RTS Data 
+sys_rts = PSY.System(cdm_dict);
 c_rts = PSY.ConcreteSystem(sys_rts);
+
