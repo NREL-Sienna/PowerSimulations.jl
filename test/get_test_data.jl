8b49f990ef6e1b827ccc5637b34089ccb260e0cb
Jose Daniel Lara <jdlara@berkeley.edu>
2019-05-07 20:20:28 -0700

    create concrete from data with renewables

--- test/get_test_data.jl
+++ test/get_test_data.jl
@@ -1,15 +1,11 @@
 time_range = 1:24
 
 base_dir = string(dirname(dirname(pathof(PowerSystems))));
+DATA_DIR = joinpath(base_dir, "data")
 include(joinpath(base_dir,"data/data_5bus_pu.jl"));
 include(joinpath(base_dir,"data/data_14bus_pu.jl"))
 bus_numbers5 = [b.number for b in nodes5]
 bus_numbers14 = [b.number for b in nodes14];
-
-
-DATA_DIR = joinpath(base_dir, "data")
-RTS_GMLC_DIR = joinpath(DATA_DIR, "RTS_GMLC")
-cdm_dict = PSY.csv2ps_dict(RTS_GMLC_DIR, 100.0);
 
 #Base Systems
 sys5 = PSY.System(nodes5, thermal_generators5, loads5, branches5, nothing,  100.0, forecasts5, nothing, nothing);
@@ -19,9 +15,12 @@ c_sys14 = PSY.ConcreteSystem(sys14)
 
 #System with Renewable Energy
 sys5_re = PSY.System(nodes5, vcat(thermal_generators5, renewable_generators5), loads5, branches5, nothing,  100.0, forecasts5, nothing, nothing);
-c_sys5_re = PSY.ConcreteSystem(sys5)
+c_sys5_re = PSY.ConcreteSystem(sys5_re)
 
 # RTS Data 
+RTS_GMLC_DIR = joinpath(DATA_DIR, "RTS_GMLC")
+cdm_dict = PSY.csv2ps_dict(RTS_GMLC_DIR, 100.0);
 sys_rts = PSY.System(cdm_dict);
 c_rts = PSY.ConcreteSystem(sys_rts);
+
 
