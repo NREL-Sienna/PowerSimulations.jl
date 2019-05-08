05641cb8fc3761f73b4f7ea956b31da499cf4ffc
Jose Daniel Lara <jdlara@berkeley.edu>
2019-05-07 20:12:04 -0700

    update constructors for forecasts

--- test/renewable_generation_constructors.jl
+++ test/renewable_generation_constructors.jl
@@ -1,6 +1,8 @@
 @testset "Renewable DCPLossLess FullDispatch" begin
+    model = DeviceModel(PSY.RenewableCurtailment, PSI.RenewableFullDispatch)
+    #5 Bus testing
     ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_range)
-    PSI.construct_device!(ps_model, PSY.RenewableCurtailment, PSI.RenewableFullDispatch, PM.DCPlosslessForm, c_sys5_re, time_range);
+    construct_device!(ps_model, model, PM.DCPlosslessForm, c_sys5_re, time_range);
     @test JuMP.num_variables(ps_model.JuMPmodel) == 72
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.Interval{Float64}) == 0
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.LessThan{Float64}) == 72
@@ -8,7 +10,7 @@
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.EqualTo{Float64}) == 0
     # No Parameters Testing
     ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_range; parameters = false)
-    PSI.construct_device!(ps_model, PSY.RenewableCurtailment, PSI.RenewableFullDispatch, PM.DCPlosslessForm, c_sys5_re, time_range; parameters = false);
+    construct_device!(ps_model, model, PM.DCPlosslessForm, c_sys5_re, time_range; parameters = false);
     @test !(:params in keys(ps_model.JuMPmodel.ext))
     @test JuMP.num_variables(ps_model.JuMPmodel) == 72
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.Interval{Float64}) == 72
@@ -19,12 +21,13 @@ end
 
 #@testset "Renewable Testing" begin
 #ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_range)
-#PSI.construct_device!(ps_model, PSY.RenewableCurtailment, PSI.RenewableFullDispatch, PM.StandardACPForm, c_sys5_re, time_range);
+#construct_device!(ps_model, PSY.RenewableCurtailment, PSI.RenewableFullDispatch, PM.StandardACPForm, c_sys5_re, time_range);
 #end
 
 @testset "Renewable DCPLossLess ConstantPowerFactor" begin
+    model = DeviceModel(PSY.RenewableCurtailment, PSI.RenewableConstantPowerFactor)
     ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_range)
-    PSI.construct_device!(ps_model, PSY.RenewableCurtailment, PSI.RenewableConstantPowerFactor, PM.DCPlosslessForm, c_sys5_re, time_range);
+    construct_device!(ps_model, model, PM.DCPlosslessForm, c_sys5_re, time_range);
     @test JuMP.num_variables(ps_model.JuMPmodel) == 72
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.Interval{Float64}) == 0
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.LessThan{Float64}) == 72
@@ -32,7 +35,7 @@ end
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.EqualTo{Float64}) == 0
     # No Parameters Testing
     ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_range; parameters = false)
-    PSI.construct_device!(ps_model, PSY.RenewableCurtailment, PSI.RenewableConstantPowerFactor, PM.DCPlosslessForm, c_sys5_re, time_range; parameters = false);
+    construct_device!(ps_model, model, PM.DCPlosslessForm, c_sys5_re, time_range; parameters = false);
     @test !(:params in keys(ps_model.JuMPmodel.ext))
     @test JuMP.num_variables(ps_model.JuMPmodel) == 72
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.Interval{Float64}) == 72
@@ -42,8 +45,9 @@ end
 end
 
 @testset "Renewable ACP ConstantPowerFactor" begin
+    model = DeviceModel(PSY.RenewableCurtailment, PSI.RenewableConstantPowerFactor)
     ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_range)
-    PSI.construct_device!(ps_model, PSY.RenewableCurtailment, PSI.RenewableConstantPowerFactor, PM.StandardACPForm, c_sys5_re, time_range);
+    construct_device!(ps_model, model, PM.StandardACPForm, c_sys5_re, time_range);
     @test JuMP.num_variables(ps_model.JuMPmodel) == 144
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.Interval{Float64}) == 0
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.LessThan{Float64}) == 72
@@ -51,7 +55,7 @@ end
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.EqualTo{Float64}) == 72
     # No Parameters Testing
     ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_range; parameters = false)
-    PSI.construct_device!(ps_model, PSY.RenewableCurtailment, PSI.RenewableConstantPowerFactor, PM.StandardACPForm, c_sys5_re, time_range; parameters = false);
+    construct_device!(ps_model, model, PM.StandardACPForm, c_sys5_re, time_range; parameters = false);
     @test !(:params in keys(ps_model.JuMPmodel.ext))
     @test JuMP.num_variables(ps_model.JuMPmodel) == 144
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.Interval{Float64}) == 72
@@ -61,15 +65,16 @@ end
 end
 
 @testset "Renewable DCPLossLess FixedOutput" begin
+    model = DeviceModel(PSY.RenewableCurtailment, PSI.RenewableFixed)
     ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_range)
-    PSI.construct_device!(ps_model, PSY.RenewableCurtailment, PSI.RenewableFixed, PM.DCPlosslessForm, c_sys5_re, time_range);
+    construct_device!(ps_model, model, PM.DCPlosslessForm, c_sys5_re, time_range);
     @test JuMP.num_variables(ps_model.JuMPmodel) == 0
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.LessThan{Float64}) == 0
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.GreaterThan{Float64}) == 0
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.EqualTo{Float64}) == 0
     # No Parameters Testing
     ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_range; parameters = false)
-    PSI.construct_device!(ps_model, PSY.RenewableCurtailment, PSI.RenewableFixed, PM.DCPlosslessForm, c_sys5_re, time_range; parameters = false);
+    construct_device!(ps_model, model, PM.DCPlosslessForm, c_sys5_re, time_range; parameters = false);
     @test !(:params in keys(ps_model.JuMPmodel.ext))
     @test JuMP.num_variables(ps_model.JuMPmodel) == 0
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.LessThan{Float64}) == 0
@@ -78,15 +83,16 @@ end
 end
 
 @testset "Renewable ACP FixedOutput" begin
+    model = DeviceModel(PSY.RenewableCurtailment, PSI.RenewableFixed)
     ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_range)
-    PSI.construct_device!(ps_model, PSY.RenewableCurtailment, PSI.RenewableFixed, PM.StandardACPForm, c_sys5_re, time_range);
+    construct_device!(ps_model, model, PM.StandardACPForm, c_sys5_re, time_range);
     @test JuMP.num_variables(ps_model.JuMPmodel) == 0
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.LessThan{Float64}) == 0
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.GreaterThan{Float64}) == 0
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.EqualTo{Float64}) == 0
     # No Parameters Testing
     ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_range; parameters = false)
-    PSI.construct_device!(ps_model, PSY.RenewableCurtailment, PSI.RenewableFixed, PM.StandardACPForm, c_sys5_re, time_range; parameters = false);
+    construct_device!(ps_model, model, PM.StandardACPForm, c_sys5_re, time_range; parameters = false);
     @test !(:params in keys(ps_model.JuMPmodel.ext))
     @test JuMP.num_variables(ps_model.JuMPmodel) == 0
     @test JuMP.num_constraints(ps_model.JuMPmodel,GenericAffExpr{Float64,VariableRef},MOI.LessThan{Float64}) == 0
