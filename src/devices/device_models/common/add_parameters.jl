2e64695dc72366f67ab7e6b40fcf4e363c13018a
Jose Daniel Lara <jdlara@berkeley.edu>
2019-05-08 17:16:30 -0700

    change name wrt to ParameterJuMP

--- src/devices/device_models/common/add_parameters.jl
+++ src/devices/device_models/common/add_parameters.jl
@@ -1,4 +1,4 @@
-function add_parameters(ps_m::CanonicalModel,
+function include_parameters(ps_m::CanonicalModel,
                         data::Matrix,
                         param_name::Symbol,
                         axs...)
@@ -15,14 +15,14 @@ function add_parameters(ps_m::CanonicalModel,
 
 end
 
-function add_parameters(ps_m::CanonicalModel,
+function include_parameters(ps_m::CanonicalModel,
                         ts_data::Vector{Tuple{String,Int64, Vector{Float64}}},
                         time_range::UnitRange{Int64},
                         param_name::Symbol,
                         expression::Symbol)
 
     ps_m.parameters[param_name] = JuMP.Containers.DenseAxisArray{PJ.ParameterRef}(undef, [r[1] for r in ts_data], time_range)
-
+    
     for t in time_range, r in ts_data
         ps_m.parameters[param_name][r[1], t] = PJ.add_parameter(ps_m.JuMPmodel, r[3][t]);
         _add_to_expression!(ps_m.expressions[expression], r[2], t, ps_m.parameters[param_name][r[1], t])
