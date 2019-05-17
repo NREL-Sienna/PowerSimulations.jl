4f115cd23d1b1f103c2fa5d49a16a368debc8825
Jose Daniel Lara <jdlara@berkeley.edu>
2019-05-17 14:26:09 -0700

    add basic printing

---
+++ src/routines/printing.jl
@@ -0,0 +1,7 @@
+function Base.show(io::IO, op_model::OperationModel)
+    println(io, "Operation Model")
+end
+
+function Base.show(io::IO, op_model::CanonicalModel)
+    println(io, "Canonical Model")
+end
