865581f0d79468cb387404a217526000aee29403
Jose Daniel Lara <jdlara@berkeley.edu>
2019-05-17 14:38:32 -0700

    add interaction methods to get and change ref

--- src/core/core_structs/operation_model.jl
+++ src/core/core_structs/operation_model.jl
@@ -7,7 +7,6 @@ mutable struct ModelReference{T <: PM.AbstractPowerFormulation}
     services::Dict{Symbol, ServiceModel}
 end
 
-
 mutable struct OperationModel{M <: AbstractOperationsModel}
     op_model::Type{M}
     model_ref::ModelReference
@@ -41,4 +40,29 @@ mutable struct OperationModel{M <: AbstractOperationsModel}
 
     end
 
+end
+
+get_transmission_ref(op_model::OperationModel) = op_model.model_ref.transmission
+get_devices_ref(op_model::OperationModel) = op_model.model_ref.devices
+get_branches_ref(op_model::OperationModel) = op_model.model_ref.branches
+get_services_ref(op_model::OperationModel) = op_model.model_ref.services
+
+function set_transmission_ref!(op_model::OperationModel, transmission::Type{T}) where {T <: PM.AbstractPowerFormulation} 
+    op_model.model_ref.transmission = transmission
+    return 
+end
+
+function set_devices_ref!(op_model::OperationModel, devices::Dict{Symbol, DeviceModel}) 
+    op_model.model_ref.devices = devices
+    return
+end
+
+function set_devices_ref!(op_model::OperationModel, branches::Dict{Symbol, DeviceModel}) 
+    op_model.model_ref.branches = branches
+    return
+end
+
+function set_devices_ref!(op_model::OperationModel, services::Dict{Symbol, DeviceModel}) 
+    op_model.model_ref.services = services
+    return
 end
