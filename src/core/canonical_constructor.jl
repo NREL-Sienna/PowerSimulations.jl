4cac059396b3acfa1de7a0ec9bfa916a295240a4
Jose Daniel Lara <jdlara@berkeley.edu>
2019-05-27 21:47:28 -0700

    add pm instantiation

--- src/core/canonical_constructor.jl
+++ src/core/canonical_constructor.jl
@@ -44,15 +44,101 @@ function _make_expressions_dict(transmission::Type{S},
     return Dict{Symbol, JuMP.Containers.DenseAxisArray}(:nodal_balance_active => _make_container_array(V, bus_numbers, time_steps; kwargs...))
 end
 
+"""
+This function performs the same actions as the call to PowerModels.GenericPowerModel{T} but performs extra actions
+    to instantiate partially the power model object
+"""
+function _powermodels_object_init(transmission::Type{S},
+                                 buses::PSY.FlattenedVectorsIterator{PSY.Bus},
+                                 base_power::Float64,
+                                 time_steps::UnitRange{Int64},
+                                 jump_model::JuMP.Model,
+                                 V::DataType) where {S <: PM.AbstractPowerFormulation}
 
-function _canonical_model_init(bus_numbers::Vector{Int64},
+    is_activepower_only = (S <: PM.AbstractActivePowerFormulation)
+
+    PM_data_dict = Dict{String,Any}(
+        "bus"            => get_buses_to_pm(buses),
+        "branch"         => Dict{String,Any}(),
+        "baseMVA"        => base_power,
+        "per_unit"       => true,
+        "storage"        => Dict{String,Any}(),
+        "dcline"         => Dict{String,Any}(),
+        "gen"            => Dict{String,Any}(),
+        "shunt"          => Dict{String,Any}(),
+        "load"           => Dict{String,Any}(),
+        )
+
+    PM_data_dict = PM.replicate(PM_data_dict,time_steps[end]);
+
+    ext = Dict{Symbol,Any}()
+    setting = Dict{String,Any}()
+    ref = PM.build_generic_ref(PM_data_dict)
+
+    var = Dict{Symbol,Any}(:nw => Dict{Int,Any}())
+    con = Dict{Symbol,Any}(:nw => Dict{Int,Any}())
+
+    for (nw_id, nw) in ref[:nw]
+        nw_var = var[:nw][nw_id] = Dict{Symbol,Any}()
+        nw_con = con[:nw][nw_id] = Dict{Symbol,Any}()
+        ref[:nw][nw_id][:arcs_from] = Vector{Tuple{Int64,Int64,Int64}}()
+        ref[:nw][nw_id][:arcs_to] = Vector{Tuple{Int64,Int64,Int64}}()
+        ref[:nw][nw_id][:arcs_from_dc] = Vector{Tuple{Int64,Int64,Int64}}()
+        ref[:nw][nw_id][:arcs_to_dc] = Vector{Tuple{Int64,Int64,Int64}}()
+        ref[:nw][nw_id][:arcs] = Vector{Tuple{Int64,Int64,Int64}}()
+        ref[:nw][nw_id][:arcs_dc] = Vector{Tuple{Int64,Int64,Int64}}()
+
+        nw_var[:cnd] = Dict{Int,Any}()
+        nw_con[:cnd] = Dict{Int,Any}()
+
+        for cnd_id in nw[:conductor_ids]
+            nw_var[:cnd][cnd_id] = Dict{Symbol,Any}()
+            nw_var[:cnd][cnd_id][:p] = Vector{V}()
+            nw_var[:cnd][cnd_id][:p_dc] = Vector{V}()
+            is_activepower_only ? nw_var[:cnd][cnd_id][:q] = Vector{V}() : true
+            is_activepower_only ? nw_var[:cnd][cnd_id][:q_dc] = Vector{V}() : true
+            nw_con[:cnd][cnd_id] = Dict{Symbol,Any}()
+        end
+    end
+
+    cnw = minimum([k for k in keys(var[:nw])])
+    ccnd = minimum([k for k in keys(var[:nw][cnw][:cnd])])
+
+    pm_object = PM.GenericPowerModel{transmission}(
+                                        jump_model,
+                                        PM_data_dict,
+                                        setting,
+                                        Dict{String,Any}(), # solution
+                                        ref,
+                                        var,
+                                        con,
+                                        cnw,
+                                        ccnd,
+                                        ext
+                                    )
+
+    return pm_object
+
+end
+
+
+function _canonical_model_init(buses::PSY.FlattenedVectorsIterator{PSY.Bus},
+                              base_power::Float64,
                               optimizer::Union{Nothing,JuMP.OptimizerFactory},
                               transmission::Type{S},
                               time_steps::UnitRange{Int64}; kwargs...) where {S <: PM.AbstractPowerFormulation}
 
+
+    bus_numbers = [b.number for b in buses]
     parameters = get(kwargs, :parameters, true)
     jump_model = _pass_abstract_jump(optimizer; kwargs...)
     V = JuMP.variable_type(jump_model)
+    pm_object = _powermodels_object_init(transmission,
+                                                buses,
+                                            base_power,
+                                            time_steps,
+                                            jump_model,
+                                            V; kwargs ...)
 
     ps_model = CanonicalModel(jump_model,
                             Dict{Symbol, JuMP.Containers.DenseAxisArray}(),
@@ -61,17 +147,19 @@ function _canonical_model_init(bus_numbers::Vector{Int64},
                             _make_expressions_dict(transmission, V, bus_numbers, time_steps; kwargs...),
                             parameters ? Dict{Symbol,JuMP.Containers.DenseAxisArray}() : nothing,
                             Dict{Symbol,Array{InitialCondition}}(),
-                            nothing);
+                            pm_object);
 
     return ps_model
 
 end
 
-function _canonical_model_init(bus_numbers::Vector{Int64},
+function _canonical_model_init(buses::PSY.FlattenedVectorsIterator{PSY.Bus},
+                               base_power::Float64,
                                optimizer::Union{Nothing,JuMP.OptimizerFactory},
                                transmission::Type{S},
                                time_steps::UnitRange{Int64}; kwargs...) where {S <: Union{StandardPTDFForm, CopperPlatePowerModel}}
 
+    bus_numbers = [b.number for b in buses]
     parameters = get(kwargs, :parameters, true)
     jump_model = _pass_abstract_jump(optimizer; kwargs...)
     V = JuMP.variable_type(jump_model)
@@ -109,9 +197,9 @@ function  build_canonical_model(transmission::Type{T},
         time_steps = 1:1
     end
 
-    bus_numbers = [b.number for b in PSY.get_components(PSY.Bus, sys)]
+    buses = PSY.get_components(PSY.Bus, sys)
 
-    ps_model = _canonical_model_init(bus_numbers, optimizer, transmission, time_steps; kwargs...)
+    ps_model = _canonical_model_init(buses, sys.basepower, optimizer, transmission, time_steps; kwargs...)
 
     # Build Injection devices
     for mod in devices
