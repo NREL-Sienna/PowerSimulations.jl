7348c9601183f61565a192f0ff857c175e5d19f2
Jose Daniel Lara <jdlara@berkeley.edu>
2019-05-27 23:03:48 -0700

    add branches variable constructors

--- src/devices/device_constructors/branch_constructor.jl
+++ src/devices/device_constructors/branch_constructor.jl
@@ -27,18 +27,19 @@ function _internal_device_constructor!(ps_m::CanonicalModel,
 end
 
 function _internal_device_constructor!(ps_m::CanonicalModel,
-                            device::Type{B},
-                            device_formulation::Type{Br},
-                            system_formulation::Type{S},
-                            sys::PSY.System,
-                            time_steps::UnitRange{Int64},
-                            resolution::Dates.Period;
-                            kwargs...) where {Br <: AbstractBranchFormulation,
-                                              B <: PSY.Branch,
-                                              S <: PM.AbstractPowerFormulation}
+                                        device::Type{B},
+                                        device_formulation::Type{Br},
+                                        system_formulation::Type{S},
+                                        sys::PSY.System,
+                                        time_steps::UnitRange{Int64},
+                                        resolution::Dates.Period;
+                                        kwargs...) where {Br <: AbstractBranchFormulation,
+                                                          B <: PSY.Branch,
+                                                          S <: PM.AbstractPowerFormulation}
 
+    devices = PSY.get_components(device, sys)
 
-    # This code is meant to do nothing and will have a constructor once the branch formulations are developed
+    flow_variables(ps_m, system_formulation, devices, time_steps)
 
     return
 
