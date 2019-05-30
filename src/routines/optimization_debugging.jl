46927ed9792db892a1b70913dede40186adedbd3
Jose Daniel Lara <jdlara@berkeley.edu>
2019-05-29 17:55:43 -0700

    add optimization debugging helpers

--- src/routines/optimization_debugging.jl
+++ src/routines/optimization_debugging.jl
@@ -6,5 +6,18 @@ function get_constraint_index(op_model::OperationModel)
             push!(con_index,(key, idx, moi_index.value))
         end
     end
+    @info "Each Tuple corresponds to (con_name, internal_index, moi_index)"
     return con_index
+end
+
+function get_var_index(op_model::OperationModel)
+    var_index = Vector{Tuple{Symbol, Int64, Int64}}()
+    for (key,value) in op_model.canonical_model.variables
+        for (idx,variable) in enumerate(value)
+            moi_index = JuMP.optimizer_index(variable);
+            push!(var_index,(key, idx, moi_index.value))
+        end
+    end
+    @info "Each Tuple corresponds to (var_name, internal_index, moi_index)"
+    return var_index
 end
