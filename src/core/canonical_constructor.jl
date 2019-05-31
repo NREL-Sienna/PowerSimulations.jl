83bfca93a841373f9d7d48f95246c7a647ba5284
Jose Daniel Lara <jdlara@berkeley.edu>
2019-05-30 17:29:35 -0700

    add time_steps and resolution to canonical

--- src/core/canonical_constructor.jl
+++ src/core/canonical_constructor.jl
@@ -1,21 +1,9 @@
 
+const DSDA = Dict{Symbol, JuMP.Containers.DenseAxisArray}
+
 function _make_container_array(V::DataType, ax...; kwargs...)
 
     parameters = get(kwargs, :parameters, true)
-
-    # While JuMP fixes the isassigned problems
-     # While JuMP fixes the isassigned problems
-    #=
-    if parameters
-            cont = JuMP.Containers.DenseAxisArray{PGAE{V}}(undef, ax...)
-            _remove_undef!(cont.data)
-        return cont
-    else
-            cont = JuMP.Containers.DenseAxisArray{GAE{V}}(undef, ax...)
-            _remove_undef!(cont.data)
-        return cont
-    end
-    =#
 
     if parameters
         return JuMP.Containers.DenseAxisArray{PGAE{V}}(undef, ax...)
@@ -32,8 +20,13 @@ function _make_expressions_dict(transmission::Type{S},
                                 bus_numbers::Vector{Int64},
                                 time_steps::UnitRange{Int64}; kwargs...) where {S <: PM.AbstractPowerFormulation}
 
-    return Dict{Symbol, JuMP.Containers.DenseAxisArray}(:nodal_balance_active =>  _make_container_array(V, bus_numbers, time_steps; kwargs...),
-                                                        :nodal_balance_reactive => _make_container_array(V, bus_numbers, time_steps; kwargs...))
+    return DSDA(:nodal_balance_active =>  _make_container_array(V, 
+                                                                bus_numbers, 
+                                                                time_steps; kwargs...),
+                :nodal_balance_reactive => _make_container_array(V, 
+                                                                 bus_numbers, 
+                                                                 time_steps; kwargs...))
+
 end
 
 function _make_expressions_dict(transmission::Type{S},
@@ -41,27 +34,39 @@ function _make_expressions_dict(transmission::Type{S},
                                 bus_numbers::Vector{Int64},
                                 time_steps::UnitRange{Int64}; kwargs...) where {S <: PM.AbstractActivePowerFormulation}
 
-    return Dict{Symbol, JuMP.Containers.DenseAxisArray}(:nodal_balance_active => _make_container_array(V, bus_numbers, time_steps; kwargs...))
+    return DSDA(:nodal_balance_active =>  _make_container_array(V, 
+                                                                bus_numbers, 
+                                                                time_steps; kwargs...))
 end
 
 
 function _canonical_model_init(bus_numbers::Vector{Int64},
                               optimizer::Union{Nothing,JuMP.OptimizerFactory},
-                              transmission::Type{S},
-                              time_steps::UnitRange{Int64}; kwargs...) where {S <: PM.AbstractPowerFormulation}
+                              transmission::Type{S},                              
+                              time_steps::UnitRange{Int64},
+                              resolution::Dates.Period; kwargs...) where 
+                                    {S <: PM.AbstractPowerFormulation}
 
     parameters = get(kwargs, :parameters, true)
     jump_model = _pass_abstract_jump(optimizer; kwargs...)
     V = JuMP.variable_type(jump_model)
 
+    # TODO: Instantiate the PM Object here
+
     ps_model = CanonicalModel(jump_model,
-                            Dict{Symbol, JuMP.Containers.DenseAxisArray}(),
-                            Dict{Symbol, JuMP.Containers.DenseAxisArray}(),
-                            zero(JuMP.GenericAffExpr{Float64, V}),
-                            _make_expressions_dict(transmission, V, bus_numbers, time_steps; kwargs...),
-                            parameters ? Dict{Symbol,JuMP.Containers.DenseAxisArray}() : nothing,
-                            Dict{Symbol,Array{InitialCondition}}(),
-                            nothing);
+                              parameters,
+                              time_steps,
+                              resolution,
+                              DSDA(),
+                              DSDA(),
+                              zero(JuMP.GenericAffExpr{Float64, V}),
+                              _make_expressions_dict(transmission, 
+                                                     V, 
+                                                     bus_numbers, 
+                                                     time_steps; kwargs...),
+                              parameters ? DSDA() : nothing,
+                              Dict{Symbol,Array{InitialCondition}}(),
+                              nothing);
 
     return ps_model
 
@@ -69,19 +74,27 @@ end
 
 function _canonical_model_init(bus_numbers::Vector{Int64},
                                optimizer::Union{Nothing,JuMP.OptimizerFactory},
-                               transmission::Type{S},
-                               time_steps::UnitRange{Int64}; kwargs...) where {S <: Union{StandardPTDFForm, CopperPlatePowerModel}}
+                               transmission::Type{S},                               
+                               time_steps::UnitRange{Int64},
+                               resolution::Dates.Period; kwargs...) where 
+                                       {S <: Union{StandardPTDFForm, CopperPlatePowerModel}}
 
     parameters = get(kwargs, :parameters, true)
     jump_model = _pass_abstract_jump(optimizer; kwargs...)
     V = JuMP.variable_type(jump_model)
 
     ps_model = CanonicalModel(jump_model,
-                              Dict{Symbol, JuMP.Containers.DenseAxisArray}(),
-                              Dict{Symbol, JuMP.Containers.DenseAxisArray}(),
+                              parameters,
+                              time_steps,
+                              resolution,      
+                              DSDA(),
+                              DSDA(),
                               zero(JuMP.GenericAffExpr{Float64, V}),
-                              _make_expressions_dict(transmission, V, bus_numbers, time_steps; kwargs...),
-                              parameters ? Dict{Symbol,JuMP.Containers.DenseAxisArray}() : nothing,
+                              _make_expressions_dict(transmission, 
+                                                     V, 
+                                                     bus_numbers, 
+                                                     time_steps; kwargs...),
+                              parameters ? DSDA() : nothing,
                               Dict{Symbol,Array{InitialCondition}}(),
                               nothing);
 
@@ -94,7 +107,6 @@ function  build_canonical_model(transmission::Type{T},
                                 branches::Dict{Symbol, DeviceModel},
                                 services::Dict{Symbol, ServiceModel},
                                 sys::PSY.System,
-                                resolution::Dates.Period,
                                 optimizer::Union{Nothing,JuMP.OptimizerFactory}=nothing;
                                 kwargs...) where {T <: PM.AbstractPowerFormulation}
 
@@ -102,29 +114,33 @@ function  build_canonical_model(transmission::Type{T},
     forecast = get(kwargs, :forecast, true)
 
     if forecast
-        first_key = PSY.get_forecasts_initial_time(sys)
         horizon = PSY.get_forecasts_horizon(sys)
         time_steps = 1:horizon
+        resolution = PSY.get_forecasts_resolution(sys)
     else
         time_steps = 1:1
     end
 
     bus_numbers = [b.number for b in PSY.get_components(PSY.Bus, sys)]
 
-    ps_model = _canonical_model_init(bus_numbers, optimizer, transmission, time_steps; kwargs...)
+    ps_model = _canonical_model_init(bus_numbers, 
+                                     optimizer, 
+                                     transmission,                                      
+                                     time_steps,
+                                     resolution; kwargs...)
 
     # Build Injection devices
     for mod in devices
-        construct_device!(ps_model, mod[2], transmission, sys, time_steps, resolution; kwargs...)
-    end
-
-    # Build Branches
-    for mod in branches
-        construct_device!(ps_model, mod[2], transmission, sys, time_steps, resolution; kwargs...)
+        construct_device!(ps_model, mod[2], transmission, sys; kwargs...)
     end
 
     # Build Network
-    construct_network!(ps_model, transmission, sys, time_steps; kwargs...)
+    construct_network!(ps_model, transmission, sys; kwargs...)
+
+    # Build Branches
+    for mod in branches
+        construct_device!(ps_model, mod[2], transmission, sys; kwargs...)
+    end
 
     #Build Service
     for mod in services
--- src/core/core_structs/canonical_model.jl
+++ src/core/core_structs/canonical_model.jl
@@ -1,12 +1,19 @@
 mutable struct CanonicalModel 
     JuMPmodel::JuMP.AbstractModel
+    parametrized::Bool
+    time_steps::UnitRange{Int64}
+    resolution::Dates.Period
     variables::Dict{Symbol, JuMP.Containers.DenseAxisArray}
     constraints::Dict{Symbol, JuMP.Containers.DenseAxisArray}
     cost_function::JuMP.AbstractJuMPScalar
     expressions::Dict{Symbol, JuMP.Containers.DenseAxisArray}
-    parameters::Union{Nothing,Dict{Symbol, JuMP.Containers.DenseAxisArray}}
+    parameteres::Union{Nothing,Dict{Symbol, JuMP.Containers.DenseAxisArray}}
     initial_conditions::Dict{Symbol, Array{InitialCondition}}
     pm_model::Union{Nothing,PM.GenericPowerModel}
 end
 
 _variable_type(cm::CanonicalModel) = JuMP.variable_type(cm.JuMPmodel)
+
+model_time_steps(ps_m::CanonicalModel) = ps_m.time_steps
+model_resolution(ps_m::CanonicalModel) = ps_m.resolution
+parameters(ps_m::CanonicalModel) = ps_m.parametrized
