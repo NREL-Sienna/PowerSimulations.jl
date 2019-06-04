667e90bcab11e2d9e208873b8dde2a14cf7c1ae5
Jose Daniel Lara <jdlara@berkeley.edu>
2019-06-04 15:55:50 -0700

    add dc lines data get

--- test/test_utils/get_test_data.jl
+++ test/test_utils/get_test_data.jl
@@ -15,7 +15,6 @@ c_sys14 = PSY.System(sys14)
 PTDF5 = PSY.PTDF(branches5, nodes5);
 PTDF14 = PSY.PTDF(branches14, nodes14);
 
-
 #System with Renewable Energy
 sys5_re = PSY._System(nodes5, vcat(thermal_generators5, renewable_generators5), loads5, branches5, nothing,  100.0, forecasts5, nothing, nothing);
 c_sys5_re = PSY.System(sys5_re)
@@ -26,6 +25,16 @@ c_sys5_re_only = PSY.System(sys5_re_only)
 #System with Storage Device
 sys5_bat = PSY._System(nodes5, thermal_generators5, loads5, branches5, battery5,  100.0, forecasts5, nothing, nothing);
 c_sys5_bat = PSY.System(sys5_bat)
+
+#Systems with HVDC data in the branches
+sys5_dc = PSY._System(nodes5, thermal_generators5, loads5, branches5_dc, nothing,  100.0, forecasts5, nothing, nothing);
+sys14_dc = PSY._System(nodes14, thermal_generators14, loads14, branches14_dc, nothing,  100.0, forecasts14, nothing, nothing);
+c_sys5_dc = PSY.System(sys5_dc)
+c_sys14_dc = PSY.System(sys14_dc)
+b_ac_5 = collect(get_components(PSY.ACBranch, c_sys5_dc))
+PTDF5_dc = PSY.PTDF(b_ac_5, nodes5);
+b_ac_14 = collect(get_components(PSY.ACBranch, c_sys14_dc))
+PTDF14_dc = PSY.PTDF(b_ac_14, nodes14);
 
 # RTS Data
 RTS_GMLC_DIR = joinpath(DATA_DIR, "RTS_GMLC")
