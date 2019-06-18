553b0fe1ce7fe1d16b3637b054250f8acc4ba628
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2019-06-18 17:31:06 -0600

    Bug fixed in pwl check function

--- src/devices/device_models/common/cost_functions.jl
+++ src/devices/device_models/common/cost_functions.jl
@@ -27,28 +27,28 @@ function ps_cost(ps_m::CanonicalModel,
 
 end
 
-function pwlparamcheck(cost_::Array{Tuple{Float64, Float64}})
+function pwl_param_check(cost_::Array{Tuple{Float64, Float64}})
     flag = true;
     for i in 1:(length(cost_)-1)
         if i == 1 
             (cost_[i][1]/cost_[i][2]) <= ((cost_[i+1][1] - cost_[i][1])/(cost_[i+1][2] - cost_[i][2])) ? nothing : flag = false;
         else
-            ((cost_[i+1][1] - cost_[i][1])/(cost_[i+1][2] - cost_[i][2])) <= ((cost_[i+1][1] - cost_[i][1])/(cost_[i+1][2] - cost_[i][2])) ? nothing : flag = false;
+            ((cost_[i][1] - cost_[i-1][1])/(cost_[i][2] - cost_[i-1][2])) <= ((cost_[i+1][1] - cost_[i][1])/(cost_[i+1][2] - cost_[i][2])) ? nothing : flag = false;
         end
     end
     return flag
 end
 
-function pwlgencost(ps_m::CanonicalModel,
+function pwl_gen_cost(ps_m::CanonicalModel,
                     variable::JV,
                     cost_component::Array{Tuple{Float64, Float64}}) where {JV <: JuMP.AbstractVariableRef}
 
     gen_cost = JuMP.GenericAffExpr{Float64, _variable_type(ps_m)}()
-    pwlparamcheck(cost_component) ? @warn("Data provide is not suitable for linear implementation of PWL cost, this will result in a INVALID SOLUTION") : nothing ;
+    pwl_param_check(cost_component) ? @warn("Data provide is not suitable for linear implementation of PWL cost, this will result in a INVALID SOLUTION") : nothing ;
     # TODO: implement a fallback to either Linear Cost function or SOS2 based PWL Cost function
-    upperbound(i) = (i == 0 & (length(cost_component) > i) ? cost_component[i+1][2] : (cost_component[i+1][2] - cost_component[i][2]));
-    pwlvars = JuMP.@variable(ps_m.JuMPmodel, [i = 0:(length(cost_component)-1)], base_name = "{$(variable)}_{pwl}", start = 0.0, lower_bound = 0.0, upper_bound = upperbound(i))
-
+    upperbound(i) = (i == 1 ? cost_component[i+1][2] : (cost_component[i+1][2] - cost_component[i][2]));
+    pwlvars = JuMP.@variable(ps_m.JuMPmodel, [i = 1:(length(cost_component)-1)], base_name = "{$(variable)}_{pwl}", start = 0.0, lower_bound = 0.0, upper_bound = upperbound(i))
+ 
     for (ix, pwlvar) in enumerate(pwlvars)
         if ix == 1 
             temp_gen_cost = cost_component[ix][1] * (pwlvar / cost_component[ix][2] ) ;
@@ -58,7 +58,7 @@ function pwlgencost(ps_m::CanonicalModel,
         gen_cost = gen_cost + temp_gen_cost
     end
 
-    c = JuMP.@constraint(ps_m.JuMPmodel, variable == sum([pwlvars[ix-1] for (ix, pwlvar) in enumerate(pwlvars)]) )
+    c = JuMP.@constraint(ps_m.JuMPmodel, variable == sum([pwlvar for (ix, pwlvar) in enumerate(pwlvars) if ix > 1]) )
 
     return gen_cost
 
@@ -72,7 +72,7 @@ function ps_cost(ps_m::CanonicalModel,
     gen_cost = JuMP.GenericAffExpr{Float64, _variable_type(ps_m)}()
 
     for var in variable
-        c = pwlgencost(ps_m, var, cost_component)
+        c = pwl_gen_cost(ps_m, var, cost_component)
         gen_cost += c
     end
 
