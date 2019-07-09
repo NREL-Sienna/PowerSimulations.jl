56268799534de11b2da4fd5daf8cd010e122d52d
Jose Daniel Lara <jdlara@berkeley.edu>
2019-07-09 10:57:02 -0600

    change utils

--- src/devices/device_models/common/model_utils.jl
+++ src/devices/device_models/common/model_utils.jl
@@ -3,8 +3,7 @@ function _add_var_container!(ps_m::CanonicalModel, var_name::Symbol, ax1, ax2)
     return
 end
 
-function _add_cons_container!(ps_m::CanonicalModel, cons_name::Symbol, var_names::Symbol)
-    time_steps = model_time_steps(ps_m)
-    ps_m.constraints[cons_name] = JuMPConstraintArray(undef, ps_m.variables[var_names[1]].axes[1], time_steps)
+function _add_cons_container!(ps_m::CanonicalModel, cons_name::Symbol, ax1, ax2)
+    ps_m.constraints[cons_name] = JuMPConstraintArray(undef, ax1, ax2)
     return
 end
