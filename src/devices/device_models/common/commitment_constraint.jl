22017405613c576faecc19c06c1cd0804db19beb
Jose Daniel Lara <jdlara@berkeley.edu>
2019-07-09 11:03:03 -0600

    minor changes for readability

--- src/devices/device_models/common/commitment_constraint.jl
+++ src/devices/device_models/common/commitment_constraint.jl
@@ -9,17 +9,17 @@ function device_commitment(ps_m::CanonicalModel,
     var3 = var(ps_m, var_names[3])
     var1_names = axes(var1,1)
     _add_cons_container!(ps_m, cons_name, var1_names, time_steps)
-    c = con(ps_m, cons_name)
+    constraint = con(ps_m, cons_name)
 
-    for i in initial_conditions
-        name = PSY.get_name(i.device)
-        c[name, 1] = JuMP.@constraint(ps_m.JuMPmodel,
-                               var3[name, 1] == i.value + var1[name, 1] - var2[name, 1])
+    for ic in initial_conditions
+        name = PSY.get_name(ic.device)
+        constraint[name, 1] = JuMP.@constraint(ps_m.JuMPmodel,
+                               var3[name, 1] == ic.value + var1[name, 1] - var2[name, 1])
     end
 
     for t in time_steps[2:end], i in initial_conditions
         name = PSY.get_name(i.device)
-        c[name, 1] = JuMP.@constraint(ps_m.JuMPmodel,
+        constraint[name, 1] = JuMP.@constraint(ps_m.JuMPmodel,
                         var3[name, t] == var3[name, t-1] + var1[name, t] - var2[name, t])
     end
 
