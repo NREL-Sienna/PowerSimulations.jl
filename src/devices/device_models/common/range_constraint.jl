0200de7d898dd8317e5b3221a54da9ed8015c984
Kratochvil <mkratoch@stc-26026s.nrel.gov>
2019-07-10 16:49:36 -0600

    readability changes

--- src/devices/device_models/common/range_constraint.jl
+++ src/devices/device_models/common/range_constraint.jl
@@ -4,17 +4,20 @@ function device_range(ps_m::CanonicalModel,
                         var_name::Symbol)
 
     time_steps = model_time_steps(ps_m)
-    ps_m.constraints[cons_name] = JuMPConstraintArray(undef, (r[1] for r in range_data), time_steps)
+    var1 = var(ps_m, var_name)
+    set_name = (r[1] for r in range_data)
+    _add_cons_container!(ps_m, cons_name, set_name, time_steps)
+    constraint = con(ps_m, cons_name)
 
     for r in range_data
           if abs(r[2].min - r[2].max) <= eps()
             @warn("The min - max values in range constraint with eps() distance to each other. Range Constraint will be modified for Equality Constraint")
                 for t in time_steps
-                    ps_m.constraints[cons_name][r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_name][r[1], t] == r[2].max)
+                    constraint[r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, var1[r[1], t] == r[2].max)
                 end
             else
                 for t in time_steps
-                    ps_m.constraints[cons_name][r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, r[2].min <= ps_m.variables[var_name][r[1], t] <= r[2].max)
+                    constraint[r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, r[2].min <= var1[r[1], t] <= r[2].max)
                 end
             end
     end
@@ -34,23 +37,30 @@ function device_semicontinuousrange(ps_m::CanonicalModel,
     time_steps = model_time_steps(ps_m)
     ub_name = Symbol(cons_name,:_ub)
     lb_name = Symbol(cons_name,:_lb)
+    
+    var1 = var(ps_m, var_name)
+    varb = var(ps_m, binvar_name)
 
-    #MOI has a semicontinous set, but after some tests is not clear most MILP solvers support it. In the future this can be updated
+    #MOI has a semicontinous set, but after some tests is not clear most MILP solvers support it. 
+    #In the future this can be updated
+    
     set_name = (r[1] for r in scrange_data)
-    ps_m.constraints[ub_name] = JuMPConstraintArray(undef, set_name, time_steps)
-    ps_m.constraints[lb_name] = JuMPConstraintArray(undef, set_name, time_steps)
+    _add_cons_container!(ps_m, ub_name, set_name, time_steps)
+    _add_cons_container!(ps_m, lb_name, set_name, time_steps)    
+    con_ub = con(ps_m, ub_name)
+    con_lb = con(ps_m, lb_name)
 
     for t in time_steps, r in scrange_data
 
             if r[2].min == 0.0
 
-                ps_m.constraints[ub_name][r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_name][r[1], t] <= r[2].max*ps_m.variables[binvar_name][r[1], t])
-                ps_m.constraints[lb_name][r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_name][r[1], t] >= 0.0)
+                con_ub[r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, var1[r[1], t] <= r[2].max*varb[r[1], t])
+                con_lb[r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, var1[r[1], t] >= 0.0)
 
             else
 
-                ps_m.constraints[ub_name][r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_name][r[1], t] <= r[2].max*ps_m.variables[binvar_name][r[1], t])
-                ps_m.constraints[lb_name][r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_name][r[1], t] >= r[2].min*ps_m.variables[binvar_name][r[1], t])
+                con_ub[r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, var1[r[1], t] <= r[2].max*varb[r[1], t])
+                con_lb[r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, var1[r[1], t] >= r[2].min*varb[r[1], t])
 
             end
 
@@ -69,25 +79,30 @@ function reserve_device_semicontinuousrange(ps_m::CanonicalModel,
     time_steps = model_time_steps(ps_m)
     ub_name = Symbol(cons_name,:_ub)
     lb_name = Symbol(cons_name,:_lb)
+    
+    var1 = var(ps_m, var_name)
+    varb = var(ps_m, binvar_name)
 
     # MOI has a semicontinous set, but after some tests is not clear most MILP solvers support it.
     # In the future this can be updated
 
     set_name = (r[1] for r in scrange_data)
-    ps_m.constraints[ub_name] = JuMPConstraintArray(undef, set_name, time_steps)
-    ps_m.constraints[lb_name] = JuMPConstraintArray(undef, set_name, time_steps)
+    _add_cons_container!(ps_m, ub_name, set_name, time_steps)
+    _add_cons_container!(ps_m, lb_name, set_name, time_steps)    
+    con_ub = con(ps_m, ub_name)
+    con_lb = con(ps_m, lb_name)
 
     for t in time_steps, r in scrange_data
 
             if r[2].min == 0.0
 
-                ps_m.constraints[ub_name][r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_name][r[1], t] <= r[2].max*(1-ps_m.variables[binvar_name][r[1], t]))
-                ps_m.constraints[lb_name][r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_name][r[1], t] >= 0.0)
+                con_ub[r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, var1[r[1], t] <= r[2].max*(1-varb[r[1], t]))
+                con_lb[r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, var1[r[1], t] >= 0.0)
 
             else
 
-                ps_m.constraints[ub_name][r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_name][r[1], t] <= r[2].max*(1-ps_m.variables[binvar_name][r[1], t]))
-                ps_m.constraints[lb_name][r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_name][r[1], t] >= r[2].min*(1-ps_m.variables[binvar_name][r[1], t]))
+                con_ub[r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, var1[r[1], t] <= r[2].max*(1-varb[r[1], t]))
+                con_lb[r[1], t] = JuMP.@constraint(ps_m.JuMPmodel, var1[r[1], t] >= r[2].min*(1-varb[r[1], t]))
 
             end
 
