961c3c4489ae57d805b9f640f298b30956d96393
Kratochvil <mkratoch@stc-26026s.nrel.gov>
2019-07-10 16:12:16 -0600

    readability changes, fix 'fraction_of_hour' inconsistency

--- src/devices/device_models/common/energy_balance_constraint.jl
+++ src/devices/device_models/common/energy_balance_constraint.jl
@@ -8,14 +8,19 @@ function energy_balance(ps_m::CanonicalModel,
     resolution = model_resolution(ps_m)
     fraction_of_hour = Dates.value(Dates.Minute(resolution))/60
     name_index = efficiency_data[1]
-    ps_m.constraints[cons_name] = JuMPConstraintArray(undef, name_index, time_steps)
-
+    var1 = var(ps_m, var_names[1])
+    var2 = var(ps_m, var_names[2])
+    var3 = var(ps_m, var_names[3])
+    _add_cons_container!(ps_m, cons_name, name_index, time_steps)
+    constraint = con(ps_m, cons_name)
+    
     for (ix,name) in enumerate(name_index)
         eff_in = efficiency_data[2][ix].in
         eff_out = efficiency_data[2][ix].out
 
-        ps_m.constraints[cons_name][name, 1] = JuMP.@constraint(ps_m.JuMPmodel,
-                                                ps_m.variables[var_names[3]][name, 1] == initial_conditions[ix].value + ps_m.variables[var_names[1]][name, 1]*eff_in*fraction_of_hour - (ps_m.variables[var_names[2]][name, 1])*fraction_of_hour/eff_out)
+        constraint[name, 1] = JuMP.@constraint(ps_m.JuMPmodel,
+                                   var3[name, 1] == initial_conditions[ix].value + var1[name, 1]*eff_in*fraction_of_hour
+                                                    - (var2[name, 1])*fraction_of_hour/eff_out)
 
     end
 
@@ -23,9 +28,9 @@ function energy_balance(ps_m::CanonicalModel,
         eff_in = efficiency_data[2][ix].in
         eff_out = efficiency_data[2][ix].out
 
-        ps_m.constraints[cons_name][name, t] = JuMP.@constraint(ps_m.JuMPmodel,
-                                                ps_m.variables[var_names[3]][name, t] == ps_m.variables[var_names[3]][name, t-1] + ps_m.variables[var_names[1]][name, t]*eff_in - (ps_m.variables[var_names[2]][name, t])*fraction_of_hour/eff_out)
-
+        constraint[name, t] = JuMP.@constraint(ps_m.JuMPmodel,
+                                   var3[name, t] == var3[name, t-1] + var1[name, t]*eff_in*fraction_of_hour
+                                                    - (var2[name, t])*fraction_of_hour/eff_out)
     end
 
     return
