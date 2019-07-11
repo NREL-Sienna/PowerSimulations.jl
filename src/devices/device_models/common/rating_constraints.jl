ea89fec0be58fb11ad0339efe82933703afa2779
Kratochvil <mkratoch@stc-26026s.nrel.gov>
2019-07-11 09:34:20 -0600

    make readability changes

--- src/devices/device_models/common/rating_constraints.jl
+++ src/devices/device_models/common/rating_constraints.jl
@@ -3,12 +3,15 @@ function norm_two_constraint(ps_m::CanonicalModel,
                             cons_name::Symbol,
                             var_names::Tuple{Symbol, Symbol})
 
-    time_steps = model_time_steps(ps_m)                                
-    ps_m.constraints[cons_name] = JuMPConstraintArray(undef, rating_data[1], time_steps)
+    time_steps = model_time_steps(ps_m)
+    var1 = var(ps_m, var_names[1])
+    var2 = var(ps_m, var_names[2])
+    _add_cons_container!(ps_m, cons_name, rating_data[1], time_steps)
+    constraint = con(ps_m, cons_name)
 
     for t in time_steps, (ix,r) in enumerate(rating_data[1])
-
-            ps_m.constraints[cons_name][r, t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_names[1]][r[1], t] + ps_m.variables[var_names[2]][r[1], t] <= rating_data[ix][2]^2)
+        
+        constraint[r, t] = JuMP.@constraint(ps_m.JuMPmodel, var1[r[1], t] + var2[r[1], t] <= rating_data[ix][2]^2)
 
     end
 
