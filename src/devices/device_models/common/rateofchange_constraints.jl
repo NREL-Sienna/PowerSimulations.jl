be82cb9d70858c22985ac9842f5513216a0b083d
Kratochvil <mkratoch@stc-26026s.nrel.gov>
2019-07-11 09:22:10 -0600

    make readability changes

--- src/devices/device_models/common/rateofchange_constraints.jl
+++ src/devices/device_models/common/rateofchange_constraints.jl
@@ -7,20 +7,24 @@ function device_linear_rateofchange(ps_m::CanonicalModel,
     time_steps = model_time_steps(ps_m)
     up_name = Symbol(cons_name,:_up)
     down_name = Symbol(cons_name,:_down)
+    
+    var1 = var(ps_m, var_name)
 
     set_name = rate_data[1]
-
-    ps_m.constraints[up_name] = JuMPConstraintArray(undef, set_name, time_steps)
-    ps_m.constraints[down_name] = JuMPConstraintArray(undef, set_name, time_steps)
+    _add_cons_container!(ps_m, up_name, set_name, time_steps)
+    _add_cons_container!(ps_m, down_name, set_name, time_steps)
+    con_up = con(ps_m, up_name)
+    con_down = con(ps_m, down_name)
 
     for (ix, name) in enumerate(rate_data[1])
-        ps_m.constraints[up_name][name, 1] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_name][name, 1] - initial_conditions[ix].value <= rate_data[2][ix].up)
-        ps_m.constraints[down_name][name, 1] = JuMP.@constraint(ps_m.JuMPmodel, initial_conditions[ix].value - ps_m.variables[var_name][name, 1] <= rate_data[2][ix].down)
+        con_up[name, 1] = JuMP.@constraint(ps_m.JuMPmodel, var1[name, 1] - initial_conditions[ix].value <= rate_data[2][ix].up)
+        con_down[name, 1] = JuMP.@constraint(ps_m.JuMPmodel, initial_conditions[ix].value - var1[name, 1] 
+                                                                <= rate_data[2][ix].down)
     end
 
     for t in time_steps[2:end], (ix, name) in enumerate(rate_data[1])
-        ps_m.constraints[up_name][name, t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_name][name, t-1] - ps_m.variables[var_name][name, t] <= rate_data[2][ix].up)
-        ps_m.constraints[down_name][name, t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_name][name, t] - ps_m.variables[var_name][name, t-1] <= rate_data[2][ix].down)
+        con_up[name, t] = JuMP.@constraint(ps_m.JuMPmodel, var1[name, t-1] - var1[name, t] <= rate_data[2][ix].up)
+        con_down[name, t] = JuMP.@constraint(ps_m.JuMPmodel, var1[name, t] - var1[name, t-1] <= rate_data[2][ix].down)
     end
 
     return
@@ -36,20 +40,29 @@ function device_mixedinteger_rateofchange(ps_m::CanonicalModel,
     time_steps = model_time_steps(ps_m)
     up_name = Symbol(cons_name,:_up)
     down_name = Symbol(cons_name,:_down)
+    
+    var1 = var(ps_m, var_names[1])
+    var2 = var(ps_m, var_names[2])
+    var3 = var(ps_m, var_names[3])
 
     set_name = rate_data[1]
-
-    ps_m.constraints[up_name] = JuMPConstraintArray(undef, set_name, time_steps)
-    ps_m.constraints[down_name] = JuMPConstraintArray(undef, set_name, time_steps)
+    _add_cons_container!(ps_m, up_name, set_name, time_steps)
+    _add_cons_container!(ps_m, down_name, set_name, time_steps)
+    con_up = con(ps_m, up_name)
+    con_down = con(ps_m, down_name)
 
     for (ix, name) in enumerate(rate_data[1])
-        ps_m.constraints[up_name][name, 1] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_names[1]][name, 1] - initial_conditions[ix].value <= rate_data[2][ix].up + rate_data[3][ix].max*ps_m.variables[var_names[2]][name, 1])
-        ps_m.constraints[down_name][name, 1] = JuMP.@constraint(ps_m.JuMPmodel, initial_conditions[ix].value - ps_m.variables[var_names[1]][name, 1] <= rate_data[2][ix].down + rate_data[3][ix].min*ps_m.variables[var_names[3]][name, 1])
+        con_up[name, 1] = JuMP.@constraint(ps_m.JuMPmodel, var1[name, 1] - initial_conditions[ix].value 
+                                                            <= rate_data[2][ix].up + rate_data[3][ix].max*var2[name, 1])
+        con_down[name, 1] = JuMP.@constraint(ps_m.JuMPmodel, initial_conditions[ix].value - var1[name, 1] 
+                                                            <= rate_data[2][ix].down + rate_data[3][ix].min*var3[name, 1])
     end
 
     for t in time_steps[2:end], (ix, name) in enumerate(rate_data[1])
-        ps_m.constraints[up_name][name, t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_names[1]][name, t-1] - ps_m.variables[var_names[1]][name, t] <= rate_data[2][ix].up + rate_data[3][ix].max*ps_m.variables[var_names[2]][name, t])
-        ps_m.constraints[down_name][name, t] = JuMP.@constraint(ps_m.JuMPmodel, ps_m.variables[var_names[1]][name, t] - ps_m.variables[var_names[1]][name, t-1] <= rate_data[2][ix].down + rate_data[3][ix].min*ps_m.variables[var_names[3]][name, t])
+        con_up[name, t] = JuMP.@constraint(ps_m.JuMPmodel, var1[name, t-1] - var1[name, t] 
+                                                            <= rate_data[2][ix].up + rate_data[3][ix].max*var2[name, t])
+        con_down[name, t] = JuMP.@constraint(ps_m.JuMPmodel, var1[name, t] - var1[name, t-1]
+                                                            <= rate_data[2][ix].down + rate_data[3][ix].min*var3[name, t])
     end
 
     return
