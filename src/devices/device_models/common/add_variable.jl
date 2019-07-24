f2dbae7431bd872257fc8a24b3617ad3a14e9b5c
Jose Daniel Lara <jdlara@berkeley.edu>
2019-07-23 18:34:39 -0600

    generalize add_variable

--- src/devices/device_models/common/add_variable.jl
+++ src/devices/device_models/common/add_variable.jl
@@ -7,68 +7,6 @@ end
     add_variable(ps_m::CanonicalModel,
                       devices::D,
                       var_name::Symbol,
-                      binary::Bool)
-
-Adds a positive variable to the optimization model. Based on the inputs, the variable can
-be specified as binary.
-
-# Bounds
-
-``` 0 <= varstart[name, t]  ```
-
-If binary = true:
-
-``` varstart[name, t] in {0,1} ```
-
-# LaTeX
-
-``  x^{device}_t >= 0.0 \forall t ``
-
-``  x^{device}_t \in {0,1} \forall t iff \text{binary = true}``
-
-
-# Arguments
-* ps_m::CanonicalModel : the canonical model built in PowerSimulations
-* devices : Vector or Iterator with the devices
-* var_name::Symbol : Base Name for the variable
-* binary::Bool : Select if the variable is binary
-"""
-function add_variable(ps_m::CanonicalModel,
-                      devices::D,
-                      var_name::Symbol,
-                      binary::Bool; kwargs...) where {D <: Union{Vector{<:PSY.Device},
-                                                      PSY.FlattenIteratorWrapper{<:PSY.Device}}}
-
-    time_steps = model_time_steps(ps_m)
-    _add_var_container!(ps_m, var_name, (PSY.get_name(d) for d in devices), time_steps)
-    variable = var(ps_m, var_name)
-    jvar_name = _remove_underscore(var_name)
-
-    lb = get(kwargs, :ub_value_field, nothing)
-    init_field = get(kwargs, :initial_value_field, nothing)
-    ub_field = get(kwargs, :ub_value_field, nothing)
-
-    for t in time_steps, d in devices
-      name = PSY.get_name(d)
-
-      variable[name, t] = JuMP.@variable(ps_m.JuMPmodel,
-                                       base_name="$(jvar_name)_{$(name),$(t)}",
-                                       binary=binary,
-                                       lower_bound = 0.0)
-
-        !isnothing(ub_field) && JuMP.set_upper_bound(variable[name, t], getfield(d,ub_field))
-        !isnothing(ub_field) && JuMP.set_lower_bound(variable[name, t], getfield(d,ub_field))
-        !isnothing(init_field) && JuMP.set_start_value(variable[name, t], getfield(d,ub_field))
-    end
-
-    return
-
-end
-
-@doc raw"""
-    add_variable(ps_m::CanonicalModel,
-                      devices::D,
-                      var_name::Symbol,
                       binary::Bool,
                       expression::Symbol,
                       sign::Float64)
@@ -98,41 +36,49 @@ If binary = true:
 * binary::Bool : Select if the variable is binary
 * expression::Symbol : Expression name stored in canonical_model.expressions to add the variable
 * sign::Float64 : sign of the addition of the variable to the expression. Default Value is 1.0
+
+# Accepted Keyword Arguments
+* ub_value_function : Provides the function over device to obtain the value for a upper_bound
+* lb_value_function : Provides the function over device to obtain the value for a lower_bound other than 0
+* initial_value_function : Provides the function over device to obtain the warm start value
+
 """
 function add_variable(ps_m::CanonicalModel,
                       devices::D,
                       var_name::Symbol,
                       binary::Bool,
-                      expression::Symbol,
+                      expression::Union{Nothing,Symbol}=nothing,
                       sign::Float64=1.0; kwargs...) where {D <: Union{Vector{<:PSY.Device},
                                           PSY.FlattenIteratorWrapper{<:PSY.Device}}}
 
     time_steps = model_time_steps(ps_m)
     _add_var_container!(ps_m, var_name, (PSY.get_name(d) for d in devices), time_steps)
     variable = var(ps_m, var_name)
-    expr = exp(ps_m, expression)
     jvar_name = _remove_underscore(var_name)
 
-    lb = get(kwargs, :ub_value_field, nothing)
-    init_field = get(kwargs, :initial_value_field, nothing)
-    ub_field = get(kwargs, :ub_value_field, nothing)
+    lb_f = get(kwargs, :ub_value, nothing)
+    init_f= get(kwargs, :initial_value, nothing)
+    ub_f = get(kwargs, :ub_value, nothing)
 
     for t in time_steps, d in devices
         name = PSY.get_name(d)
-
         variable[name, t] = JuMP.@variable(ps_m.JuMPmodel,
                                         base_name="$(jvar_name)_{$(name), $(t)}",
                                         binary=binary,
                                         lower_bound = 0.0)
 
-        !isnothing(ub_field) && JuMP.set_upper_bound(variable[name, t], getfield(d,ub_field))
-        !isnothing(ub_field) && JuMP.set_lower_bound(variable[name, t], getfield(d,ub_field))
-        !isnothing(init_field) && JuMP.set_start_value(variable[name, t], getfield(d,ub_field))
+        !isnothing(ub_f) && JuMP.set_upper_bound(variable[name, t], ub_f(d))
+        !isnothing(ub_f) && JuMP.set_lower_bound(variable[name, t], lb_f(d))
+        !isnothing(init_f) && JuMP.set_start_value(variable[name, t], init_f(d))
 
-       _add_to_expression!(expr,
-                           PSY.get_number(PSY.get_bus(d)),
-                           t,
-                           variable[name, t], sign)
+        if !(isnothing(expression))
+        bus_number = PSY.get_number(PSY.get_bus(d))
+        _add_to_expression!(exp(ps_m, expression),
+                            bus_number,
+                            t,
+                            variable[name, t],
+                            sign)
+        end
     end
 
     return
