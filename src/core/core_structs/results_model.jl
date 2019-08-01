1bc8bb1b67d7d665a6b433b1d189b32c6fb37088
Jose Daniel Lara <jdlara@berkeley.edu>
2019-08-01 15:27:54 -0600

    add accesor for variable name

--- src/core/core_structs/results_model.jl
+++ src/core/core_structs/results_model.jl
@@ -3,3 +3,7 @@ struct OpertationModelResults
     total_cost::Dict{Symbol,Float64}
     optimizer_log::Dict{Symbol, Any}
 end
+
+function get_variable(res_model::OpertationModelResults, key::Symbol)
+    return get(res_model.variables, key, nothing)
+end
