80cc84a29c961d1bb062f8650fb250f38769ae2e
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2019-08-01 12:25:03 -0600

    Updated Hydro Constructor test

--- test/test_hydro_generation_constructors.jl
+++ test/test_hydro_generation_constructors.jl
@@ -2,44 +2,40 @@
     # See https://discourse.julialang.org/t/how-to-use-test-warn/15557/5 about testing for warning throwing
     warn_message = "The data doesn't devices of type HydroDispatch, consider changing the device models"
     model = DeviceModel(PSY.HydroDispatch, PSI.HydroDispatchRunOfRiver)
-    ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_steps, Dates.Minute(5))
-    @test_logs (:warn, warn_message) construct_device!(ps_model, model, PM.DCPlosslessForm, c_sys5_hy; parameters = true);
-    ps_model = PSI._canonical_model_init(bus_numbers14, nothing, PM.AbstractPowerFormulation, time_steps, Dates.Minute(5))
-    @test_logs (:warn, warn_message) construct_device!(ps_model, model, PM.DCPlosslessForm, c_sys14; parameters = true);
+    op_model = OperationModel(TestOptModel, PM.DCPlosslessForm, c_sys5)
+    @test_logs (:warn, warn_message) construct_device!(op_model, :Hydro, model)
+    op_model = OperationModel(TestOptModel, PM.DCPlosslessForm, c_sys14)
+    @test_logs (:warn, warn_message) construct_device!(op_model, :Hydro, model)
 end
 
 
 @testset "Hydro DCPLossLess FixedOutput" begin
-    model = model = DeviceModel(PSY.HydroFix, PSI.HydroFixed)
-    ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_steps, Dates.Minute(5))
-    construct_device!(ps_model, model, PM.DCPlosslessForm, c_sys5_hy);
-    @test JuMP.num_variables(ps_model.JuMPmodel) == 0
-    @test JuMP.num_constraints(ps_model.JuMPmodel, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.LessThan{Float64}) == 0
-    @test JuMP.num_constraints(ps_model.JuMPmodel, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.GreaterThan{Float64}) == 0
-    @test JuMP.num_constraints(ps_model.JuMPmodel, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.EqualTo{Float64}) == 0
+    model = DeviceModel(PSY.HydroFix, PSI.HydroFixed)
+    
+    # Parameters Testing
+    op_model = OperationModel(TestOptModel, PM.DCPlosslessForm, c_sys5_hy ; parameters = true)
+    construct_device!(op_model, :Hydro, model)
+    moi_tests(op_model, true, 0, 0, 0, 0, 0, false)
+    psi_checkobjfun_test(op_model, GAEVF)
+
     # No Parameters Testing
-    ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_steps, Dates.Minute(5); parameters = false)
-    construct_device!(ps_model, model, PM.DCPlosslessForm, c_sys5_hy; parameters = false);
-    @test !(:params in keys(ps_model.JuMPmodel.ext))
-    @test JuMP.num_variables(ps_model.JuMPmodel) == 0
-    @test JuMP.num_constraints(ps_model.JuMPmodel, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.LessThan{Float64}) == 0
-    @test JuMP.num_constraints(ps_model.JuMPmodel, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.GreaterThan{Float64}) == 0
-    @test JuMP.num_constraints(ps_model.JuMPmodel, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.EqualTo{Float64}) == 0
+    op_model = OperationModel(TestOptModel, PM.DCPlosslessForm, c_sys5_hy)
+    construct_device!(op_model, :Hydro, model);
+    moi_tests(op_model, false, 0, 0, 0, 0, 0, false)
+    psi_checkobjfun_test(op_model, GAEVF)
+
     # No Forecast Testing
-    ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_steps, Dates.Minute(5))
-    construct_device!(ps_model, model, PM.DCPlosslessForm, c_sys5_hy; forecast = false);
-    @test JuMP.num_variables(ps_model.JuMPmodel) == 0
-    @test JuMP.num_constraints(ps_model.JuMPmodel, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.LessThan{Float64}) == 0
-    @test JuMP.num_constraints(ps_model.JuMPmodel, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.GreaterThan{Float64}) == 0
-    @test JuMP.num_constraints(ps_model.JuMPmodel, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.EqualTo{Float64}) == 0
+    op_model = OperationModel(TestOptModel, PM.DCPlosslessForm, c_sys5_hy ; parameters = true, forecast = false)
+    construct_device!(op_model, :Hydro, model);
+    moi_tests(op_model, true, 0, 0, 0, 0, 0, false)
+    psi_checkobjfun_test(op_model, GAEVF)
+
     # No Forecast - No Parameters Testing
-    ps_model = PSI._canonical_model_init(bus_numbers5, nothing, PM.AbstractPowerFormulation, time_steps, Dates.Minute(5); parameters = false)
-    construct_device!(ps_model, model, PM.DCPlosslessForm, c_sys5_hy; parameters = false, forecast = false);
-    @test !(:params in keys(ps_model.JuMPmodel.ext))
-    @test JuMP.num_variables(ps_model.JuMPmodel) == 0
-    @test JuMP.num_constraints(ps_model.JuMPmodel, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.LessThan{Float64}) == 0
-    @test JuMP.num_constraints(ps_model.JuMPmodel, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.GreaterThan{Float64}) == 0
-    @test JuMP.num_constraints(ps_model.JuMPmodel, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.EqualTo{Float64}) == 0
+    op_model = OperationModel(TestOptModel, PM.DCPlosslessForm, c_sys5_hy ; forecast = false)
+    construct_device!(op_model, :Hydro, model);
+    moi_tests(op_model, false, 0, 0, 0, 0, 0, false)
+    psi_checkobjfun_test(op_model, GAEVF)
+
 end
 
 #=
