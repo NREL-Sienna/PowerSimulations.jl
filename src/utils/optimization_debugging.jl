b804741da7d13a3f44fc706b8c6d7f330c5acfda
Jose Daniel Lara <jdlara@berkeley.edu>
2019-08-05 11:34:32 -0600

    add more optimization debugging tools

--- src/utils/optimization_debugging.jl
+++ src/utils/optimization_debugging.jl
@@ -1,4 +1,6 @@
-function get_constraint_index(op_model::OperationModel)
+
+""" "Each Tuple corresponds to (con_name, internal_index, moi_index)"""
+function get_all_constraint_index(op_model::OperationModel)
     con_index = Vector{Tuple{Symbol, Int64, Int64}}()
     for (key, value) in op_model.canonical.constraints
         for (idx, constraint) in enumerate(value)
@@ -6,11 +8,12 @@ function get_constraint_index(op_model::OperationModel)
             push!(con_index, (key, idx, moi_index.value))
         end
     end
-    @info "Each Tuple corresponds to (con_name, internal_index, moi_index)"
+
     return con_index
 end
 
-function get_var_index(op_model::OperationModel)
+""" "Each Tuple corresponds to (con_name, internal_index, moi_index)"""
+function get_all_var_index(op_model::OperationModel)
     var_index = Vector{Tuple{Symbol, Int64, Int64}}()
     for (key, value) in op_model.canonical.variables
         for (idx, variable) in enumerate(value)
@@ -18,6 +21,34 @@ function get_var_index(op_model::OperationModel)
             push!(var_index, (key, idx, moi_index.value))
         end
     end
-    @info "Each Tuple corresponds to (var_name, internal_index, moi_index)"
+
     return var_index
+end
+
+function get_con_index(op_model::OperationModel, index::Int64)
+
+    for i in get_all_constraint_index(op_model::OperationModel)
+        if i[3] == index
+            return op_model.canonical.constraints[i[1]].data[i[2]]
+        end
+    end
+
+    @info "Index not found"
+
+    return
+
+end
+
+function get_var_index(op_model::OperationModel, index::Int64)
+
+    for i in get_all_var_index(op_model::OperationModel)
+        if i[3] == index
+            return op_model.canonical.variables[i[1]].data[i[2]]
+        end
+    end
+
+    @info "Index not found"
+
+    return
+
 end
