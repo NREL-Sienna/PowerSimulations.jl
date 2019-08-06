d042c19ef397b02a809c6502d6c5df36d98173d6
Jose Daniel Lara <jdlara@berkeley.edu>
2019-08-05 19:48:48 -0600

    update docstrings

--- src/devices/device_models/common/timeseries_constraint.jl
+++ src/devices/device_models/common/timeseries_constraint.jl
@@ -1,22 +1,22 @@
 @doc raw"""
     device_timeseries_ub(ps_m::CanonicalModel,
-                     ts_data::Tuple{Vector{String}, Vector{Vector{Float64}}},
+                     ts_data::Tuple{Vector{String}, Vector{Float64}, Vector{Vector{Float64}}},
                      cons_name::Symbol,
                      var_name::Symbol)
 
-Constructs upper bound (and 0 as lower bound) for given variable and time series data.
+Constructs upper bound for given variable and time series data and a multiplier.
 
 # Constraint
 
-```variable[name, t] <= ts_data[2][ix][t] ```
+```variable[name, t] <= ts_data[2][ix]*ts_data[3][ix][t] ```
 
 # LaTeX
 
-`` x_t \leq r^{max}_t, \forall t ``
+`` x_t \leq r^{val} * r_t, \forall t ``
 
 # Arguments
 * ps_m::CanonicalModel : the canonical model built in PowerSimulations
-* ts_data::Tuple{Vector{String}, Vector{Vector{Float64}}} : timeseries data name (1) and values (2)
+* ts_data::Tuple{Vector{String}, Vector{Float64}, Vector{Vector{Float64}}} : timeseries data name (1), multiplier (2) and values (3)
 * cons_name::Symbol : name of the constraint
 * var_name::Symbol : the name of the variable
 """
@@ -42,25 +42,25 @@ end
 
 @doc raw"""
     device_timeseries_lb(ps_m::CanonicalModel,
-                     ts_data::Tuple{Vector{String}, Vector{Vector{Float64}}},
+                     ts_data::Tuple{Vector{String}, Vector{Float64}, Vector{Vector{Float64}}},
                      cons_name::Symbol,
                      var_name::Symbol)
 
-Constructs lower bound for given variable and time series data.
+Constructs lower bound for given variable subject to time series data and a multiplier.
 
 # Constraint
 
-``` ts_data[2][ix][t] <= variable[name, t] ```
+``` ts_data[2][ix]*ts_data[3][ix][t] <= variable[name, t] ```
 
 # LaTeX
 
-`` r^{min}_t \leq x_t, \forall t ``
+`` r^{var} r_t \leq x_t, \forall t ``
 
 where (ix, name) in enumerate(ts_data[1]).
 
 # Arguments
 * ps_m::CanonicalModel : the canonical model built in PowerSimulations
-* ts_data::Tuple{Vector{String}, Vector{Vector{Float64}}} : timeseries data name (1) and values (2)
+* ts_data::Tuple{Vector{String}, Vector{Float64}, Vector{Vector{Float64}}} : timeseries data name (1), multiplier (2) and values (3)
 * cons_name::Symbol : name of the constraint
 * var_name::Symbol : the name of the variable
 """
@@ -87,26 +87,27 @@ end
 #NOTE: there is a floating, unnamed lower bound constraint in this function. This may need to be changed.
 @doc raw"""
     device_timeseries_param_ub(ps_m::CanonicalModel,
-                                    ts_data::Tuple{Vector{String}, Vector{Vector{Float64}}},
+                                    ts_data::Tuple{Vector{String}, Vector{Float64}, Vector{Vector{Float64}}},
                                     cons_name::Symbol,
                                     param_reference::RefParam,
                                     var_name::Symbol)
 
-Constructs upper bound for given variable and time series data parameter as upper bound.
+Constructs upper bound for given variable using a parameter. The constraint is
+    built with a time series data vector and a multiplier
 
 # Constraint
 
-``` variable[name, t] <= param[name, t] ```
+``` variable[name, t] <= val * param[name, t] ```
 
 # LaTeX
 
-`` x^{var}_t \leq x^{param}_t, \forall t ``
+`` x^{var}_t \leq x^{val} x^{param}_t, \forall t ``
 
 # Arguments
 * ps_m::CanonicalModel : the canonical model built in PowerSimulations
-* ts_data::Tuple{Vector{String}, Vector{Vector{Float64}}} : timeseries data name (1) and values (2)
+* ts_data::Tuple{Vector{String}, Vector{Float64}, Vector{Vector{Float64}}} : timeseries data name (1), multiplier (2) and values (3)
 * cons_name::Symbol : name of the constraint
-* param_reference::RefParam : RefParam of the parameter
+* param_reference::RefParam : RefParam to access the parameter
 * var_name::Symbol : the name of the variable
 """
 function device_timeseries_param_ub(ps_m::CanonicalModel,
@@ -134,26 +135,27 @@ end
 
 @doc raw"""
     device_timeseries_param_lb(ps_m::CanonicalModel,
-                                    ts_data::Tuple{Vector{String}, Vector{Vector{Float64}}},
+                                    ts_data::Tuple{Vector{String}, Vector{Float64}, Vector{Vector{Float64}}},
                                     cons_name::Symbol,
                                     param_reference::RefParam,
                                     var_name::Symbol)
 
-Constructs upper bound for given variable and time series data parameter as upper bound.
+Constructs lower bound for given variable using a parameter. The constraint is
+    built with a time series data vector and a multiplier
 
 # Constraint
 
-``` param[name, t] <= variable[name, t] ```
+``` val * param[name, t] <= variable[name, t] ```
 
 # LaTeX
 
-`` x^{param}_t \leq x^{var}_t, \forall t ``
+`` x^{val} x^{param}_t \leq x^{var}_t, \forall t ``
 
 # Arguments
 * ps_m::CanonicalModel : the canonical model built in PowerSimulations
 * ts_data::Tuple{Vector{String}, Vector{Vector{Float64}}} : timeseries data name (1) and values (2)
 * cons_name::Symbol : name of the constraint
-* param_reference::RefParam : RefParam of the parameter
+* param_reference::RefParam : RefParam to access the parameter
 * var_name::Symbol : the name of the variable
 """
 function device_timeseries_param_lb(ps_m::CanonicalModel,
@@ -181,22 +183,23 @@ end
 
 @doc raw"""
     device_timeseries_ub_bin(ps_m::CanonicalModel,
-                                    ts_data::Tuple{Vector{String}, Vector{Vector{Float64}}},
+                                    ts_data::Tuple{Vector{String}, Vector{Float64}, Vector{Vector{Float64}}},
                                     cons_name::Symbol,
                                     var_name::Symbol,
                                     binvar_name::Symbol)
 
-Constructs upper bound (with lower bound 0) for variable and time series or confines to 0 depending on binary variable.
+Constructs upper bound for variable and time series or confines to 0 depending on binary variable.
+    The upper bound is defined by a time series and a multiplier.
 
 # Constraints
 
-``` varcts[name, t] <= varbin[name, t]*ts_data[2][ix][t] ```
+``` varcts[name, t] <= varbin[name, t]* ts_data[2][ix] * ts_data[3][ix][t] ```
 
 where (ix, name) in enumerate(ts_data[1]).
 
 # LaTeX
 
-`` x^{cts}_t \leq r^{max}_t x^{bin}_t, \forall t ``
+`` x^{cts}_t \leq r^{val} r_t x^{bin}_t, \forall t ``
 
 # Arguments
 * ps_m::CanonicalModel : the canonical model built in PowerSimulations
@@ -221,7 +224,7 @@ function device_timeseries_ub_bin(ps_m::CanonicalModel,
     con_ub = con(ps_m, ub_name)
 
     for t in time_steps, (ix, name) in enumerate(ts_data[1])
-        con_ub[name, t] =  JuMP.@constraint(ps_m.JuMPmodel, varcts[name, t] <= varbin[name, t]*ts_data[2][ix]*ts_data[3][ix][t])
+        con_ub[name, t] = JuMP.@constraint(ps_m.JuMPmodel, varcts[name, t] <= varbin[name, t]*ts_data[2][ix]*ts_data[3][ix][t])
     end
 
     return
@@ -230,25 +233,25 @@ end
 
 @doc raw"""
     device_timeseries_ub_bigM(ps_m::CanonicalModel,
-                                    ts_data::Tuple{Vector{String}, Vector{Vector{Float64}}},
+                                    ts_data::Tuple{Vector{String}, Vector{Float64}, Vector{Vector{Float64}}},
                                     cons_name::Symbol,
                                     var_name::Symbol,
                                     param_reference::RefParam,
                                     binvar_name::Symbol,
                                     M_value::Float64 = 1e6)
 
-Constructs upper bound (with lower bound 0) for variable and time series or confines to 0 depending on binary variable.
-Uses BigM constraint type to allow for parameter.
+Constructs upper bound for variable and time series and a multiplier or confines to 0 depending on binary variable.
+    Uses BigM constraint type to allow for parameter since ParameterJuMP doesn't support var*parameter
 
 # Constraints
 
-``` varcts[name, t] - param[name, t] <= (1 - varbin[name, t])*M_value ```
+``` varcts[name, t] - val * param[name, t] <= (1 - varbin[name, t])*M_value ```
 
 ``` varcts[name, t] <= varbin[name, t]*M_value ```
 
 # LaTeX
 
-`` x^{cts}_t - x^{param}_t \leq M(1 - x^{bin}_t ), forall t ``
+`` x^{cts}_t - x^{val} * x^{param}_t \leq M(1 - x^{bin}_t ), forall t ``
 
 `` x^{cts}_t \leq M x^{bin}_t, \forall t ``
 
@@ -257,7 +260,7 @@ Uses BigM constraint type to allow for parameter.
 * ts_data::Tuple{Vector{String}, Vector{Vector{Float64}}} : timeseries data name (1) and values (2)
 * cons_name::Symbol : name of the constraint
 * var_name::Symbol :  name of the variable
-param_reference::RefParam : RefParam of the parameters
+param_reference::RefParam : RefParam of access the parameters
 * binvar_name::Symbol : name of binary variable
 * M_value::Float64 : bigM
 """
