2f8cfb02da75923390eac5e1a69e9418ebf81161
Jose Daniel Lara <jdlara@berkeley.edu>
2019-08-05 23:33:37 -0600

    add enums for feedback models

--- src/simulation_models/simulation_feedback.jl
+++ src/simulation_models/simulation_feedback.jl
@@ -1,4 +1,9 @@
-function update_stage!(stage::Stage, sim::Simulation)
+@enum FeedbackModel begin
+    SYNCHRONIZE = 1
+    RECEDINGHORIZON = 2
+end
+
+function update_stage!(stage::_Stage, sim::Simulation)
 
     for (k, v) in stage.model.canonical.parameters
         parameter_update!(k, v, stage.key, sim)
