869ec45a0b127b077165bdac3b203e958dbb9200
Jose Daniel Lara <jdlara@berkeley.edu>
2019-08-15 17:24:58 -0700

    comment out recipes

--- src/PowerSimulations.jl
+++ src/PowerSimulations.jl
@@ -90,7 +90,7 @@ import ParameterJuMP
 import LinearAlgebra
 import PowerSystems
 import PowerModels
-using RecipesBase
+import RecipesBase
 
 #TimeStamp Management Imports
 import Dates
@@ -191,7 +191,7 @@ include("routines/write_model.jl")
 include("utils/optimization_debugging.jl")
 include("utils/printing.jl")
 include("utils/plot_results.jl")
-include("utils/plot_recipes.jl")
+#include("utils/plot_recipes.jl")
 
 
 end
