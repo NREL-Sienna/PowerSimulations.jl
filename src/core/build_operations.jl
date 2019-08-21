ca50ed063b45c8a3ff7a50dd7e57142755d7d31b
Jose Daniel Lara <jdlara@berkeley.edu>
2019-08-21 12:37:20 -0700

    update code

--- src/core/build_operations.jl
+++ src/core/build_operations.jl
@@ -12,7 +12,7 @@ function  _build_canonical(::Type{T},
     # Build Injection devices
     for mod in devices
         verbose && @info "Building $(mod[2].device) with $(mod[2].formulation) formulation"
-        _internal_device_constructor!(canonical, mod[2].device, mod[2].formulation, T, sys; kwargs...)
+        _internal_device_constructor!(canonical, mod[2], T, sys; kwargs...)
     end
 
     # Build Network
@@ -22,7 +22,7 @@ function  _build_canonical(::Type{T},
     # Build Branches
     for mod in branches
         verbose && @info "Building $(mod[2].device) with $(mod[2].formulation) formulation"
-        _internal_device_constructor!(canonical, mod[2].device, mod[2].formulation, T, sys; kwargs...)
+        _internal_device_constructor!(canonical, mod[2], T, sys; kwargs...)
     end
 
     #Build Service
