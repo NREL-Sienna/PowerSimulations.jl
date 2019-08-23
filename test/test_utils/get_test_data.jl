c1dde9b9436ea97086b6c956877595e483fb82bf
Jose Daniel Lara <jdlara@berkeley.edu>
2019-08-22 21:20:14 -0700

    update data for UC testing

--- test/test_utils/get_test_data.jl
+++ test/test_utils/get_test_data.jl
@@ -53,26 +53,26 @@ PTDF14_dc = PSY.PTDF(b_ac_14, nodes14);
 # Solitude and Brighton have binding time_dn constraints.
 # Sundance has non-binding Time Down constraint at an Hourly Resolution
 # Alta, Park City and Brighton start at 0.
-thermal_generators5_uc_testing = [ThermalStandard("Alta", true, nodes5[1], 0.40, 0.010,
-           TechThermal(0.5, PowerSystems.ST, PowerSystems.COAL, (min=0.0, max=0.40),  (min = -0.30, max = 0.30), nothing, nothing),
+thermal_generators5_uc_testing = [ThermalStandard("Alta", true, nodes5[1], 0.0, 0.0,
+           TechThermal(0.5, PowerSystems.ST, PowerSystems.COAL, (min=0.2, max=0.40),  (min = -0.30, max = 0.30), nothing, nothing),
            ThreePartCost((0.0, 1400.0), 0.0, 4.0, 2.0)
            ),
-           ThermalStandard("Park City", true, nodes5[1], 1.70, 0.20,
-               TechThermal(2.2125, PowerSystems.ST, PowerSystems.COAL, (min=0.0, max=1.70), (min =-1.275, max=1.275), (up=0.02, down=0.02), nothing),
+           ThermalStandard("Park City", true, nodes5[1], 0.0, 0.0,
+               TechThermal(2.2125, PowerSystems.ST, PowerSystems.COAL, (min=0.65, max=1.70), (min =-1.275, max=1.275), (up=0.02, down=0.02), nothing),
                ThreePartCost((0.0, 1500.0), 0.0, 1.5, 0.75)
            ),
-           ThermalStandard("Solitude", true, nodes5[3], 6.5, 1.00,
-               TechThermal(5.20, PowerSystems.ST, PowerSystems.COAL, (min=0.0, max=5.20), (min =-3.90, max=3.90), (up=0.0012, down=0.0012), (up=5.0, down=3.0)),
+           ThermalStandard("Solitude", true, nodes5[3], 2.7, 0.00,
+               TechThermal(5.20, PowerSystems.ST, PowerSystems.COAL, (min=1.0, max=5.20), (min =-3.90, max=3.90), (up=0.0012, down=0.0012), (up=5.0, down=3.0)),
                ThreePartCost((0.0, 3000.0), 0.0, 3.0, 1.5)
            ),
-           ThermalStandard("Sundance", true, nodes5[4], 2.0, 0.40,
-               TechThermal(2.5, PowerSystems.ST, PowerSystems.COAL, (min=0.0, max=2.0), (min =-1.5, max=1.5), (up=0.015, down=0.015), (up=2.0, down=1.0)),
+           ThermalStandard("Sundance", true, nodes5[4], 0.0, 0.00,
+               TechThermal(2.5, PowerSystems.ST, PowerSystems.COAL, (min=1.0, max=2.0), (min =-1.5, max=1.5), (up=0.015, down=0.015), (up=2.0, down=1.0)),
                ThreePartCost((0.0, 4000.0), 0.0, 4.0, 2.0)
            ),
-           ThermalStandard("Brighton", true, nodes5[5], 6.0, 1.50,
-               TechThermal(7.5, PowerSystems.ST, PowerSystems.COAL, (min=0.0, max=6.0), (min =-4.50, max=4.50), (up=0.0015, down=0.0015), (up=5.0, down=3.0)),
+           ThermalStandard("Brighton", true, nodes5[5], 6.0, 0.0,
+               TechThermal(7.5, PowerSystems.ST, PowerSystems.COAL, (min=3.0, max=6.0), (min =-4.50, max=4.50), (up=0.0015, down=0.0015), (up=5.0, down=3.0)),
                ThreePartCost((0.0, 1000.0), 0.0, 1.5, 0.75)
-           )];                        
+           )];
 c_sys5_uc = PSY.System(nodes5, thermal_generators5_uc_testing, loads5, branches5, nothing, 100.0, nothing, nothing, nothing);
 add_forecasts!(c_sys5_uc, load_forecast_DA)
 
