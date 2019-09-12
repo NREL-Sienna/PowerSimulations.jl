1f5acbd3012374d00feea3398c8200b5d7d6af89
Jose Daniel Lara <jdlara@berkeley.edu>
2019-09-11 22:55:26 -0600

    reduce testing on windows

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,25 +22,29 @@ OSQP_optimizer = JuMP.with_optimizer(OSQP.Optimizer, verbose = false)
 include("test_utils/get_test_data.jl")
 include("test_utils/model_checks.jl")
 
-@testset "Common Functionalities" begin
-    include("test_base_structs.jl")
-    include("test_PowerModels_interface.jl")
-end
+if !Sys.iswindows()
 
-@testset "Device Constructors" begin
-    include("test_thermal_generation_constructors.jl")
-    include("test_renewable_generation_constructors.jl")
-    include("test_load_constructors.jl")
-    include("test_storage_constructors.jl")
-    include("test_hydro_generation_constructors.jl")
-end
+    @testset "Common Functionalities" begin
+        include("test_base_structs.jl")
+        include("test_PowerModels_interface.jl")
+    end
 
-@testset "Network Constructors" begin
-    include("test_network_constructors.jl")
-end
+    @testset "Device Constructors" begin
+        include("test_thermal_generation_constructors.jl")
+        include("test_renewable_generation_constructors.jl")
+        include("test_load_constructors.jl")
+        include("test_storage_constructors.jl")
+        include("test_hydro_generation_constructors.jl")
+    end
 
-@testset "Services Constructors" begin
-    #include("test_services_constructor.jl")
+    @testset "Network Constructors" begin
+        include("test_network_constructors.jl")
+    end
+
+    @testset "Services Constructors" begin
+        #include("test_services_constructor.jl")
+    end
+
 end
 
 @testset "Operation Models" begin
