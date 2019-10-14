63c50489c3be8298fe99c5adad801e5d49f2b890
Jose Daniel Lara <jdlara@berkeley.edu>
2019-10-14 12:57:40 -0700

    refactor PTDF model

--- src/network_models/ptdf_model.jl
+++ src/network_models/ptdf_model.jl
@@ -5,32 +5,53 @@ function ptdf_networkflow(canonical_model::CanonicalModel,
                           PTDF::PSY.PTDF) where {B<:PSY.Branch}
 
     time_steps = model_time_steps(canonical_model)
-    canonical_model.constraints[:network_flow] = JuMPConstraintArray(undef, PTDF.axes[1], time_steps)
-    canonical_model.constraints[:nodal_balance] = JuMPConstraintArray(undef, PTDF.axes[2], time_steps)
+    _add_cons_container!(canonical_model, :network_flow, PTDF.axes[1], time_steps)
+    _add_cons_container!(canonical_model, :nodal_balance, PTDF.axes[2], time_steps)
+    network_flow = con(canonical_model, :network_flow)
+    nodal_balance = con(canonical_model, :nodal_balance)
+    nodal_balance_expressions = canonical_model.expressions[expression]
+
     branch_types = typeof.(branches)
 
-    _remove_undef!(canonical_model.expressions[expression])
+    _remove_undef!(nodal_balance_expressions)
 
-    var_dict = Dict{Type,Symbol}()
+    var_dict = Dict{Type, Symbol}()
     for btype in Set(branch_types)
         var_dict[btype] = Symbol("Fp_$(btype)")
-        typed_branches = IS.FlattenIteratorWrapper(btype, Vector([[b for b in branches if typeof(b) == btype]]))
+        typed_branches = IS.FlattenIteratorWrapper(btype,
+                                           Vector([[b for b in branches if isa(b, btype)]]))
         flow_variables(canonical_model, StandardPTDFModel, typed_branches)
     end
 
     for t in time_steps
         for br in branches
-            flow_expression = sum(PTDF[PSY.get_name(br), PSY.get_number(b)]*canonical_model.expressions[expression].data[PSY.get_number(b), t] for b in buses)
-            canonical_model.constraints[:network_flow][PSY.get_name(br), t] = JuMP.@constraint(canonical_model.JuMPmodel, canonical_model.variables[var_dict[typeof(br)]][PSY.get_name(br), t] == flow_expression)
+            flow_variable = canonical_model.variables[var_dict[typeof(br)]]
+            name = PSY.get_name(br)
+            flow_expression =
+                sum(PTDF[name, PSY.get_number(b)]*nodal_balance_expressions[PSY.get_number(b), t] for b in buses)
+            network_flow[name, t] = JuMP.@constraint(canonical_model.JuMPmodel,
+                                                  flow_variable[name, t] == flow_expression)
         end
 
+# The process is done in two separate loops to avoid modifying the nodal_balance_expressions
+# before making the flow constraints. If this two operations are done in the same loop
+# then the PTDF will multiply an expression that contains the flow variable.
         for br in branches
-            _add_to_expression!(canonical_model.expressions[expression], (PSY.get_arc(br)).from |> PSY.get_number, t, canonical_model.variables[var_dict[typeof(br)]][PSY.get_name(br), t], -1.0)
-            _add_to_expression!(canonical_model.expressions[expression], (PSY.get_arc(br)).to |> PSY.get_number, t, canonical_model.variables[var_dict[typeof(br)]][PSY.get_name(br), t], 1.0)
+            name = PSY.get_name(br)
+            from_number = (PSY.get_arc(br)).from |> PSY.get_number
+            to_number = (PSY.get_arc(br)).to |> PSY.get_number
+            flow_variable = canonical_model.variables[var_dict[typeof(br)]]
+            _add_to_expression!(nodal_balance_expressions,
+                                from_number, t, flow_variable[name, t], -1.0)
+            _add_to_expression!(nodal_balance_expressions,
+                                to_number, t, flow_variable[name, t], 1.0)
+
         end
 
         for b in buses
-            canonical_model.constraints[:nodal_balance][PSY.get_number(b), t] = JuMP.@constraint(canonical_model.JuMPmodel, canonical_model.expressions[expression][PSY.get_number(b), t] == 0)
+            number = PSY.get_number(b)
+            nodal_balance[number, t] = JuMP.@constraint(canonical_model.JuMPmodel,
+                                       nodal_balance_expressions[number, t] == 0)
         end
     end
 
