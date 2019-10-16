26b325f3e1773f2378b2e7e356e8af75803e575d
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2019-10-16 10:39:51 -0600

    Deleted storage code by mistake

--- src/devices/device_models/storage.jl
+++ src/devices/device_models/storage.jl
@@ -93,6 +93,31 @@ function active_power_constraints!(canonical::CanonicalModel,
 
 end
 
+function active_power_constraints(canonical_model::CanonicalModel,
+                                  devices::IS.FlattenIteratorWrapper{St},
+                                  ::Type{BookKeepingwReservation},
+                                  ::Type{S}) where {St<:PSY.Storage,
+                                                                      S<:PM.AbstractPowerFormulation}
+
+    range_data_in = [(PSY.get_name(s), PSY.get_inputactivepowerlimits(s)) for s in devices]
+    range_data_out = [(PSY.get_name(s), PSY.get_outputactivepowerlimits(s)) for s in devices]
+
+    reserve_device_semicontinuousrange(canonical_model,
+                                       range_data_in,
+                                       Symbol("inputpower_range_$(St)"),
+                                       Symbol("Psin_$(St)"),
+                                       Symbol("Rst_$(St)"))
+
+    reserve_device_semicontinuousrange(canonical_model,
+                                       range_data_out,
+                                       Symbol("outputpower_range_$(St)"),
+                                       Symbol("Psout_$(St)"),
+                                       Symbol("Rst_$(St)"))
+
+    return
+
+end
+
 
 """
 This function adds the reactive  power limits of generators when there are CommitmentVariables
