c2d07047ded356aaf06a5de855a45101a4155d20
Jose Daniel Lara <jdlara@berkeley.edu>
2019-10-18 18:42:43 -0700

    fix getting data

--- test/test_utils/get_test_data.jl
+++ test/test_utils/get_test_data.jl
@@ -1,51 +1,94 @@
 time_steps = 1:24
 
-base_dir = string(dirname(dirname(pathof(PowerSystems))));
+base_dir = string(dirname(dirname(pathof(PowerSystems))))
 DATA_DIR = joinpath(base_dir, "data")
-include(joinpath(base_dir, "data/data_5bus_pu.jl"));
+include(joinpath(base_dir, "data/data_5bus_pu.jl"))
 include(joinpath(base_dir, "data/data_14bus_pu.jl"))
 
 #Base Systems
-c_sys5 = PSY.System(nodes5, thermal_generators5, loads5, branches5, nothing, 100.0, nothing, nothing);
-add_forecasts!(c_sys5, load_forecast_DA)
-c_sys14 = PSY.System(nodes14, thermal_generators14, loads14, branches14, nothing, 100.0, nothing, nothing);
-add_forecasts!(c_sys14, forecast_DA14)
-PTDF5 = PSY.PTDF(branches5, nodes5);
-PTDF14 = PSY.PTDF(branches14, nodes14);
+c_sys5 = System(nodes5, thermal_generators5, loads5, branches5, nothing, 100.0, reserve5, nothing)
+for t in 1:2
+    for (ix, l) in enumerate(loads5)
+        add_forecast!(c_sys5, deepcopy(l), Deterministic("maxactivepower", load_forecast_DA[t][ix]))
+    end
+end
+
+c_sys14 = System(nodes14, thermal_generators14, loads14, branches14, nothing, 100.0, nothing, nothing)
+for (ix, l) in enumerate(loads14)
+    add_forecast!(c_sys14, deepcopy(l), Deterministic("maxactivepower", forecast_DA14[ix]))
+end
+
+PTDF5 = PTDF(branches5, nodes5);
+PTDF14 = PTDF(branches14, nodes14);
 
 #System with Renewable Energy
-c_sys5_re = PSY.System(nodes5, vcat(thermal_generators5, renewable_generators5), loads5, branches5, nothing, 100.0, nothing, nothing);
-add_forecasts!(c_sys5_re, ren_forecast_DA)
-add_forecasts!(c_sys5_re, load_forecast_DA)
+c_sys5_re = System(nodes5, vcat(thermal_generators5, renewable_generators5), loads5, branches5, nothing, 100.0, reserve5, nothing)
+for t in 1:2
+    for (ix, l) in enumerate(loads5)
+        add_forecast!(c_sys5_re, deepcopy(l), Deterministic("maxactivepower", load_forecast_DA[t][ix]))
+    end
+    for (ix, r) in enumerate(renewable_generators5)
+        add_forecast!(c_sys5_re, deepcopy(r), Deterministic("rating", ren_forecast_DA[t][ix]))
+    end
+end
 
-c_sys5_re_only = PSY.System(nodes5, renewable_generators5, loads5, branches5, nothing, 100.0, nothing, nothing);
-add_forecasts!(c_sys5_re_only, load_forecast_DA)
-add_forecasts!(c_sys5_re_only, ren_forecast_DA)
+c_sys5_re_only = System(nodes5, renewable_generators5, loads5, branches5, nothing, 100.0, reserve5, nothing)
+for t in 1:2
+    for (ix, r) in enumerate(renewable_generators5)
+        add_forecast!(c_sys5_re_only, deepcopy(r), Deterministic("rating", ren_forecast_DA[t][ix]))
+    end
+end
 
 #System with HydroPower Energy
-c_sys5_hy = PSY.System(nodes5, vcat(thermal_generators5, hydro_generators5[1]), loads5, branches5, nothing, 100.0, nothing, nothing);
-add_forecasts!(c_sys5_hy, [hydro_forecast_DA[1]])
-add_forecasts!(c_sys5_hy, load_forecast_DA)
+c_sys5_hy = System(nodes5, vcat(thermal_generators5, hydro_generators5[1]), loads5, branches5, nothing, 100.0, reserve5, nothing)
+for t in 1:2
+    for (ix, l) in enumerate(loads5)
+        add_forecast!(c_sys5_hy, deepcopy(l), Deterministic("maxactivepower", load_forecast_DA[t][ix]))
+    end
+    for (ix, h) in enumerate([hydro_generators5[1]])
+        add_forecast!(c_sys5_hy, deepcopy(h), Deterministic("rating", hydro_forecast_DA[t][ix]))
+    end
+end
 
 #System with Storage Device
-c_sys5_bat = PSY.System(nodes5, thermal_generators5, loads5, branches5, battery5, 100.0, nothing, nothing);
-add_forecasts!(c_sys5_bat, load_forecast_DA)
+c_sys5_bat = System(nodes5, thermal_generators5, loads5, branches5, battery5, 100.0, reserve5, nothing)
+for t in 1:2
+    for (ix, l) in enumerate(loads5)
+        add_forecast!(c_sys5, deepcopy(l), Deterministic("maxactivepower", load_forecast_DA[t][ix]))
+    end
+end
 
 #System with Interruptible Load
-c_sys5_il = PSY.System(nodes5, thermal_generators5, vcat(loads5, interruptible), branches5, nothing, 100.0, nothing, nothing);
-add_forecasts!(c_sys5_il, load_forecast_DA)
-add_forecasts!(c_sys5_il, Iload_forecast)
+c_sys5_il = System(nodes5, thermal_generators5, vcat(loads5, interruptible), branches5, nothing, 100.0, reserve5, nothing)
+for t in 1:2
+    for (ix, l) in enumerate(loads5)
+        add_forecast!(c_sys5_il, deepcopy(l), Deterministic("maxactivepower", load_forecast_DA[t][ix]))
+    end
+    for (ix, i) in enumerate(interruptible)
+        add_forecast!(c_sys5_il, deepcopy(i), Deterministic("maxactivepower", Iload_forecast[t][ix]))
+    end
+end
 
 #Systems with HVDC data in the branches
-c_sys5_dc = PSY.System(nodes5, vcat(thermal_generators5, renewable_generators5), loads5, branches5_dc, nothing, 100.0, nothing, nothing);
-c_sys14_dc = PSY.System(nodes14, thermal_generators14, loads14, branches14_dc, nothing, 100.0, nothing, nothing);
-add_forecasts!(c_sys5_dc, load_forecast_DA)
-add_forecasts!(c_sys5_dc, ren_forecast_DA)
-add_forecasts!(c_sys14_dc, forecast_DA14)
-b_ac_5 = collect(get_components(PSY.ACBranch, c_sys5_dc))
-PTDF5_dc = PSY.PTDF(b_ac_5, nodes5);
-b_ac_14 = collect(get_components(PSY.ACBranch, c_sys14_dc))
-PTDF14_dc = PSY.PTDF(b_ac_14, nodes14);
+c_sys5_dc = System(nodes5, vcat(thermal_generators5, renewable_generators5), loads5, branches5_dc, nothing, 100.0, reserve5, nothing)
+for t in 1:2
+    for (ix, l) in enumerate(loads5)
+        add_forecast!(c_sys5_dc, deepcopy(l), Deterministic("maxactivepower", load_forecast_DA[t][ix]))
+    end
+    for (ix, r) in enumerate(renewable_generators5)
+        add_forecast!(c_sys5_dc, deepcopy(r), Deterministic("rating", ren_forecast_DA[t][ix]))
+    end
+end
+
+c_sys14_dc = System(nodes14, thermal_generators14, loads14, branches14_dc, nothing, 100.0, reserve5, nothing)
+for (ix, l) in enumerate(loads14)
+    add_forecast!(c_sys14, deepcopy(l), Deterministic("maxactivepower", forecast_DA14[ix]))
+end
+
+b_ac_5 = collect(get_components(ACBranch, c_sys5_dc))
+PTDF5_dc = PTDF(b_ac_5, nodes5);
+b_ac_14 = collect(get_components(ACBranch, c_sys14_dc))
+PTDF14_dc = PTDF(b_ac_14, nodes14);
 
 # System to test UC Forms
 #Park City and Sundance Have non-binding Ramp Limitst at an Hourly Resolution
@@ -73,25 +116,19 @@ thermal_generators5_uc_testing = [ThermalStandard("Alta", true, nodes5[1], 0.0, 
                TechThermal(7.5, PowerSystems.ST, PowerSystems.COAL, (min=3.0, max=6.0), (min =-4.50, max=4.50), (up=0.0015, down=0.0015), (up=5.0, down=3.0)),
                ThreePartCost((0.0, 1000.0), 0.0, 1.5, 0.75)
            )];
-c_sys5_uc = PSY.System(nodes5, thermal_generators5_uc_testing, loads5, branches5, nothing, 100.0, nothing, nothing);
-add_forecasts!(c_sys5_uc, load_forecast_DA)
+c_sys5_uc = System(nodes5, thermal_generators5_uc_testing, loads5, branches5, nothing, 100.0, reserve5, nothing);
 
-#= RTS Data
-RTS_GMLC_DIR = joinpath(DATA_DIR, "RTS_GMLC")
-const DESCRIPTORS = joinpath(RTS_GMLC_DIR, "user_descriptors.yaml")
-
-function create_rts_system(forecast_resolution=Dates.Hour(1))
-    data = PSY.PowerSystemRaw(RTS_GMLC_DIR, 100.0, DESCRIPTORS)
-    return PSY.System(data; forecast_resolution=forecast_resolution)
+for t in 1:2
+    for (ix, l) in enumerate(loads5)
+        add_forecast!(c_sys5_uc, l, Deterministic("maxactivepower", load_forecast_DA[t][ix]))
+    end
 end
-c_rts = create_rts_system();
-=#
 
 function build_init(gens, data)
     init = Vector{InitialCondition}(undef, length(collect(gens)))
     for (ix,g) in enumerate(gens)
         init[ix] = InitialCondition(g,
-                    PSI.UpdateRef{PSY.Device}(Symbol("P_$(typeof(g))")),
+                    PSI.UpdateRef{Device}(Symbol("P_$(typeof(g))")),
                     data[ix],TimeStatusChange)
     end
     return init
