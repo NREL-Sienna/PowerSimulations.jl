b0f9a64712c62358b63df6b0d5fe02545cb358ea
Jose Daniel Lara <jdlara@berkeley.edu>
2019-11-10 16:26:34 -0800

    WIP: Organize Services Construction [CI SKIP]

--- src/services_models/services_constructor.jl
+++ src/services_models/services_constructor.jl
@@ -1,13 +1,10 @@
-function construct_service!(canonical::Canonical,sys::PSY.System,
-                            model::ServiceModel{S, Sr},
-                            ::Type{T};
-                            kwargs...) where {S<:PSY.Service,
-                                              Sr<:AbstractServiceFormulation,
-                                              T<:PM.AbstractPowerModel}
-
+function construct_services!(canonical::Canonical,
+                             sys::PSY.System,
+                             services_template;
+                             kwargs...)
 
     services = PSY.get_components(S, sys)
-    _add_nodal_expressions(canonical,services,sys)
+    _add_nodal_expressions(canonical, services, sys)
     for service in services
         for (type_device,devices) in _get_devices_bytype(PSY.get_contributingdevices(service))
             #Variables
@@ -28,9 +25,9 @@ function _get_devices_bytype(devices::IS.FlattenIteratorWrapper{T}) where {T<:PS
     dict = Dict{DataType,Vector}()
     device_type_pairs = [(typeof(d),d) for d in devices]
     for d in devices
-        push!(dict[typeof(d)],d)
+        push!(dict[typeof(d)], d)
     end
-    return  [(k,v) for (k,v) in dict]
+    return  [(k, v) for (k, v) in dict]
 end
 
 function _add_nodal_expressions!(canonical::Canonical,
@@ -43,56 +40,8 @@ function _add_nodal_expressions!(canonical::Canonical,
     for service in S
         name = Symbol(PSY.get_name(service),"_","balance")
         add_expression(canonical,
-                    name, 
+                    name,
                     _make_container_array(V, parameters, time_steps))
     end
-    return 
+    return
 end
-
-#=
-
-##################
-This code still need to be rewritten for the new infrastructure in PowerSimulations
-##################
-
-
-function get_devices(sys::PSY.System, device::Type{PSY.ThermalGen})
-    return sys.generators.thermal
-end
-function get_devices(sys::PSY.System, device::Type{PSY.RenewableGen})
-    return sys.generators.renewable
-end
-function get_devices(sys::PSY.System, device::Type{PSY.HydroGen})
-    return sys.generators.hydro
-end
-function get_devices(sys::PSY.System, device::Type{PSY.PSY.ElectricLoad})
-    return sys.loads
-end
-
-
-function construct_service!(m::JuMP.AbstractModel, service::PSY.StaticReserve, device_formulation::Type{RampLimitedReserve}, devices::Array{NamedTuple{(:device, :formulation), Tuple{DataType, DataType}}}, sys::PSY.System; kwargs...)
-
-    dev_set = Array{NamedTuple{(:device, :formulation), Tuple{PSY.Device, DataType}}}([])
-
-    for device in devices
-        if device != nothing
-            D = get_devices(sys, device.device)
-            for d in D
-                if d in service.contributingdevices
-                    push!(dev_set, (device=d, formulation=device.formulation))
-                end
-            end
-        end
-    end
-
-    if !isempty(dev_set)
-
-        p_rsv =  reservevariables!(m, dev_set, sys.time_periods)
-        m =  reserves(m, dev_set, service, sys.time_periods)
-
-    end
-
-    return m
-
-end
-=#
