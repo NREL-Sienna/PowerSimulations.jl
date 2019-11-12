a3fbd7ce9aab61551691b7ac0e72df8d72549680
Lily Hanig <lanig12345@gmail.com>
2019-11-12 11:28:27 -0700

    making sure the time_stamp doesn't get double shortened

--- src/core/core_structs/results.jl
+++ src/core/core_structs/results.jl
@@ -84,8 +84,8 @@ function load_operation_results(folder_path::AbstractString)
     variables = Dict{Symbol, DataFrames.DataFrame}()
 
     for name in variable_list
-        variable_name = splitext([name])[1]
-        file_path = joinpath(folder_path, [name])
+        variable_name = splitext(name)[1]
+        file_path = joinpath(folder_path, name)
         variables[Symbol(variable_name)] = Feather.read(file_path) 
         if !isnothing(variables[Symbol(variable_name)][!,:Range])
             delete!(variables[Symbol(variable_name)], :Range)
@@ -123,8 +123,14 @@ function load_operation_results(folder_path::AbstractString, file_type)
     end
     optimizer = JSON.parse(open(joinpath(folder_path, "optimizer_log.json")))
     time_stamp = file_type.read(joinpath(folder_path, "time_stamp.$(lowercase("$file_type"))"))
-    time_stamp = shorten_time_stamp(time_stamp)
+    if size(time_stamp,1) > find_var_length(variables, variable_list)
+        time_stamp = shorten_time_stamp(time_stamp)
+    end
     obj_value = Dict{Symbol, Any}(:OBJECTIVE_FUNCTION => optimizer["obj_value"])
     results = make_results(variables, obj_value, optimizer, time_stamp)
     return results
+end
+# this ensures that the time_stamp is not double shortened
+function find_var_length(variables::Dict, variable_list::Array)
+    return size(variables[Symbol(splitext(variable_list[1])[1])], 1)
 end
