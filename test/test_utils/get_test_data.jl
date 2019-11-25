ee9b6dfc9fcdd55478541ecefd8549cbbf1f4be3
Jose Daniel Lara <jdlara@berkeley.edu>
2019-11-25 15:23:28 -0700

    update reserve data

--- test/test_utils/get_test_data.jl
+++ test/test_utils/get_test_data.jl
@@ -184,10 +184,13 @@ for t in 1:2
     end
 end
 
-#reserve_uc = reserve5(get_components(ThermalStandard, c_sys5_uc))
-#add_component!(c_sys5_uc, reserve_uc[1])
-#add_forecast!(c_sys5_uc, reserve_uc[1], Deterministic("get_requirement", Reserve_ts[1][1]))
-#add_forecast!(c_sys5_uc, reserve_uc[1], Deterministic("get_requirement", Reserve_ts[2][1]))
+reserve_uc = reserve5(get_components(ThermalStandard, c_sys5_uc))
+add_service!(c_sys5_uc, reserve_uc[1], get_components(ThermalStandard, c_sys5_uc))
+add_service!(c_sys5_uc, reserve_uc[2], [collect(get_components(ThermalStandard, c_sys5_uc))[end]])
+for (ix, serv) in enumerate(get_components(StaticReserve, c_sys5_uc))
+    add_forecast!(c_sys5_uc, serv, Deterministic("get_requirement", Reserve_ts[ix][1]))
+    add_forecast!(c_sys5_uc, serv, Deterministic("get_requirement", Reserve_ts[ix][1]))
+end
 
 function build_init(gens, data)
     init = Vector{InitialCondition}(undef, length(collect(gens)))
