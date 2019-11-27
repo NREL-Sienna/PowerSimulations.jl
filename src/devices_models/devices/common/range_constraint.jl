c3e5043add71ebbf59a21f1211020897997fe1e6
Lily Hanig <lanig12345@gmail.com>
2019-11-27 12:42:38 -0700

    changes to reserve_device_semincontinuous, and docstrings

--- src/devices_models/devices/common/range_constraint.jl
+++ src/devices_models/devices/common/range_constraint.jl
@@ -17,23 +17,23 @@ Constructs min/max range constraint from device variable.
 # Constraints
 If min and max within an epsilon width:
 
-``` variable[r[1], t] == r[2].max ```
+``` variable[name, t] == limits.max ```
 
 Otherwise:
 
-``` r[2].min <= variable[r[1], t] <= r[2].max ```
+``` limits.min <= variable[name, t] <= limits.max ```
 
-where r in range_data.
+where limits in range_data.
 
 # LaTeX
 
-`` x = r^{max}, \text{ for } |r^{max} - r^{min}| < \varepsilon ``
+`` x = limits^{max}, \text{ for } |limits^{max} - limits^{min}| < \varepsilon ``
 
-`` r^{min} \leq x \leq r^{max}, \text{ otherwise } ``
+`` limits^{min} \leq x \leq limits^{max}, \text{ otherwise } ``
 
 # Arguments
 * psi_container::PSIContainer : the psi_container model built in PowerSimulations
-* range_data::DeviceRange : contains name of device (1) and its min/max (2)
+* range_data::DeviceRange : contains names and vector of min/max
 * cons_name::Symbol : name of the constraint
 * var_name::Symbol : the name of the continuous variable
 """
@@ -70,7 +70,7 @@ end
 
 @doc raw"""
     device_semicontinuousrange(psi_container::PSIContainer,
-                                    scrange_data::DeviceRange,
+                                    sc_range_data::DeviceRange,
                                     cons_name::Symbol,
                                     var_name::Symbol,
                                     binvar_name::Symbol)
@@ -80,27 +80,27 @@ Constructs min/max range constraint from device variable and on/off decision var
 # Constraints
 If device min = 0:
 
-``` varcts[r[1], t] <= r[2].max*varbin[r[1], t]) ```
+``` varcts[name, t] <= limits.max*varbin[name, t]) ```
 
-``` varcts[r[1], t] >= 0.0 ```
+``` varcts[name, t] >= 0.0 ```
 
 Otherwise:
 
-``` varcts[name, t] <= r[2].max*varbin[name, t] ```
+``` varcts[name, t] <= limits.max*varbin[name, t] ```
 
-``` varcts[name, t] >= r[2].min*varbin[name, t] ```
+``` varcts[name, t] >= limits.min*varbin[name, t] ```
 
-where r in range_data.
+where limits in sc_range_data.
 
 # LaTeX
 
-`` 0 \leq x^{cts} \leq r^{max} x^{bin}, \text{ for } r^{min} = 0 ``
+`` 0 \leq x^{cts} \leq limits^{max} x^{bin}, \text{ for } limits^{min} = 0 ``
 
-`` r^{min} x^{bin} \leq x^{cts} \leq r^{max} x^{bin}, \text{ otherwise } ``
+`` limits^{min} x^{bin} \leq x^{cts} \leq limits^{max} x^{bin}, \text{ otherwise } ``
 
 # Arguments
 * psi_container::PSIContainer : the psi_container model built in PowerSimulations
-* scrange_data::DeviceRange : contains name of device (1) and its min/max (2)
+* sc_range_data::DeviceRange : contains names and vector of min/max
 * cons_name::Symbol : name of the constraint
 * var_name::Symbol : the name of the continuous variable
 * binvar_name::Symbol : the name of the binary variable
@@ -138,7 +138,7 @@ end
 
 @doc raw"""
     reserve_device_semicontinuousrange(psi_container::PSIContainer,
-                                    scrange_data::Vector{NamedMinMax},
+                                    sc_range_data::DeviceRange,
                                     cons_name::Symbol,
                                     var_name::Symbol,
                                     binvar_name::Symbol)
@@ -148,67 +148,59 @@ Constructs min/max range constraint from device variable and on/off decision var
 # Constraints
 If device min = 0:
 
-``` varcts[r[1], t] <= r[2].max*(1-varbin[r[1], t]) ```
+``` varcts[name, t] <= limits.max*(1-varbin[name, t]) ```
 
-``` varcts[r[1], t] >= 0.0 ```
+``` varcts[name, t] >= 0.0 ```
 
 Otherwise:
 
-``` varcts[r[1], t] <= r[2].max*(1-varbin[r[1], t]) ```
+``` varcts[name, t] <= limits.max*(1-varbin[name, t]) ```
 
-``` varcts[r[1], t] >= r[2].min*(1-varbin[r[1], t]) ```
+``` varcts[name, t] >= limits.min*(1-varbin[name, t]) ```
 
-where r in range_data.
+where limits in range_data.
 
 # LaTeX
 
-`` 0 \leq x^{cts} \leq r^{max} (1 - x^{bin} ), \text{ for } r^{min} = 0 ``
+`` 0 \leq x^{cts} \leq limits^{max} (1 - x^{bin} ), \text{ for } limits^{min} = 0 ``
 
-`` r^{min} (1 - x^{bin} ) \leq x^{cts} \leq r^{max} (1 - x^{bin} ), \text{ otherwise } ``
+`` limits^{min} (1 - x^{bin} ) \leq x^{cts} \leq limits^{max} (1 - x^{bin} ), \text{ otherwise } ``
 
 # Arguments
 * psi_container::PSIContainer : the psi_container model built in PowerSimulations
-* scrange_data::Vector{NamedMinMax} : contains name of device (1) and its min/max (2)
+* sc_range_data::DeviceRange : contains names and vector of min/max
 * cons_name::Symbol : name of the constraint
 * var_name::Symbol : the name of the continuous variable
 * binvar_name::Symbol : the name of the binary variable
 """
 function reserve_device_semicontinuousrange(psi_container::PSIContainer,
-                                            scrange_data::Vector{NamedMinMax},
+                                            sc_range_data::DeviceRange,
                                             cons_name::Symbol,
                                             var_name::Symbol,
                                             binvar_name::Symbol)
 
     time_steps = model_time_steps(psi_container)
-    ub_name = _middle_rename(cons_name, "_", "ub")
-    lb_name = _middle_rename(cons_name, "_", "lb")
-
     varcts = get_variable(psi_container, var_name)
     varbin = get_variable(psi_container, binvar_name)
 
-    # MOI has a semicontinous set, but after some tests is not clear most MILP solvers support it.
-    # In the future this can be updated
+    ub_name = _middle_rename(cons_name, "_", "ub")
+    lb_name = _middle_rename(cons_name, "_", "lb")
+    #MOI has a semicontinous set, but after some tests is not clear most MILP solvers support it.
+    #In the future this can be updated
+    con_ub = add_cons_container!(psi_container, ub_name, sc_range_data.names, time_steps)
+    con_lb = add_cons_container!(psi_container, lb_name, sc_range_data.names, time_steps)
 
-    set_name = (r[1] for r in scrange_data)
-    con_ub = add_cons_container!(psi_container, ub_name, set_name, time_steps)
-    con_lb = add_cons_container!(psi_container, lb_name, set_name, time_steps)
-
-    for t in time_steps, r in scrange_data
-
-            if r[2].min == 0.0
-
-                con_ub[r[1], t] = JuMP.@constraint(psi_container.JuMPmodel, varcts[r[1], t] <= r[2].max*(1-varbin[r[1], t]))
-                con_lb[r[1], t] = JuMP.@constraint(psi_container.JuMPmodel, varcts[r[1], t] >= 0.0)
-
-            else
-
-                con_ub[r[1], t] = JuMP.@constraint(psi_container.JuMPmodel, varcts[r[1], t] <= r[2].max*(1-varbin[r[1], t]))
-                con_lb[r[1], t] = JuMP.@constraint(psi_container.JuMPmodel, varcts[r[1], t] >= r[2].min*(1-varbin[r[1], t]))
-
+    for (ix, name) in enumerate(sc_range_data.names)
+        limits = sc_range_data.values[ix]
+        for t in time_steps
+            if JuMP.has_lower_bound(varcts[name, t])
+                JuMP.set_lower_bound(varcts[name, t], 0.0)
             end
-
+            expression_ub = varcts[name, t]
+            expression_lb = varcts[name, t]
+            con_ub[name, t] = JuMP.@constraint(psi_container.JuMPmodel, expression_ub <= limits.max*(1-varbin[name, t]))
+            con_lb[name, t] = JuMP.@constraint(psi_container.JuMPmodel, expression_lb >= limits.min*(1-varbin[name, t]))
+        end
     end
-
     return
-
  end
