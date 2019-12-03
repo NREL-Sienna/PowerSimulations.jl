c6eac0485286d1a8e59614b1e2ed6f5519691af5
Jose Daniel Lara <jdlara@berkeley.edu>
2019-12-03 14:49:15 -0700

    add tests

---
+++ test/test_simulation sequence.jl
@@ -0,0 +1,33 @@
+@testset "Make SimulationSequence" begin
+devices = Dict{Symbol, DeviceModel}(:Generators => DeviceModel( ThermalStandard,  ThermalBasicUnitCommitment),
+                                    :DRen => DeviceModel( RenewableDispatch,  RenewableFixed),
+                                    :FRen => DeviceModel( RenewableFix,  RenewableFixed),
+                                    :Loads =>  DeviceModel( InterruptibleLoad,  StaticPowerLoad),
+                                    )
+
+model_ref_uc= OperationsProblemTemplate(CopperPlatePowerModel, devices, branches, services)
+
+devices = Dict{Symbol, DeviceModel}(:Generators => DeviceModel( ThermalStandard,  ThermalDispatchNoMin),
+                                    :DRen => DeviceModel( RenewableDispatch,  RenewableFullDispatch),
+                                    :FRen => DeviceModel( RenewableFix,  RenewableFixed),
+                                    :Loads =>  DeviceModel( InterruptibleLoad,  DispatchablePowerLoad),
+                                    )
+model_ref_ed= OperationsProblemTemplate(CopperPlatePowerModel, devices, branches, services)
+
+stages_definition = Dict("UC" => Stage(GenericOpProblem, model_ref_uc, c_sys5_uc, GLPK_optimizer),
+                         "ED" => Stage(GenericOpProblem, model_ref_ed, c_sys5_ed, GLPK_optimizer))
+
+sequence = SimulationSequence(stage_order = Dict(1 => "UC", 2 => "ED")
+                   feed_forward_chronologies = Dict(("UC"=>"ED") => Synchronize(from_steps = 24, to_executions = 1)),
+                   horizons = Dict("UC" => 48, "ED" => 12),
+                   intervals = Dict("UC" => Hour(24), "ED" => Hour(1)),
+                   feedforward = Dict((:Devices, "ED") => SemiContinuousFF(:Generators, binary_from_stage = :ON, affected_variables = [:P])),
+                   cache = Dict("ED" => [TimeStatusChange(:ON_ThermalStandard)]),
+                   ini_cond_chronology = Dict("UC" => Consecutive(), "ED" => Consecutive())
+                   )
+
+    for field in fieldnames(SimulationSequence)
+          @test !isempty(getfield(sequence, field))
+    end
+
+end
