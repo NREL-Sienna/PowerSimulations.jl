2afcf2bf4386053125295eb545c9b66089bff477
Jose Daniel Lara <jdlara@berkeley.edu>
2019-12-03 13:59:32 -0700

    update chronology objects

--- src/core/core_structs/chronology.jl
+++ src/core/core_structs/chronology.jl
@@ -1,43 +1,18 @@
 abstract type AbstractChronology end
 
+struct Consecutive <: AbstractChronology end
+
 struct Synchronize <: AbstractChronology
-    from_horizon::Int64 #number of time periods to grab data from
-    to_steps::Int64 #number of times to run using the same data
+    from_steps::Int64    #number of time periods to grab data from
+    to_executions::Int64 #number of times to run using the same data
+    function Synchronize(;from_steps, to_executions)
+        new(from_steps, to_executions)
+    end
 end
 
 struct RecedingHorizon <: AbstractChronology
     step::Int64
-end
-
-RecedingHorizon() = RecedingHorizon(1)
-
-struct Consecutive <: AbstractChronology end
-
-mutable struct SimulationSequence
-    initial_time::Dates.DateTime
-    horizons::Dict{Int64, Int64}
-    intervals::Dict{Int64, Dates.Period}
-    chronologies::Dict{Pair{Int64,Int64}, <:AbstractChronology}
-    feedforward::Dict{Int64, Any}
-    ini_cond_chronology::Union{Dict{Int64, <:AbstractChronology}, Nothing}
-    cache::Dict{Int64, Vector{<:AbstractCache}}
-
-    function SimulationSequence(;initial_time::Dates.DateTime,
-                                 horizons::Dict{Int64, Int64},
-                                 intervals::Dict{Int64, Dates.Period},
-                                 chronologies::Dict{Pair{Int64,Int64}, <:AbstractChronology},
-                                 feedforward::Dict{Int64, Any},
-                                 ini_cond_chronology::Union{Dict{Int64, <:AbstractChronology}, Nothing}
-                                 cache::Dict{Int64, Vector{<:AbstractCache}})
-        new(
-            initial_time,
-            horizons,
-            intervals,
-            chronologies,
-            feedforward,
-            ini_cond_chronology,
-            cache
-        )
-
+    function RecedingHorizon(;from_step::Int64=1)
+        new(step)
     end
 end
