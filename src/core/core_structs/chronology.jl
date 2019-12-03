2a332cbb7562ae1c34237be07d1ffde5f320e106
Jose Daniel Lara <jdlara@berkeley.edu>
2019-12-02 20:04:03 -0700

    WIP: Add Simulaion Sequence

--- src/core/core_structs/chronology.jl
+++ src/core/core_structs/chronology.jl
@@ -12,3 +12,13 @@ end
 RecedingHorizon() = RecedingHorizon(1)
 
 struct Consecutive <: AbstractChronology end
+
+mutable struct SimulationSequence
+    initial_time::Dates.DateTime
+    horizons::Dict{Int64, Int64}
+    intervals::Dict{Int64, Dates.Period}
+    chronologies::Dict{Pair{Int64,Int64}, <:AbstractChronology}
+    feedforward::Dict{Int64, Any}
+    ini_cond_chronology::Union{Dict{Int64, <:AbstractChronology}, Nothing}
+    cache::Dict{Int64, Vector{<:AbstractCache}}
+end
