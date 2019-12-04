29efc349612d3b524f371f5307b4a6a297a1ff74
Jose Daniel Lara <jdlara@berkeley.edu>
2019-12-03 19:58:08 -0700

    update chronology validation

--- src/core/chronology.jl
+++ src/core/chronology.jl
@@ -14,3 +14,31 @@ struct RecedingHorizon <: AbstractChronology
         new(step)
     end
 end
+
+function validate_chronology(synch::Synchronize,
+                                 horizons::Dict{String, Dict{String, Int64}})
+    from_stage_horizon = horizons[stages.first]
+    to_stage_synch = synch.to_executions
+    from_stage_synch = synch.from_steps
+
+    if from_stage_synch > from_stage_horizon
+        error("The lookahead length $(from_stage_horizon) in stage is insufficient to synchronize with $(from_stage_synch) feed_forward steps")
+    end
+
+    if (from_stage_horizon % from_stage_synch) != 0
+        error("The number of feed_forward steps $(from_stage_horizon) in stage
+               needs to be a mutiple of the horizon length $(from_stage_horizon)
+               of stage to use Synchronize with parameters ($(from_stage_synch), $(to_stage_synch))")
+    end
+
+    return
+end
+
+validate_chronology(sync::Consecutive, horizons::Dict{String, Dict{String, Int64}}) = nothing
+
+validate_chronology(sync::RecedingHorizon, horizons::Dict{String, Dict{String, Int64}}) = nothing
+
+function validate_chronology(::T, horizons::Dict{String, Dict{String, Int64}}) where T <: AbstractChronology
+    error("Feedforward Model $(T) not implemented")
+    return
+end
