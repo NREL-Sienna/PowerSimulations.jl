23c46403239ae8174348defbe6dad05d743ac12c
Barrows <clayton.barrows@nrel.gov>
2019-12-14 21:20:52 -0700

    adding support for timeseries constraints with paramaters, bin, and bigM

--- src/devices_models/devices/common/timeseries_constraint.jl
+++ src/devices_models/devices/common/timeseries_constraint.jl
@@ -22,17 +22,17 @@ Constructs upper bound for given variable and time series data and a multiplier.
 """
 function device_timeseries_ub(psi_container::PSIContainer,
                               ts_data::Vector{Tuple{String, Int64, Float64, Vector{Float64}}},
-                              range_data::DeviceRange,
+                              range_data::DeviceRange, #TODO: there is some repititve info in range_data and ts_data, and consistend order/size are assumed, we should combine into a single argument
                               cons_name::Symbol,
                               var_name::Symbol)
     time_steps = model_time_steps(psi_container)
     names = (v[1] for v in ts_data)
     variable = get_variable(psi_container, var_name)
     ub_name = _middle_rename(cons_name, "_", "ub")
-    lb_name = _middle_rename(cons_name, "_", "lb")
     con_ub = add_cons_container!(psi_container, ub_name, names, time_steps)
     add_lower_bound = !all(isempty.(range_data.additional_terms_lb))
     if add_lower_bound
+        lb_name = _middle_rename(cons_name, "_", "lb")
         con_lb = add_cons_container!(psi_container, lb_name, names, time_steps)
     end
 
@@ -94,13 +94,15 @@ function device_timeseries_lb(psi_container::PSIContainer,
                               cons_name::Symbol,
                               var_name::Symbol)
     time_steps = model_time_steps(psi_container)
-    names = (v[1] for v in ts_data)
-    constraint = add_cons_container!(psi_container, cons_name, names, time_steps)
     variable = get_variable(psi_container, var_name)
+    lb_name = _middle_rename(cons_name, "_", "lb")
+    names = (v[1] for v in ts_data)
+    constraint = add_cons_container!(psi_container, lb_name, names, time_steps)
 
     for (ix, name) in enumerate(range_data.names)
-        for t in time_steps, data in ts_data
-            name = data[1]
+        data = ts_data[ix]
+        for t in time_steps
+            @assert name == data[1] # ensures that ts_data and range_data have same order
             forecast = data[4][t]
             multiplier = data[3]
             expression_lb = JuMP.AffExpr(0.0, variable[name, t] => 1.0)
@@ -143,25 +145,45 @@ Constructs upper bound for given variable using a parameter. The constraint is
 """
 function device_timeseries_param_ub(psi_container::PSIContainer,
                                     ts_data::Vector{Tuple{String, Int64, Float64, Vector{Float64}}},
+                                    range_data::DeviceRange, #TODO: there is some repititve info in range_data and ts_data, and consistend order/size are assumed, we should combine into a single argument
                                     cons_name::Symbol,
                                     param_reference::UpdateRef,
                                     var_name::Symbol)
     time_steps = model_time_steps(psi_container)
-    ub_name = _middle_rename(cons_name, "_", "ub")
-    variable = get_variable(psi_container, var_name)
     names = (v[1] for v in ts_data)
-    constraint = add_cons_container!(psi_container, ub_name, names, time_steps)
+    variable = get_variable(psi_container, var_name)
+    ub_name = _middle_rename(cons_name, "_", "ub")
+    con_ub = add_cons_container!(psi_container, ub_name, names, time_steps)
     param = _add_param_container!(psi_container, param_reference, names, time_steps)
+    add_lower_bound = !all(isempty.(range_data.additional_terms_lb))
+    if add_lower_bound
+        lb_name = _middle_rename(cons_name, "_", "lb")
+        con_lb = add_cons_container!(psi_container, lb_name, names, time_steps)
+    end
 
     for (ix, name) in enumerate(range_data.names)
-        limits = range_data.values[ix]
-        for t in time_steps, data in ts_data
-            name = data[1]
+        data = ts_data[ix]
+        for t in time_steps
+            @assert name == data[1] # ensures that ts_data and range_data have same order
             forecast = data[4][t]
             multiplier = data[3]
+            expression_ub = JuMP.AffExpr(0.0, variable[name, t] => 1.0)
+            for val in range_data.additional_terms_ub[ix]
+                JuMP.add_to_expression!(expression_ub, 
+                                        get_variable(psi_container, val)[name, t])
+            end
             param[name, t] = PJ.add_parameter(psi_container.JuMPmodel, forecast)
-            constraint[name, t] = JuMP.@constraint(psi_container.JuMPmodel,
-                                            variable[name, t] <= multiplier*param[name, t])
+            con_ub[name, t] = JuMP.@constraint(psi_container.JuMPmodel,
+                                               expression_ub <= multiplier*param[name, t])
+            if add_lower_bound
+                expression_lb = JuMP.AffExpr(0.0, variable[name, t] => 1.0)
+                for val in range_data.additional_terms_lb[ix]
+                    JuMP.add_to_expression!(expression_lb, 
+                                            get_variable(psi_container, val)[name, t], -1.0)
+                end
+                con_lb[name, t] = JuMP.@constraint(psi_container.JuMPmodel,
+                                                expression_lb >= 0.0)
+            end
         end
     end
 
@@ -195,6 +217,7 @@ Constructs lower bound for given variable using a parameter. The constraint is
 """
 function device_timeseries_param_lb(psi_container::PSIContainer,
                                     ts_data::Vector{Tuple{String, Int64, Float64, Vector{Float64}}},
+                                    range_data::DeviceRange, #TODO: there is some repititve info in range_data and ts_data, and consistend order/size are assumed, we should combine into a single argument
                                     cons_name::Symbol,
                                     param_reference::UpdateRef,
                                     var_name::Symbol)
@@ -205,12 +228,21 @@ function device_timeseries_param_lb(psi_container::PSIContainer,
     constraint =add_cons_container!(psi_container, lb_name, names, time_steps)
     param =_add_param_container!(psi_container, param_reference, names, time_steps)
 
-    for t in time_steps, data in ts_data
-        name = data[1]
-        forecast = data[4][t]
-        multiplier = data[3]
-        param[name, t] = PJ.add_parameter(psi_container.JuMPmodel, forecast)
-        constraint[name, t] = JuMP.@constraint(psi_container.JuMPmodel, multiplier*param[name, t] <= variable[name, t])
+    for (ix, name) in enumerate(range_data.names)
+        data = ts_data[ix]
+        for t in time_steps
+            @assert name == data[1] # ensures that ts_data and range_data have same order
+            forecast = data[4][t]
+            multiplier = data[3]
+            expression_lb = JuMP.AffExpr(0.0, variable[name, t] => 1.0)
+            for val in range_data.additional_terms_lb[ix]
+                JuMP.add_to_expression!(expression_lb, 
+                                        get_variable(psi_container, val)[name, t], -1.0)
+            end
+            param[name, t] = PJ.add_parameter(psi_container.JuMPmodel, forecast)
+            constraint[name, t] = JuMP.@constraint(psi_container.JuMPmodel,
+                                                   expression_lb >= multiplier*aram[name, t])
+        end
     end
 
     return
@@ -246,6 +278,7 @@ where (ix, name) in enumerate(ts_data[1]).
 """
 function device_timeseries_ub_bin(psi_container::PSIContainer,
                                     ts_data::Vector{Tuple{String, Int64, Float64, Vector{Float64}}},
+                                    range_data::DeviceRange, #TODO: there is some repititve info in range_data and ts_data, and consistend order/size are assumed, we should combine into a single argument
                                     cons_name::Symbol,
                                     var_name::Symbol,
                                     binvar_name::Symbol)
@@ -259,11 +292,20 @@ function device_timeseries_ub_bin(psi_container::PSIContainer,
     names = (v[1] for v in ts_data)
     con_ub =add_cons_container!(psi_container, ub_name, names, time_steps)
 
-    for t in time_steps, data in ts_data
-        name = data[1]
-        forecast = data[4][t]
-        multiplier = data[3]
-        con_ub[name, t] = JuMP.@constraint(psi_container.JuMPmodel, varcts[name, t] <= varbin[name, t]*multiplier*forecast)
+    for (ix, name) in enumerate(range_data.names)
+        data = ts_data[ix]
+        for t in time_steps
+            @assert name == data[1] # ensures that ts_data and range_data have same order
+            forecast = data[4][t]
+            multiplier = data[3]
+            expression_ub = JuMP.AffExpr(0.0, variable[name, t] => 1.0)
+            for val in range_data.additional_terms_ub[ix]
+                JuMP.add_to_expression!(expression_ub, 
+                                        get_variable(psi_container, val)[name, t])
+            end
+            con_ub[name, t] = JuMP.@constraint(psi_container.JuMPmodel, 
+                                               expression_ub <= varbin[name, t]*multiplier*forecast)
+        end
     end
 
     return
@@ -305,6 +347,7 @@ param_reference::UpdateRef : UpdateRef of access the parameters
 """
 function device_timeseries_ub_bigM(psi_container::PSIContainer,
                                     ts_data::Vector{Tuple{String, Int64, Float64, Vector{Float64}}},
+                                    range_data::DeviceRange, #TODO: there is some repititve info in range_data and ts_data, and consistend order/size are assumed, we should combine into a single argument
                                     cons_name::Symbol,
                                     var_name::Symbol,
                                     param_reference::UpdateRef,
@@ -321,13 +364,22 @@ function device_timeseries_ub_bigM(psi_container::PSIContainer,
     con_status =add_cons_container!(psi_container, key_status, names, time_steps)
     param =_add_param_container!(psi_container, param_reference, names, time_steps)
 
-    for t in time_steps, data in ts_data
-        name = data[1]
-        forecast = data[4][t]
-        multiplier = data[3]
-        param[name, t] = PJ.add_parameter(psi_container.JuMPmodel, forecast)
-        con_ub[name, t] = JuMP.@constraint(psi_container.JuMPmodel, varcts[name, t] - param[name, t]*multiplier <= (1 - varbin[name, t])*M_value)
-        con_status[name, t] =  JuMP.@constraint(psi_container.JuMPmodel, varcts[name, t] <= varbin[name, t]*M_value)
+    for (ix, name) in enumerate(range_data.names)
+        data = ts_data[ix]
+        for t in time_steps
+            @assert name == data[1] # ensures that ts_data and range_data have same order
+            forecast = data[4][t]
+            multiplier = data[3]
+            expression_ub = JuMP.AffExpr(0.0, variable[name, t] => 1.0)
+            for val in range_data.additional_terms_ub[ix]
+                JuMP.add_to_expression!(expression_ub, 
+                                        get_variable(psi_container, val)[name, t])
+            end
+            param[name, t] = PJ.add_parameter(psi_container.JuMPmodel, forecast)
+            con_ub[name, t] = JuMP.@constraint(psi_container.JuMPmodel, 
+                expression_ub - param[name, t]*multiplier <= (1 - varbin[name, t])*M_value)
+            con_status[name, t] =  JuMP.@constraint(psi_container.JuMPmodel, 
+                expression_ub <= varbin[name, t]*M_value)
     end
 
     return
