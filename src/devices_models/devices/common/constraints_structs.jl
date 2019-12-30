6cba745124656083d63fbc5074cba4f52494e256
Barrows <clayton.barrows@nrel.gov>
2019-12-30 13:15:48 -0700

    refactoring constraint structs

--- src/devices_models/devices/common/constraints_structs.jl
+++ src/devices_models/devices/common/constraints_structs.jl
@@ -1,12 +1,19 @@
 """ Data Container to construct range constraints"""
 struct DeviceRange
+    name::String
     limits::MinMax
     additional_terms_ub::Vector{Symbol}
     additional_terms_lb::Vector{Symbol}
 end
 
+function DeviceRange(name::String, limits::MinMax)
+    return DeviceRange(name, limits, Vector{Symbol}(), Vector{Symbol}())
+end
+
 struct DeviceTimeSeries
+    name::String
     bus_number::Int64
     multiplier::Float64
     timeseries::Vector{Float64}
+    range::Union{Nothing,DeviceRange}
 end
