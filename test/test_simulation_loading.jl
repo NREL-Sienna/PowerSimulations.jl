8d3c14aa5eec2f14a1cc3ce58e229ec0765a6ae4
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2019-12-30 18:07:17 -0700

    Updating test for result aggregation

--- test/test_simulation_loading.jl
+++ test/test_simulation_loading.jl
@@ -5,33 +5,48 @@ else
 end
 
 function test_load_simulation()
-    stages = Dict(1 => Stage(template_uc, 24, Hour(24), 1, c_sys5_uc, GLPK_optimizer,  Dict(0 => Consecutive())),
-                2 => Stage(template_ed, 12, Minute(5), 24, c_sys5_ed, GLPK_optimizer, Dict(1 => Synchronize(24,1), 0 => Consecutive()), TimeStatusChange(:ON_ThermalStandard)))
+    stages_definition = Dict("UC" => Stage(GenericOpProblem, template_uc, c_sys5_uc, GLPK_optimizer),
+                        "ED" => Stage(GenericOpProblem, template_ed, c_sys5_ed, GLPK_optimizer))
 
-    sim = Simulation("test", 2, stages, file_path; verbose = true)
+    sequence = SimulationSequence(order = Dict(1 => "UC", 2 => "ED"),
+                   intra_stage_chronologies = Dict(("UC"=>"ED") => Synchronize(from_steps = 24, to_executions = 1)),
+                   horizons = Dict("UC" => 24, "ED" => 12),
+                   intervals = Dict("UC" => Hour(24), "ED" => Hour(1)),
+                   feed_forward = Dict(("ED", :devices, :Generators) => SemiContinuousFF(binary_from_stage = :ON, affected_variables = [:P])),
+                   cache = Dict("ED" => [TimeStatusChange(:ON_ThermalStandard)]),
+                   ini_cond_chronology = Dict("UC" => Consecutive(), "ED" => Consecutive())
+                   )
+    sim = Simulation(name = "aggregation",
+                 steps = 1, step_resolution =Hour(24),
+                 stages = stages_definition,
+                 stages_sequence = sequence,
+                 simulation_folder= file_path,
+                 verbose = true)
+    build!(sim)
     sim_results = execute!(sim)
-    stage = [1, 2]
+    stage_names = keys(sim.stages)
     step = ["step-1", "step-2"]
+
     @testset "testing reading and writing to the results folder" begin
-        for ix in stage
+        for name in stage_names
             files = collect(readdir(sim_results.results_folder))
             for f in files
                 rm("$(sim_results.results_folder)/$f")
             end
-        res = load_simulation_results(sim_results, ix; write = true)
+        res = load_simulation_results(sim_results, name; write = true)
         loaded_res = load_operation_results(sim_results.results_folder)
         @test loaded_res.variables == res.variables
         end
     end
 
     @testset "testing file names" begin
-        for ix in stage
+        for name in stage_names
             files = collect(readdir(sim_results.results_folder))
             for f in files
                 rm("$(sim_results.results_folder)/$f")
             end
-            res = load_simulation_results(sim_results, ix; write = true)
-            variable_list = String.(collect(keys(sim.stages[ix].psi_container.variables)))
+            res = load_simulation_results(sim_results, name; write = true)
+            variable_list = String.(PSI.get_variable_names(sim, name))
             variable_list = [variable_list; "optimizer_log"; "time_stamp"; "check"]
             file_list = collect(readdir(sim_results.results_folder))
             for name in file_list
@@ -42,51 +57,113 @@ function test_load_simulation()
     end
 
     @testset "testing argument errors" begin
-        for ix in stage
+        for name in stage_names
             files = collect(readdir(sim_results.results_folder))
             for f in files
                 rm("$(sim_results.results_folder)/$f")
             end
-            res = load_simulation_results(sim_results, ix)
-            @test_throws ArgumentError write_results(res, "nothing", "results")
+            res = load_simulation_results(sim_results, name)
+            @test_throws IS.ConflictingInputsError write_results(res, "nothing", "results")
         end
     end
     @testset "testing load simulation results between the two methods of load simulation" begin
-        for ix in stage
-            variable = (collect(keys(sim.stages[ix].psi_container.variables)))
-            results = load_simulation_results(sim_results, ix)
-            res = load_simulation_results(sim_results, ix, step, variable)
+        for name in stage_names
+            variable = PSI.get_variable_names(sim, name)
+            results = load_simulation_results(sim_results, name)
+            res = load_simulation_results(sim_results, name, step, variable)
             @test results.variables == res.variables
         end
     end
+    
+    @testset "Testing to verify raw output results correctly match aggregated for 
+    Receding Horizon" begin
+        stages_definition = Dict("UC" => Stage(GenericOpProblem, template_uc, c_sys5_uc, GLPK_optimizer),
+                               "ED" => Stage(GenericOpProblem, template_ed, c_sys5_ed, GLPK_optimizer))
+
+        sequence = SimulationSequence(order = Dict(1 => "UC", 2 => "ED"),
+                   intra_stage_chronologies = Dict(("UC"=>"ED") => Synchronize(from_steps = 1, to_executions = 1)),
+                   horizons = Dict("UC" => 24, "ED" => 12),
+                   intervals = Dict("UC" => Hour(1), "ED" => Minute(5)),
+                   feed_forward = Dict(("ED", :devices, :Generators) => SemiContinuousFF(binary_from_stage = :ON, affected_variables = [:P])),
+                   cache = Dict("ED" => [TimeStatusChange(:ON_ThermalStandard)]),
+                   ini_cond_chronology = Dict("UC" => RecedingHorizon(), "ED" => RecedingHorizon())
+                   )
+
+        sim = Simulation(name = "receding_results",
+                 steps = 2, step_resolution =Hour(1),
+                 stages = stages_definition,
+                 stages_sequence = sequence,
+                 simulation_folder= file_path,
+                 verbose = true)
+        build!(sim)
+        sim_results = execute!(sim)
+        stages = ["UC", "ED"]
+        for stage in stages
+            results = load_simulation_results(sim_results, stage)
+            vars_names = [:P_ThermalStandard]
+            for name in vars_names
+                results.variables[name]
+                variable_ref = sim_results.ref["stage-$stage"][name]
+                vars = results.variables[name]
+                for step in sim.steps
+                    output = vars[step, :]
+                    raw_result = first(Feather.read(variable_ref[step, 3]))
+                    @test isapprox(output, raw_result, atol = 1e-4)
+                end
+            end
+        end
+    end
+    
     @testset "negative test checking total sums" begin
-        for ix in stage
+        stages_definition = Dict("UC" => Stage(GenericOpProblem, template_uc, c_sys5_uc, GLPK_optimizer),
+                        "ED" => Stage(GenericOpProblem, template_ed, c_sys5_ed, GLPK_optimizer))
+
+        sequence = SimulationSequence(order = Dict(1 => "UC", 2 => "ED"),
+                   intra_stage_chronologies = Dict(("UC"=>"ED") => Synchronize(from_steps = 1, to_executions = 1)),
+                   horizons = Dict("UC" => 24, "ED" => 12),
+                   intervals = Dict("UC" => Hour(24), "ED" => Hour(1)),
+                   feed_forward = Dict(("ED", :devices, :Generators) => SemiContinuousFF(binary_from_stage = :ON, affected_variables = [:P])),
+                   cache = Dict("ED" => [TimeStatusChange(:ON_ThermalStandard)]),
+                   ini_cond_chronology = Dict("UC" => Consecutive(), "ED" => Consecutive())
+                   )
+
+        sim = Simulation(name = "aggregation",
+                 steps = 2, step_resolution = Hour(24),
+                 stages = stages_definition,
+                 stages_sequence = sequence,
+                 simulation_folder= file_path,
+                 verbose = true)
+        build!(sim)
+        sim_results = execute!(sim)
+        stage_names = keys(sim.stages)
+        for name in stage_names
             files = collect(readdir(sim_results.results_folder))
             for f in files
                 rm("$(sim_results.results_folder)/$f")
             end
-            variable_list = collect(keys(sim.stages[ix].psi_container.variables))
-            res = load_simulation_results(sim_results, ix; write = true)
+            variable_list = PSI.get_variable_names(sim, name)
+            res = load_simulation_results(sim_results, name; write = true)
             file_path = joinpath(sim_results.results_folder,"$(variable_list[1]).feather")
             rm(file_path)
             fake_df = DataFrames.DataFrame(:A => Array(1:10))
             Feather.write(file_path, fake_df)
-            @test_throws IS.DataFormatError check_file_integrity(dirname(file_path))
+               @test_logs((:error, r"hash mismatch"), match_mode=:any,
+                    @test_throws(IS.HashMismatchError, check_file_integrity(dirname(file_path)))
+                )
         end
-        for ix in stage
-            files = collect(readdir(sim_results.results_folder))
-            for f in files
-                rm("$(sim_results.results_folder)/$f")
-            end
-            variable_list = collect(keys(sim.stages[ix].psi_container.variables))
-            check_file_path = sim_results.ref["stage-$ix"][(variable_list[1])][1,3]
+        for name in stage_names
+            variable_list = PSI.get_variable_names(sim, name)
+            check_file_path = PSI.get_reference(sim_results, name, 1, variable_list[1])[1]
             rm(check_file_path)
-            time_length = sim_results.chronologies["stage-$ix"]
+            time_length = sim_results.chronologies["stage-$name"]
             fake_df = DataFrames.DataFrame(:A => Array(1:time_length))
             Feather.write(check_file_path, fake_df)
-            @test_throws IS.DataFormatError check_file_integrity(dirname(check_file_path))
+                @test_logs((:error, r"hash mismatch"), match_mode=:any,
+                    @test_throws(IS.HashMismatchError, check_file_integrity(dirname(check_file_path)))
+                )
         end
     end
+
 end
 try
     test_load_simulation()
