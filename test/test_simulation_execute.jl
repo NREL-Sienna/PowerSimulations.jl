68f06af291296d2a0a2de09dbf2178f59b8b302d
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2019-12-31 11:43:00 -0700

    bug fix in the test

--- test/test_simulation_execute.jl
+++ test/test_simulation_execute.jl
@@ -41,7 +41,7 @@ function test_chronology(file_path::String)
             time_1 = convert(Dates.DateTime, Feather.read(time_file_path_1)[1,1]) # first time
             time_2 = convert(Dates.DateTime, Feather.read(time_file_path_2)[1,1])
             time_change = time_2 - time_1
-            interval = get_interval(get_sequence(sim),name)
+            interval = PSI.get_interval(PSI.get_sequence(sim),name)
             @test Dates.Hour(time_change) == Dates.Hour(interval)
         end
     end
