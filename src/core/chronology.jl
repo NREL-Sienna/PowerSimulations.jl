4c7b2785ba0062ff4c20c7e5c8f6c179abeed03f
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2020-01-03 16:02:02 -0700

    Adding a new Chronology

--- src/core/chronology.jl
+++ src/core/chronology.jl
@@ -21,6 +21,21 @@ struct Synchronize <: AbstractChronology
     end
 end
 
+
+@doc raw"""
+    SynchronizeTime(from_steps::Int64, 
+                to_blocks::Int64
+                )
+""" # TODO: Add DocString  
+struct SynchronizeTime <: AbstractChronology
+    from_steps::Int64    
+    to_blocks::Int64 
+    function SynchronizeTime(;from_steps, to_blocks)
+        new(from_steps, to_blocks)
+    end
+end
+
+
 """
     RecedingHorizon(step::Int64
                     )                             
@@ -37,6 +52,26 @@ function check_chronology(sync::Synchronize,
                              horizons::Dict{String, Int64})
     from_stage_horizon = horizons[stages.first]
     to_stage_sync = sync.to_executions
+    from_stage_sync = sync.from_steps
+
+    if from_stage_sync > from_stage_horizon
+        throw(IS.ConflictingInputsError("The lookahead length $(from_stage_horizon) in stage is insufficient to syncronize with $(from_stage_sync) feed_forward steps"))
+    end
+
+    if (from_stage_horizon % from_stage_sync) != 0
+        throw(IS.ConflictingInputsError("The number of feed_forward steps $(from_stage_horizon) in stage
+               needs to be a mutiple of the horizon length $(from_stage_horizon)
+               of stage to use Synchronize with parameters ($(from_stage_sync), $(to_stage_sync))"))
+    end
+
+    return
+end
+
+function check_chronology(sync::SynchronizeTime,
+                             stages::Pair,
+                             horizons::Dict{String, Int64})
+    from_stage_horizon = horizons[stages.first]
+    to_stage_sync = sync.to_blocks
     from_stage_sync = sync.from_steps
 
     if from_stage_sync > from_stage_horizon
