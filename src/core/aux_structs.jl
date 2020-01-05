75d3a1e7651fe2063576e10c4b8d41f70d96874d
Jose Daniel Lara <jdlara@berkeley.edu>
2020-01-05 16:44:33 -0600

    whitespace

--- src/core/aux_structs.jl
+++ src/core/aux_structs.jl
@@ -1,9 +1,9 @@
 """Reference for parameters update when present"""
 struct UpdateRef{T}
-    access_ref::Union{Symbol,String}
+    access_ref::Union{Symbol, String}
     tag::Union{Nothing, String}
 end
 
-UpdateRef{T}(ar::Union{Symbol,String}) where T = UpdateRef{T}(ar, nothing)
+UpdateRef{T}(ar::Union{Symbol, String}) where T = UpdateRef{T}(ar, nothing)
 
 const DRDA = Dict{UpdateRef, JuMP.Containers.DenseAxisArray}
