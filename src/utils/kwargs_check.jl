36e2ea3e74d834fa7086c0068b71f779eae4bb6f
Jose Daniel Lara <jdlara@berkeley.edu>
2020-01-09 10:04:10 -0600

    add missing end

--- src/utils/kwargs_check.jl
+++ src/utils/kwargs_check.jl
@@ -5,6 +5,7 @@ function check_kwargs(input_kwargs, valid_set::Array{Symbol}, function_name::Str
         for (key, value) in input_kwargs
             if !(key in valid_set)
                 throw(ArgumentError("keyword argument $(key) is not a valid input for $(function_name)"))
+            end
         end
     end
     return
