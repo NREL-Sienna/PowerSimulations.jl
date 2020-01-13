ec88d53a9a13b94fe4af9e54c6ef77400fa9f7b8
Lily Hanig <lanig12345@gmail.com>
2020-01-13 15:41:47 -0700

    adding a test for creating plots

--- test/test_plot_results.jl
+++ test/test_plot_results.jl
@@ -1,38 +1,64 @@
-devices = Dict{Symbol, DeviceModel}(:Generators => DeviceModel(ThermalStandard, ThermalDispatch),
-                                    :Loads =>  DeviceModel(PowerLoad, StaticPowerLoad))
-branches = Dict{Symbol, DeviceModel}(:L => DeviceModel(Line, StaticLine),
-                                     :T => DeviceModel(Transformer2W, StaticTransformer),
-                                     :TT => DeviceModel(TapTransformer , StaticTransformer))
-services = Dict{Symbol, ServiceModel}()
-template = OperationsProblemTemplate(CopperPlatePowerModel, devices, branches, services);
-op_problem = OperationsProblem(TestOpProblem, template, c_sys5; optimizer = OSQP_optimizer, use_parameters = true)
-res = solve_op_problem!(op_problem)
+import Plots
 
-@testset "testing bar plot" begin
-    results = PSI.OperationsProblemResults(res.variables, res.total_cost, res.optimizer_log, res.time_stamp)
-    for name in keys(results.variables)
-        variable_bar = get_bar_plot_data(results, string(name))
-        sort = sort!(names(results.variables[name]))
-        sorted_results = res.variables[name][:, sort]
-        for i in 1:length(sort)
-            @test isapprox(variable_bar.bar_data[i], sum(sorted_results[:, i]))
+path = joinpath(pwd(), "plots")
+!isdir(path) && mkdir(path)
+
+function test_plots(file_path::String)
+    devices = Dict{Symbol, DeviceModel}(:Generators => DeviceModel(ThermalStandard, ThermalDispatch),
+                                        :Loads =>  DeviceModel(PowerLoad, StaticPowerLoad))
+    branches = Dict{Symbol, DeviceModel}(:L => DeviceModel(Line, StaticLine),
+                                        :T => DeviceModel(Transformer2W, StaticTransformer),
+                                        :TT => DeviceModel(TapTransformer , StaticTransformer))
+    services = Dict{Symbol, ServiceModel}()
+    template = OperationsProblemTemplate(CopperPlatePowerModel, devices, branches, services);
+    op_problem = OperationsProblem(TestOpProblem, template, c_sys5; optimizer = OSQP_optimizer, use_parameters = true)
+    res = solve_op_problem!(op_problem)
+
+    @testset "testing bar plot" begin
+        results = PSI.OperationsProblemResults(res.variables, res.total_cost, res.optimizer_log, res.time_stamp)
+        for name in keys(results.variables)
+            variable_bar = get_bar_plot_data(results, string(name))
+            sort = sort!(names(results.variables[name]))
+            sorted_results = res.variables[name][:, sort]
+            for i in 1:length(sort)
+                @test isapprox(variable_bar.bar_data[i], sum(sorted_results[:, i]))
+            end
+            @test typeof(variable_bar) == PSI.BarPlot
         end
-        @test typeof(variable_bar) == PSI.BarPlot
+        bar_gen = get_bar_gen_data(results)
+        @test typeof(bar_gen) == PSI.BarGeneration
     end
-    bar_gen = get_bar_gen_data(results)
-    @test typeof(bar_gen) == PSI.BarGeneration
+
+    @testset "testing size of stack plot data" begin
+        results = PSI.OperationsProblemResults(res.variables, res.total_cost, res.optimizer_log, res.time_stamp)
+        for name in keys(results.variables)
+            variable_stack = get_stacked_plot_data(results, string(name))
+            data = variable_stack.data_matrix
+            legend = variable_stack.labels
+            @test size(data) == size(res.variables[name])
+            @test length(legend) == size(data, 2)
+            @test typeof(variable_stack) == PSI.StackedArea
+        end
+        gen_stack = get_stacked_generation_data(results)
+        @test typeof(gen_stack) == PSI.StackedGeneration
+    end
+
+    @testset "testing plot production" begin
+        PSI.bar_plot(res)
+        b_path = joinpath(file_path, "bar_plot.png")
+        Plots.savefig(b_path)
+        PSI.stack_plot(res)
+        s_path = joinpath(file_path, "stack_plot.png")
+        Plots.savefig(s_path)
+        list = readdir(file_path)
+        @test list == ["bar_plot.png", "stack_plot.png"]
+    end
+
+end
+try
+    test_plots(path)
+finally
+    @info("removing test files")
+    rm(path, recursive=true)
 end
 
-@testset "testing size of stack plot data" begin
-    results = PSI.OperationsProblemResults(res.variables, res.total_cost, res.optimizer_log, res.time_stamp)
-    for name in keys(results.variables)
-        variable_stack = get_stacked_plot_data(results, string(name))
-        data = variable_stack.data_matrix
-        legend = variable_stack.labels
-        @test size(data) == size(res.variables[name])
-        @test length(legend) == size(data, 2)
-        @test typeof(variable_stack) == PSI.StackedArea
-    end
-    gen_stack = get_stacked_generation_data(results)
-    @test typeof(gen_stack) == PSI.StackedGeneration
-end
