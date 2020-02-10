8758a14dca661a9c931bb123af0df4cc95ea90c1
Jose Daniel Lara <jdlara@berkeley.edu>
2020-02-10 15:49:29 -0800

    add missing docstrings

--- src/core/initial_conditions.jl
+++ src/core/initial_conditions.jl
@@ -1,4 +1,16 @@
+"""
+    InterStageChronology()
+
+    Type struct to select an information sharing model between stages that uses results from the most recent stage executed to calculate the initial conditions. This model takes into account solutions from stages defined finer resolutions
+"""
+
 struct InterStageChronology <: IniCondChronology end
+
+"""
+    InterStageChronology()
+
+    Type struct to select an information sharing model between stages that uses results from the same recent stage to calculate the initial conditions. This model ignores solutions from stages defined finer resolutions.
+"""
 struct IntraStageChronology <: IniCondChronology end
 
 #########################Initial Conditions Definitions#####################################
