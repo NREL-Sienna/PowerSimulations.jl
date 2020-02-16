d87b3f0342b243f97d5e724cdfea1fa9b17aedf2
Jose Daniel Lara <jdlara@berkeley.edu>
2020-02-15 22:17:17 -0800

    fix bad merge

--- src/core/simulation.jl
+++ src/core/simulation.jl
@@ -9,6 +9,7 @@ mutable struct SimulationInternal
     current_time::Dates.DateTime
     reset::Bool
     compiled_status::Bool
+    simulation_cache::Dict{<:CacheKey, AbstractCache}
 end
 
 function SimulationInternal(steps::Int, stages_keys::Base.KeySet)
@@ -32,6 +33,7 @@ function SimulationInternal(steps::Int, stages_keys::Base.KeySet)
         Dates.now(),
         true,
         false,
+        Dict{CacheKey, AbstractCache}(),
     )
 end
 
@@ -74,21 +76,35 @@ get_sequence(s::Simulation) = s.sequence
 get_steps(s::Simulation) = s.steps
 get_date_range(s::Simulation) = s.internal.date_range
 get_stage(s::Simulation, name::String) = get(s.stages, name, nothing)
+get_stage_interval(s::Simulation, name::String) = get_stage_interval(s.sequence, name)
 get_stage(s::Simulation, number::Int) = get(s.stages, s.sequence.order[number], nothing)
-get_last_stage(s::Simulation) = get_stage(s, s.internal.stages_count)
+get_stages_quantity(s::Simulation) = s.internal.stages_count
 function get_simulation_time(s::Simulation, stage_number::Int)
     return s.internal.date_ref[stage_number]
 end
-get_ini_cond_chronology(s::Simulation, number::Int) =
-    get(s.sequence.ini_cond_chronology, s.sequence.order[number], nothing)
-get_name(s::Simulation, stage::Stage) = get(s.sequence.order, get_number(stage), nothing)
+get_ini_cond_chronology(s::Simulation) = s.sequence.ini_cond_chronology
+get_stage_name(s::Simulation, stage::Stage) = get_stage_name(s.sequence, stage)
+get_name(s::Simulation) = s.name
+get_simulation_folder(s::Simulation) = s.simulation_folder
+get_execution_order(s::Simulation) = s.sequence.execution_order
+get_current_execution_index(s::Simulation) = s.sequence.current_execution_index
+get_stage_cache_definition(s::Simulation, stage::String) =
+    get(s.sequence.cache, stage, nothing)
+get_cache(s::Simulation, key::CacheKey) = get(s.internal.simulation_cache, key, nothing)
+function get_cache(
+    s::Simulation,
+    ::Type{T},
+    ::Type{D},
+) where {T <: AbstractCache, D <: PSY.Device}
+    return get_cache(s, CacheKey(T, D))
+end
 
 function _check_forecasts_sequence(sim::Simulation)
     for (stage_number, stage_name) in sim.sequence.order
         stage = get_stage(sim, stage_name)
         resolution = PSY.get_forecasts_resolution(get_sys(stage))
-        horizon = get_horizon(get_sequence(sim), stage_name)
-        interval = get_interval(get_sequence(sim), stage_name)
+        horizon = get_stage_horizon(get_sequence(sim), stage_name)
+        interval = get_stage_interval(get_sequence(sim), stage_name)
         horizon_time = resolution * horizon
         if horizon_time < interval
             throw(IS.ConflictingInputsError("horizon ($horizon_time) is
@@ -117,8 +133,7 @@ function _assign_feedforward_chronologies(sim::Simulation)
 
     for (key, chron) in sim.sequence.feedforward_chronologies
         to_stage = get_stage(sim, key.second)
-        to_stage_interval =
-            IS.time_period_conversion(get(sim.sequence.intervals, key.second, nothing))
+        to_stage_interval = IS.time_period_conversion(get_stage_interval(sim, key.second))
         from_stage_number = find_key_with_value(sim.sequence.order, key.first)
         if isempty(from_stage_number)
             throw(ArgumentError("Stage $(key.first) not specified in the order dictionary"))
@@ -128,8 +143,10 @@ function _assign_feedforward_chronologies(sim::Simulation)
             from_stage = get_stage(sim, stage_number)
             from_stage_resolution =
                 IS.time_period_conversion(PSY.get_forecasts_resolution(from_stage.sys))
-            to_stage.internal.synchronized_executions[stage_number] =
-                Int(from_stage_resolution / to_stage_interval)
+            # This line keeps track of the executions of a stage relative to other stages.
+            # This might be needed in the future to run multiple stages. For now it is disabled
+            #to_stage.internal.synchronized_executions[stage_number] =
+            #Int(from_stage_resolution / to_stage_interval)
         end
     end
     return
@@ -162,8 +179,8 @@ function _get_simulation_initial_times!(sim::Simulation)
         stage_system = sim.stages[stage_name].sys
         PSY.check_forecast_consistency(stage_system)
         interval = PSY.get_forecasts_interval(stage_system)
-        horizon = get_horizon(get_sequence(sim), stage_name)
-        seq_interval = get_interval(get_sequence(sim), stage_name)
+        horizon = get_stage_horizon(get_sequence(sim), stage_name)
+        seq_interval = get_stage_interval(get_sequence(sim), stage_name)
         if PSY.are_forecasts_contiguous(stage_system)
             stage_initial_times[stage_number] =
                 PSY.generate_initial_times(stage_system, seq_interval, horizon)
@@ -210,7 +227,7 @@ function _attach_feedforward!(sim::Simulation, stage_name::String)
     stage = get(sim.stages, stage_name, nothing)
     feedforward = filter(p -> (p.first[1] == stage_name), sim.sequence.feedforward)
     for (key, ff) in feedforward
-        #Note: key[1] = Stage name, key[2] = template field name, key[3] = device model key
+        # Note: key[1] = Stage name, key[2] = template field name, key[3] = device model key
         field_dict = getfield(stage.template, key[2])
         device_model = get(field_dict, key[3], nothing)
         isnothing(device_model) &&
@@ -225,9 +242,12 @@ function _check_steps(
     stage_initial_times::Dict{Int, Vector{Dates.DateTime}},
 )
     for (stage_number, stage_name) in sim.sequence.order
-        forecast_count = length(stage_initial_times[stage_number])
         stage = sim.stages[stage_name]
-        if get_steps(sim) * get_executions(stage) > forecast_count
+        execution_counts = get_executions(stage)
+        @assert length(findall(x -> x == stage_number, sim.sequence.execution_order)) ==
+                execution_counts
+        forecast_count = length(stage_initial_times[stage_number])
+        if get_steps(sim) * execution_counts > forecast_count
             throw(IS.ConflictingInputsError("The number of available time series ($(forecast_count)) is not enough to perform the
             desired amount of simulation steps ($(sim.steps*stage.internal.execution_count))."))
         end
@@ -235,13 +255,34 @@ function _check_steps(
     return
 end
 
+function _check_required_ini_cond_caches(sim::Simulation)
+    for (stage_number, stage_name) in sim.sequence.order
+        stage = get_stage(sim, stage_name)
+        for (k, v) in get_initial_conditions(stage.internal.psi_container)
+            # No cache needed for the initial condition -> continue
+            isnothing(v[1].cache_type) && continue
+            c = get_cache(sim, v[1].cache_type, k.device_type)
+            if isnothing(c)
+                throw(ArgumentError("Cache $(v[1].cache_type) not defined for initial condition $(k) in stage $stage_name"))
+            end
+            @debug "found cache $(v[1].cache_type) for initial condition $(k) in stage $(stage_name)"
+        end
+    end
+    return
+end
+
 function _populate_caches!(sim::Simulation, stage_name::String)
-    caches = get(sim.sequence.cache, stage_name, nothing)
+    caches = get_stage_cache_definition(sim, stage_name)
     isnothing(caches) && return
-    cache_dict = Dict{Type{<:AbstractCache}, AbstractCache}()
+    stage = get_stage(sim, stage_name)
     for c in caches
-        sim.stages[stage_name].internal.cache_dict[typeof(c)] = c
-        build_cache!(c, sim.stages[stage_name].internal.psi_container)
+        cache_key = CacheKey(c)
+        push!(stage.internal.caches, cache_key)
+        if !haskey(sim.internal.simulation_cache, cache_key)
+            @debug "Cache $(cache_key) added to he simulation"
+            sim.internal.simulation_cache[cache_key] = c
+        end
+        sim.internal.simulation_cache[cache_key].value = get_initial_cache(c, stage)
     end
     return
 end
@@ -250,32 +291,21 @@ function _build_stages!(sim::Simulation; kwargs...)
     system_to_file = get(kwargs, :system_to_file, true)
     for (stage_number, stage_name) in sim.sequence.order
         @info("Building Stage $(stage_number)-$(stage_name)")
-        horizon = sim.sequence.horizons[stage_name]
-        stage = sim.stages[stage_name]
-        stage.internal.psi_container = PSIContainer(
-            stage.template.transmission,
-            stage.sys,
-            stage.optimizer;
-            use_parameters = true,
-            initial_time = get_initial_time(sim),
-            horizon = horizon,
-        )
-        _build!(stage.internal.psi_container, stage.template, stage.sys; kwargs...)
+        horizon = get_stage_horizon(get_sequence(sim), stage_name)
+        stage = get_stage(sim, stage_name)
+        stage_interval = get_stage_interval(get_sequence(sim), stage_name)
+        initial_time = get_initial_time(sim)
+        build!(stage, initial_time, horizon, stage_interval; kwargs...)
         _populate_caches!(sim, stage_name)
-
         if PSY.are_forecasts_contiguous(stage.sys)
-            sim.internal.date_ref[stage_number] = PSY.generate_initial_times(
-                stage.sys,
-                get_interval(get_sequence(sim), stage_name),
-                get_horizon(get_sequence(sim), stage_name),
-            )[1]
+            initial_date = PSY.generate_initial_times(stage.sys, stage_interval, horizon)[1]
         else
-            sim.internal.date_ref[stage_number] =
-                PSY.get_forecast_initial_times(stage.sys)[1]
+            initial_date = PSY.get_forecast_initial_times(stage.sys)[1]
         end
+        @assert initial_date == initial_time
         sim.internal.date_ref[stage_number] = initial_date
     end
-
+    _check_required_ini_cond_caches(sim)
     return
 end
 
@@ -320,11 +350,12 @@ function build!(sim::Simulation; kwargs...)
     sim.internal = SimulationInternal(sim.steps, keys(sim.sequence.order))
     stage_initial_times = _get_simulation_initial_times!(sim)
     for (stage_number, stage_name) in sim.sequence.order
-        stage = get(sim.stages, stage_name, nothing)
-        isnothing(stage) &&
-        throw(IS.ConflictingInputsError("Stage $(stage_name) not found in the stages definitions"))
-        stage_interval = sim.sequence.intervals[stage_name]
-        executions = Int(sim.sequence.step_resolution / stage_interval)
+        stage = get_stage(sim, stage_name)
+        if isnothing(stage)
+            throw(IS.ConflictingInputsError("Stage $(stage_name) not found in the stages definitions"))
+        end
+        stage_interval = get_stage_interval(sim, stage_name)
+        executions = Int(get_step_resolution(sim.sequence) / stage_interval)
         stage.internal = StageInternal(stage_number, executions, 0, nothing)
         _attach_feedforward!(sim, stage_name)
     end
@@ -333,4 +364,162 @@ function build!(sim::Simulation; kwargs...)
     _build_stages!(sim; kwargs...)
     sim.internal.compiled_status = true
     return
+end
+
+#Defined here because it requires Stage and Simulation to defined
+
+#############################Interfacing Functions##########################################
+# These are the functions that the user will have to implement to update a custom IC Chron #
+# or custom InitialConditionType #
+
+""" Updates the initial conditions of the stage"""
+function initial_condition_update!(
+    stage::Stage,
+    ini_cond_key::ICKey,
+    chronology::IntraStageChronology,
+    sim::Simulation,
+)
+    ini_cond_vector = get_initial_conditions(stage.internal.psi_container)[ini_cond_key]
+    for ic in get_initial_conditions(stage.internal.psi_container)[ini_cond_key]
+        name = device_name(ic)
+        interval_chronology = get_stage_interval_chronology(sim, stage.name)
+        var_value =
+            get_stage_variable(interval_chronology, (stage => stage), name, ic.update_ref)
+        cache = get_cache(stage, ic.cache_key)
+        quantity = calculate_ic_quantity(ini_cond_key, ic, var_value, cache)
+        PJ.fix(ic.value, quantity)
+    end
+    return
+end
+
+""" Updates the initial conditions of the stage"""
+function initial_condition_update!(
+    stage::Stage,
+    ini_cond_key::ICKey,
+    chronology::InterStageChronology,
+    sim::Simulation,
+)
+    execution_index = get_execution_order(sim)
+    ini_cond_vector = get_initial_conditions(stage.internal.psi_container)[ini_cond_key]
+    for ic in ini_cond_vector
+        name = device_name(ic)
+        current_ix = get_current_execution_index(sim)
+        source_stage_ix = current_ix == 1 ? length(execution_index) : current_ix - 1
+        source_stage = get_stage(sim, execution_index[source_stage_ix])
+        source_stage_name = get_stage_name(sim, source_stage)
+        interval_chronology = get_stage_interval_chronology(sim.sequence, source_stage_name)
+        var_value = get_stage_variable(
+            interval_chronology,
+            (source_stage => stage),
+            name,
+            ic.update_ref,
+        )
+        if isnothing(ic.cache_type)
+            cache = nothing
+        else
+            cache = get_cache(sim, ic.cache_type, ini_cond_key.device_type)
+        end
+        quantity = calculate_ic_quantity(ini_cond_key, ic, var_value, cache)
+        PJ.fix(ic.value, quantity)
+    end
+
+    return
+end
+
+function _update_caches!(sim::Simulation, stage::Stage)
+    for cache in stage.internal.caches
+        update_cache!(sim, cache, stage)
+    end
+    return
+end
+
+################################Cache Update################################################
+# TODO: Need to be careful here if 2 stages modify the same cache. This function might need
+# dispatch on the Statge{OpModel} to assing different actions. e.g. HAUC and DAUC
+function update_cache!(
+    sim::Simulation,
+    key::CacheKey{TimeStatusChange, D},
+    stage::Stage,
+) where {D <: PSY.Device}
+    c = get_cache(sim, key)
+    variable = get_variable(stage.internal.psi_container, c.ref)
+
+    for t in 1:get_end_of_interval_step(stage), name in variable.axes[1]
+        device_status = JuMP.value(variable[name, t])
+        if c.value[name][:status] == device_status
+            c.value[name][:count] += 1.0
+        elseif c.value[name][:status] != device_status
+            c.value[name][:count] = 1.0
+            c.value[name][:status] = device_status
+        end
+    end
+
+    return
+end
+
+"""
+    execute!(sim::Simulation; kwargs...)
+
+Solves the simulation model for sequential Simulations
+and populates a nested folder structure created in Simulation()
+with a dated folder of featherfiles that contain the results for
+each stage and step.
+
+# Arguments
+- `sim::Simulation=sim`: simulation object created by Simulation()
+
+# Example
+```julia
+sim = Simulation("test", 7, stages, "/Users/folder")
+execute!!(sim::Simulation; kwargs...)
+```
+
+# Accepted Key Words
+- `constraints_duals::Vector{Symbol}`: if dual variables are desired in the
+results, include a vector of the variable names to be included
+"""
+
+function execute!(sim::Simulation; kwargs...)
+    if sim.internal.reset
+        sim.internal.reset = false
+    elseif sim.internal.reset == false
+        error("Re-build the simulation")
+    end
+
+    isnothing(sim.internal) &&
+    error("Simulation not built, build the simulation to execute")
+    name = get_name(sim)
+    folder = get_simulation_folder(sim)
+    sim.internal.raw_dir, sim.internal.models_dir, sim.internal.results_dir =
+        _prepare_workspace(name, folder)
+    _build_stage_paths!(sim; kwargs...)
+    execution_order = get_execution_order(sim)
+    for step in 1:get_steps(sim)
+        println("Executing Step $(step)")
+        for (ix, stage_number) in enumerate(execution_order)
+            # TODO: implement some efficient way of indexing with stage name.
+            stage = get_stage(sim, stage_number)
+            stage_name = get_stage_name(sim, stage)
+            stage_interval = get_stage_interval(sim, stage_name)
+            run_name = "step-$(step)-stage-$(stage_name)"
+            sim.internal.current_time = sim.internal.date_ref[stage_number]
+            sim.sequence.current_execution_index = ix
+            @info "Starting run $run_name $(sim.internal.current_time)"
+            raw_results_path = joinpath(
+                sim.internal.raw_dir,
+                run_name,
+                replace_chars("$(sim.internal.current_time)", ":", "-"),
+            )
+            mkpath(raw_results_path)
+            # Is first run of first stage? Yes -> don't update stage
+            !(step == 1 && ix == 1) && update_stage!(stage, step, sim)
+            run_stage(stage, sim.internal.current_time, raw_results_path; kwargs...)
+            _update_caches!(sim, stage)
+            sim.internal.run_count[step][stage_number] += 1
+            sim.internal.date_ref[stage_number] += stage_interval
+        end
+    end
+    constraints_duals = get(kwargs, :constraints_duals, nothing)
+    sim_results = SimulationResultsReference(sim; constraints_duals = constraints_duals)
+    return sim_results
 end
