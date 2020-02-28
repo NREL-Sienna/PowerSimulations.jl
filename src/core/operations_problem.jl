cc7a5ad0fc16313ac2ffede9b7521685a59b2e82
Jose Daniel Lara <jdlara@berkeley.edu>
2020-02-28 14:48:30 -0800

    Update src/core/operations_problem.jl
    Co-Authored-By: Daniel Thom <daniel.thom@nrel.gov>

--- src/core/operations_problem.jl
+++ src/core/operations_problem.jl
@@ -492,8 +492,7 @@ function get_parameters_value(op_m::OperationsProblem)
     # the system
     params_dict = Dict{Symbol, DataFrames.DataFrame}()
     parameters = get_parameters(op_m.psi_container)
-    isnothing(parameters) && return params_dict
-    isempty(parameters) && return params_dict
+    isnothing(parameters) || isempty(parameters) && return params_dict
     for (k, v) in parameters
         !isa(v.update_ref, UpdateRef{<:PSY.Component}) && continue
         params_key_tuple = decode_symbol(k)
