70812bf83e111a9fe6e0754a01b3c3c42d0d353d
Daniel Thom <daniel.thom@nrel.gov>
2020-03-12 15:20:42 -0600

    Make OperationsProblem constructor more explicit

--- src/core/operations_problem.jl
+++ src/core/operations_problem.jl
@@ -85,7 +85,6 @@ function OperationsProblem(
         PSIContainer(template.transmission, sys, optimizer; kwargs...),
     )
 
-    parameters = get(kwargs, :parameters, nothing)
     build_op_problem!(op_problem; parameters = parameters)
 
     return op_problem
