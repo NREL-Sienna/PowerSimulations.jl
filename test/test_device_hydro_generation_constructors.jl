3553f92be616e9f6ec76fd2a0a589cda4be570f5
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2020-03-18 13:43:35 -0600

    Adding new test for hydro to improve coverage

--- test/test_device_hydro_generation_constructors.jl
+++ test/test_device_hydro_generation_constructors.jl
@@ -90,6 +90,88 @@ end
 
 end
 
+@testset "Hydro ACPPowerModel HydroDispatch with HydroDispatchRunOfRiver formulations" begin
+    model = DeviceModel(HydroDispatch, HydroDispatchRunOfRiver)
+
+    # Parameters Testing
+    op_problem =
+        OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_hyd; use_parameters = true)
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, true, 0, 0, 0, 0, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Parameters Testing
+    op_problem = OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_hyd)
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, false, 0, 0, 0, 0, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Forecast Testing
+    op_problem = OperationsProblem(
+        TestOpProblem,
+        ACPPowerModel,
+        c_sys5_hyd;
+        use_parameters = true,
+        use_forecast_data = false,
+    )
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, true, 0, 0, 0, 0, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Forecast - No Parameters Testing
+    op_problem = OperationsProblem(
+        TestOpProblem,
+        ACPPowerModel,
+        c_sys5_hyd;
+        use_forecast_data = false,
+    )
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, false, 0, 0, 0, 0, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+end
+
+@testset "Hydro DCPLossLess HydroEnergyReservoir with HydroFixed formulations" begin
+    model = DeviceModel(HydroEnergyReservoir, HydroFixed)
+
+    # Parameters Testing
+    op_problem =
+        OperationsProblem(TestOpProblem, DCPPowerModel, c_sys5_hy; use_parameters = true)
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, true, 0, 0, 0, 0, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Parameters Testing
+    op_problem = OperationsProblem(TestOpProblem, DCPPowerModel, c_sys5_hy)
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, false, 0, 0, 0, 0, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Forecast Testing
+    op_problem = OperationsProblem(
+        TestOpProblem,
+        DCPPowerModel,
+        c_sys5_hy;
+        use_parameters = true,
+        use_forecast_data = false,
+    )
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, true, 0, 0, 0, 0, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Forecast - No Parameters Testing
+    op_problem = OperationsProblem(
+        TestOpProblem,
+        DCPPowerModel,
+        c_sys5_hy;
+        use_forecast_data = false,
+    )
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, false, 0, 0, 0, 0, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+end
+
 @testset "Hydro DCPLossLess HydroEnergyReservoir with HydroDispatchRunOfRiver formulations" begin
     model = DeviceModel(HydroEnergyReservoir, HydroDispatchRunOfRiver)
 
@@ -122,6 +204,47 @@ end
     op_problem = OperationsProblem(
         TestOpProblem,
         DCPPowerModel,
+        c_sys5_hyd;
+        use_forecast_data = false,
+    )
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, false, 1, 0, 1, 1, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+end
+
+@testset "Hydro ACPPowerModel HydroEnergyReservoir with HydroDispatchRunOfRiver formulations" begin
+    model = DeviceModel(HydroEnergyReservoir, HydroDispatchRunOfRiver)
+
+    # Parameters Testing
+    op_problem =
+        OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_hyd; use_parameters = true)
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, true, 24, 0, 24, 0, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Parameters Testing
+    op_problem = OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_hyd)
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, false, 24, 0, 24, 0, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Forecast Testing
+    op_problem = OperationsProblem(
+        TestOpProblem,
+        ACPPowerModel,
+        c_sys5_hyd;
+        use_parameters = true,
+        use_forecast_data = false,
+    )
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, true, 1, 0, 1, 0, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Forecast - No Parameters Testing
+    op_problem = OperationsProblem(
+        TestOpProblem,
+        ACPPowerModel,
         c_sys5_hyd;
         use_forecast_data = false,
     )
@@ -168,6 +291,47 @@ end
     )
     construct_device!(op_problem, :Hydro, model)
     moi_tests(op_problem, false, 1, 0, 2, 1, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+end
+
+@testset "Hydro ACPPowerModel HydroEnergyReservoir with HydroDispatchReservoirFlow Formulations" begin
+    model = DeviceModel(HydroEnergyReservoir, HydroDispatchReservoirFlow)
+
+    # Parameters Testing
+    op_problem =
+        OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_hy_uc; use_parameters = true)
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, true, 48, 0, 25, 24, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Parameters Testing
+    op_problem = OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_hy_uc)
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, false, 48, 0, 25, 24, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Forecast Testing
+    op_problem = OperationsProblem(
+        TestOpProblem,
+        ACPPowerModel,
+        c_sys5_hy_uc;
+        use_parameters = true,
+        use_forecast_data = false,
+    )
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, true, 2, 0, 2, 1, 0, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Forecast - No Parameters Testing
+    op_problem = OperationsProblem(
+        TestOpProblem,
+        ACPPowerModel,
+        c_sys5_hy_uc;
+        use_forecast_data = false,
+    )
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, false, 2, 0, 2, 1, 0, false)
     psi_checkobjfun_test(op_problem, GAEVF)
 
 end
@@ -291,6 +455,49 @@ end
     op_problem = OperationsProblem(
         TestOpProblem,
         DCPPowerModel,
+        c_sys5_hyd;
+        use_forecast_data = false,
+    )
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, false, 4, 0, 2, 2, 1, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+    =#
+end
+
+@testset "Hydro ACPPowerModel HydroEnergyReservoir with HydroDispatchReservoirStorage Formulations" begin
+    model = DeviceModel(HydroEnergyReservoir, HydroDispatchReservoirStorage)
+
+    # Parameters Testing
+    op_problem =
+        OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_hyd; use_parameters = true)
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, true, 72, 0, 24, 24, 24, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Parameters Testing
+    op_problem = OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_hyd)
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, false, 72, 0, 24, 24, 24, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Forecast Testing
+    op_problem = OperationsProblem(
+        TestOpProblem,
+        ACPPowerModel,
+        c_sys5_hyd;
+        use_parameters = true,
+        use_forecast_data = false,
+    )
+    construct_device!(op_problem, :Hydro, model)
+    moi_tests(op_problem, true, 3, 0, 1, 1, 1, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+
+    # No Forecast - No Parameters Testing
+    # not supported
+    #=
+    op_problem = OperationsProblem(
+        TestOpProblem,
+        ACPPowerModel,
         c_sys5_hyd;
         use_forecast_data = false,
     )
