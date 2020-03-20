c812f2a466174b607a2585be8e70ba831d8fc1b6
Daniel Thom <daniel.thom@nrel.gov>
2020-03-20 15:51:15 -0600

    Move PSISettings to its own file

---
+++ src/core/psi_settings.jl
@@ -0,0 +1,109 @@
+struct PSISettings
+    horizon::Base.RefValue{Int}
+    initial_conditions::Union{Nothing, InitialConditionsContainer}
+    use_forecast_data::Bool
+    use_parameters::Bool
+    use_warm_start::Base.RefValue{Bool}
+    initial_time::Base.RefValue{Dates.DateTime}
+    PTDF::Union{Nothing, PSY.PTDF}
+    optimizer::Union{Nothing, JuMP.MOI.OptimizerWithAttributes}
+    constraint_duals::Vector{Symbol}
+    ext::Dict{String, Any}
+end
+
+function PSISettings(
+    sys;
+    initial_time::Union{Nothing, Dates.DateTime} = nothing,
+    use_parameters::Bool = false,
+    use_forecast_data::Bool = true,
+    initial_conditions::Union{Nothing, InitialConditionsContainer} = nothing,
+    use_warm_start::Bool = true,
+    horizon::Int = 0,
+    PTDF::Union{Nothing, PSY.PTDF} = nothing,
+    optimizer::Union{Nothing, JuMP.MOI.OptimizerWithAttributes} = nothing,
+    constraint_duals::Vector{Symbol} = Vector{Symbol}(),
+    ext::Dict{String, Any} = Dict{String, Any}(),
+)
+
+    if isnothing(initial_time)
+        initial_time = PSY.get_forecasts_initial_time(sys)
+    end
+
+    if horizon == 0
+        horizon = PSY.get_forecasts_horizon(sys)
+    end
+
+    if isnothing(initial_conditions)
+        initial_conditions = InitialConditionsContainer(use_parameters = use_parameters)
+    end
+
+    return PSISettings(
+        Ref(horizon),
+        initial_conditions,
+        use_forecast_data,
+        use_parameters,
+        Ref(use_warm_start),
+        Ref(initial_time),
+        PTDF,
+        optimizer,
+        constraint_duals,
+        ext,
+    )
+end
+
+function copy_for_serialization(settings::PSISettings)
+    vals = []
+    for name in fieldnames(PSISettings)
+        if name == :optimizer
+            # Cannot guarantee that the optimizer can be serialized.
+            val = nothing
+        else
+            val = getfield(settings, name)
+        end
+
+        push!(vals, val)
+    end
+
+    return deepcopy(PSISettings(vals...))
+end
+
+function restore_from_copy(
+    settings::PSISettings;
+    optimizer::Union{Nothing, JuMP.MOI.OptimizerWithAttributes},
+)
+    vals = []
+    for name in fieldnames(PSISettings)
+        if name == :optimizer
+            val = optimizer
+        else
+            val = getfield(settings, name)
+        end
+
+        push!(vals, val)
+    end
+
+    return PSISettings(vals...)
+end
+
+function set_horizon!(settings::PSISettings, horizon::Int)
+    settings.horizon[] = horizon
+    return
+end
+get_horizon(settings::PSISettings)::Int = settings.horizon[]
+get_initial_conditions(settings::PSISettings) = settings.initial_conditions
+get_use_forecast_data(settings::PSISettings) = settings.use_forecast_data
+get_use_parameters(settings::PSISettings) = settings.use_parameters
+function set_initial_time!(settings::PSISettings, initial_time::Dates.DateTime)
+    settings.initial_time[] = initial_time
+    return
+end
+get_initial_time(settings::PSISettings)::Dates.DateTime = settings.initial_time[]
+get_PTDF(settings::PSISettings) = settings.PTDF
+get_optimizer(settings::PSISettings) = settings.optimizer
+get_ext(settings::PSISettings) = settings.ext
+function set_use_warm_start!(settings::PSISettings, use_warm_start::Bool)
+    settings.use_warm_start[] = use_warm_start
+    return
+end
+get_use_warm_start(settings::PSISettings) = settings.use_warm_start[]
+get_constraint_duals(settings::PSISettings) = settings.constraint_duals
