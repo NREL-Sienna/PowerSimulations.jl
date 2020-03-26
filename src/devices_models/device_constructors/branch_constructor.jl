62bfc9655ed52c4e8206205c40a56a7ba74d2d16
Jose Daniel Lara <jdlara@berkeley.edu>
2020-03-26 15:46:52 -0700

    clean up branch constructors

--- src/devices_models/device_constructors/branch_constructor.jl
+++ src/devices_models/device_constructors/branch_constructor.jl
@@ -1,32 +1,88 @@
 construct_device!(
     psi_container::PSIContainer,
     sys::PSY.System,
-    model::DeviceModel{B, Br},
+    model::DeviceModel{<:PSY.ACBranch, <:AbstractBranchFormulation},
     ::Type{CopperPlatePowerModel},
-) where {B <: PSY.DCBranch, Br <: AbstractBranchFormulation} = nothing
+) = nothing
 
 construct_device!(
     psi_container::PSIContainer,
     sys::PSY.System,
-    model::DeviceModel{B, Br},
+    model::DeviceModel{<:PSY.DCBranch, <:AbstractDCLineFormulation},
     ::Type{CopperPlatePowerModel},
-) where {B <: PSY.ACBranch, Br <: AbstractBranchFormulation} = nothing
+) = nothing
+
+# This method might be redundant but added for completness of the formulations
+construct_device!(
+    psi_container::PSIContainer,
+    sys::PSY.System,
+    ::DeviceModel{<:PSY.Branch, <:Type{UnboundedBranches}},
+    ::Type{<:PM.AbstractPowerModel},
+) = nothing
+
+# For DC Power only. Implements Bounds only
+function construct_device!(
+    psi_container::PSIContainer,
+    sys::PSY.System,
+    model::DeviceModel{B, Br},
+    ::Type{S},
+) where {
+    B <: PSY.ACBranch,
+    Br <: AbstractBoundedBranchFormulation,
+    S <: PM.AbstractActivePowerModel,
+}
+    devices = PSY.get_components(B, sys)
+    if validate_available_devices(devices, B)
+        return
+    end
+    !isnothing(get_feedforward(model)) &&
+    throw(IS.ConflictingInputsError("$(Br) formulation doesn't support FeedForward. Use Constrained Branch Formulation instead"))
+    branch_rate_bounds!(psi_container, devices, model, S)
+    return
+end
+
+# For DC Power only. Implements Constraints only
+function construct_device!(
+    psi_container::PSIContainer,
+    sys::PSY.System,
+    model::DeviceModel{B, <:AbstractBranchFormulation},
+    ::Type{S},
+) where {B <: PSY.ACBranch, S <: PM.AbstractActivePowerModel}
+    devices = PSY.get_components(B, sys)
+    if validate_available_devices(devices, B)
+        return
+    end
+    branch_rate_constraints!(psi_container, devices, model, S, get_feedforward(model))
+    return
+end
+
+# For AC Power only. Implements Bounds on the active power and rating constraints on the aparent power
+function construct_device!(
+    psi_container::PSIContainer,
+    sys::PSY.System,
+    model::DeviceModel{B, <:AbstractBranchFormulation},
+    ::Type{S},
+) where {B <: PSY.ACBranch, S <: PM.AbstractPowerModel}
+    devices = PSY.get_components(B, sys)
+    if validate_available_devices(devices, B)
+        return
+    end
+    branch_rate_bounds!(psi_container, devices, model, S)
+    branch_rate_constraints!(psi_container, devices, model, S, get_feedforward(model))
+    return
+end
 
 function construct_device!(
     psi_container::PSIContainer,
     sys::PSY.System,
     model::DeviceModel{B, Br},
     ::Type{S},
-) where {B <: PSY.Branch, Br <: AbstractBranchFormulation, S <: PM.AbstractPowerModel}
+) where {B <: PSY.DCBranch, Br <: AbstractDCLineFormulation, S <: PM.AbstractPowerModel}
     devices = PSY.get_components(B, sys)
-
     if validate_available_devices(devices, B)
         return
     end
-
-    branch_rate_bounds!(psi_container, devices, Br, S)
     branch_rate_constraints!(psi_container, devices, model, S, get_feedforward(model))
-
     return
 end
 
@@ -47,33 +103,6 @@ function construct_device!(
     branch_rate_constraints!(psi_container, devices, model, S, get_feedforward(model))
 
     branch_flow_constraints!(psi_container, devices, model, S, get_feedforward(model))
-
-    return
-end
-
-construct_device!(
-    psi_container::PSIContainer,
-    sys::PSY.System,
-    ::DeviceModel{
-        <:PSY.Branch,
-        <:Union{Type{StaticLineUnbounded}, Type{StaticTransformerUnbounded}},
-    },
-    ::Type{<:PM.AbstractPowerModel},
-) = nothing
-
-function construct_device!(
-    psi_container::PSIContainer,
-    sys::PSY.System,
-    model::DeviceModel{B, Br},
-    ::Type{S},
-) where {B <: PSY.DCBranch, Br <: AbstractBranchFormulation, S <: PM.AbstractPowerModel}
-    devices = PSY.get_components(B, sys)
-
-    if validate_available_devices(devices, B)
-        return
-    end
-
-    branch_rate_constraints!(psi_container, devices, model, S, get_feedforward(model))
 
     return
 end
