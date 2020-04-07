2e7dd369218208f1c278d944d1b81bf83f91a830
Barrows <clayton.barrows@nrel.gov>
2020-04-06 21:42:00 -0600

    avoiding construction of containers for services that dont exist

--- src/devices_models/device_constructors/common/constructor_validations.jl
+++ src/devices_models/device_constructors/common/constructor_validations.jl
@@ -8,3 +8,14 @@ function validate_available_devices(devices, device::Type{D}) where {D <: PSY.De
     return false
 
 end
+
+function validate_available_services(services, service::Type{S}) where {S <: PSY.Service}
+
+    if isempty(services)
+        @warn("The data doesn't include services of type $(service), consider changing the service models")
+        return true
+    end
+
+    return false
+
+end
