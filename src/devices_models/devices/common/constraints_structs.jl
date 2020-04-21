78d87558d06d6738f35d3eee12afc217ad8e33f9
Jose Daniel Lara <jdlara@berkeley.edu>
2020-04-20 21:35:57 -0700

    common time series updates

--- src/devices_models/devices/common/constraints_structs.jl
+++ src/devices_models/devices/common/constraints_structs.jl
@@ -21,3 +21,20 @@ struct DeviceTimeSeries
         return new(name, bus_number, multiplier, timeseries, range)
     end
 end
+
+function DeviceTimeSeries(
+    device::PSY.Device,
+    multiplier_function::Function,
+    ts_vector::Vector{Float64},
+    get_constraint_values::Union{Function, Nothing} = nothing,
+)
+    name = PSY.get_name(device)
+    bus_number = PSY.get_number(PSY.get_bus(device))
+    multiplier = multiplier_function(device)
+    if isnothing(get_constraint_values)
+        range_data = nothing
+    else
+        range_data = DeviceRange(name, get_constraint_values(device))
+    end
+    return DeviceTimeSeries(name, bus_number, multiplier, ts_vector, range_data)
+end
--- src/devices_models/devices/common/get_time_series.jl
+++ src/devices_models/devices/common/get_time_series.jl
@@ -1,65 +1,57 @@
 function get_time_series(
     psi_container::PSIContainer,
+    device::PSY.Device,
+    forecast_label::String,
+)
+    initial_time = model_initial_time(psi_container)
+    @debug initial_time
+    use_forecast_data = model_uses_forecasts(psi_container)
+    time_steps = model_time_steps(psi_container)
+
+    if use_forecast_data && PSY.has_forecast(device)
+        forecast = PSY.get_forecast(
+            PSY.Deterministic,
+            device,
+            initial_time,
+            forecast_label,
+            length(time_steps),
+        )
+        return ts_vector = TS.values(PSY.get_data(forecast))
+    else
+        @debug "device $(PSY.get_name(device)) $(PSY.has_forecast(device)) forecast"
+        return ts_vector = ones(time_steps[end])
+    end
+end
+
+#=
+function get_time_series_vector(
+    psi_container::PSIContainer,
     devices::IS.FlattenIteratorWrapper{T},
-    model::Union{Nothing, DeviceModel},
-    get_constraint_values::Function,
+    forecast_label::String
 )   where T <: PSY.Device
     initial_time = model_initial_time(psi_container)
     @debug initial_time
     use_forecast_data = model_uses_forecasts(psi_container)
-    parameters = model_has_parameters(psi_container)
     time_steps = model_time_steps(psi_container)
-    device_total = length(devices)
+    ts_vector = Vector{Vector{Float64}}(undef, length(devices))
 
-    constraint_data = Vector{DeviceRange}()
-    active_timeseries = Vector{DeviceTimeSeries}()
-    reactive_timeseries = Vector{DeviceTimeSeries}()
-
-    for device in devices
-        bus_number = PSY.get_number(PSY.get_bus(device))
-        name = PSY.get_name(device)
-        pf = sin(acos(PSY.get_powerfactor(device)))
-
+    for (ix, device) in enumerate(devices)
         if use_forecast_data
-            active_power = PSY.get_rating(device)
-            if hasfield(T, :powerfactor)
-                pf = sin(acos(PSY.get_powerfactor(device)))
-                reactive_power = PSY.get_rating(device) * pf
-            elseif hasfield(T, :maxreactivepower)
-                reactive_power = PSY.get_maxreactivepower(device)
-            else
-                throw(IS.ConflictingInputsError("Device of type $(T) does not contain information to model reactive power injection into the system"))
-            end
-            forecast = PSY.get_forecast(
+                forecast = PSY.get_forecast(
                 PSY.Deterministic,
                 device,
                 initial_time,
-                "get_rating",
+                forecast_label,
                 length(time_steps),
             )
             ts_vector = TS.values(PSY.get_data(forecast))
         else
-            active_power = PSY.get_activepower(device)
-            reactive_power = PSY.get_reactivepower(device)
             ts_vector = ones(time_steps[end])
         end
-
-        range_data = DeviceRange(name, get_constraint_values(device))
-        _device_services!(range_data, device, model)
-        push!(constraint_data, range_data)
-        push!(
-            active_timeseries,
-            DeviceTimeSeries(name, bus_number, active_power, ts_vector, range_data),
-        )
-        push!(
-            reactive_timeseries,
-            DeviceTimeSeries(name, bus_number, reactive_power, ts_vector, range_data),
-        )
-
     end
-    return active_timeseries, reactive_timeseries, constraint_data
+    return ts_vector
 end
-#=
+
 function get_time_series(
     psi_container::PSIContainer,
     devices::IS.FlattenIteratorWrapper{<:PSY.ElectricLoad},
@@ -97,7 +89,7 @@ function get_time_series(
             ts_vector = ones(time_steps[end])
         end
         range_data = DeviceRange(name, get_constraint_values(device))
-        _device_services!(range_data, device, model)
+        add_device_services!(range_data, device, model)
         push!(constraint_data, range_data)
         push!(
             ts_data_active,
@@ -146,7 +138,7 @@ function get_time_series(
             ts_vector = ones(time_steps[end])
         end
         range_data = DeviceRange(name, get_constraint_values(device))
-        _device_services!(range_data, device, model)
+        add_device_services!(range_data, device, model)
         push!(constraint_data, range_data)
         push!(
             active_timeseries,
