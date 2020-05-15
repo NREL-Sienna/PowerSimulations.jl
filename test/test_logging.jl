9d5bd32237bf3c852d2b983ff5825571fa7ce64c
Daniel Thom <daniel.thom@nrel.gov>
2020-05-15 17:08:28 -0600

    Don't override global logger in tests

--- test/test_logging.jl
+++ test/test_logging.jl
@@ -1,6 +1,7 @@
 @testset "Test configure_logging" begin
     logfile = "testlog.txt"
     msg = "test log message"
+    orig = Logging.global_logger()
     logger = configure_logging(; filename = logfile)
 
     try
@@ -18,5 +19,6 @@
         end
     finally
         rm(logfile)
+        Logging.global_logger(orig)
     end
 end
