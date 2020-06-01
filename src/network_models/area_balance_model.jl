6d9384c3d430c5bf32211159947f70ac12ff51de
Jose Daniel Lara <jdlara@berkeley.edu>
2020-06-01 11:57:29 -0700

    fix the assignemnt of area AGC model

--- src/network_models/area_balance_model.jl
+++ src/network_models/area_balance_model.jl
@@ -7,18 +7,18 @@ function area_balance(
     time_steps = model_time_steps(psi_container)
     remove_undef!(psi_container.expressions[expression])
     nodal_net_balance = psi_container.expressions[expression]
+    constraint_bal = JuMPConstraintArray(undef, keys(area_mapping), time_steps)
+    assign_constraint!(psi_container, "area_balance", constraint_bal)
+    area_balance = get_variable(psi_container, variable_name("area_balance"))
 
     for (k, buses_in_area) in area_mapping
-        constraint_val = JuMPConstraintArray(undef, time_steps)
-        assign_constraint!(psi_container, "Balance_area_$(k)", constraint_val)
-        area_balance = get_variable(psi_container, variable_name("area_balance", "$(k)"))
         for t in time_steps
             area_net = JuMP.AffExpr(0.0)
             for b in buses_in_area
                 JuMP.add_to_expression!(area_net, nodal_net_balance[PSY.get_number(b), t])
-                constraint_val[t] =
-                    JuMP.@constraint(psi_container.JuMPmodel, area_balance[t] == area_net)
             end
+            constraint_bal[k, t] =
+                    JuMP.@constraint(psi_container.JuMPmodel, area_balance[k, t] == area_net)
         end
     end
     return
