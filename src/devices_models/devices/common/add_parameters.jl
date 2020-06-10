382c157dd43c90036ff7120e82cf411bac867c75
Jose Daniel Lara <jdlara@berkeley.edu>
2020-06-09 20:07:49 -0700

    add new include parameters method

--- src/devices_models/devices/common/add_parameters.jl
+++ src/devices_models/devices/common/add_parameters.jl
@@ -25,5 +25,25 @@ function include_parameters(
             r.multiplier * multiplier,
         )
     end
-    return param
+    return container
+end
+
+function include_parameters(
+    psi_container::PSIContainer,
+    constraint_infos::Vector{DeviceTimeSeriesConstraintInfo},
+    param_reference::UpdateRef,
+    multiplier::Float64 = 1.0,
+)
+    @assert model_has_parameters(psi_container)
+    time_steps = model_time_steps(psi_container)
+    ## TODO, replace multiplier
+    names = (get_name(r) for r in constraint_infos)
+    container = add_param_container!(psi_container, param_reference, names, time_steps)
+    param = get_parameter_array(container)
+    mult = get_multiplier_array(container)
+    for t in time_steps, r in constraint_infos
+        param[get_name(r), t] = PJ.add_parameter(psi_container.JuMPmodel, r.timeseries[t])
+        mult[get_name(r), t] = r.multiplier * multiplier
+    end
+    return container
 end
