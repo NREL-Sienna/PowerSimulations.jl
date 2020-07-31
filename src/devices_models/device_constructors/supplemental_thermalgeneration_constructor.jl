80fefc44f9c273d1df9e4063f61d1b1c77f0d5ca
Andrew David Werner Rosemberg <andrew@ele.puc-rio.br>
2020-07-31 07:42:31 -0300

    Update src/devices_models/device_constructors/supplemental_thermalgeneration_constructor.jl
    Typo
    
    Co-authored-by: Raphael Saavedra <raphael.saavedra93@gmail.com>

--- src/devices_models/device_constructors/supplemental_thermalgeneration_constructor.jl
+++ src/devices_models/device_constructors/supplemental_thermalgeneration_constructor.jl
@@ -1,5 +1,5 @@
 """
-This function creates the model for a full themal dispatch formulation depending on combination of devices, device_formulation and system_formulation
+This function creates the model for a full thermal dispatch formulation depending on combination of devices, device_formulation and system_formulation
 """
 function construct_device!(
     psi_container::PSIContainer,
