f55e3b5e36d385e592ace5c383070ec5b087db84
Andrew.Rosemberg <andrew.rosemberg@invenialabs.co.uk>
2020-08-03 18:59:11 +0100

    add include for StaticReserveGroup

--- src/PowerSimulations.jl
+++ src/PowerSimulations.jl
@@ -306,6 +306,7 @@ include("services_models/agc.jl")
 include("services_models/reserves.jl")
 include("services_models/service_slacks.jl")
 include("services_models/services_constructor.jl")
+include("services_models/StaticReserveGroup.jl")
 
 #Network models
 include("network_models/copperplate_model.jl")
