42d0e90bef3df9834abdbfbee7227a36ddac14a8
Jose Daniel Lara <jdlara@berkeley.edu>
2020-09-15 10:09:47 -0700

    remove old comment

--- src/services_models/reserves.jl
+++ src/services_models/reserves.jl
@@ -31,8 +31,6 @@ function AddVariableSpec(
 end
 
 ################################## Reserve Requirement Constraint ##########################
-# This function can be generalized later for any constraint of type Sum(req_var) >= requirement,
-# it will only need to be specific to the names and get forecast string.
 function service_requirement_constraint!(
     psi_container::PSIContainer,
     service::SR,
@@ -108,7 +106,6 @@ function service_requirement_constraint!(
     service::SR,
     ::ServiceModel{SR, StepwiseCostReserve},
 ) where {SR <: PSY.Reserve}
-
     initial_time = model_initial_time(psi_container)
     @debug initial_time
     time_steps = model_time_steps(psi_container)
@@ -127,12 +124,29 @@ function service_requirement_constraint!(
     return
 end
 
+function _convert_to_variablecost(val::TS.TimeArray)
+    cost_col = Vector{Symbol}()
+    load_col = Vector{Symbol}()
+    variable_costs = Vector{Array{NTuple{2, Float64}}}()
+    col_names = TS.colnames(val)
+    for c in col_names
+        if occursin("cost_bp", String(c))
+            push!(cost_col, c)
+        elseif occursin("load_bp", String(c))
+            push!(load_col, c)
+        end
+    end
+    for row in DataFrames.eachrow(DataFrames.DataFrame(val))
+        push!(variable_costs, [Tuple(row[[c, l]]) for (c, l) in zip(cost_col, load_col)])
+    end
+    return variable_costs
+end
+
 function cost_function!(
     psi_container::PSIContainer,
     service::SR,
     ::Type{StepwiseCostReserve},
 ) where {SR <: PSY.Reserve}
-
     use_forecast_data = model_uses_forecasts(psi_container)
     initial_time = model_initial_time(psi_container)
     @debug initial_time
@@ -193,23 +207,6 @@ function cost_function!(
     return
 end
 
-function _convert_to_variablecost(val::TS.TimeArray)
-    cost_col = Vector{Symbol}()
-    load_col = Vector{Symbol}()
-    variable_costs = Vector{Array{NTuple{2, Float64}}}()
-    col_names = TS.colnames(val)
-    for c in col_names
-        if occursin("cost_bp", String(c))
-            push!(cost_col, c)
-        elseif occursin("load_bp", String(c))
-            push!(load_col, c)
-        end
-    end
-    for row in DataFrames.eachrow(DataFrames.DataFrame(val))
-        push!(variable_costs, [Tuple(row[[c, l]]) for (c, l) in zip(cost_col, load_col)])
-    end
-    return variable_costs
-end
 
 function modify_device_model!(
     devices_template::Dict{Symbol, DeviceModel},
