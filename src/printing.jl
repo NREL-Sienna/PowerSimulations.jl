d5787ec2fe9c1ff2d6c88d448c0dbf601d515988
Jose Daniel Lara <jdlara@berkeley.edu>
2020-10-05 16:49:56 -0700

    remove stale comments

--- src/printing.jl
+++ src/printing.jl
@@ -61,28 +61,18 @@ end
 function Base.show(io::IO, psi_container::PSIContainer)
     println(io, "PSIContainer()")
 end
-#=
-function Base.show(io::IO, op_problem::SimulationSequence)
-    # Here ASCII Art
-    println(io, "SimulationSequence()")
-end
-=#
+
 function Base.show(io::IO, sim::Simulation)
     println(io, "Simulation()")
 end
-#=
-function Base.show(io::IO, results::OperationsProblemResults)
-    println(io, "Results Model")
- end
 
-=#
 
 function Base.show(io::IO, ::MIME"text/plain", results::PSIResults)
     println(io, "\nResults")
     println(io, "========\n")
     println(io, "Variables")
     println(io, "=========\n")
-    times = IS.get_time_stamp(results)
+    times = get_timestamp(results)
     variables = IS.get_variables(results)
     if (length(keys(variables)) > 5)
         for (k, v) in variables
@@ -141,7 +131,7 @@ end
 function Base.show(io::IO, ::MIME"text/html", results::PSIResults)
     println(io, "<h1>Results</h1>")
     println(io, "<h2>Variables</h2>")
-    times = IS.get_time_stamp(results)
+    times = get_timestamp(results)
     variables = IS.get_variables(results)
     if (length(keys(variables)) > 5)
         for (k, v) in variables
