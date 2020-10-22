0d266523b72e276d26a69d10377730f113867153
Barrows <clayton.barrows@nrel.gov>
2020-10-22 06:35:54 -0600

    resolving  time_seriies access for StaticiGroupReserve

--- src/services_models/GroupReserve.jl
+++ src/services_models/GroupReserve.jl
@@ -36,7 +36,8 @@ function service_requirement_constraint!(
         for r in contributing_services
     ]
 
-    ts_vector = get_time_series(psi_container, service, "requirement")
+    ts_vectors = [get_time_series(psi_container, s, "requirement") for s in contributing_services]
+    ts_vector = sum(hcat(ts_vectors...), dims = 2)
 
     requirement = PSY.get_requirement(service)
     if parameters
