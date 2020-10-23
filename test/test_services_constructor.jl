37e14409d543ba57e80632b53a1e6f2b8d8397b9
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2020-10-23 16:59:15 -0600

    updating service tests to add ReserveDemandCurve

--- test/test_services_constructor.jl
+++ test/test_services_constructor.jl
@@ -195,7 +195,9 @@ end
     services = get_components(Service, c_sys5_uc)
     contributing_services = Vector{Service}()
     for service in services
-        push!(contributing_services, service)
+        if !(typeof(service) <: PSY.ReserveDemandCurve)
+            push!(contributing_services, service)
+        end
     end
     groupservice = StaticReserveGroup{ReserveDown}(;
         name = "init",
@@ -234,7 +236,9 @@ end
     services = get_components(Service, c_sys5_uc)
     contributing_services = Vector{Service}()
     for service in services
-        push!(contributing_services, service)
+        if !(typeof(service) <: PSY.ReserveDemandCurve)
+            push!(contributing_services, service)
+        end
     end
     groupservice = StaticReserveGroup{ReserveDown}(;
         name = "init",
