fdf72a286fd6ccd6bfb0c9034b09a3a9e0e21d4e
Jose Daniel Lara <jdlara@berkeley.edu>
2020-12-15 16:56:02 -0800

    add selective test

--- test/runtests.jl
+++ test/runtests.jl
@@ -77,6 +77,42 @@ function get_logging_level(env_name::String, default)
     return log_level
 end
 
+"""
+Includes the given test files, given as a list without their ".jl" extensions.
+If none are given it will scan the directory of the calling file and include all
+the julia files.
+"""
+macro includetests(testarg...)
+    if length(testarg) == 0
+        tests = []
+    elseif length(testarg) == 1
+        tests = testarg[1]
+    else
+        error("@includetests takes zero or one argument")
+    end
+
+    quote
+        tests = $tests
+        rootfile = @__FILE__
+        if length(tests) == 0
+            tests = readdir(dirname(rootfile))
+            tests = filter(
+                f ->
+                    startswith(f, "test_") && endswith(f, ".jl") && f != basename(rootfile),
+                tests,
+            )
+        else
+            tests = map(f -> string(f, ".jl"), tests)
+        end
+        println()
+        for test in tests
+            print(splitext(test)[1], ": ")
+            include(test)
+            println()
+        end
+    end
+end
+
 function run_tests()
     console_level = get_logging_level("SYS_CONSOLE_LOG_LEVEL", "Error")
     console_logger = ConsoleLogger(stderr, console_level)
