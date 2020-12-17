80a0b159100f4aa6f9cde928a0f131ec8e628f2d
Jose Daniel Lara <jdlara@berkeley.edu>
2020-12-17 11:30:51 -0800

    add more tests

--- test/test_simulation_execute.jl
+++ test/test_simulation_execute.jl
@@ -26,6 +26,7 @@ function test_simulation_single_ed(file_path::String)
         stage_single = PSI.get_stage(sim_single, "ED")
         @test JuMP.termination_status(stage_single.internal.psi_container.JuMPmodel) in
               [MOI.OPTIMAL, MOI.LOCALLY_SOLVED]
+        _test_plain_print_methods([sim_single, sim_single.sequence])
     end
 end
 
@@ -100,9 +101,60 @@ function test_simulation_without_caches(file_path::String)
 end
 
 function test_simulation_with_cache(file_path::String)
-    @testset "Simulation with Cache" begin
-        c_sys5_hy_uc = build_system("c_sys5_hy_uc")
-        c_sys5_hy_ed = build_system("c_sys5_hy_ed")
+    c_sys5_hy_uc = build_system("c_sys5_hy_uc")
+    c_sys5_hy_ed = build_system("c_sys5_hy_ed")
+    @testset "Simulation Single Stage with Cache" begin
+        single_stage_definition = Dict(
+            "ED" => Stage(
+                GenericOpProblem,
+                template_hydro_st_ed,
+                c_sys5_hy_ed,
+                GLPK_optimizer,
+            ),
+        )
+
+        single_sequence = SimulationSequence(
+            step_resolution = Hour(1),
+            order = Dict(1 => "ED"),
+            horizons = Dict("ED" => 12),
+            intervals = Dict("ED" => (Hour(1), Consecutive())),
+            cache = Dict(("ED",) => StoredEnergy(PSY.HydroEnergyReservoir, PSI.ENERGY)),
+            ini_cond_chronology = IntraStageChronology(),
+        )
+
+        sim_single_wcache = Simulation(
+            name = "cache_st",
+            steps = 2,
+            stages = single_stage_definition,
+            stages_sequence = single_sequence,
+            simulation_folder = file_path,
+        )
+        build_out = build!(sim_single_wcache)
+        @test build_out == PSI.BUILT
+        execute_out = execute!(sim_single_wcache)
+        @test execute_out == PSI.SUCCESSFUL_RUN
+
+        #=
+        @testset "Test verify initial condition update using StoredEnergy cache" begin
+            ic_keys = [PSI.ICKey(PSI.EnergyLevel, PSY.HydroEnergyReservoir)]
+            vars_names = [PSI.make_variable_name(PSI.ENERGY, PSY.HydroEnergyReservoir)]
+            for (ik, key) in enumerate(ic_keys)
+                variable_ref =
+                    PSI.get_reference(sim_cache_results, "ED", 1, vars_names[ik])[1]
+                initial_conditions =
+                    get_initial_conditions(PSI.get_psi_container(sim_single, "ED"), key)
+                for ic in initial_conditions
+                    raw_result =
+                        PSI.read_arrow_file(variable_ref)[end, Symbol(PSI.device_name(ic))] # last value of last hour
+                    initial_cond = value(PSI.get_value(ic))
+                    @test isapprox(raw_result, initial_cond)
+                end
+            end
+        end
+        =#
+    end
+
+    @testset "Simulation with 2-Stages and Cache" begin
         stages_definition = Dict(
             "UC" => Stage(
                 GenericOpProblem,
@@ -185,10 +237,154 @@ function test_simulation_with_cache(file_path::String)
                 end
             end
         end
+
+        @testset "Test verify initial condition feedforward for consecutive ED to UC" begin
+            ic_keys = [PSI.ICKey(PSI.DevicePower, PSY.ThermalStandard)]
+            vars_names = [PSI.make_variable_name(PSI.ACTIVE_POWER, PSY.ThermalStandard)]
+            for (ik, key) in enumerate(ic_keys)
+                variable_ref = PSI.get_reference(sim_results, "ED", 1, vars_names[ik])[24]
+                initial_conditions =
+                    get_initial_conditions(PSI.get_psi_container(sim, "UC"), key)
+                for ic in initial_conditions
+                    name = PSI.device_name(ic)
+                    raw_result = PSI.read_arrow_file(variable_ref)[end, Symbol(name)] # last value of last hour
+                    initial_cond = value(PSI.get_value(ic))
+                    @test isapprox(raw_result, initial_cond; atol = 1e-2)
+                end
+            end
+        end
+
+        @testset "Test verify parameter feedforward for consecutive UC to ED" begin
+            P_keys = [
+                (PSI.ACTIVE_POWER, PSY.HydroEnergyReservoir),
+                #(PSI.ON, PSY.ThermalStandard),
+                #(PSI.ACTIVE_POWER, PSY.HydroEnergyReservoir),
+            ]
+
+            vars_names = [
+                PSI.make_variable_name(PSI.ACTIVE_POWER, PSY.HydroEnergyReservoir),
+                #PSI.make_variable_name(PSI.ON, PSY.ThermalStandard),
+                #PSI.make_variable_name(PSI.ACTIVE_POWER, PSY.HydroEnergyReservoir),
+            ]
+            for (ik, key) in enumerate(P_keys)
+                variable_ref = PSI.get_reference(sim_results, "UC", 1, vars_names[ik])[1] # 1 is first step
+                array = PSI.get_parameter_array(PSI.get_parameter_container(
+                    sim.stages["ED"].internal.psi_container,
+                    Symbol(key[1]),
+                    key[2],
+                ))
+                parameter = collect(values(value.(array.data)))  # [device, time] 1 is first execution
+                raw_result = PSI.read_arrow_file(variable_ref)
+                for j in 1:size(parameter, 1)
+                    result = raw_result[end, j] # end is last result [time, device]
+                    initial = parameter[1] # [device, time]
+                    @test isapprox(initial, result)
+                end
+            end
+        end
+
+
         =#
+
+
     end
 end
+#=
+function test_stage_chronologies(file_path)
+    stages_definition = Dict(
+        "UC" => Stage(
+            GenericOpProblem,
+            template_hydro_basic_uc,
+            c_sys5_hy_uc,
+            GLPK_optimizer,
+        ),
+        "ED" =>
+            Stage(GenericOpProblem, template_hydro_ed, c_sys5_hy_ed, ipopt_optimizer),
+    )
 
+    sequence = SimulationSequence(
+        order = Dict(1 => "UC", 2 => "ED"),
+        step_resolution = Hour(24),
+        feedforward_chronologies = Dict(("UC" => "ED") => RecedingHorizon()),
+        horizons = Dict("UC" => 24, "ED" => 12),
+        intervals = Dict(
+            "UC" => (Hour(24), RecedingHorizon()),
+            "ED" => (Minute(60), RecedingHorizon()),
+        ),
+        feedforward = Dict(
+            ("ED", :devices, :Generators) => SemiContinuousFF(
+                binary_source_stage = PSI.ON,
+                affected_variables = [PSI.ACTIVE_POWER],
+            ),
+        ),
+        ini_cond_chronology = InterStageChronology(),
+    )
+
+    sim = Simulation(
+        name = "receding_horizon",
+        steps = 2,
+        stages = stages_definition,
+        stages_sequence = sequence,
+        simulation_folder = file_path,
+    )
+    build!(sim)
+    sim_results = execute!(sim)
+
+    @testset "Test verify time gap for Receding Horizon" begin
+        names = ["UC"] # TODO why doesn't this work for ED??
+        for name in names
+            variable_list = PSI.get_variable_names(sim, name)
+            reference_1 = PSI.get_reference(sim_results, name, 1, variable_list[1])[1]
+            reference_2 = PSI.get_reference(sim_results, name, 2, variable_list[1])[1]
+            time_file_path_1 = joinpath(dirname(reference_1), "time_stamp.arrow") #first line, file path
+            time_file_path_2 = joinpath(dirname(reference_2), "time_stamp.arrow")
+            time_1 = convert(Dates.DateTime, PSI.read_arrow_file(time_file_path_1)[1, 1]) # first time
+            time_2 = convert(Dates.DateTime, PSI.read_arrow_file(time_file_path_2)[1, 1])
+            time_change = time_2 - time_1
+            interval = PSI.get_stage_interval(PSI.get_sequence(sim), name)
+            @test Dates.Hour(time_change) == Dates.Hour(interval)
+        end
+    end
+
+    @testset "Test verify parameter feedforward for Receding Horizon" begin
+        P_keys = [(PSI.ON, PSY.ThermalStandard)]
+        vars_names = [PSI.make_variable_name(PSI.ON, PSY.ThermalStandard)]
+        for (ik, key) in enumerate(P_keys)
+            variable_ref = PSI.get_reference(sim_results, "UC", 2, vars_names[ik])[1]
+            raw_result = PSI.read_arrow_file(variable_ref)
+            ic = PSI.get_parameter_array(PSI.get_parameter_container(
+                sim.stages["ED"].internal.psi_container,
+                Symbol(key[1]),
+                key[2],
+            ))
+            for name in names(raw_result)
+                result = raw_result[1, name] # first time period of results  [time, device]
+                initial = value(ic[String(name)]) # [device, time]
+                @test isapprox(initial, result, atol = 1.0e-4)
+            end
+        end
+    end
+
+    @testset "Test verify initial condition feedforward for Receding Horizon" begin
+        results = load_simulation_results(sim_results, "ED")
+        ic_keys = [PSI.ICKey(PSI.DevicePower, PSY.ThermalStandard)]
+        vars_names = [PSI.make_variable_name(PSI.ACTIVE_POWER, PSY.ThermalStandard)]
+        ed_horizon = PSI.get_stage_horizon(sim.sequence, "ED")
+        no_steps = PSI.get_steps(sim)
+        for (ik, key) in enumerate(ic_keys)
+            initial_conditions =
+                get_initial_conditions(PSI.get_psi_container(sim, "UC"), key)
+            vars = results.variable_values[vars_names[ik]] # change to getter function
+            for ic in initial_conditions
+                output = vars[ed_horizon * (no_steps - 1), Symbol(PSI.device_name(ic))] # change to getter function
+                initial_cond = value(PSI.get_value(ic))
+                @test isapprox(output, initial_cond, atol = 1.0e-4)
+            end
+        end
+    end
+
+end
+=#
 function test_simulation_utils(file_path)
     stage_info = Dict(
         "UC" => Dict("optimizer" => GLPK_optimizer, "jump_model" => nothing),
@@ -306,7 +502,10 @@ function test_simulation_utils(file_path)
         end
     end
 
-
+    @testset "Test print methods" begin
+        list = [sim, sim.sequence, sim.stages["UC"]]
+        _test_plain_print_methods(list)
+    end
 end
 
 @testset "Test simulation execution" begin
