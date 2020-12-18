9fe6bfd00642d8913b79c07b1f151465d01d49b2
Jose Daniel Lara <jdlara@berkeley.edu>
2020-12-18 15:40:29 -0800

    use correct bound

--- src/devices_models/devices/renewable_generation.jl
+++ src/devices_models/devices/renewable_generation.jl
@@ -12,7 +12,7 @@ get_variable_binary(::ActivePowerVariable, ::Type{<:PSY.RenewableGen}) = false
 get_variable_expression_name(::ActivePowerVariable, ::Type{<:PSY.RenewableGen}) = :nodal_balance_active
 
 get_variable_lower_bound(::ActivePowerVariable, d::PSY.RenewableGen, _) = 0.0
-get_variable_upper_bound(::ActivePowerVariable, d::PSY.RenewableGen, _) = PSY.get_active_power(d)
+get_variable_upper_bound(::ActivePowerVariable, d::PSY.RenewableGen, _) = PSY.get_max_active_power(d)
 
 ########################### ReactivePowerVariable, RenewableGen #################################
 
