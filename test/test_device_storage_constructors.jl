9314e8cf4fe018be9a2e4e0cb66a4fd85ca2c0b7
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2021-01-12 19:46:23 -0700

    adding storage tests

--- test/test_device_storage_constructors.jl
+++ test/test_device_storage_constructors.jl
@@ -45,3 +45,93 @@ end
     moi_tests(op_problem, false, 120, 0, 96, 96, 24, true)
     psi_checkobjfun_test(op_problem, GAEVF)
 end
+
+@testset "BatteryEMS with EndOfPeriodStaticEnergyTarget with DC - PF" begin
+    model = DeviceModel(BatteryEMS, EndOfPeriodStaticEnergyTarget)
+    c_sys5_bat = build_system("c_sys5_bat_ems")
+    op_problem = OperationsProblem(TestOpProblem, DCPPowerModel, c_sys5_bat)
+    construct_device!(op_problem, :Storage, model)
+    moi_tests(op_problem, false, 72, 0, 72, 73, 24, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+end
+
+@testset "BatteryEMS with EndOfPeriodStaticEnergyTarget With AC - PF" begin
+    model = DeviceModel(BatteryEMS, EndOfPeriodStaticEnergyTarget)
+    c_sys5_bat = build_system("c_sys5_bat_ems")
+    op_problem = OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_bat)
+    construct_device!(op_problem, :Storage, model)
+    moi_tests(op_problem, false, 96, 0, 96, 97, 24, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+end
+
+@testset "BatteryEMS with EndOfPeriodEnergyTarget with DC - PF" begin
+    model = DeviceModel(BatteryEMS, EndOfPeriodEnergyTarget)
+    c_sys5_bat = build_system("c_sys5_bat_ems")
+    op_problem = OperationsProblem(TestOpProblem, DCPPowerModel, c_sys5_bat)
+    construct_device!(op_problem, :Storage, model)
+    moi_tests(op_problem, false, 72, 0, 72, 73, 24, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+end
+
+@testset "BatteryEMS with EndOfPeriodEnergyTarget With AC - PF" begin
+    model = DeviceModel(BatteryEMS, EndOfPeriodEnergyTarget)
+    c_sys5_bat = build_system("c_sys5_bat_ems")
+    op_problem = OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_bat)
+    construct_device!(op_problem, :Storage, model)
+    moi_tests(op_problem, false, 96, 0, 96, 97, 24, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+end
+
+@testset "BatteryEMS with EndOfPeriodStaticEnergySoftTarget with DC - PF" begin
+    model = DeviceModel(BatteryEMS, EndOfPeriodStaticEnergySoftTarget)
+    c_sys5_bat = build_system("c_sys5_bat_ems")
+    op_problem = OperationsProblem(TestOpProblem, DCPPowerModel, c_sys5_bat)
+    construct_device!(op_problem, :Storage, model)
+    moi_tests(op_problem, false, 96, 0, 72, 73, 24, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+end
+
+@testset "BatteryEMS with EndOfPeriodStaticEnergySoftTarget With AC - PF" begin
+    model = DeviceModel(BatteryEMS, EndOfPeriodStaticEnergySoftTarget)
+    c_sys5_bat = build_system("c_sys5_bat_ems")
+    op_problem = OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_bat)
+    construct_device!(op_problem, :Storage, model)
+    moi_tests(op_problem, false, 120, 0, 96, 97, 24, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+end
+
+@testset "BatteryEMS with EndOfPeriodEnergySoftTarget with DC - PF" begin
+    model = DeviceModel(BatteryEMS, EndOfPeriodEnergySoftTarget)
+    c_sys5_bat = build_system("c_sys5_bat_ems")
+    op_problem = OperationsProblem(TestOpProblem, DCPPowerModel, c_sys5_bat)
+    construct_device!(op_problem, :Storage, model)
+    moi_tests(op_problem, false, 96, 0, 72, 73, 24, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+end
+
+@testset "BatteryEMS with EndOfPeriodEnergySoftTarget With AC - PF" begin
+    model = DeviceModel(BatteryEMS, EndOfPeriodEnergySoftTarget)
+    c_sys5_bat = build_system("c_sys5_bat_ems")
+    op_problem = OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_bat)
+    construct_device!(op_problem, :Storage, model)
+    moi_tests(op_problem, false, 120, 0, 96, 97, 24, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+end
+
+@testset "BatteryEMS with EndOfPeriodEnergyValue with DC - PF" begin
+    model = DeviceModel(BatteryEMS, EndOfPeriodEnergyValue)
+    c_sys5_bat = build_system("c_sys5_bat_ems")
+    op_problem = OperationsProblem(TestOpProblem, DCPPowerModel, c_sys5_bat)
+    construct_device!(op_problem, :Storage, model)
+    moi_tests(op_problem, false, 72, 0, 72, 72, 24, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+end
+
+@testset "BatteryEMS with EndOfPeriodEnergyValue With AC - PF" begin
+    model = DeviceModel(BatteryEMS, EndOfPeriodEnergyValue)
+    c_sys5_bat = build_system("c_sys5_bat_ems")
+    op_problem = OperationsProblem(TestOpProblem, ACPPowerModel, c_sys5_bat)
+    construct_device!(op_problem, :Storage, model)
+    moi_tests(op_problem, false, 96, 0, 96, 96, 24, false)
+    psi_checkobjfun_test(op_problem, GAEVF)
+end
