58ae9f26d8cc746c8d8b09e63521e5c7256652b3
Jose Daniel Lara <jdlara@berkeley.edu>
2021-02-09 23:52:54 -0800

    enable ascii art tests

--- test/test_simulation_sequence.jl
+++ test/test_simulation_sequence.jl
@@ -1,5 +1,5 @@
 @testset "Simulation Sequence Correct Execution Order" begin
-problems =  SimulationProblems(
+    problems = SimulationProblems(
         DAUC = OperationsProblem(MockOperationProblem; horizon = 48),
         HAUC = OperationsProblem(MockOperationProblem; horizon = 24),
         ED = OperationsProblem(MockOperationProblem; horizon = 12),
@@ -34,7 +34,9 @@ problems =  SimulationProblems(
     end
 
     test_sequence = SimulationSequence(
-        problems = SimulationProblems(DAUC = OperationsProblem(MockOperationProblem; horizon = 48)),
+        problems = SimulationProblems(
+            DAUC = OperationsProblem(MockOperationProblem; horizon = 48),
+        ),
         intervals = Dict("DAUC" => (Hour(24), Consecutive())),
         ini_cond_chronology = InterStageChronology(),
     )
@@ -61,7 +63,7 @@ end
     )
 
     @test_throws IS.ConflictingInputsError SimulationSequence(
-       problems = problems = mock_uc_ed_simulation_problems(24, 12),
+        problems = mock_uc_ed_simulation_problems(24, 12),
         feedforward_chronologies = Dict(("UC" => "ED") => Synchronize(periods = 24)),
         intervals = Dict(
             "UC" => (Hour(2), Consecutive()),
@@ -78,108 +80,93 @@ end
     )
 end
 
-#= ASCII printing broken
-    @testset "Test print methods of sequence ascii art" begin
-        sequence_2 = SimulationSequence(
-            order = Dict(1 => "UC", 2 => "ED"),
-            step_resolution = Hour(1),
-            feedforward_chronologies = Dict(("UC" => "ED") => RecedingHorizon(periods = 2)),
-            horizons = Dict("UC" => 24, "ED" => 12),
-            intervals = Dict(
-                "UC" => (Hour(1), RecedingHorizon()),
-                "ED" => (Minute(5), RecedingHorizon()),
+@testset "Test print methods of sequence ascii art" begin
+    sequence_2 = SimulationSequence(
+        problems = mock_uc_ed_simulation_problems(24, 12),
+        feedforward_chronologies = Dict(("UC" => "ED") => RecedingHorizon(periods = 2)),
+        intervals = Dict(
+            "UC" => (Hour(1), RecedingHorizon()),
+            "ED" => (Minute(5), RecedingHorizon()),
+        ),
+        feedforward = Dict(
+            ("ED", :devices, :Generators) => SemiContinuousFF(
+                binary_source_stage = PSI.ON,
+                affected_variables = [PSI.ACTIVE_POWER],
             ),
-            feedforward = Dict(
-                ("ED", :devices, :Generators) => SemiContinuousFF(
-                    binary_source_stage = PSI.ON,
-                    affected_variables = [PSI.ACTIVE_POWER],
-                ),
-            ),
-            ini_cond_chronology = InterStageChronology(),
-        )
+        ),
+        ini_cond_chronology = InterStageChronology(),
+    )
 
-        sequence_4 = SimulationSequence(
-            order = Dict(1 => "UC", 2 => "ED"),
-            step_resolution = Hour(1),
-            feedforward_chronologies = Dict(("UC" => "ED") => RecedingHorizon(periods = 4)),
-            horizons = Dict("UC" => 24, "ED" => 12),
-            intervals = Dict(
-                "UC" => (Hour(1), RecedingHorizon()),
-                "ED" => (Minute(5), RecedingHorizon()),
+    sequence_4 = SimulationSequence(
+        problems = mock_uc_ed_simulation_problems(24, 12),
+        feedforward_chronologies = Dict(("UC" => "ED") => RecedingHorizon(periods = 4)),
+        intervals = Dict(
+            "UC" => (Hour(1), RecedingHorizon()),
+            "ED" => (Minute(5), RecedingHorizon()),
+        ),
+        feedforward = Dict(
+            ("ED", :devices, :Generators) => SemiContinuousFF(
+                binary_source_stage = PSI.ON,
+                affected_variables = [PSI.ACTIVE_POWER],
             ),
-            feedforward = Dict(
-                ("ED", :devices, :Generators) => SemiContinuousFF(
-                    binary_source_stage = PSI.ON,
-                    affected_variables = [PSI.ACTIVE_POWER],
-                ),
-            ),
-            ini_cond_chronology = InterStageChronology(),
-        )
+        ),
+        ini_cond_chronology = InterStageChronology(),
+    )
 
-        sequence_3 = SimulationSequence(
-            order = Dict(1 => "UC", 2 => "ED"),
-            step_resolution = Hour(1),
-            feedforward_chronologies = Dict(("UC" => "ED") => RecedingHorizon(periods = 3)),
-            horizons = Dict("UC" => 24, "ED" => 12),
-            intervals = Dict(
-                "UC" => (Hour(1), RecedingHorizon()),
-                "ED" => (Minute(5), RecedingHorizon()),
+    sequence_3 = SimulationSequence(
+        problems = mock_uc_ed_simulation_problems(24, 12),
+        feedforward_chronologies = Dict(("UC" => "ED") => RecedingHorizon(periods = 3)),
+        intervals = Dict(
+            "UC" => (Hour(1), RecedingHorizon()),
+            "ED" => (Minute(5), RecedingHorizon()),
+        ),
+        feedforward = Dict(
+            ("ED", :devices, :Generators) => SemiContinuousFF(
+                binary_source_stage = PSI.ON,
+                affected_variables = [PSI.ACTIVE_POWER],
             ),
-            feedforward = Dict(
-                ("ED", :devices, :Generators) => SemiContinuousFF(
-                    binary_source_stage = PSI.ON,
-                    affected_variables = [PSI.ACTIVE_POWER],
-                ),
-            ),
-            ini_cond_chronology = InterStageChronology(),
-        )
+        ),
+        ini_cond_chronology = InterStageChronology(),
+    )
 
-        sequence_5 = SimulationSequence(
-            order = Dict(1 => "UC", 2 => "ED"),
-            step_resolution = Hour(1),
-            feedforward_chronologies = Dict(("UC" => "ED") => RecedingHorizon(periods = 2)),
-            horizons = Dict("UC" => 24, "ED" => 12),
-            intervals = Dict(
-                "UC" => (Hour(1), RecedingHorizon()),
-                "ED" => (Minute(5), RecedingHorizon()),
+    sequence_5 = SimulationSequence(
+        problems = mock_uc_ed_simulation_problems(24, 12),
+        feedforward_chronologies = Dict(("UC" => "ED") => RecedingHorizon(periods = 2)),
+        intervals = Dict(
+            "UC" => (Hour(1), RecedingHorizon()),
+            "ED" => (Minute(5), RecedingHorizon()),
+        ),
+        feedforward = Dict(
+            ("ED", :devices, :Generators) => RangeFF(
+                variable_source_stage_ub = PSI.ON,
+                variable_source_stage_lb = PSI.ON,
+                affected_variables = [PSI.ACTIVE_POWER],
             ),
-            feedforward = Dict(
-                ("ED", :devices, :Generators) => RangeFF(
-                    variable_source_stage_ub = PSI.ON,
-                    variable_source_stage_lb = PSI.ON,
-                    affected_variables = [PSI.ACTIVE_POWER],
-                ),
-            ),
-            ini_cond_chronology = InterStageChronology(),
-        )
+        ),
+        ini_cond_chronology = InterStageChronology(),
+    )
 
-        sequence_13 = SimulationSequence(
-            order = Dict(1 => "UC", 2 => "ED"),
-            step_resolution = Hour(1),
-            feedforward_chronologies = Dict(
-                ("UC" => "ED") => RecedingHorizon(periods = 13),
+    sequence_13 = SimulationSequence(
+        problems = mock_uc_ed_simulation_problems(24, 12),
+        feedforward_chronologies = Dict(("UC" => "ED") => RecedingHorizon(periods = 13)),
+        intervals = Dict(
+            "UC" => (Hour(1), RecedingHorizon()),
+            "ED" => (Minute(5), RecedingHorizon()),
+        ),
+        feedforward = Dict(
+            ("ED", :devices, :Generators) => SemiContinuousFF(
+                binary_source_stage = PSI.ON,
+                affected_variables = [PSI.ACTIVE_POWER],
             ),
-            horizons = Dict("UC" => 24, "ED" => 12),
-            intervals = Dict(
-                "UC" => (Hour(1), RecedingHorizon()),
-                "ED" => (Minute(5), RecedingHorizon()),
-            ),
-            feedforward = Dict(
-                ("ED", :devices, :Generators) => SemiContinuousFF(
-                    binary_source_stage = PSI.ON,
-                    affected_variables = [PSI.ACTIVE_POWER],
-                ),
-            ),
-            ini_cond_chronology = InterStageChronology(),
-        )
-        list = [sequence_2, sequence_3, sequence_4, sequence_5, sequence_13]
-        _test_plain_print_methods(list)
-        stage_1 = FakeStagesStruct(Dict(1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6)) # testing 5 stages
-        stage_3 = FakeStagesStruct(Dict(1 => 1, 2 => 100)) #testing 3 digits
-        stage_4 = FakeStagesStruct(Dict(1 => 1, 2 => 1000)) #testing 4 digits
-        stage_12 = FakeStagesStruct(Dict(1 => 1, 2 => 12, 3 => 5, 4 => 6))
-        list = [stage_1, stage_3, stage_4, stage_12]
-        _test_plain_print_methods(list)
-    end
+        ),
+        ini_cond_chronology = InterStageChronology(),
+    )
+    list = [sequence_2, sequence_3, sequence_4, sequence_5, sequence_13]
+    _test_plain_print_methods(list)
+    stage_1 = FakeStagesStruct(Dict(1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6)) # testing 5 stages
+    stage_3 = FakeStagesStruct(Dict(1 => 1, 2 => 100)) #testing 3 digits
+    stage_4 = FakeStagesStruct(Dict(1 => 1, 2 => 1000)) #testing 4 digits
+    stage_12 = FakeStagesStruct(Dict(1 => 1, 2 => 12, 3 => 5, 4 => 6))
+    list = [stage_1, stage_3, stage_4, stage_12]
+    _test_plain_print_methods(list)
 end
-=#
