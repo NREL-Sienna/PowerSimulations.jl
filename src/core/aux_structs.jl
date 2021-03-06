152cbd7fd47b6a815c25e6d45a44b57eb48adf10
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2021-03-05 18:25:00 -0700

    updating UpdateRef to take 2 refs

--- src/core/aux_structs.jl
+++ src/core/aux_structs.jl
@@ -1,10 +1,16 @@
 """Reference for parameters update when present"""
 struct UpdateRef{T}
-    access_ref::Symbol
+    access_ref::Union{Symbol, Tuple{Symbol, Symbol}}
     data_label::Union{Nothing, String}
 end
 
 function UpdateRef{T}(name::Symbol) where {T <: Union{JuMP.VariableRef, PJ.ParameterRef}}
+    return UpdateRef{T}(name, nothing)
+end
+
+function UpdateRef{T}(
+    name::Tuple{Symbol, Symbol},
+) where {T <: Union{JuMP.VariableRef, PJ.ParameterRef}}
     return UpdateRef{T}(name, nothing)
 end
 
@@ -16,10 +22,24 @@ function UpdateRef{T}(
 end
 
 function UpdateRef{T}(
+    name::Tuple{String, String},
+    data_label::Union{Nothing, String} = nothing,
+) where {T <: Union{JuMP.VariableRef, PJ.ParameterRef}}
+    return UpdateRef{T}(Symbol.(name), data_label)
+end
+
+function UpdateRef{T}(
     ::Type{U},
     name::AbstractString,
 ) where {T <: Union{JuMP.VariableRef, PJ.ParameterRef}, U <: PSY.Component}
     return UpdateRef{T}(encode_symbol(U, name), nothing)
+end
+
+function UpdateRef{T}(
+    ::Type{U},
+    name::Tuple{AbstractString, AbstractString},
+) where {T <: Union{JuMP.VariableRef, PJ.ParameterRef}, U <: PSY.Component}
+    return UpdateRef{T}((encode_symbol(U, name[1]), encode_symbol(U, name[2])), nothing)
 end
 
 function UpdateRef{T}(
@@ -28,6 +48,14 @@ function UpdateRef{T}(
 ) where {T <: PSY.Component}
     # Combine these three fields together in order to guarantee uniqueness.
     return UpdateRef{T}(encode_symbol(T, name, data_label), data_label)
+end
+
+function UpdateRef{T}(
+    name::Tuple{AbstractString, AbstractString},
+    data_label::AbstractString,
+) where {T <: PSY.Component}
+    # Combine these three fields together in order to guarantee uniqueness.
+    return UpdateRef{T}((encode_symbol(T, name[1], data_label), encode_symbol(T, name[2], data_label)), data_label)
 end
 
 function get_data_label(ref::UpdateRef{T}) where {T <: PSY.Component}
