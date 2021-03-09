f7ba959716577a0500233be86fd25e32910cb6df
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2021-03-09 16:24:10 -0700

    updating storage initial conditions

--- src/devices_models/devices/storage.jl
+++ src/devices_models/devices/storage.jl
@@ -33,6 +33,7 @@ get_variable_expression_name(::ReactivePowerVariable, ::Type{<:PSY.Storage}) = :
 
 get_variable_binary(::EnergyVariable, ::Type{<:PSY.Storage}, ::AbstractStorageFormulation) = false
 get_variable_lower_bound(::EnergyVariable, d::PSY.Storage, ::AbstractStorageFormulation) = 0.0
+get_variable_initial_value(::EnergyVariable, d::PSY.Storage, ::AbstractStorageFormulation) = PSY.get_initial_energy(d)
 
 ############## ReserveVariable, Storage ####################
 
@@ -180,24 +181,24 @@ function initial_conditions!(
     devices::IS.FlattenIteratorWrapper{St},
     formulation::AbstractStorageFormulation,
 ) where {St <: PSY.Storage}
-    storage_energy_init(optimization_container, devices, formulation)
+    storage_energy_initial_condition!(optimization_container, devices, formulation)
     return
 end
 
 ######################### Initialize Functions for Storage #################################
 # TODO: This IC needs a cache for Simulation over long periods of tim
-function storage_energy_init(
+function storage_energy_initial_condition!(
     optimization_container::OptimizationContainer,
     devices::IS.FlattenIteratorWrapper{T},
-    ::AbstractStorageFormulation,
-) where {T <: PSY.Storage}
+    ::D,
+) where {T <: PSY.Storage, D <: AbstractStorageFormulation}
     key = ICKey(EnergyLevel, T)
     _make_initial_conditions!(
         optimization_container,
         devices,
         key,
         _make_initial_condition_energy,
-        _get_initial_energy_value,
+        (x, y) -> get_variable_initial_value(EnergyVariable(), x, D()),
         StoredEnergy,
     )
 
