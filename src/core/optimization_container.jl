44ca6289eb2d44a262b97e25eed4dd04ef7c1591
Jose Daniel Lara <jdlara@berkeley.edu>
2021-03-11 17:22:22 -0800

    simplify timeroutputs print

--- src/core/optimization_container.jl
+++ src/core/optimization_container.jl
@@ -653,7 +653,7 @@ function build_impl!(
 )
     transmission = template.transmission
     # Order is required
-    TimerOutputs.@timeit BUILD_PROBLEMS_TIMER "Construct Services" begin
+    TimerOutputs.@timeit BUILD_PROBLEMS_TIMER "Services" begin
         construct_services!(
             optimization_container,
             sys,
@@ -663,13 +663,13 @@ function build_impl!(
     end
     for device_model in values(template.devices)
         @debug "Building $(device_model.component_type) with $(device_model.formulation) formulation"
-        TimerOutputs.@timeit BUILD_PROBLEMS_TIMER "Construct $(device_model.component_type)" begin
+        TimerOutputs.@timeit BUILD_PROBLEMS_TIMER "$(device_model.component_type)" begin
             construct_device!(optimization_container, sys, device_model, transmission)
             @debug get_problem_size(optimization_container)
         end
     end
 
-    TimerOutputs.@timeit BUILD_PROBLEMS_TIMER "Construct $(transmission)" begin
+    TimerOutputs.@timeit BUILD_PROBLEMS_TIMER "$(transmission)" begin
         @debug "Building $(transmission) network formulation"
         construct_network!(optimization_container, sys, transmission, template)
         @debug get_problem_size(optimization_container)
@@ -677,13 +677,13 @@ function build_impl!(
 
     for branch_model in values(template.branches)
         @debug "Building $(branch_model.component_type) with $(branch_model.formulation) formulation"
-        TimerOutputs.@timeit BUILD_PROBLEMS_TIMER "Construct $(branch_model.component_type)" begin
+        TimerOutputs.@timeit BUILD_PROBLEMS_TIMER "$(branch_model.component_type)" begin
             construct_device!(optimization_container, sys, branch_model, transmission)
             @debug get_problem_size(optimization_container)
         end
     end
 
-    TimerOutputs.@timeit BUILD_PROBLEMS_TIMER "Construct Objective" begin
+    TimerOutputs.@timeit BUILD_PROBLEMS_TIMER "Objective" begin
         @debug "Building Objective"
         JuMP.@objective(
             optimization_container.JuMPmodel,
