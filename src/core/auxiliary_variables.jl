1639bc3a205962effdf5901dcef91e225db13c34
Jose Daniel Lara <jdlara@berkeley.edu>
2021-05-07 16:41:59 -0700

    add aux variables containers

--- src/core/auxiliary_variables.jl
+++ src/core/auxiliary_variables.jl
@@ -1,7 +1,16 @@
+#! format: off
+
 abstract type AuxVariableType end
+
+struct AuxVarKey{T <: AuxVariableType, U <: PSY.Component} <: OptimizationContainerKey
+    aux_var_type::Type{T}
+    device_type::Type{U}
+end
 
 struct TimeDurationON <: AuxVariableType end
 struct TimeDurationOFF <: AuxVariableType end
 struct EnergyLevel <: AuxVariableType end
-struct EnergyLevelUP <: AuxVariableType end
-struct EnergyLevelDOWN <: AuxVariableType end
+
+make_variable_name(::Type{TimeDurationON}, ::Type{T}) where {T <: PSY.Component} = encode_symbol(T, "TimeON")
+
+make_variable_name(::Type{TimeDurationOFF}, ::Type{T}) where {T <: PSY.Component} = encode_symbol(T, "TimeOFF")
--- src/devices_models/devices/common/add_auxiliary_variable.jl
+++ src/devices_models/devices/common/add_auxiliary_variable.jl
@@ -12,54 +12,26 @@ end
 
 
 @doc raw"""
-Adds an auxiliary variable to the model. These variables are populated after the model is
-solved.
+Default implementation of adding auxiliary variable to the model.
 """
 function add_variable!(
     optimization_container::OptimizationContainer,
-    variable_type::AuxVariableType,
+    ::T,
     devices::U,
     formulation,
-) where {U <: Union{Vector{D}, IS.FlattenIteratorWrapper{D}}} where {D <: PSY.Component}
+) where {T <: AuxVariableType, U <: Union{Vector{D}, IS.FlattenIteratorWrapper{D}}} where {D <: PSY.Component}
     @assert !isempty(devices)
     time_steps = model_time_steps(optimization_container)
-    var_name = make_variable_name(typeof(variable_type), D)
     variable = add_aux_var_container!(
         optimization_container,
-        var_name,
+        AuxVarKey(T, D),
         [PSY.get_name(d) for d in devices],
         time_steps,
     )
 
     for t in time_steps, d in devices
         name = PSY.get_name(d)
-        variable[name, t] = JuMP.@variable(
-            optimization_container.JuMPmodel,
-            base_name = "$(var_name)_{$(name), $(t)}",
-            binary = binary
-        )
-
-        ub = get_variable_upper_bound(variable_type, d, formulation)
-        !(ub === nothing) && JuMP.set_upper_bound(variable[name, t], ub)
-
-        lb = get_variable_lower_bound(variable_type, d, formulation)
-        !(lb === nothing) && !binary && JuMP.set_lower_bound(variable[name, t], lb)
-
-        if get_warm_start(settings)
-            init = get_variable_initial_value(variable_type, d, formulation)
-            !(init === nothing) && JuMP.set_start_value(variable[name, t], init)
-        end
-
-        if !((expression_name === nothing))
-            bus_number = PSY.get_number(PSY.get_bus(d))
-            add_to_expression!(
-                get_expression(optimization_container, expression_name),
-                bus_number,
-                t,
-                variable[name, t],
-                get_variable_sign(variable_type, eltype(devices), formulation),
-            )
-        end
+        variable[name, t] = 0.0
     end
 
     return
