a588c6b5b75000f40e781a8d824451a02b6ca5a2
Jose Daniel Lara <jdlara@berkeley.edu>
2021-05-26 15:51:22 -0700

    standarize container keys

--- src/core/abstract_types.jl
+++ src/core/abstract_types.jl
@@ -1,5 +1,19 @@
 # All subtypes of AbstractAffectFeedForward must define the field affected_variables.
 # TODO: make a unit test that checks for this.
+
+abstract type OptimizationContainerKey end
+abstract type VariableType end
+abstract type AuxVariableType end
+
+function encode_key(key::OptimizationContainerKey)
+    return encode_symbol(key.entry_type, key.component_type, key.meta)
+end
+
+function encode_symbol(::Type{U}, ::Type{T}, meta::String = CONTAINER_KEY_EMPTY_META) where {T, U <: PSY.Component}
+    meta_ = isempty(meta) ? meta : "_"*meta
+    return Symbol("$(IS.strip_module_name(string(T)))_$(IS.strip_module_name(string(U)))"*meta_)
+end
+
 abstract type AbstractAffectFeedForward end
 
 abstract type AbstractCache end
--- src/core/auxiliary_variables.jl
+++ src/core/auxiliary_variables.jl
@@ -1,14 +1,11 @@
-#! format: off
-
-abstract type AuxVariableType end
-
 struct AuxVarKey{T <: AuxVariableType, U <: PSY.Component} <: OptimizationContainerKey
-    aux_var_type::Type{T}
-    device_type::Type{U}
+    entry_type::Type{T}
+    component_type::Type{U}
+    meta::String
 end
 
-function encode_key(::AuxVarKey{T, U}) where {T <: AuxVariableType, U <: PSY.Component}
-    return Symbol("$(IS.strip_module_name(string(T)))_$(IS.strip_module_name(string(U)))")
+function AuxVarKey(::Type{T}, ::Type{U}) where {T <: AuxVariableType, U <: PSY.Component}
+    return AuxVarKey(T, U, CONTAINER_KEY_EMPTY_META)
 end
 
 struct TimeDurationOn <: AuxVariableType end
