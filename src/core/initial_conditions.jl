d8740f144450fe1d76f2c2ec6b2fbf3581859452
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2021-06-25 12:30:24 -0600

    updating initial condition structs

--- src/core/initial_conditions.jl
+++ src/core/initial_conditions.jl
@@ -11,43 +11,5 @@ function ICKey(
     return ICKey{T, U}(meta)
 end
 
-mutable struct InitialConditions
-    use_parameters::Bool
-    data::Dict{ICKey, Vector{InitialCondition}}
-end
-
 get_entry_type(::ICKey{T, U}) where {T <: InitialConditionType, U <: PSY.Component} = T
 get_component_type(::ICKey{T, U}) where {T <: InitialConditionType, U <: PSY.Component} = U
-
-function InitialConditions(;
-    use_parameters = false,
-    data = Dict{ICKey, Array{InitialCondition}}(),
-)
-    return InitialConditions(use_parameters, data)
-end
-
-get_use_parameters(container::InitialConditions) = container.use_parameters
-set_use_parameters!(ini_cond::InitialConditions, val::Bool) = ini_cond.use_parameters = val
-
-function has_initial_conditions(container::InitialConditions, key::ICKey)
-    return key in keys(container.data)
-end
-
-function get_initial_conditions(container::InitialConditions, key::ICKey)
-    initial_conditions = get(container.data, key, nothing)
-    if initial_conditions === nothing
-        throw(IS.InvalidValue("initial conditions are not stored for $(key)"))
-    end
-
-    return initial_conditions
-end
-
-function set_initial_conditions!(container::InitialConditions, key::ICKey, value)
-    @debug "set_initial_condition_container" key
-    container.data[key] = value
-end
-
-"""
-Iterate over the keys and vectors of initial conditions.
-"""
-iterate_initial_conditions(container::InitialConditions) = pairs(container.data)
