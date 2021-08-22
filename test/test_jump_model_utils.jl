8a71eb45937b0ddbcfd95b0b24903364c5c7e8be
Jose Daniel Lara <jdlara@berkeley.edu>
2021-08-22 12:15:19 -0700

    adding method to calculate numerical bounds

--- test/test_jump_model_utils.jl
+++ test/test_jump_model_utils.jl
@@ -15,7 +15,7 @@ end
     c_sys5 = PSB.build_system(PSITestSystems, "c_sys5")
     valid_bounds =
         (coefficient = (min = 1.0, max = 1.0), rhs = (min = 0.4, max = 9.930296584))
-    model = OperationsProblem(template, c_sys5; optimizer = GLPK_optimizer)
+    model = DecisionModel(template, c_sys5; optimizer = GLPK_optimizer)
     @test build!(model; output_dir = mktempdir(cleanup = true)) == PSI.BuildStatus.BUILT
 
     bounds = PSI.get_constraint_numerical_bounds(model; verbose = false)
@@ -23,17 +23,20 @@ end
 
     model_bounds = PSI.get_constraint_numerical_bounds(model; verbose = true)
     valid_model_bounds = Dict(
-        :CopperPlateBalance => (
+        :CopperPlateBalanceConstraint_System => (
             coefficient = (min = 1.0, max = 1.0),
             rhs = (min = 6.434489705000001, max = 9.930296584),
         ),
-        :P_lb__ThermalStandard__RangeConstraint =>
+        :ActivePowerVariableLimitsConstraint_ThermalStandard_lb =>
             (coefficient = (min = 1.0, max = 1.0), rhs = (min = Inf, max = -Inf)),
-        :P_ub__ThermalStandard__RangeConstraint =>
+        :ActivePowerVariableLimitsConstraint_ThermalStandard_ub =>
             (coefficient = (min = 1.0, max = 1.0), rhs = (min = 0.4, max = 6.0)),
     )
     for (constriant_key, constriant_bounds) in model_bounds
-        _check_constraint_bounds(constriant_bounds, valid_model_bounds[constriant_key])
+        _check_constraint_bounds(
+            constriant_bounds,
+            valid_model_bounds[PSI.encode_key(constriant_key)],
+        )
     end
 end
 
@@ -41,7 +44,7 @@ end
     template = get_template_basic_uc_simulation()
     c_sys5 = PSB.build_system(PSITestSystems, "c_sys5_uc")
     valid_bounds = (min = 0.0, max = 6.0)
-    model = OperationsProblem(template, c_sys5; optimizer = GLPK_optimizer)
+    model = DecisionModel(template, c_sys5; optimizer = GLPK_optimizer)
     @test build!(model; output_dir = mktempdir(cleanup = true)) == PSI.BuildStatus.BUILT
 
     bounds = PSI.get_variable_numerical_bounds(model; verbose = false)
@@ -49,12 +52,15 @@ end
 
     model_bounds = PSI.get_variable_numerical_bounds(model; verbose = true)
     valid_model_bounds = Dict(
-        :stop__ThermalStandard => (min = 0.0, max = 1.0),
-        :start__ThermalStandard => (min = 0.0, max = 1.0),
-        :P__ThermalStandard => (min = 0.4, max = 6.0),
-        :On__ThermalStandard => (min = 0.0, max = 1.0),
+        :StopVariable_ThermalStandard => (min = 0.0, max = 1.0),
+        :StartVariable_ThermalStandard => (min = 0.0, max = 1.0),
+        :ActivePowerVariable_ThermalStandard => (min = 0.4, max = 6.0),
+        :OnVariable_ThermalStandard => (min = 0.0, max = 1.0),
     )
     for (variable_key, variable_bounds) in model_bounds
-        _check_variable_bounds(variable_bounds, valid_model_bounds[variable_key])
+        _check_variable_bounds(
+            variable_bounds,
+            valid_model_bounds[PSI.encode_key(variable_key)],
+        )
     end
 end
