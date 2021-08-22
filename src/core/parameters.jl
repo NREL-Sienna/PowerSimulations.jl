ebc24b222575099a0ab710241f78308b92f2bbbe
Jose Daniel Lara <jdlara@berkeley.edu>
2021-08-22 12:03:59 -0700

    add parameter container methods

--- src/core/parameters.jl
+++ src/core/parameters.jl
@@ -1,5 +1,34 @@
+struct ParameterKey{T <: ParameterType, U <: PSY.Component} <: OptimizationContainerKey
+    meta::String
+end
+
+function ParameterKey(
+    ::Type{T},
+    ::Type{U},
+    meta = CONTAINER_KEY_EMPTY_META,
+) where {T <: VariableType, U <: PSY.Component}
+    check_meta_chars(meta)
+    return ParameterKey{T, U}(meta)
+end
+
+function ParameterKey(::Type{T}) where {T <: VariableType}
+    return ParameterKey(T, PSY.Component, CONTAINER_KEY_EMPTY_META)
+end
+
+function ParameterKey(::Type{T}, meta::String) where {T <: VariableType}
+    return ParameterKey(T, PSY.Component, meta)
+end
+
+get_entry_type(::ParameterKey{T, U}) where {T <: VariableType, U <: PSY.Component} = T
+get_component_type(::ParameterKey{T, U}) where {T <: VariableType, U <: PSY.Component} = U
+
+"""
+Parameters implemented through ParameterJuMP
+"""
+abstract type RightHandSideParameter end
+abstract type ObjectiveFunctionParameter end
+
 struct ParameterContainer
-    update_ref::UpdateRef
     parameter_array::JuMP.Containers.DenseAxisArray
     multiplier_array::JuMP.Containers.DenseAxisArray
 end
@@ -8,5 +37,3 @@ get_parameter_array(c::ParameterContainer) = c.parameter_array
 get_multiplier_array(c::ParameterContainer) = c.multiplier_array
 Base.length(c::ParameterContainer) = length(c.parameter_array)
 Base.size(c::ParameterContainer) = size(c.parameter_array)
-
-const ParametersContainer = Dict{Symbol, ParameterContainer}
