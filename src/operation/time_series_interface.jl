285846073c800cc9536ce18828a76b0124076520
Jose Daniel Lara <jdlara@berkeley.edu>
2021-08-24 22:21:45 -0700

    time series interface refactoring

--- src/operation/time_series_interface.jl
+++ src/operation/time_series_interface.jl
@@ -1,7 +1,59 @@
+function make_time_series_cache(
+    ::Type{PSY.StaticTimeSeries},
+    component,
+    name,
+    initial_time,
+    ::Int;
+    ignore_scaling_factors = true,
+)
+    return IS.StaticTimeSeriesCache(
+        PSY.SingleTimeSeries,
+        component,
+        name,
+        start_time = initial_time,
+        ignore_scaling_factors = ignore_scaling_factors,
+    )
+end
+
+function make_time_series_cache(
+    ::Type{PSY.Deterministic},
+    component,
+    name,
+    initial_time,
+    horizon::Int;
+    ignore_scaling_factors = true,
+)
+    return IS.ForecastCache(
+        IS.AbstractDeterministic,
+        component,
+        name,
+        start_time = initial_time,
+        horizon = horizon,
+        ignore_scaling_factors = ignore_scaling_factors,
+    )
+end
+
+function make_time_series_cache(
+    ::Type{PSY.Probabilistic},
+    component,
+    name,
+    initial_time,
+    horizon::Int;
+    ignore_scaling_factors = true,
+)
+    return IS.ForecastCache(
+        IS.AbstractDeterministic,
+        component,
+        name,
+        start_time = initial_time,
+        horizon = horizon,
+        ignore_scaling_factors = ignore_scaling_factors,
+    )
+end
 
 function get_time_series_values!(
-    time_series_type::Type{<:IS.TimeSeriesData},
-    model::EmulationModel,
+    time_series_type::Type{<:PSY.Forecast},
+    model::DecisionModel,
     component,
     name,
     initial_time,
@@ -15,7 +67,7 @@ function get_time_series_values!(
             name,
             start_time = initial_time,
             len = horizon,
-            ignore_scaling_factors = true,
+            ignore_scaling_factors = ignore_scaling_factors,
         )
     end
 
@@ -30,7 +82,7 @@ function get_time_series_values!(
             name,
             initial_time,
             horizon,
-            ignore_scaling_factors = true,
+            ignore_scaling_factors = ignore_scaling_factors,
         )
         cache[key] = ts_cache
     end
@@ -39,45 +91,42 @@ function get_time_series_values!(
     return TimeSeries.values(ts)
 end
 
-function make_time_series_cache(
-    time_series_type::Type{T},
+function get_time_series_values!(
+    time_series_type::Type{PSY.StaticTimeSeries},
+    model::EmulationModel,
     component,
     name,
     initial_time,
-    horizon;
+    len::Int = 1;
     ignore_scaling_factors = true,
-) where {T <: IS.TimeSeriesData}
-    key = TimeSeriesCacheKey(IS.get_uuid(component), T, name)
-    if T <: IS.SingleTimeSeries
-        cache = IS.StaticTimeSeriesCache(
-            PSY.SingleTimeSeries,
+)
+    if !use_time_series_cache(get_settings(model))
+        return IS.get_time_series_values(
+            time_series_type,
             component,
             name,
             start_time = initial_time,
+            len = len,
             ignore_scaling_factors = ignore_scaling_factors,
         )
-    elseif T <: IS.Deterministic
-        cache = IS.ForecastCache(
-            IS.AbstractDeterministic,
-            component,
-            name,
-            start_time = initial_time,
-            horizon = horizon,
-            ignore_scaling_factors = ignore_scaling_factors,
-        )
-    elseif T <: IS.Probabilistic
-        cache = IS.ForecastCache(
-            IS.Probabilistic,
-            component,
-            name,
-            start_time = initial_time,
-            horizon = horizon,
-            ignore_scaling_factors = ignore_scaling_factors,
-        )
-    else
-        error("not supported yet: $T")
     end
 
-    @debug "Made time series cache for $(summary(component))" name initial_time
-    return cache
+    cache = get_time_series_cache(model)
+    key = TimeSeriesCacheKey(IS.get_uuid(component), time_series_type, name)
+    if haskey(cache, key)
+        ts_cache = cache[key]
+    else
+        ts_cache = make_time_series_cache(
+            time_series_type,
+            component,
+            name,
+            initial_time,
+            len,
+            ignore_scaling_factors = ignore_scaling_factors,
+        )
+        cache[key] = ts_cache
+    end
+
+    ts = IS.get_time_series_array!(ts_cache, initial_time)
+    return TimeSeries.values(ts)
 end
