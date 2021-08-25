3db7f342494152e1bbf35b3c1472513f15b4904f
Jose Daniel Lara <jdlara@berkeley.edu>
2021-08-25 16:42:42 -0700

    update creation of parameters

--- src/devices_models/devices/common/add_parameters.jl
+++ src/devices_models/devices/common/add_parameters.jl
@@ -4,23 +4,25 @@ function add_parameters!(
     devices::U,
     model::DeviceModel{D, W},
 ) where {
-    T <: ParameterType,
+    T <: TimeSeriesParameter,
     U <: Union{Vector{D}, IS.FlattenIteratorWrapper{D}},
     W <: AbstractDeviceFormulation,
 } where {D <: PSY.Component}
     time_steps = get_time_steps(container)
     names = [PSY.get_name(d) for d in devices]
-    @debug "adding" parameter_type
-    parameter_container = add_param_container!(container, T(), D, names, time_steps)
+    label = get_time_series_labels(model)[parameter_type]
+    parameter = T(label, get_default_time_series_type(container))
+    @debug "adding" parameter
+    parameter_container = add_param_container!(container, parameter, D, names, time_steps)
     param = get_parameter_array(parameter_container)
     mult = get_multiplier_array(parameter_container)
-    label = get(get_time_series_labels(model), parameter_type, nothing)
+
     if !isnothing(label)
         for d in devices, t in time_steps
             name = PSY.get_name(d)
             ts_vector = get_time_series(container, d, label)
-            mult[name, t] = get_multiplier_value(T(), d, W())
-            param[name, t] = PJ.add_parameter(container.JuMPmodel, ts_vector[t])
+            mult[name, t] = get_multiplier_value(parameter, d, W())
+            param[name, t] = add_parameter(container.JuMPmodel, ts_vector[t])
         end
     end
     return
