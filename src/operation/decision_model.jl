e4069b400bc67f7e5fd38707be4d3678b3d3ede5
Jose Daniel Lara <jdlara@berkeley.edu>
2021-08-29 11:02:44 -0700

    modify model checks

--- src/operation/decision_model.jl
+++ src/operation/decision_model.jl
@@ -70,7 +70,7 @@ mutable struct DecisionModel{M <: DecisionProblem} <: OperationModel
                 "The system does not contain forecast data. A DecisionModel can't be built.",
             )
         end
-        internal = ModelInternal(
+        internal = ProblemInternal(
             OptimizationContainer(sys, settings, jump_model, PSY.Deterministic),
         )
         new{M}(name, template, sys, internal, Dict{String, Any}())
