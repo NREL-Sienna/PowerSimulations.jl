38c1d2c3fc144de6ad3f58d70d48ded68f749976
Jose Daniel Lara <jdlara@berkeley.edu>
2021-09-10 16:13:56 -0700

    refactor initial conditions

--- src/initial_conditions/add_initial_condition.jl
+++ src/initial_conditions/add_initial_condition.jl
@@ -1,9 +1,9 @@
-function _set_initial_condition!(ini_cond::Vector{InitialCondition{T, PJ.ParameterRef}}, ix::Int, val::Float64, container::OptimizationContainer)
+function _set_initial_condition!(ini_cond::Vector{InitialCondition{T, PJ.ParameterRef}}, ix::Int, val::Float64, container::OptimizationContainer) where T <: InitialConditionType
     ini_cond[ix] = add_jump_parameter(container.JuMPmodel, val)
     return
 end
 
-function _set_initial_condition!(ini_cond::Vector{InitialCondition{T, Float64}}, ix::Int, val::Float64, ::OptimizationContainer)
+function _set_initial_condition!(ini_cond::Vector{InitialCondition{T, Float64}}, ix::Int, val::Float64, ::OptimizationContainer) where T <: InitialConditionType
     ini_cond[ix] = val
     return
 end
@@ -11,16 +11,16 @@ end
 function add_initial_condition!(
     container::OptimizationContainer,
     devices::Union{Vector{T}, IS.FlattenIteratorWrapper{T}},
-    ::D,
     ::Type{U},
+    ::D,
 ) where {
     T <: PSY.Component,
-    U <: InitialConditionType,
-    D <: Union{AbstractDeviceFormulation, AbstractServiceFormulation},
+    U <: Union{AbstractDeviceFormulation, AbstractServiceFormulation},
+    D <: InitialConditionType,
 }
-    add_initial_condition_container!(container, U(), T, devices)
+    add_initial_condition_container!(container, D(), T, devices)
     for (ix, device) in enumerate(devices)
-        val = get_initial_condition_value(U(), T, device, D())
+        val = get_initial_condition_value(D(), device, U())
         set_initial_condition!(ini_cond, ix, val, container)
     end
 end
