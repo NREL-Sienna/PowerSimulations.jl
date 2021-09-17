5f0da8e0b4acc82c9401ea134a29b5b64a776494
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2021-09-17 11:10:51 -0600

    updating the tests

--- test/test_simulation_results.jl
+++ test/test_simulation_results.jl
@@ -80,10 +80,6 @@ function test_simulation_results(file_path::String, export_path; in_memory = fal
                     binary_source_problem = PSI.ON,
                     affected_variables = [PSI.ACTIVE_POWER],
                 ),
-                ("ED", :devices, :HydroEnergyReservoir) => IntegralLimitFF(
-                    variable_source_problem = PSI.ACTIVE_POWER,
-                    affected_variables = [PSI.ACTIVE_POWER],
-                ),
             ),
             cache = Dict(
                 ("UC",) => TimeStatusChange(PSY.ThermalStandard, PSI.ON),
