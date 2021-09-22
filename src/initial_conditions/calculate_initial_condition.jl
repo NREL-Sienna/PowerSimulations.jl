0f757ae95ebfff4839c5ad9baeb0ced6cd9a4e2c
Jose Daniel Lara <jdlara@berkeley.edu>
2021-09-21 18:00:33 -0700

    refactor initial condition updating

--- src/initial_conditions/calculate_initial_condition.jl
+++ src/initial_conditions/calculate_initial_condition.jl
@@ -1,69 +1,22 @@
-function calculate_ic_quantity(ic::InitialCondition, ::Float64, ::Dates.Period)
-    error("Initial condition calculation for $(typeof(ic)) not implemented")
-end
-
-function calculate_ic_quantity(
-    ic::InitialCondition{InitialTimeDurationOn, T},
+"""
+Default implementation of set_initial_condition_value
+"""
+function set_ic_quantity!(
+    ic::InitialCondition{T, PJ.ParameterRef},
     var_value::Float64,
-    elapsed_period::Dates.Period,
-) where {T <: Union{Float64, PJ.ParameterRef}}
-    name = get_component_name(ic)
-    time_cache = cache_value(cache, name)
-
-    current_counter = time_cache[:count]
-    last_status = time_cache[:status]
-    var_status = isapprox(var_value, 0.0, atol = ABSOLUTE_TOLERANCE) ? 0.0 : 1.0
-
-    @assert abs(last_status - var_status) < ABSOLUTE_TOLERANCE
-    last_status >= 1.0 ? current_counter : 0.0
-
+) where {T <: InitialConditionType}
+    PJ.set_value(ic.value, var_value)
     return
 end
 
-function calculate_ic_quantity(
-    ic::InitialCondition{InitialTimeDurationOff, T},
+"""
+Default implementation of set_initial_condition_value
+"""
+function set_ic_quantity!(
+    ic::InitialCondition{T, Float64},
     var_value::Float64,
-    elapsed_period::Dates.Period,
-) where {T <: Union{Float64, PJ.ParameterRef}}
-    name = get_component_name(ic)
-    time_cache = cache_value(cache, name)
-
-    current_counter = time_cache[:count]
-    last_status = time_cache[:status]
-    var_status = isapprox(var_value, 0.0, atol = ABSOLUTE_TOLERANCE) ? 0.0 : 1.0
-
-    @assert abs(last_status - var_status) < ABSOLUTE_TOLERANCE
-    last_status >= 1.0 ? 0.0 : current_counter
-    return
-end
-
-function calculate_ic_quantity(
-    ic::InitialCondition{DeviceStatus, T},
-    var_value::Float64,
-    elapsed_period::Dates.Period,
-) where {T <: Union{Float64, PJ.ParameterRef}}
-    current_status = isapprox(var_value, 0.0, atol = ABSOLUTE_TOLERANCE) ? 0.0 : 1.0
-    return current_status
-end
-
-function calculate_ic_quantity(
-    ic::InitialCondition{DevicePower, T},
-    var_value::Float64,
-    elapsed_period::Dates.Period,
-) where {T <: Union{Float64, PJ.ParameterRef}} 
-end
-
-function calculate_ic_quantity(
-    ic::InitialCondition{InitialEnergyLevel, T},
-    var_value::Float64,
-    ::Dates.Period,
-) where {T <: Union{Float64, PJ.ParameterRef}}
-    #cache = get_cache(simulation_cache, ic.cache_type, T)
-    #name = get_component_name(ic)
-    #energy_cache = cache_value(cache, name)
-    #if energy_cache != var_value
-    #    return var_value
-    #end
-    #
+) where {T <: InitialConditionType}
+    @debug "Initial condition value set with Float64. Won't update the model until rebuild"
+    ic.value = var_value
     return
 end
--- src/initial_conditions/update_initial_conditions.jl
+++ src/initial_conditions/update_initial_conditions.jl
@@ -1,100 +1,62 @@
-######################### Initial Condition Updating #########################################
-function _set_first_initial_conditions!(
-    initial_condition_vector::Vector,
-    variable::JuMP.Containers.DenseAxisArray{JuMP.VariableRef},
+function update_initial_conditions(
+    ::Vector{T},
+    store::EmulationModelOptimizerResults,
     elapsed_period::Dates.Period,
-)
-    error("expected")
-    for ic in initial_condition_vector
-        name = get_component_name(ic)
-        var_value = JuMP.value(variable[name, 1])
-        calculate_ic_quantity(initial_condition, var_value, elapsed_period)
+) where {T <: InitialCondition{InitialTimeDurationOn, PJ.ParameterRef}}
+    index = store.data.last_recorded_row
+    for ic in ic_vector
+        values = get_aux_variable(store, TimeDurationOn(), get_component_type(ic))
+        set_ic_quantity!(ic, values[index, get_component_name(ic)])
     end
     return
 end
 
-"""
-    Used to set the model's first initial conditions
-"""
-function set_first_initial_conditions!(
-    model::OperationModel,
-    key::ICKey{InitialTimeDurationOn, T},
-) where {T <: PSY.Component}
-    ic_container = odel.internal.ic_model_container
-    container = get_optimization_container(model)
-    ic_var = get_aux_variable(ic_container, TimeDurationOn, T)
-    ini_conditions_vector = get_initial_condition(container, key)
-    _set_first_initial_conditions(ini_conditions_vector, ic_var)
-    # @debug last_status, var_status, abs(last_status - var_status) _group = LOG_GROUP_INITIAL_CONDITIONS
-end
-
-function set_first_initial_conditions!(
-    model::OperationModel,
-    key::ICKey{InitialTimeDurationOff, T},
-) where {T <: PSY.Component}
-    ic_container = odel.internal.ic_model_container
-    container = get_optimization_container(model)
-    ic_var = get_aux_variable(ic_container, TimeDurationOff, T)
-    ini_conditions_vector = get_initial_condition(container, key)
-    _set_first_initial_conditions(ini_conditions_vector, ic_var, 0.0)
-    # @debug last_status, var_status, abs(last_status - var_status) _group = LOG_GROUP_INITIAL_CONDITIONS
-    return
-end
-
-function set_first_initial_conditions!(
-    model::OperationModel,
-    key::ICKey{DevicePower, T},
-) where {T <: PSY.Component}
-    ic_container = odel.internal.ic_model_container
-    container = get_optimization_container(model)
-    ic_var = get_variable(ic_container, ActivePowerVariable, T)
-    ini_conditions_vector = get_initial_condition(container, key)
-    _set_first_initial_conditions(ini_conditions_vector, ic_var, 0.0)
-    # @debug last_status, var_status, abs(last_status - var_status) _group = LOG_GROUP_INITIAL_CONDITIONS
-    return
-end
-
-#=
-""" Updates the initial conditions of the problem"""
-function initial_condition_update!(
-    model,
-    ini_cond_key::ICKey,
-    initial_conditions::Vector{InitialCondition},
-    ::IntraProblemChronology,
-    sim,
-)
-    # TODO: Replace this convoluted way to get information with access to data store
-    execution_count = get_execution_count(problem)
-    execution_count == 0 && return
-    simulation_cache = sim.internal.simulation_cache
-    for ic in initial_conditions
-        name = get_component_name(ic)
-        interval_chronology = get_model_interval_chronology(sim.sequence, get_name(model))
-        var_value = get_model_variable(
-            interval_chronology,
-            (problem => problem),
-            name,
-            ic.update_ref,
-        )
-        # We pass the simulation cache instead of the whole simulation to avoid definition dependencies.
-        # All the inputs to calculate_ic_quantity are defined before the simulation object
-        quantity = calculate_ic_quantity(
-            ini_cond_key,
-            ic,
-            var_value,
-            simulation_cache,
-            get_resolution(model),
-        )
-        previous_value = get_condition(ic)
-        PJ.set_value(ic.value, quantity)
-        IS.@record :simulation InitialConditionUpdateEvent(
-            get_current_time(sim),
-            ini_cond_key,
-            ic,
-            quantity,
-            previous_value,
-            get_simulation_number(model),
-        )
+function update_initial_conditions(
+    ::Vector{T},
+    store::EmulationModelOptimizerResults,
+    ::Dates.Period,
+) where {T <: InitialCondition{InitialTimeDurationOff, PJ.ParameterRef}}
+    index = store.data.last_recorded_row
+    for ic in ic_vector
+        values = get_aux_variable(store, TimeDurationOff(), get_component_type(ic))
+        set_ic_quantity!(ic, values[index, get_component_name(ic)])
     end
+    return
 end
-=#
+
+function update_initial_conditions(
+    ::Vector{T},
+    store::EmulationModelOptimizerResults,
+    ::Dates.Period,
+) where {T <: InitialCondition{DevicePower, PJ.ParameterRef}}
+    index = store.data.last_recorded_row
+    for ic in ic_vector
+        values = get_variable(store, ActivePowerVariable(), get_component_type(ic))
+        set_ic_quantity!(ic, values[index, get_component_name(ic)])
+    end
+    return
+end
+
+function update_initial_conditions(
+    ::Vector{T},
+    store::EmulationModelOptimizerResults,
+    ::Dates.Period,
+) where {T <: InitialCondition{DeviceStatus, PJ.ParameterRef}}
+    index = store.data.last_recorded_row
+    for ic in ic_vector
+        values = get_aux_variable(store, OnVariable(), get_component_type(ic))
+        set_ic_quantity!(ic, values[index, get_component_name(ic)])
+    end
+    return
+end
+
+function update_initial_conditions!(
+    model::OperationModel,
+    key::ICKey{T, U},
+) where {T <: InitialConditionType, U <: PSY.Component}
+    # TODO: Add Recorder Event here
+    container = get_optimization_container(model)
+    interval = get_interval(model.internal.store_parameters)
+    ini_conditions_vector = get_initial_condition(container, key)
+    update_initial_conditions(ini_conditions_vector, store, interval)
+end
