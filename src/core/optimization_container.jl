eb7f474646ea867968e2d43c560842d49a7799c2
Jose Daniel Lara <jdlara@berkeley.edu>
2021-11-15 10:32:59 -0800

    fix typo

--- src/core/optimization_container.jl
+++ src/core/optimization_container.jl
@@ -584,7 +584,7 @@ function _add_variable_container!(
 ) where {T <: VariableType, U <: Union{PSY.Component, PSY.System}}
     if sparse
         var_container = sparse_container_spec(Float64, axs...)
-        # We initialize sparse containers with Floats64, not ideal and introduces type instability,
+        # We initialize sparse containers with Float64, not ideal and introduces type instability,
         # because JuMP.Containers.SparseAxisArrays can't be initialized with undef
     else
         var_container = container_spec(JuMP.VariableRef, axs...)
