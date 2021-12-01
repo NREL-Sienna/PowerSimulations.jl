159787931060bc09802f5fc0e6cb42e702380b8b
Jose Daniel Lara <jdlara@berkeley.edu>
2021-12-01 14:20:21 -0800

    Update src/initial_conditions/update_initial_conditions.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/initial_conditions/update_initial_conditions.jl
+++ src/initial_conditions/update_initial_conditions.jl
@@ -130,7 +130,7 @@ end
 function update_initial_conditions!(
     model::OperationModel,
     key::ICKey{T, U},
-    source # Store or State are used in simulations by default
+    source, # Store or State are used in simulations by default
 ) where {T <: InitialConditionType, U <: PSY.Component}
     container = get_optimization_container(model)
     interval = get_interval(model.internal.store_parameters)
