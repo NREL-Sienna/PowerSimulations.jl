1a51649b8b175e46be74fad22082293d28221aad
Jose Daniel Lara <jdlara@berkeley.edu>
2021-12-03 16:21:59 -0800

    add new methods for initial condition

--- src/initial_conditions/update_initial_conditions.jl
+++ src/initial_conditions/update_initial_conditions.jl
@@ -1,5 +1,128 @@
 function _update_initial_conditions!(
     ic_vector::Vector{T},
+    state::StateInfo,
+    ::Dates.Period,
+) where {
+    T <:
+    InitialCondition{InitialTimeDurationOn, S},
+} where {S <: Union{Float64, PJ.ParameterRef}}
+    for ic in ic_vector
+        var_val = get_aux_variable_value(store, TimeDurationOn(), get_component_type(ic))
+        set_ic_quantity!(ic, var_val[index, get_component_name(ic)])
+    end
+    return
+end
+
+function _update_initial_conditions!(
+    ic_vector::Vector{T},
+    state::StateInfo,
+    ::Dates.Period,
+) where {
+    T <: InitialCondition{
+        InitialTimeDurationOff,
+        S,
+    },
+} where {S <: Union{Float64, PJ.ParameterRef}}
+    for ic in ic_vector
+        var_val = get_aux_variable_value(store, TimeDurationOff(), get_component_type(ic))
+        set_ic_quantity!(ic, var_val[index, get_component_name(ic)])
+    end
+    return
+end
+
+function _update_initial_conditions!(
+    ic_vector::Vector{T},
+    state::StateInfo,
+    ::Dates.Period,
+) where {T <: InitialCondition{DevicePower, S}} where {S <: Union{Float64, PJ.ParameterRef}}
+    for ic in ic_vector
+        var_val = get_variable_value(store, ActivePowerVariable(), get_component_type(ic))
+        set_ic_quantity!(ic, var_val[index, get_component_name(ic)])
+    end
+    return
+end
+
+function _update_initial_conditions!(
+    ic_vector::Vector{T},
+    state::StateInfo,
+    ::Dates.Period,
+) where {
+    T <: InitialCondition{DeviceStatus, S},
+} where {S <: Union{Float64, PJ.ParameterRef}}
+    for ic in ic_vector
+        var_val = get_variable_value(store, OnVariable(), get_component_type(ic))
+        set_ic_quantity!(ic, var_val[index, get_component_name(ic)])
+    end
+    return
+end
+
+function _update_initial_conditions!(
+    ic_vector::Vector{T},
+    state::StateInfo,
+    ::Dates.Period,
+) where {
+    T <:
+    InitialCondition{DeviceAboveMinPower, S},
+} where {S <: Union{Float64, PJ.ParameterRef}}
+    for ic in ic_vector
+        var_val =
+            get_variable_value(store, PowerAboveMinimumVariable(), get_component_type(ic))
+        set_ic_quantity!(ic, var_val[index, get_component_name(ic)])
+    end
+    return
+end
+
+function _update_initial_conditions!(
+    ic_vector::Vector{T},
+    state::StateInfo,
+    ::Dates.Period,
+) where {
+    T <:
+    InitialCondition{InitialEnergyLevel, S},
+} where {S <: Union{Float64, PJ.ParameterRef}}
+    for ic in ic_vector
+        var_val = get_variable_value(store, EnergyVariable(), get_component_type(ic))
+        set_ic_quantity!(ic, var_val[index, get_component_name(ic)])
+    end
+    return
+end
+
+function _update_initial_conditions!(
+    ic_vector::Vector{T},
+    state::StateInfo,
+    ::Dates.Period,
+) where {
+    T <:
+    InitialCondition{InitialEnergyLevelUp, S},
+} where {S <: Union{Float64, PJ.ParameterRef}}
+    for ic in ic_vector
+        var_val = get_variable_value(store, EnergyVariableUp(), get_component_type(ic))
+        set_ic_quantity!(ic, var_val[index, get_component_name(ic)])
+    end
+    return
+end
+
+function _update_initial_conditions!(
+    ic_vector::Vector{T},
+    state::StateInfo,
+    ::Dates.Period,
+) where {
+    T <: InitialCondition{
+        InitialEnergyLevelDown,
+        S,
+    },
+} where {S <: Union{Float64, PJ.ParameterRef}}
+    for ic in ic_vector
+        var_val = get_variable_value(store, EnergyVariableDown(), get_component_type(ic))
+        set_ic_quantity!(ic, var_val[index, get_component_name(ic)])
+    end
+    return
+end
+
+################## ic updates from store for emulation problems simulation #################
+
+function _update_initial_conditions!(
+    ic_vector::Vector{T},
     store::InMemoryModelStore,
     ::Dates.Period,
 ) where {
