137261804cb275ae8a48f707dd421579a0c9cee1
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2021-12-21 17:06:15 -0700

    adding hybrid test simulation

--- test/test_simulation_execute.jl
+++ test/test_simulation_execute.jl
@@ -353,3 +353,37 @@ end
     # execute_out = execute!(sim)
     # @test execute_out == PSI.RunStatus.SUCCESSFUL
 end
+
+@testset "Test HybridSystem simulations" begin
+    sys_uc = PSB.build_system(PSITestSystems, "c_sys5_hybrid_uc")
+
+    template_uc = get_template_standard_uc_simulation()
+    set_network_model!(template_uc, NetworkModel(CopperPlatePowerModel, use_slacks = true))
+
+    models = SimulationModels(
+        decision_models = [
+            DecisionModel(
+                template_uc,
+                sys_uc;
+                name = "UC",
+                optimizer = Cbc_optimizer,
+                initialize_model = false,
+            ),
+        ],
+    )
+
+    sequence =
+        SimulationSequence(models = models, ini_cond_chronology = InterProblemChronology())
+
+    sim = Simulation(
+        name = "hybrid_test",
+        steps = 2,
+        models = models,
+        sequence = sequence,
+        simulation_folder = mktempdir(cleanup = true),
+    )
+    build_out = build!(sim)
+    @test build_out == PSI.BuildStatus.BUILT
+    execute_out = execute!(sim)
+    @test execute_out == PSI.RunStatus.SUCCESSFUL
+end
