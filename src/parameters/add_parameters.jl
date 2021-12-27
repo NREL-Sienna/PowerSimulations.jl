ea033a7a43217195ae4aa44e792637ba4efe306a
cbarrows <clayton.barrows@nrel.gov>
2021-12-27 13:53:21 -0700

    shortcutting singular time_series multiplier index calculation

--- src/parameters/add_parameters.jl
+++ src/parameters/add_parameters.jl
@@ -8,8 +8,13 @@ function create_time_series_multiplier_index(
     ::Type{T},
 ) where {T <: TimeSeriesParameter}
     ts_names = get_time_series_names(model)
-    ts_name = ts_names[T]
-    return findfirst(x -> x == T, [k for (k, v) in ts_names if v == ts_name])
+    if length(ts_names) > 1
+        ts_name = ts_names[T]
+        ts_id = findfirst(x -> x == T, [k for (k, v) in ts_names if v == ts_name])
+    else
+        ts_id = 1
+    end
+    return ts_id
 end
 
 function add_parameters!(
