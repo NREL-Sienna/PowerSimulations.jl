70aa493926bb4fbed8508041704082fc61491ee8
Clayton Barrows <clayton.barrows@nrel.gov>
2022-01-26 13:53:43 -0700

    Update src/initial_conditions/add_initial_condition.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/initial_conditions/add_initial_condition.jl
+++ src/initial_conditions/add_initial_condition.jl
@@ -55,10 +55,7 @@ function _get_initial_conditions_value(
     container::OptimizationContainer,
 ) where {
     T <: InitialCondition{U, Float64},
-    V <: Union{
-        AbstractDeviceFormulation,
-        AbstractServiceFormulation,
-    },
+    V <: Union{AbstractDeviceFormulation, AbstractServiceFormulation},
     W <: PSY.Component,
 } where {U <: InitialTimeDurationOff}
     ic_data = get_initial_conditions_data(container)
