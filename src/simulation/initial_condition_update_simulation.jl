458f20870ed3d97e6511ca2bd054ac6271f26849
Clayton Barrows <clayton.barrows@nrel.gov>
2022-01-26 14:33:17 -0700

    Update src/simulation/initial_condition_update_simulation.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/simulation/initial_condition_update_simulation.jl
+++ src/simulation/initial_condition_update_simulation.jl
@@ -117,8 +117,7 @@ function update_initial_conditions!(
     state::SimulationState,
     ::Dates.Millisecond,
 ) where {
-    T <:
-    InitialCondition{DeviceAboveMinPower, S},
+    T <: InitialCondition{DeviceAboveMinPower, S},
 } where {S <: Union{Float64, PJ.ParameterRef}}
     for ic in ics
         var_val = get_system_state_value(
