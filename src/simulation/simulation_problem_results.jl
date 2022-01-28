539b658c40053877172d44fc43ee4ae7d24874af
Jose Daniel Lara <jdlara@berkeley.edu>
2022-01-28 00:02:04 -0600

    add missins arguments in docstring

--- src/simulation/simulation_problem_results.jl
+++ src/simulation/simulation_problem_results.jl
@@ -169,6 +169,15 @@ end
 Set the system in the results instance.
 
 Throws InvalidValue if the system UUID is incorrect.
+
+# Arguments
+- `results::SimulationProblemResults`: Results object
+- `system::AbstractString`: Path to the system json file
+
+# Examples
+```julia
+julia> set_system!(res, "my_path/system_data.json")
+```
 """
 function set_system!(results::SimulationProblemResults, system::AbstractString)
     set_system!(results, System(system))
