9a324c916d14fbdf1d82faacd65f47bc436ac643
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2022-02-11 09:55:51 -0700

    adding new function for hybrid time series labels

--- src/devices_models/devices/hybrid_generation.jl
+++ src/devices_models/devices/hybrid_generation.jl
@@ -81,6 +81,10 @@ does_subcomponent_exist(v::PSY.HybridSystem, ::Type{PSY.ElectricLoad}) =
 does_subcomponent_exist(v::PSY.HybridSystem, ::Type{PSY.Storage}) =
     !isnothing(PSY.get_storage(v))
 
+function make_subsystem_time_series_name(subcomponent::PSY.Component, label::String)
+    return IS.strip_module_name(typeof(subcomponent)) * "__" * label
+end
+    
 function get_default_time_series_names(
     ::Type{<:PSY.HybridSystem},
     ::Type{<:Union{FixedOutput, AbstractHybridFormulation}},
