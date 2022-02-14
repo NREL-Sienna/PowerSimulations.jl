60536477ab0b6a35cbad7054a925ab74fabf88ee
Jose Daniel Lara <jdlara@berkeley.edu>
2022-02-14 12:57:10 -0800

    update formatter code

--- .github/workflows/formatter/formatter_code.jl
+++ .github/workflows/formatter/formatter_code.jl
@@ -8,10 +8,14 @@ main_paths = ["."]
 for main_path in main_paths
     format(
         main_path;
-        whitespace_ops_in_indices = true,
-        remove_extra_newlines = true,
-        verbose = true,
-        always_for_in = true,
-        whitespace_typedefs = true,
+        whitespace_ops_in_indices=true,
+        remove_extra_newlines=true,
+        verbose=true,
+        always_for_in=true,
+        whitespace_typedefs=true,
+        whitespace_in_kwargs=false,
+        format_docstrings=true,
+
+        # always_use_return = true # removed since it has false positives.
     )
 end
