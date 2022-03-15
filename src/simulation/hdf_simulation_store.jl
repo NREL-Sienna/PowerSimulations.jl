57f73fecf02018072fc6687ca5a0061c9f631ad2
Jose Daniel Lara <jdlara@berkeley.edu>
2022-03-15 00:24:21 -0700

    use correct resolution

--- src/simulation/hdf_simulation_store.jl
+++ src/simulation/hdf_simulation_store.jl
@@ -311,9 +311,7 @@ function initialize_problem_storage!(
                 datasets[key] = HDF5Dataset(
                     dataset,
                     column_dataset,
-                    # TODO: Josè, why is this broken?
-                    #get_resolution(emulation_params),
-                    get_resolution(first(values(params.decision_models_params))),
+                    get_resolution(emulation_params),
                     initial_time,
                 )
                 container_key_lookup[encode_key_as_string(key)] = key
