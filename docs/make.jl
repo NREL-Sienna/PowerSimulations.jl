82266b834253431a44f0608baf86318889fb037a
Jose Daniel Lara <jdlara@berkeley.edu>
2022-05-23 12:21:33 -0700

    formatter

--- docs/make.jl
+++ docs/make.jl
@@ -5,39 +5,36 @@ using Literate
 using DataStructures
 
 pages = OrderedDict(
-        "Welcome Page" => "index.md",
-        "Quick Start Guide" => "quick_start_guide.md",
-        "Tutorials" =>  "tutorials/intro_page.md",
-        "Modeler Guide" =>
-            Any[
-            #"modeler_guide/type_structure.md",
-            #"modeler_guide/system.md",
-            #"modeler_guide/time_series.md",
-            #"modeler_guide/enumerated_types.md",
-            #"modeler_guide/example_dynamic_data.md",
-            "modeler_guide/definitions.md",
-            "modeler_guide/simulation_recorder.md",
-            "modeler_guide/Logging.md"
-            ],
-        "Model Developer Guide" =>
-            Any[
-                "Adding Formulation" => "model_developer_guide/adding_device_formulation.md"
-                "Adding Problems" => "model_developer_guide/adding_new_problem_model.md"
-                "Troubleshooting" => "model_developer_guide/troubleshooting.md"
-            ],
-        "Code Base Developer Guide" =>
-            Any[
-                "Developer Guide" => "code_base_developer_guide/developer.md",
-                "Troubleshooting" => "code_base_developer_guide/troubleshooting.md"
-            ],
-        "Formulation Library" => Any[
-            "Thermal Generation" => "formulation_library/ThermalGen.md",
-            "Hydro Generation" => "formulation_library/HydroGen.md",
-            "Storage" => "formulation_library/Storage.md",
-            "Network" => "formulation_library/Network.md"
-        ],
-        "Public API Reference" => "api/public.md",
-        "Internal API Reference" => "api/internal.md"
+    "Welcome Page" => "index.md",
+    "Quick Start Guide" => "quick_start_guide.md",
+    "Tutorials" => "tutorials/intro_page.md",
+    "Modeler Guide" => Any[
+        #"modeler_guide/type_structure.md",
+        #"modeler_guide/system.md",
+        #"modeler_guide/time_series.md",
+        #"modeler_guide/enumerated_types.md",
+        #"modeler_guide/example_dynamic_data.md",
+        "modeler_guide/definitions.md",
+        "modeler_guide/simulation_recorder.md",
+        "modeler_guide/Logging.md",
+    ],
+    "Model Developer Guide" => Any[
+        "Adding Formulation" => "model_developer_guide/adding_device_formulation.md"
+        "Adding Problems" => "model_developer_guide/adding_new_problem_model.md"
+        "Troubleshooting" => "model_developer_guide/troubleshooting.md"
+    ],
+    "Code Base Developer Guide" => Any[
+        "Developer Guide" => "code_base_developer_guide/developer.md",
+        "Troubleshooting" => "code_base_developer_guide/troubleshooting.md",
+    ],
+    "Formulation Library" => Any[
+        "Thermal Generation" => "formulation_library/ThermalGen.md",
+        "Hydro Generation" => "formulation_library/HydroGen.md",
+        "Storage" => "formulation_library/Storage.md",
+        "Network" => "formulation_library/Network.md",
+    ],
+    "Public API Reference" => "api/public.md",
+    "Internal API Reference" => "api/internal.md",
 )
 
 deploydocs(
