b9932e47578ec10568d369b51f51e74e77f347ad
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2022-07-29 15:41:40 -0600

    adding tests

--- test/test_device_storage_constructors.jl
+++ test/test_device_storage_constructors.jl
@@ -96,6 +96,42 @@ end
     psi_checkobjfun_test(model, GAEVF)
 end
 
+@testset "BatteryEMS with EnergyValue with DC - PF" begin
+    device_model = DeviceModel(GenericBattery, EnergyValue)
+    c_sys5_bat = PSB.build_system(PSITestSystems, "c_sys5_batt_energy_value")
+    model = DecisionModel(MockOperationProblem, DCPPowerModel, c_sys5_bat)
+    mock_construct_device!(model, device_model)
+    moi_tests(model, false, 144, 0, 72, 72, 24, true)
+    psi_checkobjfun_test(model, GAEVF)
+end
+
+@testset "BatteryEMS with EnergyValue With AC - PF" begin
+    device_model = DeviceModel(GenericBattery, EnergyValue)
+    c_sys5_bat = PSB.build_system(PSITestSystems, "c_sys5_batt_energy_value")
+    model = DecisionModel(MockOperationProblem, ACPPowerModel, c_sys5_bat)
+    mock_construct_device!(model, device_model)
+    moi_tests(model, false, 168, 0, 96, 96, 24, true)
+    psi_checkobjfun_test(model, GAEVF)
+end
+
+@testset "BatteryEMS with EnergyValueCurve with MarketBidCost and DC - PF" begin
+    device_model = DeviceModel(GenericBattery, EnergyValueCurve)
+    c_sys5_bat = PSB.build_system(PSITestSystems, "c_sys5_batt_energy_value_curve")
+    model = DecisionModel(MockOperationProblem, DCPPowerModel, c_sys5_bat)
+    mock_construct_device!(model, device_model)
+    moi_tests(model, false, 144, 0, 72, 72, 24, true)
+    psi_checkobjfun_test(model, GAEVF)
+end
+
+@testset "BatteryEMS with EnergyValueCurve with MarketBidCost and AC - PF" begin
+    device_model = DeviceModel(GenericBattery, EnergyValueCurve)
+    c_sys5_bat = PSB.build_system(PSITestSystems, "c_sys5_batt_energy_value_curve")
+    model = DecisionModel(MockOperationProblem, ACPPowerModel, c_sys5_bat)
+    mock_construct_device!(model, device_model)
+    moi_tests(model, false, 168, 0, 96, 96, 24, true)
+    psi_checkobjfun_test(model, GAEVF)
+end
+
 @testset "BatteryEMS with EnergyTarget Formulations (energy target - cases 1b-2b" begin
     template = ProblemTemplate(CopperPlatePowerModel)
     set_device_model!(template, BatteryEMS, EnergyTarget)
