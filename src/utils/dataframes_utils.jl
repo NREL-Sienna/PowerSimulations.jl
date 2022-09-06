68e3de1f1b2b41eac7eef8d772cd3221264e9420
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2022-09-06 15:01:45 -0600

    adding fix for duals with SparseArrays

--- src/utils/dataframes_utils.jl
+++ src/utils/dataframes_utils.jl
@@ -8,7 +8,14 @@ Creates a DataFrame from a JuMP DenseAxisArray or SparseAxisArray.
   - `key::OptimizationContainerKey`:
 """
 function axis_array_to_dataframe(
-    array::Union{DenseAxisArray, SparseAxisArray},
+    array::DenseAxisArray,
+    key::OptimizationContainerKey,
+)
+    return DataFrames.DataFrame(to_matrix(array), get_column_names(key, array))
+end
+
+function axis_array_to_dataframe(
+    array::SparseAxisArray,
     key::OptimizationContainerKey,
 )
     return DataFrames.DataFrame(to_matrix(array), get_column_names(key, array))
