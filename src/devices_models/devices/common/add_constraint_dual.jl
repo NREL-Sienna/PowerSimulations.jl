a41ed93bb9375c7216b6a559cc228ceef2ab9188
Jose Daniel Lara <jdlara@berkeley.edu>
2022-12-01 15:23:53 -0700

    fix dual addition breaking changes

--- src/devices_models/devices/common/add_constraint_dual.jl
+++ src/devices_models/devices/common/add_constraint_dual.jl
@@ -47,7 +47,7 @@ function add_constraint_dual!(
     if !isempty(get_duals(model))
         service = PSY.get_component(T, sys, model.service_name)
         for constraint_type in get_duals(model)
-            assign_dual_variable!(container, constraint_type, service, D())
+            assign_dual_variable!(container, constraint_type, service, D)
         end
     end
     return
@@ -57,7 +57,7 @@ function assign_dual_variable!(
     container::OptimizationContainer,
     constraint_type::Type{<:ConstraintType},
     service::D,
-    ::AbstractServiceFormulation,
+    ::Type{AbstractServiceFormulation},
 ) where {D <: PSY.Service}
     time_steps = get_time_steps(container)
     service_name = PSY.get_name(service)
@@ -76,8 +76,26 @@ function assign_dual_variable!(
     container::OptimizationContainer,
     constraint_type::Type{<:ConstraintType},
     devices::U,
-    ::AbstractDeviceFormulation,
+    ::Type{<:AbstractDeviceFormulation},
 ) where {U <: Union{Vector{D}, IS.FlattenIteratorWrapper{D}}} where {D <: PSY.Device}
+    @assert !isempty(devices)
+    time_steps = get_time_steps(container)
+    add_dual_container!(
+        container,
+        constraint_type,
+        D,
+        [PSY.get_name(d) for d in devices],
+        time_steps,
+    )
+    return
+end
+
+function assign_dual_variable!(
+    container::OptimizationContainer,
+    constraint_type::Type{<:ConstraintType},
+    devices::U,
+    ::Type{<:PM.AbstractPowerModel},
+) where {U <: Union{Vector{D}, IS.FlattenIteratorWrapper{D}}} where {D <: PSY.Bus}
     @assert !isempty(devices)
     time_steps = get_time_steps(container)
     add_dual_container!(
