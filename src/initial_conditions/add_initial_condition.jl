d98c2a76f8f4fe509fed049c24ea2a340448c85b
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2022-12-08 16:12:39 -0700

    use initial condition data for energy vars ic

--- src/initial_conditions/add_initial_condition.jl
+++ src/initial_conditions/add_initial_condition.jl
@@ -164,7 +164,12 @@ function _get_initial_conditions_value(
     W <: PSY.Component,
 } where {U <: Union{InitialEnergyLevel, InitialEnergyLevelUp, InitialEnergyLevelDown}}
     ic_data = get_initial_conditions_data(container)
-    val = initial_condition_default(U(), component, V())
+    var_type = initial_condition_variable(U(), component, V())
+    if !has_initial_condition_value(ic_data, var_type, W)
+        val = initial_condition_default(U(), component, V())
+    else
+        val = get_initial_condition_value(ic_data, var_type, W)[1, PSY.get_name(component)]
+    end
     @debug "Device $(PSY.get_name(component)) initialized DeviceStatus as $var_type" _group =
         LOG_GROUP_BUILD_INITIAL_CONDITIONS
     return T(component, add_jump_parameter(get_jump_model(container), val))
@@ -182,7 +187,12 @@ function _get_initial_conditions_value(
     W <: PSY.Component,
 } where {U <: Union{InitialEnergyLevel, InitialEnergyLevelUp, InitialEnergyLevelDown}}
     ic_data = get_initial_conditions_data(container)
-    val = initial_condition_default(U(), component, V())
+    var_type = initial_condition_variable(U(), component, V())
+    if !has_initial_condition_value(ic_data, var_type, W)
+        val = initial_condition_default(U(), component, V())
+    else
+        val = get_initial_condition_value(ic_data, var_type, W)[1, PSY.get_name(component)]
+    end
     @debug "Device $(PSY.get_name(component)) initialized DeviceStatus as $var_type" _group =
         LOG_GROUP_BUILD_INITIAL_CONDITIONS
     return T(component, val)
