69a486e037976ad97e47eda5c252afb5bb9925be
Jose Daniel Lara <jdlara@berkeley.edu>
2022-12-21 17:19:56 -0700

    fix use of names for vars

--- src/core/optimization_container.jl
+++ src/core/optimization_container.jl
@@ -278,9 +278,7 @@ function _finalize_jump_model!(container::OptimizationContainer, settings::Setti
     solver_supports_warm_start = _validate_warm_start_support(JuMPmodel, warm_start_enabled)
     set_warm_start!(settings, solver_supports_warm_start)
 
-    if get_store_variable_names(settings)
-        JuMP.set_string_names_on_creation(JuMPmodel, false)
-    end
+    JuMP.set_string_names_on_creation(JuMPmodel, get_store_variable_names(settings))
 
     @debug begin
         JuMP.set_string_names_on_creation(JuMPmodel, true)
