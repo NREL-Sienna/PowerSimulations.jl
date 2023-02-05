aa747e698a8f64d536b13c22acb7a7cd3471f7d1
Jose Daniel Lara <jdlara@berkeley.edu>
2023-02-05 15:42:27 -0700

    methods converter dc balance

--- src/devices_models/devices/HVDCsystems.jl
+++ src/devices_models/devices/HVDCsystems.jl
@@ -1,4 +1,10 @@
 #! format: off
+get_variable_binary(::ActivePowerVariable, ::Type{PSY.InterconnectingConverter}, ::AbstractConverterFormulation) = false
+get_variable_warm_start_value(::ActivePowerVariable, d::PSY.InterconnectingConverter, ::AbstractConverterFormulation) = PSY.get_active_power(d)
+get_variable_lower_bound(::ActivePowerVariable, d::PSY.InterconnectingConverter, ::AbstractConverterFormulation) = PSY.get_active_power_limits(d).min
+get_variable_upper_bound(::ActivePowerVariable, d::PSY.InterconnectingConverter, ::AbstractConverterFormulation) = PSY.get_active_power_limits(d).max
+get_variable_multiplier(_, ::Type{PSY.InterconnectingConverter}, ::AbstractConverterFormulation) = 1.0
+
 requires_initialization(::AbstractConverterFormulation) = false
 requires_initialization(::LossLessLine) = false
 
@@ -43,4 +49,32 @@ function get_default_attributes(
     ::Type{<:AbstractBranchFormulation},
 )
     return Dict{String, Any}()
+end
+
+function add_to_expression!(
+    container::OptimizationContainer,
+    ::Type{T},
+    ::Type{U},
+    devices::IS.FlattenIteratorWrapper{V},
+    ::DeviceModel{V, W},
+    ::Type{X},
+) where {
+    T <: ActivePowerBalanceDC,
+    U <: ActivePowerVariable,
+    V <: PSY.InterconnectingConverter,
+    W <: AbstractDeviceFormulation,
+    X <: PM.AbstractPowerModel,
+}
+    variable = get_variable(container, U(), V)
+    expression = get_expression(container, T(), PSY.DCBus)
+    for d in devices, t in get_time_steps(container)
+        name = PSY.get_name(d)
+        bus_number = PSY.get_number(PSY.get_dc_bus(d))
+        _add_to_jump_expression!(
+            expression[bus_number, t],
+            variable[name, t],
+            get_variable_multiplier(U(), V, W()),
+        )
+    end
+    return
 end
