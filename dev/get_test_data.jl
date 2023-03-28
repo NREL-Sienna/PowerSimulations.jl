523feb721e024565f616440d59a26794915c5b23
Documenter.jl <documenter@juliadocs.github.io>
2023-03-28 18:24:56 +0000

    build based on c37aad1

--- dev/get_test_data.jl
+++ dev/get_test_data.jl
@@ -11,7 +11,7 @@ include("../../../test/test_utils/get_test_data.jl")
 
 abstract type TestOpProblem <: PSI.DecisionProblem end
 
-system = build_c_sys5_re(; add_reserves=true)
+system = build_c_sys5_re(; add_reserves = true)
 solver = optimizer_with_attributes(Cbc.Optimizer)
 
 devices = Dict{Symbol, DeviceModel}(
@@ -27,7 +27,7 @@ services = Dict{Symbol, ServiceModel}();
 
 template = PSI.ProblemTemplate(CopperPlatePowerModel, devices, branches, services);
 
-operation_problem = PSI.DecisionModel(TestOpProblem, template, system; optimizer=solver);
+operation_problem = PSI.DecisionModel(TestOpProblem, template, system; optimizer = solver);
 
 set_services_template!(
     operation_problem,
@@ -38,4 +38,4 @@ set_services_template!(
 )
 
 op_results = solve!(operation_problem)
-re_results = PSI.run_economic_dispatch(system; optimizer=solver, use_parameters=true)
+re_results = PSI.run_economic_dispatch(system; optimizer = solver, use_parameters = true)
