1252bed5bde3082cd62fb2463a64042e3802bdb4
rodrigomha <rodrigomha@gmail.com>
2023-04-20 17:20:07 -0700

    update docstrings

--- docs/src/api/PowerSimulations.md
+++ docs/src/api/PowerSimulations.md
@@ -5,6 +5,16 @@ DocTestSetup  = quote
 end
 ```
 
+# API Reference
+
+
+### Table of Contents
+
+1. [Device Models](#Device-Models)
+2. [Decision Models](#Decision-Models)
+
+
+
 # Device Models
 
 List of structures and methods for Device models
@@ -15,12 +25,19 @@ DeviceModel
 
 ### Formulations
 
-Refer to the Formulations Page for each Abstract Device Formulation.
+Refer to the [Formulations Page](https://nrel-siip.github.io/PowerSimulations.jl/latest/formulation_library/General/) for each Abstract Device Formulation.
+
+```@raw html
+&nbsp;
+&nbsp;
+```
 
 # Decision Models
 
 ```@docs
 DecisionModel
+DecisionModel(::Type{M} where {M <: DecisionProblem}, ::ProblemTemplate, ::PSY.System, ::Union{Nothing, JuMP.Model}) 
+DecisionModel(::AbstractString, ::MOI.OptimizerWithAttributes)
 build!(::DecisionModel)
 solve!(::DecisionModel)
 ```
--- src/core/device_model.jl
+++ src/core/device_model.jl
@@ -16,25 +16,33 @@ function _check_device_formulation(
 end
 
 """
+    DeviceModel(
+        ::Type{D},
+        ::Type{B},
+        feedforwards::Vector{<:AbstractAffectFeedforward}
+        use_slacks::Bool,
+        duals::Vector{DataType},
+        services::Vector{ServiceModel}
+        attributes::Dict{String, Any}
+    )
+
 Establishes the model for a particular device specified by type. Uses the keyword argument
 feedforward to enable passing values between operation model at simulation time
 
 # Arguments
 
--`::Type{D}`: Power System Device Type
--`::Type{B}`: Abstract Device Formulation
-
-# Accepted Key Words
-
-  - `feedforward::Array{<:AbstractAffectFeedforward}` : use to pass parameters between models
-  - `use_slacks::Bool : Add slacks to the device model`
-  - `duals::Vector{DataType} : use to pass constraint type to calculate the duals`
-  - `time_series_names::Dict{Type{<:TimeSeriesParameter}, String} : use to specify time series names associated to the device`
-  - `attributes::Dict{String, Any} : use to specify attributes to the device`
+  - `::Type{D}`: Power System Device Type
+  - `::Type{B}`: Abstract Device Formulation
+  - `feedforward::Array{<:AbstractAffectFeedforward} = Vector{AbstractAffectFeedforward}()` : use to pass parameters between models
+  - `use_slacks::Bool = false` : Add slacks to the device model
+  - `duals::Vector{DataType} = Vector{DataType}()`: use to pass constraint type to calculate the duals
+  - `time_series_names::Dict{Type{<:TimeSeriesParameter}, String} = get_default_time_series_names(D, B)` : use to specify time series names associated to the device`
+  - `attributes::Dict{String, Any} = get_default_attributes(D, B)` : use to specify attributes to the device
 
 # Example
-
-thermal_gens = DeviceModel(ThermalStandard, ThermalBasicUnitCommitment),
+```julia
+thermal_gens = DeviceModel(ThermalStandard, ThermalBasicUnitCommitment)
+```
 """
 mutable struct DeviceModel{D <: PSY.Device, B <: AbstractDeviceFormulation}
     feedforwards::Vector{<:AbstractAffectFeedforward}
