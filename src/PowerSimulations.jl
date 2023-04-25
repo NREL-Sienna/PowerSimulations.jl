58fd05994dbd4a35857cff142a49b83a34a46539
Jose Daniel Lara <jdlara@berkeley.edu>
2023-04-25 13:59:51 -0600

    move more code

--- src/PowerSimulations.jl
+++ src/PowerSimulations.jl
@@ -69,7 +69,6 @@ export ReserveLimitedRegulation
 
 ###### Hydro #######
 export HydroDispatchRunOfRiver
-export HydroCommitmentRunOfRiver
 
 # feedforward models
 export UpperBoundFeedforward
@@ -529,7 +528,6 @@ include("devices_models/devices/thermal_generation.jl")
 include("devices_models/devices/electric_loads.jl")
 include("devices_models/devices/AC_branches.jl")
 include("devices_models/devices/DC_branches.jl")
-include("devices_models/devices/hydro_generation.jl")
 include("devices_models/devices/regulation_device.jl")
 
 # Services Models
@@ -551,7 +549,6 @@ include("initial_conditions/initialization.jl")
 # Device constructors
 include("devices_models/device_constructors/constructor_validations.jl")
 include("devices_models/device_constructors/thermalgeneration_constructor.jl")
-include("devices_models/device_constructors/hydrogeneration_constructor.jl")
 include("devices_models/device_constructors/branch_constructor.jl")
 include("devices_models/device_constructors/renewablegeneration_constructor.jl")
 include("devices_models/device_constructors/load_constructor.jl")
