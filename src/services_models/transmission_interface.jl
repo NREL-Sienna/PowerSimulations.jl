f7ef29a68d74994ee47a0c7f60d7b15cdb625faa
Jose Daniel Lara <jdlara@berkeley.edu>
2023-05-23 18:44:47 -0600

    implement interface constraints

--- src/services_models/transmission_interface.jl
+++ src/services_models/transmission_interface.jl
@@ -17,3 +17,37 @@ function get_initial_conditions_service_model(
 ) where {T <: PSY.TransmissionInterface, D <: ConstantMaxInterfaceFlow}
     return ServiceModel(T, D)
 end
+
+function add_constraints!(container::OptimizationContainer,
+    ::Type{InterfaceFlowLimit},
+    interface::T,
+    model::ServiceModel{T, ConstantMaxInterfaceFlow},
+) where {T <: PSY.TransmissionInterface}
+    expr = get_expression(container, InterfaceTotalFlow(), T)
+    interfaces, timesteps = axes(expr)
+    constraint_container_ub = lazy_container_addition!(
+        container,
+        InterfaceFlowLimit(),
+        T,
+        interfaces,
+        timesteps;
+        meta = "ub",
+    )
+    constraint_container_lb = lazy_container_addition!(
+        container,
+        InterfaceFlowLimit(),
+        T,
+        interfaces,
+        timesteps;
+        meta = "lb",
+    )
+    int_name = PSY.get_name(interface)
+    min_flow, max_flow = PSY.get_active_power_flow_limits(interface)
+    for t in timesteps
+        constraint_container_ub[int_name, t] =
+            JuMP.@constraint(get_jump_model(container), expr[int_name, t] <= max_flow)
+        constraint_container_lb[int_name, t] =
+            JuMP.@constraint(get_jump_model(container), expr[int_name, t] >= min_flow)
+    end
+    return
+end
