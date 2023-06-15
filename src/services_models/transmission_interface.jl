21eb5737b3cbe416fc12ae58a7280345ef6e8833
Jose Daniel Lara <jdlara@berkeley.edu>
2023-06-15 17:07:53 -0600

    add slacks for interface

--- src/services_models/transmission_interface.jl
+++ src/services_models/transmission_interface.jl
@@ -1,3 +1,12 @@
+#! format: off
+get_variable_binary(_, ::Type{PSY.TransmissionInterface}, ::ConstantMaxInterfaceFlow) = false
+get_variable_lower_bound(::InterfaceFlowSlackUp, ::PSY.TransmissionInterface, ::ConstantMaxInterfaceFlow) = 0.0
+get_variable_lower_bound(::InterfaceFlowSlackDown, ::PSY.TransmissionInterface, ::ConstantMaxInterfaceFlow) = 0.0
+
+get_variable_multiplier(::InterfaceFlowSlackUp, ::PSY.TransmissionInterface, ::ConstantMaxInterfaceFlow) = 1.0
+get_variable_multiplier(::InterfaceFlowSlackDown, ::PSY.TransmissionInterface, ::ConstantMaxInterfaceFlow) = -1.0
+
+#! format: On
 function get_default_time_series_names(
     ::Type{PSY.TransmissionInterface},
     ::Type{ConstantMaxInterfaceFlow},
@@ -17,6 +26,8 @@ function get_initial_conditions_service_model(
 ) where {T <: PSY.TransmissionInterface, D <: ConstantMaxInterfaceFlow}
     return ServiceModel(T, D)
 end
+
+
 
 function add_constraints!(container::OptimizationContainer,
     ::Type{InterfaceFlowLimit},
@@ -48,6 +59,27 @@ function add_constraints!(container::OptimizationContainer,
             JuMP.@constraint(get_jump_model(container), expr[int_name, t] <= max_flow)
         constraint_container_lb[int_name, t] =
             JuMP.@constraint(get_jump_model(container), expr[int_name, t] >= min_flow)
+    end
+    return
+end
+
+function objective_function!(
+    container::OptimizationContainer,
+    service::T,
+    model::ServiceModel{T, U},
+) where {T <: PSY.TransmissionInterface, U <: ConstantMaxInterfaceFlow}
+    # At the moment the interfaces have no costs associated with them
+    if get_use_slacks(model)
+        variable_up = get_variable(container, InterfaceFlowSlackDown(), T)
+        variable_dn = get_variable(container, InterfaceFlowSlackDown(), T)
+        penalty = PSY.get_violation_penalty(service)
+        name = PSY.get_name(service)
+        for t in get_time_steps(container)
+            add_to_objective_invariant_expression!(
+                container,
+                (variable_dn[name, t] + variable_up[name, t]) * penalty,
+            )
+        end
     end
     return
 end
