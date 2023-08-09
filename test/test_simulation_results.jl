NREL-Sienna/PowerSimulations.jl

commit 21f6dff464b322f4785a426ef6dd215082ed0510

Refactor storage and access of results
This commit changes storage and access of results in order to improve
performance. DataFrames are replaced by DenseAxisArrays.

It also optimizes the generation of realized results.

src/simulation/realized_meta.jl
@@ -47,41 +47,48 @@ function RealizedMeta(
 end
 
 function get_realization(
-    result_values::Dict{
-        OptimizationContainerKey,
-        SortedDict{Dates.DateTime, DataFrames.DataFrame},
-    },
+    results::Dict{OptimizationContainerKey, ResultsByTime{Matrix{Float64}}},
     meta::RealizedMeta,
 )
     realized_values = Dict{OptimizationContainerKey, DataFrames.DataFrame}()
-    for (key, result_value) in result_values
-        results_concat = Dict{Symbol, Vector{Float64}}()
-        for (step, (t, df)) in enumerate(result_value)
+    lk = ReentrantLock()
+    num_rows = length(meta.realized_timestamps)
+    start = time()
+    Threads.@threads for key in collect(keys(results))
+        results_by_time = results[key]
+        columns = get_column_names(results_by_time)
+        num_cols = length(columns)
+        matrix = Matrix{Float64}(undef, num_rows, num_cols)
+        for (step, (_, array)) in enumerate(results_by_time)
             first_id = step > 1 ? 1 : meta.start_offset
             last_id =
                 step == meta.len ? meta.interval_len - meta.end_offset : meta.interval_len
-            if last_id - first_id > size(df, 1)
+            if last_id - first_id > size(array, 1)
                 error(
-                    "Variable $(encode_key_as_string(key)) has $(size(df, 1)) number of steps, that is different than the default problem horizon. \
+                    "Variable $(encode_key_as_string(key)) has $(size(array, 1)) number of steps, that is different than the default problem horizon. \
               Can't calculate the realized variables. Use `read_variables` instead and write your own concatenation",
                 )
             end
-            for colname in propertynames(df)
-                colname == :DateTime && continue
-                col = df[!, colname][first_id:last_id]
-                if !haskey(results_concat, colname)
-                    results_concat[colname] = col
-                else
-                    results_concat[colname] = vcat(results_concat[colname], col)
-                end
-            end
+            row_start = (step - 1) * meta.interval_len + 1
+            row_end = row_start + last_id - first_id
+            matrix[row_start:row_end, :] = array[first_id:last_id, :]
         end
-        realized_values[key] = DataFrames.DataFrame(results_concat; copycols = false)
+        df = DataFrames.DataFrame(matrix, collect(columns); copycols = false)
         DataFrames.insertcols!(
-            realized_values[key],
+            df,
             1,
             :DateTime => meta.realized_timestamps,
         )
+        lock(lk) do
+            realized_values[key] = df
+        end
     end
+
+    duration = time() - start
+    if Threads.nthreads() == 1 && duration > 10.0
+        @info "Time to read results: $duration seconds. You will likely get faster " *
+              "results by starting Julia with multiple threads."
+    end
+
     return realized_values
 end

test/test_simulation_results.jl
@@ -417,10 +417,16 @@ function test_decision_problem_results_values(
     )
 
     @test !isempty(
-        results_ed.values.variables[PSI.VariableKey(ActivePowerVariable, ThermalStandard)],
+        PSI.get_cached_variables(results_ed)[PSI.VariableKey(
+            ActivePowerVariable,
+            ThermalStandard,
+        )].data,
     )
     @test length(
-        results_ed.values.variables[PSI.VariableKey(ActivePowerVariable, ThermalStandard)],
+        PSI.get_cached_variables(results_ed)[PSI.VariableKey(
+            ActivePowerVariable,
+            ThermalStandard,
+        )].data,
     ) == 3
     @test length(results_ed) == 3
 
@@ -447,8 +453,9 @@ function test_decision_problem_results_values(
     )
 
     empty!(results_ed)
-    @test isempty(
-        results_ed.values.variables[PSI.VariableKey(ActivePowerVariable, ThermalStandard)],
+    @test !haskey(
+        PSI.get_cached_variables(results_ed),
+        PSI.VariableKey(ActivePowerVariable, ThermalStandard),
     )
 
     initial_time = DateTime("2024-01-01T00:00:00")
@@ -462,18 +469,24 @@ function test_decision_problem_results_values(
     )
 
     @test !isempty(
-        results_ed.values.variables[PSI.VariableKey(ActivePowerVariable, ThermalStandard)],
+        PSI.get_cached_variables(results_ed)[PSI.VariableKey(
+            ActivePowerVariable,
+            ThermalStandard,
+        )].data,
     )
     @test !isempty(
-        results_ed.values.duals[PSI.ConstraintKey(CopperPlateBalanceConstraint, System)],
+        PSI.get_cached_duals(results_ed)[PSI.ConstraintKey(
+            CopperPlateBalanceConstraint,
+            System,
+        )].data,
     )
     @test !isempty(
-        results_ed.values.parameters[PSI.ParameterKey{
+        PSI.get_cached_parameters(results_ed)[PSI.ParameterKey{
             ActivePowerTimeSeriesParameter,
             RenewableDispatch,
         }(
             "",
-        )],
+        )].data,
     )
 end
 

src/PowerSimulations.jl
@@ -452,6 +452,7 @@ include("core/cache_utils.jl")
 include("core/optimizer_stats.jl")
 include("core/dataset.jl")
 include("core/dataset_container.jl")
+include("core/results_by_time.jl")
 
 include("core/optimization_container.jl")
 include("core/store_common.jl")
@@ -496,9 +497,9 @@ include("simulation/simulation_store_params.jl")
 include("simulation/hdf_simulation_store.jl")
 include("simulation/in_memory_simulation_store.jl")
 include("simulation/simulation_problem_results.jl")
-include("simulation/realized_meta.jl")
 include("simulation/decision_model_simulation_results.jl")
 include("simulation/emulation_model_simulation_results.jl")
+include("simulation/realized_meta.jl")
 include("simulation/simulation_partitions.jl")
 include("simulation/simulation_partition_results.jl")
 include("simulation/simulation_sequence.jl")
@@ -574,5 +575,13 @@ include("utils/powersystems_utils.jl")
 include("utils/recorder_events.jl")
 include("utils/datetime_utils.jl")
 include("utils/generate_valid_formulations.jl")
+
+# TODO: These exist for backward compatibility and need to be deprecated and removed.
+read_aux_variables_with_keys(args...; kwargs...) =
+    read_results_with_keys(args...; kwargs...)
+read_duals_with_keys(args...; kwargs...) = read_results_with_keys(args...; kwargs...)
+read_expressions_with_keys(args...; kwargs...) = read_results_with_keys(args...; kwargs...)
+read_parameters_with_keys(args...; kwargs...) = read_results_with_keys(args...; kwargs...)
+read_variables_with_keys(args...; kwargs...) = read_results_with_keys(args...; kwargs...)
 
 end

src/core/dataset.jl
@@ -1,6 +1,5 @@
 abstract type AbstractDataset end
 
-Base.length(s::AbstractDataset) = size(s.values)[1]
 get_data_resolution(s::AbstractDataset)::Dates.Millisecond = s.resolution
 get_last_recorded_row(s::AbstractDataset) = s.last_recorded_row
 
@@ -21,8 +20,9 @@ end
 
 # Values field is accessed with dot syntax to avoid type instability
 
-mutable struct DataFrameDataset <: AbstractDataset
-    values::DataFrames.DataFrame
+mutable struct InMemoryDataset <: AbstractDataset
+    "Data with dimensions (column names, row indexes)"
+    values::DenseAxisArray{Float64, 2}
     # We use Array here to allow for overwrites when updating the state
     timestamps::Vector{Dates.DateTime}
     # Resolution is needed because AbstractDataset might have just one row
@@ -32,13 +32,13 @@ mutable struct DataFrameDataset <: AbstractDataset
     update_timestamp::Dates.DateTime
 end
 
-function DataFrameDataset(
-    values::DataFrames.DataFrame,
+function InMemoryDataset(
+    values::DenseAxisArray{Float64, 2},
     timestamps::Vector{Dates.DateTime},
     resolution::Dates.Millisecond,
     end_of_step_index::Int,
 )
-    return DataFrameDataset(
+    return InMemoryDataset(
         values,
         timestamps,
         resolution,
@@ -48,8 +48,8 @@ function DataFrameDataset(
     )
 end
 
-function DataFrameDataset(values::DataFrames.DataFrame)
-    return DataFrameDataset(
+function InMemoryDataset(values::DenseAxisArray{Float64, 2})
+    return InMemoryDataset(
         values,
         Vector{Dates.DateTime}(),
         Dates.Second(0.0),
@@ -59,41 +59,42 @@ function DataFrameDataset(values::DataFrames.DataFrame)
     )
 end
 
+Base.length(s::InMemoryDataset) = size(s.values)[2]
+
 function make_system_state(
-    values::DataFrames.DataFrame,
+    values::DenseAxisArray{Float64, 2},
     timestamp::Dates.DateTime,
     resolution::Dates.Millisecond,
 )
-    return DataFrameDataset(values, [timestamp], resolution, 0, 1, UNSET_INI_TIME)
+    return InMemoryDataset(values, [timestamp], resolution, 0, 1, UNSET_INI_TIME)
 end
 
-function get_dataset_value(s::DataFrameDataset, date::Dates.DateTime)
+function get_dataset_value(s::InMemoryDataset, date::Dates.DateTime)
     s_index = find_timestamp_index(s.timestamps, date)
     if isnothing(s_index)
         error("Request time stamp $date not in the state")
     end
-    return s.values[s_index, :]
+    return s.values[:, s_index]
 end
 
-function get_column_names(::OptimizationContainerKey, s::DataFrameDataset)
-    return DataFrames.names(s.values)
-end
+get_column_names(s::InMemoryDataset) = axes(s.values)[1]
+get_column_names(::OptimizationContainerKey, s::InMemoryDataset) = get_column_names(s)
 
-function get_last_recorded_value(s::DataFrameDataset)
+function get_last_recorded_value(s::InMemoryDataset)
     if get_last_recorded_row(s) == 0
         error("The Dataset hasn't been written yet")
     end
-    return s.values[get_last_recorded_row(s), :]
+    return s.values[:, get_last_recorded_row(s)]
 end
 
-function get_end_of_step_timestamp(s::DataFrameDataset)
+function get_end_of_step_timestamp(s::InMemoryDataset)
     return s.timestamps[s.end_of_step_index]
 end
 
 """
 Return the timestamp from most recent data row updated in the dataset. This value may not be the same as the result from `get_update_timestamp`
 """
-function get_last_updated_timestamp(s::DataFrameDataset)
+function get_last_updated_timestamp(s::InMemoryDataset)
     last_recorded_row = get_last_recorded_row(s)
     if last_recorded_row == 0
         return UNSET_INI_TIME
@@ -101,7 +102,7 @@ function get_last_updated_timestamp(s::DataFrameDataset)
     return s.timestamps[last_recorded_row]
 end
 
-function get_value_timestamp(s::DataFrameDataset, date::Dates.DateTime)
+function get_value_timestamp(s::InMemoryDataset, date::Dates.DateTime)
     s_index = find_timestamp_index(s.timestamps, date)
     if isnothing(s_index)
         error("Request time stamp $date not in the state")
@@ -109,14 +110,13 @@ function get_value_timestamp(s::DataFrameDataset, date::Dates.DateTime)
     return s.timestamps[s_index]
 end
 
-function set_value!(s::DataFrameDataset, vals, index::Int)
-    setindex!(s.values, vals, index, :)
+function set_value!(s::InMemoryDataset, vals::DenseAxisArray{Float64, 2}, index::Int)
+    s.values[:, index] = vals[:, index]
     return
 end
 
-function set_value!(s::DataFrameDataset, vals::DataFrames.DataFrame, index::Int)
-    @assert_op size(vals)[1] == 1
-    set_value!(s, vals[1, :], index)
+function set_value!(s::InMemoryDataset, vals::DenseAxisArray{Float64, 1}, index::Int)
+    s.values[:, index] = vals
     return
 end
 
@@ -129,14 +129,35 @@ mutable struct HDF5Dataset <: AbstractDataset
     resolution::Dates.Millisecond
     initial_timestamp::Dates.DateTime
     update_timestamp::Dates.DateTime
+    column_names::Vector{String}
+
+    function HDF5Dataset(values, column_dataset, write_index, last_recorded_row, resolution,
+        initial_timestamp,
+        update_timestamp, column_names,
+    )
+        new(values, column_dataset, write_index, last_recorded_row, resolution,
+            initial_timestamp,
+            update_timestamp, column_names)
+    end
 end
+
+#Base.length(s::HDF5Dataset) = size(s.values)[1]  # TODO DT: what about the 3-dim case?
+# Not getting called by tests
+Base.length(s::HDF5Dataset) = error("die")
 
 HDF5Dataset(values, column_dataset, resolution, initial_time) =
-    HDF5Dataset(values, column_dataset, 1, 0, resolution, initial_time, UNSET_INI_TIME)
+    HDF5Dataset(
+        values,
+        column_dataset,
+        1,
+        0,
+        resolution,
+        initial_time,
+        UNSET_INI_TIME,
+        column_dataset[:],
+    )
 
-function get_column_names(::OptimizationContainerKey, s::HDF5Dataset)
-    return s.column_dataset[:]
-end
+get_column_names(::OptimizationContainerKey, s::HDF5Dataset) = s.column_names
 
 """
 Return the timestamp from most recent data row updated in the dataset. This value may not be the same as the result from `get_update_timestamp`
@@ -150,6 +171,8 @@ function get_last_updated_timestamp(s::HDF5Dataset)
 end
 
 function get_value_timestamp(s::HDF5Dataset, date::Dates.DateTime)
+    # TODO: This code is broken because timestamps is not a field.
+    # The function is called for InMemoryDataset but not HDF5Dataset.
     s_index = find_timestamp_index(s.timestamps, date)
     if isnothing(s_index)
         error("Request time stamp $date not in the state")

src/core/dataset_container.jl
@@ -27,23 +27,23 @@ function Base.empty!(container::DatasetContainer)
     return
 end
 
-function get_duals_values(container::DatasetContainer{DataFrameDataset})
+function get_duals_values(container::DatasetContainer{InMemoryDataset})
     return container.duals
 end
 
-function get_aux_variables_values(container::DatasetContainer{DataFrameDataset})
+function get_aux_variables_values(container::DatasetContainer{InMemoryDataset})
     return container.aux_variables
 end
 
-function get_variables_values(container::DatasetContainer{DataFrameDataset})
+function get_variables_values(container::DatasetContainer{InMemoryDataset})
     return container.variables
 end
 
-function get_parameters_values(container::DatasetContainer{DataFrameDataset})
+function get_parameters_values(container::DatasetContainer{InMemoryDataset})
     return container.parameters
 end
 
-function get_expression_values(container::DatasetContainer{DataFrameDataset})
+function get_expression_values(container::DatasetContainer{InMemoryDataset})
     return container.expressions
 end
 
@@ -133,7 +133,6 @@ function get_dataset(
     return get_dataset(container, ExpressionKey(T, U))
 end
 
-# Get dataset values is currently type unstable since the values field could be a DF
 function get_dataset_values(container::DatasetContainer, key::OptimizationContainerKey)
     return get_dataset(container, key).values
 end

src/core/optimization_container.jl
@@ -939,7 +939,7 @@ function get_constraint(
 end
 
 function read_duals(container::OptimizationContainer)
-    return Dict(k => axis_array_to_dataframe(v, k) for (k, v) in get_duals(container))
+    return Dict(k => to_dataframe(jump_value.(v), k) for (k, v) in get_duals(container))
 end
 
 ##################################### Parameter Container ##################################
@@ -1203,9 +1203,9 @@ function read_parameters(container::OptimizationContainer)
     for (k, v) in parameters
         # TODO: all functions similar to calculate_parameter_values should be in one
         # place and be consistent in behavior.
-        #params_dict[k] = axis_array_to_dataframe(calculate_parameter_values(v))
-        param_array = axis_array_to_dataframe(get_parameter_values(v), k)
-        multiplier_array = axis_array_to_dataframe(get_multiplier_array(v), k)
+        #params_dict[k] = to_dataframe(calculate_parameter_values(v))
+        param_array = to_dataframe(get_parameter_values(v), k)
+        multiplier_array = to_dataframe(get_multiplier_array(v), k)
         params_dict[k] = _calculate_parameter_values(k, param_array, multiplier_array)
     end
     return params_dict
@@ -1322,7 +1322,7 @@ end
 
 function read_expressions(container::OptimizationContainer)
     return Dict(
-        k => axis_array_to_dataframe(v, k) for (k, v) in get_expressions(container) if
+        k => to_dataframe(jump_value.(v), k) for (k, v) in get_expressions(container) if
         !(get_entry_type(k) <: SystemBalanceExpressions)
     )
 end
@@ -1395,7 +1395,7 @@ function write_initial_conditions_data!(
             if field == STORE_CONTAINER_PARAMETERS
                 ic_data_dict[key] = ic_container_dict[key]
             else
-                ic_data_dict[key] = axis_array_to_dataframe(field_container, key)
+                ic_data_dict[key] = to_dataframe(jump_value.(field_container), key)
             end
         end
     end

src/core/results_by_time.jl
@@ -1,0 +1,84 @@
+struct ResultsByTime{T}
+    key::OptimizationContainerKey
+    data::SortedDict{Dates.DateTime, T}
+    resolution::Dates.Period
+    column_names::Vector{String}
+end
+
+function ResultsByTime(key, data, resolution, column_names)
+    _check_column_consistency(data, column_names)
+    ResultsByTime(key, data, resolution, column_names)
+end
+
+function _check_column_consistency(
+    data::SortedDict{Dates.DateTime, DenseAxisArray{Float64, 2}},
+    cols,
+)
+    for val in values(data)
+        if axes(val)[1] != cols
+            error("Mismatch in DenseAxisArray column names: $(axes(val)[1]) $cols")
+        end
+    end
+end
+
+function _check_column_consistency(data::SortedDict{Dates.DateTime, Matrix{Float64}}, cols)
+    for val in values(data)
+        if size(val)[2] != length(cols)
+            error("Mismatch in length of Matrix columns: $(size(val)[2]) $(length(cols))")
+        end
+    end
+end
+
+# This struct behaves like a dict, delegating to its 'data' field.
+Base.length(res::ResultsByTime) = length(res.data)
+Base.iterate(res::ResultsByTime) = iterate(res.data)
+Base.iterate(res::ResultsByTime, state) = iterate(res.data, state)
+Base.getindex(res::ResultsByTime, i) = getindex(res.data, i)
+Base.setindex!(res::ResultsByTime, v, i) = setindex!(res.data, v, i)
+Base.firstindex(res::ResultsByTime) = firstindex(res.data)
+Base.lastindex(res::ResultsByTime) = lastindex(res.data)
+
+get_column_names(x::ResultsByTime) = x.column_names
+get_num_rows(::ResultsByTime{DenseAxisArray{Float64, 2}}, data) = length(axes(data)[2])
+get_num_rows(::ResultsByTime{Matrix{Float64}}, data) = size(data)[1]
+
+function _add_timestamps!(df::DataFrames.DataFrame, results::ResultsByTime, timestamp, data)
+    time_col =
+        range(timestamp; length = get_num_rows(results, data), step = results.resolution)
+    DataFrames.insertcols!(df, 1, :DateTime => time_col)
+end
+
+function make_dataframe(
+    results::ResultsByTime{DenseAxisArray{Float64, 2}},
+    timestamp::Dates.DateTime,
+)
+    array = results.data[timestamp]
+    df = DataFrames.DataFrame(permutedims(array.data), axes(array)[1])
+    _add_timestamps!(df, results, timestamp, array)
+    return df
+end
+
+function make_dataframe(results::ResultsByTime{Matrix{Float64}}, timestamp::Dates.DateTime)
+    array = results.data[timestamp]
+    df = DataFrames.DataFrame(array, results.column_names)
+    _add_timestamps!(df, results, timestamp, array)
+    return df
+end
+
+function make_dataframes(results::ResultsByTime)
+    return SortedDict(k => make_dataframe(results, k) for k in keys(results.data))
+end
+
+struct ResultsByKeyAndTime
+    "Contains all keys stored in the model."
+    result_keys::Vector{OptimizationContainerKey}
+    "Contains the results that have been read from the store and cached."
+    cached_results::Dict{OptimizationContainerKey, ResultsByTime}
+end
+
+ResultsByKeyAndTime(result_keys) = ResultsByKeyAndTime(
+    collect(result_keys),
+    Dict{OptimizationContainerKey, ResultsByTime}(),
+)
+
+Base.empty!(res::ResultsByKeyAndTime) = empty!(res.cached_results)

src/core/store_common.jl
@@ -53,14 +53,15 @@ function write_model_dual_results!(
 
     for (key, constraint) in get_duals(container)
         !should_write_resulting_value(key) && continue
-        write_result!(store, model_name, key, index, update_timestamp, constraint)
+        data = jump_value.(constraint)
+        write_result!(store, model_name, key, index, update_timestamp, data)
 
         if export_params !== nothing &&
            should_export_dual(export_params[:exports], index, model_name, key)
             horizon = export_params[:horizon]
             resolution = export_params[:resolution]
             file_type = export_params[:file_type]
-            df = axis_array_to_dataframe(constraint, key)
+            df = to_dataframe(jump_value.(constraint), key)
             time_col = range(index; length = horizon, step = resolution)
             DataFrames.insertcols!(df, 1, :DateTime => time_col)
             export_result(file_type, exports_path, key, index, df)
@@ -95,7 +96,11 @@ function write_model_parameter_results!(
            should_export_parameter(export_params[:exports], index, model_name, key)
             resolution = export_params[:resolution]
             file_type = export_params[:file_type]
-            df = axis_array_to_dataframe(data, key)
+            # TODO DT: why was jump_value being called again?
+            # (first time was in calculate_parameter_values)
+            # Was it always a noop?
+            df = to_dataframe(data, key)
+            #df = to_dataframe(jump_value.(data), key)
             time_col = range(index; length = horizon, step = resolution)
             DataFrames.insertcols!(df, 1, :DateTime => time_col)
             export_result(file_type, exports_path, key, index, df)
@@ -126,14 +131,15 @@ function write_model_variable_results!(
 
     for (key, variable) in variables
         !should_write_resulting_value(key) && continue
-        write_result!(store, model_name, key, index, update_timestamp, variable)
+        data = jump_value.(variable)
+        write_result!(store, model_name, key, index, update_timestamp, data)
 
         if export_params !== nothing &&
            should_export_variable(export_params[:exports], index, model_name, key)
             horizon = export_params[:horizon]
             resolution = export_params[:resolution]
             file_type = export_params[:file_type]
-            df = axis_array_to_dataframe(variable, key)
+            df = to_dataframe(data, key)
             time_col = range(index; length = horizon, step = resolution)
             DataFrames.insertcols!(df, 1, :DateTime => time_col)
             export_result(file_type, exports_path, key, index, df)
@@ -158,14 +164,15 @@ function write_model_aux_variable_results!(
 
     for (key, variable) in get_aux_variables(container)
         !should_write_resulting_value(key) && continue
-        write_result!(store, model_name, key, index, update_timestamp, variable)
+        data = jump_value.(variable)
+        write_result!(store, model_name, key, index, update_timestamp, data)
 
         if export_params !== nothing &&
            should_export_aux_variable(export_params[:exports], index, model_name, key)
             horizon = export_params[:horizon]
             resolution = export_params[:resolution]
             file_type = export_params[:file_type]
-            df = axis_array_to_dataframe(variable, key)
+            df = to_dataframe(data, key)
             time_col = range(index; length = horizon, step = resolution)
             DataFrames.insertcols!(df, 1, :DateTime => time_col)
             export_result(file_type, exports_path, key, index, df)
@@ -196,14 +203,15 @@ function write_model_expression_results!(
 
     for (key, expression) in expressions
         !should_write_resulting_value(key) && continue
-        write_result!(store, model_name, key, index, update_timestamp, expression)
+        data = jump_value.(expression)
+        write_result!(store, model_name, key, index, update_timestamp, data)
 
         if export_params !== nothing &&
            should_export_expression(export_params[:exports], index, model_name, key)
             horizon = export_params[:horizon]
             resolution = export_params[:resolution]
             file_type = export_params[:file_type]
-            df = axis_array_to_dataframe(expression, key)
+            df = to_dataframe(data, key)
             time_col = range(index; length = horizon, step = resolution)
             DataFrames.insertcols!(df, 1, :DateTime => time_col)
             export_result(file_type, exports_path, key, index, df)

commit a262204e157167514c8e31d1003ea685af09f53a

Fix bug in custom indexing for realized results
This bug was introduced in commit
21f6dff464b322f4785a426ef6dd215082ed0510. Passing custom values of
start_time and len that cause the code to span arrays from two initial
times caused an array indexing error.

src/simulation/realized_meta.jl
@@ -59,6 +59,7 @@ function get_realization(
         columns = get_column_names(results_by_time)
         num_cols = length(columns)
         matrix = Matrix{Float64}(undef, num_rows, num_cols)
+        row_index = 1
         for (step, (_, array)) in enumerate(results_by_time)
             first_id = step > 1 ? 1 : meta.start_offset
             last_id =
@@ -69,9 +70,9 @@ function get_realization(
               Can't calculate the realized variables. Use `read_variables` instead and write your own concatenation",
                 )
             end
-            row_start = (step - 1) * meta.interval_len + 1
-            row_end = row_start + last_id - first_id
-            matrix[row_start:row_end, :] = array[first_id:last_id, :]
+            row_end = row_index + last_id - first_id
+            matrix[row_index:row_end, :] = array[first_id:last_id, :]
+            row_index += last_id - first_id + 1
         end
         df = DataFrames.DataFrame(matrix, collect(columns); copycols = false)
         DataFrames.insertcols!(

test/test_simulation_results.jl
@@ -311,6 +311,17 @@ function test_decision_problem_results_values(
         @test size(var)[1] == 48
     end
 
+    # Test custom indexing.
+    realized_variable_uc2 =
+        read_realized_variables(
+            results_uc,
+            [(ActivePowerVariable, ThermalStandard)];
+            start_time = Dates.DateTime("2024-01-01T01:00:00"),
+            len = 47,
+        )
+    @test realized_variable_uc["ActivePowerVariable__ThermalStandard"][2:end, :] ==
+          realized_variable_uc2["ActivePowerVariable__ThermalStandard"]
+
     realized_param_uc = read_realized_parameters(results_uc)
     @test length(keys(realized_param_uc)) == 3
     for param in values(realized_param_uc)