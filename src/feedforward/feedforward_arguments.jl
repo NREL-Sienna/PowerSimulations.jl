3e8da3b3f46a6347a01fa6921ce32589eee642b6
Jose Daniel Lara <jdlara@berkeley.edu>
2023-09-05 11:12:05 -0600

    formatter

--- src/feedforward/feedforward_arguments.jl
+++ src/feedforward/feedforward_arguments.jl
@@ -80,11 +80,10 @@ function _add_feedforward_slack_variables!(container::OptimizationContainer,
                 lower_bound = 0.0
             )
             add_to_objective_invariant_expression!(
-            container,
-            variable_container[name, t] * BALANCE_SLACK_COST,
-        )
+                container,
+                variable_container[name, t] * BALANCE_SLACK_COST,
+            )
         end
-
     end
     return
 end
