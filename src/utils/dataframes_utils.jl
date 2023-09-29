c5ca8c212fc5f590feb259dc36792c08fb13a3e6
Jose Daniel Lara <jdlara@berkeley.edu>
2023-09-28 20:02:53 -0600

    add missing method

--- src/utils/dataframes_utils.jl
+++ src/utils/dataframes_utils.jl
@@ -7,7 +7,11 @@ Creates a DataFrame from a JuMP DenseAxisArray or SparseAxisArray.
   - `array`: JuMP DenseAxisArray or SparseAxisArray to convert
   - `key::OptimizationContainerKey`:
 """
-function to_dataframe(array::DenseAxisArray{T, 2}, key::OptimizationContainerKey) where {T}
+function to_dataframe(array::DenseAxisArray{T, 2}, key::OptimizationContainerKey) where {T <: Number}
+    return DataFrames.DataFrame(to_matrix(array), get_column_names(key, array)[1])
+end
+
+function to_dataframe(array::DenseAxisArray{T, 1}, key::OptimizationContainerKey) where {T <: Number}
     return DataFrames.DataFrame(to_matrix(array), get_column_names(key, array)[1])
 end
 
