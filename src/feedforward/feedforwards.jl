acd5f6e99d0f8b8e4d978d210a6f5048c3bec701
Jose Daniel Lara <jdlara@berkeley.edu>
2023-10-09 13:36:36 -0600

    remove FF code

--- src/feedforward/feedforwards.jl
+++ src/feedforward/feedforwards.jl
@@ -202,42 +202,6 @@ function has_semicontinuous_feedforward(
     return has_semicontinuous_feedforward(model, ActivePowerVariable)
 end
 
-"""
-Adds a constraint to limit the sum of a variable over the number of periods to the source value
-"""
-struct EnergyLimitFeedforward <: AbstractAffectFeedforward
-    optimization_container_key::OptimizationContainerKey
-    affected_values::Vector{<:OptimizationContainerKey}
-    number_of_periods::Int
-    function EnergyLimitFeedforward(;
-        component_type::Type{<:PSY.Component},
-        source::Type{T},
-        affected_values::Vector{DataType},
-        number_of_periods::Int,
-        meta = CONTAINER_KEY_EMPTY_META,
-    ) where {T}
-        values_vector = Vector{VariableKey}(undef, length(affected_values))
-        for (ix, v) in enumerate(affected_values)
-            if v <: VariableType
-                values_vector[ix] =
-                    get_optimization_container_key(v(), component_type, meta)
-            else
-                error(
-                    "EnergyLimitFeedforward is only compatible with VariableType or ParamterType affected values",
-                )
-            end
-        end
-        new(
-            get_optimization_container_key(T(), component_type, meta),
-            values_vector,
-            number_of_periods,
-        )
-    end
-end
-
-get_default_parameter_type(::EnergyLimitFeedforward, _) = EnergyLimitParameter
-get_optimization_container_key(ff) = ff.optimization_container_key
-get_number_of_periods(ff) = ff.number_of_periods
 
 """
 Fixes a Variable or Parameter Value in the model. Is the only Feed Forward that can be used
@@ -269,42 +233,3 @@ end
 
 get_default_parameter_type(::FixValueFeedforward, _) = FixValueParameter
 get_optimization_container_key(ff::FixValueFeedforward) = ff.optimization_container_key
-
-"""
-Adds a constraint to enforce a minimum energy level target with a slack variable associated witha penalty term.
-"""
-struct EnergyTargetFeedforward <: AbstractAffectFeedforward
-    optimization_container_key::OptimizationContainerKey
-    affected_values::Vector{<:OptimizationContainerKey}
-    target_period::Int
-    penalty_cost::Float64
-    function EnergyTargetFeedforward(;
-        component_type::Type{<:PSY.Component},
-        source::Type{T},
-        affected_values::Vector{DataType},
-        target_period::Int,
-        penalty_cost::Float64,
-        meta = CONTAINER_KEY_EMPTY_META,
-    ) where {T}
-        values_vector = Vector{VariableKey}(undef, length(affected_values))
-        for (ix, v) in enumerate(affected_values)
-            if v <: VariableType
-                values_vector[ix] =
-                    get_optimization_container_key(v(), component_type, meta)
-            else
-                error(
-                    "EnergyTargetFeedforward is only compatible with VariableType or ParamterType affected values",
-                )
-            end
-        end
-        new(
-            get_optimization_container_key(T(), component_type, meta),
-            values_vector,
-            target_period,
-            penalty_cost,
-        )
-    end
-end
-
-get_default_parameter_type(::EnergyTargetFeedforward, _) = EnergyTargetParameter
-get_optimization_container_key(ff::EnergyTargetFeedforward) = ff.optimization_container_key
