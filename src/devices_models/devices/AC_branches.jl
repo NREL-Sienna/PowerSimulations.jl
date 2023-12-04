d8212f907decc340176608f302d3035a5f349d4a
alefcastelli <alef.castelli@gmail.com>
2023-12-04 14:41:03 -0700

    afc/ mistakenly removed input argument

--- src/devices_models/devices/AC_branches.jl
+++ src/devices_models/devices/AC_branches.jl
@@ -22,7 +22,7 @@ get_variable_binary(::FlowActivePowerVariable, ::Type{<:PSY.ACBranch}, ::Abstrac
 get_variable_binary(::PhaseShifterAngle, ::Type{PSY.PhaseShiftingTransformer}, ::AbstractBranchFormulation,) = false
 
 get_parameter_multiplier(::FixValueParameter, ::PSY.ACBranch, ::StaticBranch) = 1.0
-get_variable_multiplier(::PhaseShifterAngle, ::PSY.PhaseShiftingTransformer, ::PhaseAngleControl) = 1.0/PSY.get_x(d)
+get_variable_multiplier(::PhaseShifterAngle, d::PSY.PhaseShiftingTransformer, ::PhaseAngleControl) = 1.0/PSY.get_x(d)
 
 get_initial_conditions_device_model(::OperationModel, ::DeviceModel{T, <:AbstractBranchFormulation}) where {T <: PSY.ACBranch} = DeviceModel(T, StaticBranch)
 get_initial_conditions_device_model(::OperationModel, ::DeviceModel{T, <:AbstractBranchFormulation},) where {T <: PSY.MonitoredLine} = DeviceModel(T, StaticBranchUnbounded)
