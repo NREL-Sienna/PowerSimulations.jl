61ee4b5841f2a7dd9974732715f8a705dd6a9a84
Jose Daniel Lara <jdlara@berkeley.edu>
2024-01-09 14:24:22 -0700

    fix incorrect var type

--- src/initial_conditions/initialization.jl
+++ src/initial_conditions/initialization.jl
@@ -12,7 +12,7 @@ function get_initial_conditions_template(model::OperationModel)
     network_model.radial_branches = get_radial_branches(get_network_model(model.template))
     network_model.subnetworks = get_subnetworks(get_network_model(model.template))
     # Initialization does not support PowerFlow evaluation
-    network_model.powerflow_evaluation = Nothing
+    network_model.powerflow_evaluation = nothing
     bus_area_map = get_bus_area_map(get_network_model(model.template))
     if !isempty(bus_area_map)
         network_model.bus_area_map = get_bus_area_map(get_network_model(model.template))
