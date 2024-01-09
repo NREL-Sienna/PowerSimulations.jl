5606f659d7cc877d5ec6ad4aba81d0b78afdd1ba
Jose Daniel Lara <jdlara@berkeley.edu>
2024-01-09 14:25:18 -0700

    WIP: function for the power flow evaluation

--- src/network_models/powerflow_evaluation.jl
+++ src/network_models/powerflow_evaluation.jl
@@ -1,27 +1,72 @@
-function add_power_flow_data!(container::OptimizationContainer, evaluator::Nothing)
-# NO OP function
+function add_power_flow_data!(::OptimizationContainer, ::Nothing, ::PSY.System)
+    # NO OP function
 end
 
-add_aux_variable_container!(
-    container,
-    var_type,
-    D,
-    [PSY.get_name(d) for d in devices],
-    time_steps,
+function _add_branches_aux_variables!(
+    container::OptimizationContainer,
+    vars::Vector{DataType},
+    branch_types::Vector{DataType},
+    branch_names::Vector{String},
 )
-
-function add_power_flow_data!(container::OptimizationContainer, evaluator::T) where T <: Union{PFS.PTDFDCPowerFlow, PFS.vPTDFDCPowerFlow}
-    @info "Building PowerFlow evaluator using $(evaluator)"
-    container.power_flow_data = PFS.PowerFlowData(evaluator, sys; time_steps = length(get_time_steps(container)))
+    time_steps = get_time_steps(container)
+    for var_type in vars
+        for D in Set(branch_types)
+            add_aux_variable_container!(
+                container,
+                var_type,
+                D,
+                branch_names[branch_types == D],
+                time_steps,
+            )
+        end
+    end
+    return
 end
 
-function add_power_flow_data!(container::OptimizationContainer, evaluator::PFS.DCPowerFlow())
-    @info "Building PowerFlow evaluator using $(evaluator)"
-    container.power_flow_data = PFS.PowerFlowData(evaluator, sys; time_steps = length(get_time_steps(container)))
+function _add_buses_aux_variables!(
+    container::OptimizationContainer,
+    vars::Vector{DataType},
+    bus_names::Vector{String},
+)
+    time_steps = get_time_steps(container)
+    for var_type in vars
+        add_aux_variable_container!(
+            container,
+            var_type,
+            D,
+            bus_names,
+            time_steps,
+        )
+    end
+    return
 end
 
-
-function add_power_flow_data!(container::OptimizationContainer, evaluator::PFS.ACPowerFlow())
+function add_power_flow_data!(
+    container::OptimizationContainer,
+    evaluator::T,
+    sys::PSY.System,
+) where {T <: Union{PFS.PTDFDCPowerFlow, PFS.vPTDFDCPowerFlow}}
     @info "Building PowerFlow evaluator using $(evaluator)"
-    container.power_flow_data = PFS.PowerFlowData(evaluator, sys; time_steps = length(get_time_steps(container)))
+    container.power_flow_data =
+        PFS.PowerFlowData(evaluator, sys; time_steps = length(get_time_steps(container)))
+end
+
+function add_power_flow_data!(
+    container::OptimizationContainer,
+    evaluator::PFS.DCPowerFlow,
+    sys::PSY.System,
+)
+    @info "Building PowerFlow evaluator using $(evaluator)"
+    container.power_flow_data =
+        PFS.PowerFlowData(evaluator, sys; time_steps = length(get_time_steps(container)))
+end
+
+function add_power_flow_data!(
+    container::OptimizationContainer,
+    evaluator::PFS.ACPowerFlow,
+    sys::PSY.System,
+)
+    @info "Building PowerFlow evaluator using $(evaluator)"
+    container.power_flow_data =
+        PFS.PowerFlowData(evaluator, sys; time_steps = length(get_time_steps(container)))
 end
