035ef787b24976d0e868847c0e0d207d8d8b391e
Jose Daniel Lara <jdlara@berkeley.edu>
2024-01-15 14:30:50 -0700

    use radial network reduction in slacks specs

--- src/network_models/network_slack_variables.jl
+++ src/network_models/network_slack_variables.jl
@@ -37,7 +37,13 @@ function add_variables!(
     U <: PM.AbstractActivePowerModel,
 }
     time_steps = get_time_steps(container)
-    bus_numbers = PSY.get_number.(get_available_components(PSY.ACBus, sys))
+    radial_network_reduction = get_radial_network_reduction(network_model)
+    if isempty(radial_network_reduction)
+        bus_numbers = PSY.get_number.(get_available_components(PSY.ACBus, sys))
+    else
+        bus_numbers = collect(keys(PNM.get_bus_reduction_map(radial_network_reduction)))
+    end
+
     variable = add_variable_container!(container, T(), PSY.ACBus, bus_numbers, time_steps)
     for t in time_steps, n in bus_numbers
         variable[n, t] = JuMP.@variable(
@@ -59,7 +65,12 @@ function add_variables!(
     U <: PM.AbstractPowerModel,
 }
     time_steps = get_time_steps(container)
-    bus_numbers = PSY.get_number.(get_available_components(PSY.ACBus, sys))
+    radial_network_reduction = get_radial_network_reduction(network_model)
+    if isempty(radial_network_reduction)
+        bus_numbers = PSY.get_number.(get_available_components(PSY.ACBus, sys))
+    else
+        bus_numbers = collect(keys(PNM.get_bus_reduction_map(radial_network_reduction)))
+    end
     variable_active =
         add_variable_container!(container, T(), PSY.ACBus, "P", bus_numbers, time_steps)
     variable_reactive =
