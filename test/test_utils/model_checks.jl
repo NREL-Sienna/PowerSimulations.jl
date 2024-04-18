68bf3501f641af31f4993b72a59fa19d233cfafa
rodrigomha <rodrigomha@gmail.com>
2024-04-18 13:04:03 -0700

    extra updates of ic to initial_conditions

--- test/test_utils/model_checks.jl
+++ test/test_utils/model_checks.jl
@@ -421,7 +421,8 @@ function check_initialization_constraint_count(
     filter_func = PSY.get_available,
     meta = PSI.IS.Optimization.CONTAINER_KEY_EMPTY_META,
 ) where {S <: PSI.ConstraintType, T <: PSY.Component}
-    container = IS.Optimization.get_ic_model_container(PSI.get_internal(model))
+    container =
+        IS.Optimization.get_initial_conditions_model_container(PSI.get_internal(model))
     no_component = length(PSY.get_components(filter_func, T, model.sys))
     time_steps = PSI.get_time_steps(container)[end]
     constraint = PSI.get_constraint(container, S(), T, meta)
