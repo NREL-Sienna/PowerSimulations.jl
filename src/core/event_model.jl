e703845bfc2ebfc5485051165e307df0fa4331bf
Jose Daniel Lara <jdlara@berkeley.edu>
2024-10-09 12:13:21 -0600

    add events model public API

---
+++ src/core/event_model.jl
@@ -0,0 +1,14 @@
+abstract type AbstractEventModel end
+
+
+mutable struct EventModel{D <: PSY.SupplementalAttribute, B <: AbstractEventModel}
+    attributes::Dict{String, Any}
+
+    function EventModel(
+        ::Type{D},
+        ::Type{B};
+        attributes = Dict{String, Any}(),
+    ) where {D <: PSY.SupplementalAttribute, B <: AbstractEventModel}
+        new{D, B}(attributes)
+    end
+end
