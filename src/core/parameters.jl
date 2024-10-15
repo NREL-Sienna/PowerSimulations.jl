067d29aa3068c0a2d863a7a504d04b9e02109b9f
rodrigomha <rodrigomha@gmail.com>
2024-10-15 16:35:14 -0700

    add new params and exprs

--- src/core/parameters.jl
+++ src/core/parameters.jl
@@ -276,11 +276,6 @@ Parameter to define Min Flow limit for interface time series
 """
 struct MinInterfaceFlowLimitParameter <: TimeSeriesParameter end
 
-"""
-Parameter to define fuel cost time series
-"""
-struct FuelCostTimeSeriesParameter <: TimeSeriesParameter end
-
 abstract type VariableValueParameter <: RightHandSideParameter end
 
 """
