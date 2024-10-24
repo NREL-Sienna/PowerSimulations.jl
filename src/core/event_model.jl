0d17d945ff02e0a2b22890cb40ecdfea82a9b15f
Jose Daniel Lara <jdlara@berkeley.edu>
2024-10-24 08:50:05 -0400

    add events to the models

--- src/core/event_model.jl
+++ src/core/event_model.jl
@@ -1,21 +1,22 @@
 abstract type AbstractEventModel end
 struct TotalOutage <: AbstractEventModel end
 
-mutable struct EventModel{D <: PSY.SupplementalAttribute, B <: AbstractEventModel}
+mutable struct EventModel{D <: PSY.Contingency, B <: AbstractEventModel}
     attributes::Dict{String, Any}
 
     function EventModel(
         ::Type{D},
         ::Type{B};
         attributes = Dict{String, Any}(),
-    ) where {D <: PSY.SupplementalAttribute, B <: AbstractEventModel}
+    ) where {D <: PSY.Contingency, B <: AbstractEventModel}
         new{D, B}(attributes)
     end
 end
 
 get_event_type(
     ::EventModel{D, B},
-) where {D <: PSY.SupplementalAttribute, B <: AbstractEventModel} = D
-get_formulation(
+) where {D <: PSY.Contingency, B <: AbstractEventModel} = D
+
+get_event_model(
     ::EventModel{D, B},
-) where {D <: PSY.SupplementalAttribute, B <: AbstractEventModel} = B
+) where {D <: PSY.Contingency, B <: AbstractEventModel} = B
