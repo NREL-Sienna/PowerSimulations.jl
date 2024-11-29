c0ddc51a2e994684f00841ed266ca38ecae0f6d9
Jose Daniel Lara <jdlara@berkeley.edu>
2024-11-29 10:47:50 -0700

    WIP events update parameters

--- src/simulation/simulation_events.jl
+++ src/simulation/simulation_events.jl
@@ -2,23 +2,18 @@ function apply_simulation_events!(simulation::Simulation)
     sequence = get_sequence(simulation)
     events = get_events(sequence)
     simulation_state = get_simulation_state(simulation)
-    # TODO: Undo once we have the caching properly implemented
     for event_model in events
-        @show "new event"
         if check_condition(simulation_state, event_model)
             # TODO: for other event categories we need to do something else
             em_model = get_emulation_model(get_models(simulation))
             sys = get_system(em_model)
             model_name = get_name(em_model)
-            @show event_model.attribute_device_map[model_name]
-            for (event_uuid, device_types) in event_model.attribute_device_map[model_name]
-                @show event_uuid
+            for (event_uuid, device_type_maps) in event_model.attribute_device_map[model_name]
                 event = PSY.get_supplemental_attribute(sys, event_uuid)
-                apply_affect!(simulation, event_model, event, device_types)
+                apply_affect!(simulation, event_model, event, device_type_maps)
             end
         end
     end
-    error("here run the events")
 end
 
 function check_condition(
@@ -39,30 +34,18 @@ function apply_affect!(
 )
     sim_state = get_simulation_state(simulation)
     sim_time = get_current_time(simulation)
-    rng = get_rng(simulation)
-    λ = PSY.get_outage_transition_probability(event)
-    mttr = PSY.get_mean_time_to_recovery(event)
-    outage_status = Float64(rand(rng, Bernoulli(λ)))
     for (dtype, device_names) in device_type_maps
         if dtype == PSY.RenewableDispatch
             continue
         end
-        current_status_data =
-            get_system_state_data(sim_state, AvailableStatusParameter(), dtype)
-        current_status_values = get_last_recorded_value(current_status_data)
         em_model = get_emulation_model(get_models(simulation))
         em_model_store = get_store_params(em_model)
-        for d in device_names
-            if current_status_values[d] < 1.0
-                continue
-            else
-                @show d
-                @show current_status_values[d] = outage_status
-                update_decision_state!(sim_state, ParameterKey(AvailableStatusParameter, dtype), mttr, sim_time, em_model_store)
-                # update_decision_state!(sim_state, VariableKey(ActivePowerVariable, dtype), mttr, sim_time, em_model_store)
-                error()
-            end
-        end
+        update_system_state!(sim_state, ParameterKey(AvailableStatusParameter, dtype), device_names, event, sim_time,)
+        update_system_state!(sim_state, VariableKey(ActivePowerVariable, dtype), device_names, event, sim_time,)
+        update_system_state!(sim_state, VariableKey(OnVariable, dtype), device_names, event, sim_time,)
+        update_decision_state!(sim_state, ParameterKey(AvailableStatusParameter, dtype), device_names, event, sim_time, em_model_store)
+        update_decision_state!(sim_state, VariableKey(ActivePowerVariable, dtype), device_names, event, sim_time, em_model_store)
+        update_decision_state!(sim_state, VariableKey(OnVariable, dtype), device_names, event, sim_time, em_model_store)
     end
     # Put a StateUpdateEvent here
 end
