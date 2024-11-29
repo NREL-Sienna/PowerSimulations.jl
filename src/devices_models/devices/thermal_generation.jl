f30e8c1c4ef06e9d5c71c7aa26a864888ed5dee8
Jose Daniel Lara <jdlara@berkeley.edu>
2024-11-29 15:40:48 -0700

    remove unused parameter call

--- src/devices_models/devices/thermal_generation.jl
+++ src/devices_models/devices/thermal_generation.jl
@@ -52,7 +52,6 @@ get_variable_binary(::Union{ColdStartVariable, WarmStartVariable, HotStartVariab
 ########################### Parameter related set functions ################################
 get_multiplier_value(::ActivePowerTimeSeriesParameter, d::PSY.ThermalGen, ::AbstractThermalFormulation) = PSY.get_max_active_power(d)
 get_multiplier_value(::FuelCostParameter, d::PSY.ThermalGen, ::AbstractThermalFormulation) = 1.0
-get_multiplier_value(::FuelCostTimeSeriesParameter, d::PSY.ThermalGen, ::AbstractThermalFormulation) = PSY.get_proportional_term(d.operation_cost.variable.value_curve)
 get_parameter_multiplier(::VariableValueParameter, d::PSY.ThermalGen, ::AbstractThermalFormulation) = 1.0
 get_initial_parameter_value(::VariableValueParameter, d::PSY.ThermalGen, ::AbstractThermalFormulation) = 1.0
 get_expression_multiplier(::OnStatusParameter, ::ActivePowerRangeExpressionUB, d::PSY.ThermalGen, ::AbstractThermalFormulation) = PSY.get_active_power_limits(d).max
