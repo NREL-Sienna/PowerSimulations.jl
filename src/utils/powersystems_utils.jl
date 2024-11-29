30e24778f562c52da57d6d81ec89f70438730e71
Jose Daniel Lara <jdlara@berkeley.edu>
2024-11-29 14:19:14 -0700

    change time variant check

--- src/utils/powersystems_utils.jl
+++ src/utils/powersystems_utils.jl
@@ -338,6 +338,10 @@ function _get_piecewise_incrementalcurve_per_system_unit(
     return PSY.PiecewiseStepData(x_coords_normalized, y_coords_normalized)
 end
 
+function is_time_variant(cost_function::PSY.FuelCurve{PSY.PiecewisePointCurve})
+    return isa(PSY.get_fuel_cost(cost_function), IS.TimeSeriesKey)
+end
+
 function get_deterministic_time_series_type(sys::PSY.System)
     time_series_types = IS.get_time_series_counts_by_type(sys.data)
     @show existing_types = Set(d["type"] for d in time_series_types)
