89a168fededd65dba3b1c187526bf8cb9f428342
Jose Daniel Lara <jdlara@berkeley.edu>
2024-12-01 21:38:03 -0700

    clean up of code

--- src/simulation/simulation_events.jl
+++ src/simulation/simulation_events.jl
@@ -8,7 +8,8 @@ function apply_simulation_events!(simulation::Simulation)
             em_model = get_emulation_model(get_models(simulation))
             sys = get_system(em_model)
             model_name = get_name(em_model)
-            for (event_uuid, device_type_maps) in event_model.attribute_device_map[model_name]
+            for (event_uuid, device_type_maps) in
+                event_model.attribute_device_map[model_name]
                 event = PSY.get_supplemental_attribute(sys, event_uuid)
                 apply_affect!(simulation, event_model, event, device_type_maps)
             end
@@ -25,7 +26,7 @@ end
 
 function apply_affect!(
     simulation::Simulation,
-    event_model::EventModel{
+    ::EventModel{
         PSY.GeometricDistributionForcedOutage,
         <:AbstractEventCondition,
     },
@@ -34,18 +35,61 @@ function apply_affect!(
 )
     sim_state = get_simulation_state(simulation)
     sim_time = get_current_time(simulation)
+    rng = get_rng(simulation)
     for (dtype, device_names) in device_type_maps
         if dtype == PSY.RenewableDispatch
             continue
         end
         em_model = get_emulation_model(get_models(simulation))
         em_model_store = get_store_params(em_model)
-        update_system_state!(sim_state, ParameterKey(AvailableStatusParameter, dtype), device_names, event, sim_time,)
-        update_system_state!(sim_state, VariableKey(ActivePowerVariable, dtype), device_names, event, sim_time,)
-        update_system_state!(sim_state, VariableKey(OnVariable, dtype), device_names, event, sim_time,)
-        update_decision_state!(sim_state, ParameterKey(AvailableStatusParameter, dtype), device_names, event, sim_time, em_model_store)
-        update_decision_state!(sim_state, VariableKey(ActivePowerVariable, dtype), device_names, event, sim_time, em_model_store)
-        update_decision_state!(sim_state, VariableKey(OnVariable, dtype), device_names, event, sim_time, em_model_store)
+        update_system_state!(
+            sim_state,
+            ParameterKey(AvailableStatusParameter, dtype),
+            device_names,
+            event,
+            sim_time,
+            rng,
+        )
+        update_system_state!(
+            sim_state,
+            VariableKey(ActivePowerVariable, dtype),
+            device_names,
+            event,
+            sim_time,
+            rng,
+        )
+        update_system_state!(
+            sim_state,
+            VariableKey(OnVariable, dtype),
+            device_names,
+            event,
+            sim_time,
+            rng,
+        )
+        update_decision_state!(
+            sim_state,
+            ParameterKey(AvailableStatusParameter, dtype),
+            device_names,
+            event,
+            sim_time,
+            em_model_store,
+        )
+        update_decision_state!(
+            sim_state,
+            VariableKey(ActivePowerVariable, dtype),
+            device_names,
+            event,
+            sim_time,
+            em_model_store,
+        )
+        update_decision_state!(
+            sim_state,
+            VariableKey(OnVariable, dtype),
+            device_names,
+            event,
+            sim_time,
+            em_model_store,
+        )
     end
     # Put a StateUpdateEvent here
 end
