5257eaa849b28221847559d4f3c504c7ab31bde0
Jose Daniel Lara <jdlara@berkeley.edu>
2025-03-03 16:56:06 -0700

    add variable exports

--- src/PowerSimulations.jl
+++ src/PowerSimulations.jl
@@ -203,6 +203,7 @@ export ColdStartVariable
 export EnergyVariable
 export LiftVariable
 export OnVariable
+export OnVariableSquared
 export ReactivePowerVariable
 export ReservationVariable
 export ActivePowerReserveVariable
@@ -235,6 +236,7 @@ export LowerBoundFeedForwardSlack
 export InterfaceFlowSlackUp
 export InterfaceFlowSlackDown
 export PieceWiseLinearCostVariable
+export PieceWiseLinearInterpolationVariable
 export RateofChangeConstraintSlackUp
 export RateofChangeConstraintSlackDown
 
@@ -573,6 +575,7 @@ include("devices_models/devices/common/add_constraint_dual.jl")
 include("devices_models/devices/common/rateofchange_constraints.jl")
 include("devices_models/devices/common/duration_constraints.jl")
 include("devices_models/devices/common/get_time_series.jl")
+include("devices_models/devices/common/pwl_function_approximations.jl")
 
 # Device Modeling components
 include("devices_models/devices/default_interface_methods.jl")
