b8782c32946f8ecfc0131fbb9ce9a50ce24e0775
Sebastián M <104728742+SebastianManriqueM@users.noreply.github.com>
2025-03-27 04:59:46 -0600

    Add SCUC tests

--- test/test_network_constructors.jl
+++ test/test_network_constructors.jl
@@ -185,6 +185,180 @@ end
     end
 end
 
+@testset "Security Constrained Network DC-PF with PTDF/LODF Model" begin
+    template = get_thermal_dispatch_template_network(SecurityConstrainedPTDFPowerModel)
+    c_sys5 = PSB.build_system(PSITestSystems, "c_sys5")
+    c_sys14 = PSB.build_system(PSITestSystems, "c_sys14")
+    c_sys14_dc = PSB.build_system(PSITestSystems, "c_sys14_dc")
+    systems = [c_sys5, c_sys14, c_sys14_dc]
+    objfuncs = [GAEVF, GQEVF, GQEVF]
+    constraint_keys = [
+        PSI.ConstraintKey(RateLimitConstraint, PSY.Line, "lb"),
+        PSI.ConstraintKey(RateLimitConstraint, PSY.Line, "ub"),
+        PSI.ConstraintKey(CopperPlateBalanceConstraint, PSY.System),
+        PSI.ConstraintKey(NetworkFlowConstraint, PSY.Line),
+        PSI.ConstraintKey(OutageActivePowerFlowsConstraint, PSY.Line, "lb"),
+        PSI.ConstraintKey(OutageActivePowerFlowsConstraint, PSY.Line, "ub")
+    ]
+    PTDF_ref = IdDict{System, PTDF}(
+        c_sys5 => PTDF(c_sys5),
+        c_sys14 => PTDF(c_sys14),
+        c_sys14_dc => PTDF(c_sys14_dc),
+    )
+    LODF_ref = IdDict{System, LODF}(
+        c_sys5 => LODF(c_sys5),
+        c_sys14 => LODF(c_sys14),
+        c_sys14_dc => LODF(c_sys14_dc),
+    )
+    lines_outages = IdDict{System, Vector{String}}(
+        c_sys5 => ["1", "2", "3"],
+        c_sys14 => ["Line1", "Line2", "Line9", "Line10", "Line12", "Trans2"],
+        c_sys14_dc => ["Line1", "Line9", "Line10", "Line12", "Trans2"],
+    )
+    test_results = IdDict{System, Vector{Int}}(
+        c_sys5 => [264, 0, 624, 624, 168],
+        c_sys14 => [600, 0, 3336, 3336, 504],
+        c_sys14_dc => [600, 0, 2688, 2592, 456],
+    )
+    
+    test_obj_values = IdDict{System, Float64}(
+        c_sys5 => 445689.358,
+        c_sys14 => 141964.156,
+        c_sys14_dc => 141964.156,
+    )
+    for (ix, sys) in enumerate(systems)
+        template = get_thermal_dispatch_template_network(
+            NetworkModel(
+                SecurityConstrainedPTDFPowerModel;
+                PTDF_matrix = PTDF_ref[sys],
+                LODF_matrix = LODF_ref[sys],
+            ),
+        )
+
+        ps_model = DecisionModel(template, sys; optimizer = HiGHS_optimizer)
+
+        transition_data = GeometricDistributionForcedOutage(;
+            mean_time_to_recovery = 10,
+            outage_transition_probability = 0.9999,
+        )
+        for line_name in lines_outages[sys]
+            component = get_component(ACBranch, sys, line_name)
+            add_supplemental_attribute!(sys, component, transition_data)
+        end
+
+        @test build!(ps_model; output_dir = mktempdir(; cleanup = true)) ==
+            PSI.ModelBuildStatus.BUILT
+        psi_constraint_test(ps_model, constraint_keys)
+       
+        moi_tests(
+            ps_model,
+            test_results[sys][1],
+            test_results[sys][2],
+            test_results[sys][3],
+            test_results[sys][4],
+            test_results[sys][5],
+            false,
+        )
+        psi_checkobjfun_test(ps_model, objfuncs[ix])
+        psi_checksolve_test(
+            ps_model,
+            [MOI.OPTIMAL, MOI.ALMOST_OPTIMAL],
+            test_obj_values[sys],
+            10000,
+        )
+    end
+    # SecurityConstrainedPTDFF input Error testing
+    ps_model = DecisionModel(template, c_sys5; optimizer = HiGHS_optimizer)
+    @test build!(
+        ps_model;
+        console_level = Logging.AboveMaxLevel,  # Ignore expected errors.
+        output_dir = mktempdir(; cleanup = true),
+    ) == PSI.ModelBuildStatus.FAILED
+end
+
+@testset "Security Constrained Network DC-PF with Virtual PTDF/LODF Model" begin
+    template = get_thermal_dispatch_template_network(SecurityConstrainedPTDFPowerModel)
+    c_sys5 = PSB.build_system(PSITestSystems, "c_sys5")
+    c_sys14 = PSB.build_system(PSITestSystems, "c_sys14")
+    c_sys14_dc = PSB.build_system(PSITestSystems, "c_sys14_dc")
+    systems = [c_sys5, c_sys14, c_sys14_dc]
+    objfuncs = [GAEVF, GQEVF, GQEVF]
+    constraint_keys = [
+        PSI.ConstraintKey(RateLimitConstraint, PSY.Line, "lb"),
+        PSI.ConstraintKey(RateLimitConstraint, PSY.Line, "ub"),
+        PSI.ConstraintKey(CopperPlateBalanceConstraint, PSY.System),
+        PSI.ConstraintKey(NetworkFlowConstraint, PSY.Line),
+        PSI.ConstraintKey(OutageActivePowerFlowsConstraint, PSY.Line, "lb"),
+        PSI.ConstraintKey(OutageActivePowerFlowsConstraint, PSY.Line, "ub")
+    ]
+    PTDF_ref = IdDict{System, VirtualPTDF}(
+        c_sys5 => VirtualPTDF(c_sys5),
+        c_sys14 => VirtualPTDF(c_sys14),
+        c_sys14_dc => VirtualPTDF(c_sys14_dc),
+    )
+    LODF_ref = IdDict{System, VirtualLODF}(
+        c_sys5 => VirtualLODF(c_sys5),
+        c_sys14 => VirtualLODF(c_sys14),
+        c_sys14_dc => VirtualLODF(c_sys14_dc),
+    )
+    lines_outages = IdDict{System, Vector{String}}(
+        c_sys5 => ["1", "2", "3"],
+        c_sys14 => ["Line1", "Line2", "Line9", "Line10", "Line12", "Trans2"],
+        c_sys14_dc => ["Line1", "Line9", "Line10", "Line12", "Trans2"],
+    )
+    test_results = IdDict{System, Vector{Int}}(
+        c_sys5 => [264, 0, 624, 624, 168],
+        c_sys14 => [600, 0, 3336, 3336, 504],
+        c_sys14_dc => [600, 0, 2688, 2592, 456],
+    )
+    
+    test_obj_values = IdDict{System, Float64}(
+        c_sys5 => 445689.358,
+        c_sys14 => 141964.156,
+        c_sys14_dc => 141964.156,
+    )
+    for (ix, sys) in enumerate(systems)
+        template = get_thermal_dispatch_template_network(
+            NetworkModel(
+                SecurityConstrainedPTDFPowerModel;
+                PTDF_matrix = PTDF_ref[sys],
+                LODF_matrix = LODF_ref[sys],
+            ),
+        )
+
+        ps_model = DecisionModel(template, sys; optimizer = HiGHS_optimizer)
+
+        transition_data = GeometricDistributionForcedOutage(;
+            mean_time_to_recovery = 10,
+            outage_transition_probability = 0.9999,  
+        )
+        for branch_name in lines_outages[sys]
+            component = get_component(ACBranch, sys, branch_name)
+            add_supplemental_attribute!(sys, component, transition_data)
+        end
+
+        @test build!(ps_model; output_dir = mktempdir(; cleanup = true)) ==
+            PSI.ModelBuildStatus.BUILT
+        psi_constraint_test(ps_model, constraint_keys)
+       
+        moi_tests(
+            ps_model,
+            test_results[sys][1],
+            test_results[sys][2],
+            test_results[sys][3],
+            test_results[sys][4],
+            test_results[sys][5],
+            false,
+        )
+        psi_checkobjfun_test(ps_model, objfuncs[ix])
+        psi_checksolve_test(
+            ps_model,
+            [MOI.OPTIMAL, MOI.ALMOST_OPTIMAL],
+            test_obj_values[sys],
+            10000,
+        )
+    end
+end
 @testset "Network DC lossless -PF network with PowerModels DCPlosslessForm" begin
     c_sys5 = PSB.build_system(PSITestSystems, "c_sys5")
     c_sys14 = PSB.build_system(PSITestSystems, "c_sys14")
