6b46a6ccc0c477b1d0115028c9fa36968973149d
rodrigomha <rodrigomha@gmail.com>
2025-04-28 11:42:43 -0700

    update comments

--- src/core/definitions.jl
+++ src/core/definitions.jl
@@ -40,6 +40,7 @@ const JumpSupportedLiterals =
 
 const TwoTerminalHVDCTypes =
     Union{PSY.TwoTerminalGenericHVDCLine, PSY.TwoTerminalVSCLine, PSY.TwoTerminalLCCLine}
+const OfferCurveCost = Union{PSY.MarketBidCost, PSY.ImportExportCost}
 # Settings constants
 const UNSET_HORIZON = Dates.Millisecond(0)
 const UNSET_RESOLUTION = Dates.Millisecond(0)
