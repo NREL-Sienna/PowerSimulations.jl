96e10b41605a32133c1c8b8e1eb1ae60708105ef
m-bossart <bossart.matt@gmail.com>
2025-05-29 12:25:36 -0700

    add methods for outages of fixed components

--- src/contingency_model/contingency_arguments.jl
+++ src/contingency_model/contingency_arguments.jl
@@ -30,11 +30,12 @@ end
 function add_event_arguments!(
     container::OptimizationContainer,
     devices::T,
-    device_model::DeviceModel{U, StaticPowerLoad},
+    device_model::DeviceModel{U, V},
     network_model::NetworkModel{<:PM.AbstractActivePowerModel},
 ) where {
     T <: Union{Vector{U}, IS.FlattenIteratorWrapper{U}},
-} where {U <: PSY.StaticInjection}
+    V <: Union{StaticPowerLoad, PowerLoadDispatch, PowerLoadInterruption},
+} where {U <: PSY.PowerLoad}
     for (key, event_model) in get_events(device_model)
         event_type = get_entry_type(key)
         devices_with_attrbts =
@@ -61,6 +62,65 @@ function add_event_arguments!(
             container,
             ActivePowerBalance,
             ActivePowerOffsetParameter,
+            devices_with_attrbts,
+            device_model,
+            network_model,
+        )
+    end
+
+    return
+end
+
+function add_event_arguments!(
+    container::OptimizationContainer,
+    devices::T,
+    device_model::DeviceModel{U, V},
+    network_model::NetworkModel{<:PM.AbstractPowerModel},
+) where {
+    T <: Union{Vector{U}, IS.FlattenIteratorWrapper{U}},
+    V <: Union{StaticPowerLoad, PowerLoadDispatch, PowerLoadInterruption},
+} where {U <: PSY.PowerLoad}
+    for (key, event_model) in get_events(device_model)
+        event_type = get_entry_type(key)
+        devices_with_attrbts =
+            [d for d in devices if PSY.has_supplemental_attributes(d, event_type)]
+        @assert !isempty(devices_with_attrbts)
+        parameter_type = get_parameter_type(event_type, event_model, U)
+        for p_type in [AvailableStatusChangeCountdownParameter, parameter_type]
+            add_parameters!(
+                container,
+                p_type,
+                devices_with_attrbts,
+                device_model,
+                event_model,
+            )
+        end
+        add_parameters!(
+            container,
+            ActivePowerOffsetParameter,
+            devices_with_attrbts,
+            device_model,
+            event_model,
+        )
+        add_to_expression!(
+            container,
+            ActivePowerBalance,
+            ActivePowerOffsetParameter,
+            devices_with_attrbts,
+            device_model,
+            network_model,
+        )
+        add_parameters!(
+            container,
+            ReactivePowerOffsetParameter,
+            devices_with_attrbts,
+            device_model,
+            event_model,
+        )
+        add_to_expression!(
+            container,
+            ReactivePowerBalance,
+            ReactivePowerOffsetParameter,
             devices_with_attrbts,
             device_model,
             network_model,
@@ -109,7 +169,63 @@ function add_event_arguments!(
             network_model,
         )
     end
+    return
+end
 
+function add_event_arguments!(
+    container::OptimizationContainer,
+    devices::T,
+    device_model::DeviceModel{U, FixedOutput},
+    network_model::NetworkModel{<:PM.AbstractPowerModel},
+) where {
+    T <: Union{Vector{U}, IS.FlattenIteratorWrapper{U}},
+} where {U <: PSY.StaticInjection}
+    for (key, event_model) in get_events(device_model)
+        event_type = get_entry_type(key)
+        devices_with_attrbts =
+            [d for d in devices if PSY.has_supplemental_attributes(d, event_type)]
+        @assert !isempty(devices_with_attrbts)
+        parameter_type = get_parameter_type(event_type, event_model, U)
+        for p_type in [AvailableStatusChangeCountdownParameter, parameter_type]
+            add_parameters!(
+                container,
+                p_type,
+                devices_with_attrbts,
+                device_model,
+                event_model,
+            )
+        end
+        add_parameters!(
+            container,
+            ActivePowerOffsetParameter,
+            devices_with_attrbts,
+            device_model,
+            event_model,
+        )
+        add_to_expression!(
+            container,
+            ActivePowerBalance,
+            ActivePowerOffsetParameter,
+            devices_with_attrbts,
+            device_model,
+            network_model,
+        )
+        add_parameters!(
+            container,
+            ReactivePowerOffsetParameter,
+            devices_with_attrbts,
+            device_model,
+            event_model,
+        )
+        add_to_expression!(
+            container,
+            ReactivePowerBalance,
+            ReactivePowerOffsetParameter,
+            devices_with_attrbts,
+            device_model,
+            network_model,
+        )
+    end
     return
 end
 
@@ -192,6 +308,103 @@ function add_to_expression!(
                 multiplier[name, t],
             )
         end
+    end
+    return
+end
+
+"""
+Default implementation to add parameters to SystemBalanceExpressions
+"""
+function add_to_expression!(
+    container::OptimizationContainer,
+    ::Type{T},
+    ::Type{U},
+    devices::Union{Vector{V}, IS.FlattenIteratorWrapper{V}},
+    device_model::DeviceModel{V, W},
+    network_model::NetworkModel{X},
+) where {
+    T <: SystemBalanceExpressions,
+    U <: EventParameter,
+    V <: PSY.StaticInjection,
+    W <: AbstractDeviceFormulation,
+    X <: AbstractPTDFModel,
+}
+    param_container = get_parameter(container, U(), V)
+    multiplier = get_multiplier_array(param_container)
+    sys_expr = get_expression(container, T(), _system_expression_type(X))
+    nodal_expr = get_expression(container, T(), PSY.ACBus)
+    network_reduction = get_network_reduction(network_model)
+    for d in devices
+        name = PSY.get_name(d)
+        device_bus = PSY.get_bus(d)
+        bus_no_ = PSY.get_number(device_bus)
+        bus_no = PNM.get_mapped_bus_number(network_reduction, bus_no_)
+        ref_index = _ref_index(network_model, device_bus)
+        for t in get_time_steps(container)
+            param = get_parameter_array(param_container)[name, t]
+            _add_to_jump_expression!(sys_expr[ref_index, t], param, multiplier[name, t])
+            _add_to_jump_expression!(nodal_expr[bus_no, t], param, multiplier[name, t])
+        end
+    end
+    return
+end
+
+"""
+Default implementation to add parameters to SystemBalanceExpressions
+"""
+function add_to_expression!(
+    container::OptimizationContainer,
+    ::Type{T},
+    ::Type{U},
+    devices::Union{Vector{V}, IS.FlattenIteratorWrapper{V}},
+    model::DeviceModel{V, W},
+    network_model::NetworkModel{X},
+) where {
+    T <: SystemBalanceExpressions,
+    U <: EventParameter,
+    V <: PSY.Device,
+    W <: AbstractDeviceFormulation,
+    X <: PM.AbstractPowerModel,
+}
+    param_container = get_parameter(container, U(), V)
+    multiplier = get_multiplier_array(param_container)
+    network_reduction = get_network_reduction(network_model)
+    for d in devices, t in get_time_steps(container)
+        bus_no = PNM.get_mapped_bus_number(network_reduction, PSY.get_bus(d))
+        name = PSY.get_name(d)
+        _add_to_jump_expression!(
+            get_expression(container, T(), PSY.ACBus)[bus_no, t],
+            get_parameter_array(param_container)[name, t],
+            multiplier[name, t],
+        )
+    end
+    return
+end
+
+function add_to_expression!(
+    container::OptimizationContainer,
+    ::Type{T},
+    ::Type{U},
+    devices::Union{Vector{V}, IS.FlattenIteratorWrapper{V}},
+    model::DeviceModel{V, W},
+    network_model::NetworkModel{AreaBalancePowerModel},
+) where {
+    T <: SystemBalanceExpressions,
+    U <: EventParameter,
+    V <: PSY.Device,
+    W <: AbstractDeviceFormulation,
+}
+    param_container = get_parameter(container, U(), V)
+    multiplier = get_multiplier_array(param_container)
+    for d in devices, t in get_time_steps(container)
+        bus = PSY.get_bus(d)
+        area_name = PSY.get_name(PSY.get_area(bus))
+        name = PSY.get_name(d)
+        _add_to_jump_expression!(
+            get_expression(container, T(), PSY.Area)[area_name, t],
+            get_parameter_array(param_container)[name, t],
+            multiplier[name, t],
+        )
     end
     return
 end
