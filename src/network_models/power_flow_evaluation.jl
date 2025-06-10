74586fdf3a53927fa12d963cdf298980bd0c455b
mcllerena <marckcode@gmail.com>
2025-06-10 14:18:29 -0600

    add new fix

--- src/network_models/power_flow_evaluation.jl
+++ src/network_models/power_flow_evaluation.jl
@@ -321,15 +321,15 @@ function update_pf_data!(
     return
 end
 
+_update_component!(comp::PSY.Component, ::Val{:active_power}, value, sys_base) =
+    (comp.constant_active_power = value * sys_base / PSY.get_base_power(comp))
+_update_component!(comp::PSY.StandardLoad, ::Val{:active_power}, value, sys_base) =
+    (comp.constant_active_power = -value * sys_base / PSY.get_base_power(comp))
+_update_component!(comp::PSY.Component, ::Val{:reactive_power}, value, sys_base) =
+    (comp.constant_reactive_power = value * sys_base / PSY.get_base_power(comp))
+_update_component!(comp::PSY.StandardLoad, ::Val{:reactive_power}, value, sys_base) =
+    (comp.constant_reactive_power = -value * sys_base / PSY.get_base_power(comp))
 
-_update_component!(comp::PSY.StandardLoad, ::Val{:constant_active_power}, value) =
-    (comp.constant_active_power = -value * sys_base / PSY.get_base_power(comp))
-_update_component!(comp::PSY.Component, ::Val{:constant_reactive_power}, value) =
-    (comp.constant_reactive_power = value * sys_base / PSY.get_base_power(comp))
-_update_component!(comp::PSY.StandardLoad, ::Val{:constant_active_power}, value) =
-    (comp.constant_active_power = -value * sys_base / PSY.get_base_power(comp))
-_update_component!(comp::PSY.Component, ::Val{:constant_reactive_power}, value) =
-    (comp.constant_reactive_power = value * sys_base / PSY.get_base_power(comp))
 
 # PERF we use direct dot access here, and implement our own unit conversions, for performance and convenience
 _update_component!(comp::PSY.Component, ::Val{:active_power}, value, sys_base) =
