401f3a39928f5de5b520885a0685727f755e7dfb
Sebastián M <104728742+SebastianManriqueM@users.noreply.github.com>
2025-07-24 23:17:51 -0600

    Add preliminar tests for Static Injection Security Constrained Models

---
+++ test/test_static_injection_security_constrained_models.jl
@@ -0,0 +1,92 @@
+#Preliminar tests for Static Injection Security Constrained Models
+@testset "Network DC-PF with Reserves Deliverability Constraints" begin
+    template = get_thermal_dispatch_template_network(PTDFPowerModel)
+    c_sys5 = PSB.build_system(PSITestSystems, "c_sys5_uc"; add_reserves = true)
+    systems = [c_sys5]
+    objfuncs = [GAEVF, GQEVF, GQEVF]
+    constraint_keys = [
+        PSI.ConstraintKey(RateLimitConstraint, PSY.Line, "lb"),
+        PSI.ConstraintKey(RateLimitConstraint, PSY.Line, "ub"),
+        PSI.ConstraintKey(PostContingencyRateLimitConstraintB, PSY.ThermalStandard, "lb"),
+        PSI.ConstraintKey(PostContingencyRateLimitConstraintB, PSY.ThermalStandard, "ub"),
+        PSI.ConstraintKey(CopperPlateBalanceConstraint, PSY.System),
+        PSI.ConstraintKey(NetworkFlowConstraint, PSY.Line),
+        PSI.ConstraintKey(RequirementConstraint, PSY.VariableReserve{ReserveUp}, "Reserve1"),
+        PSI.ConstraintKey(RequirementConstraint, PSY.VariableReserve{ReserveDown}, "Reserve2"),
+        PSI.ConstraintKey(PostContingengyGenerationBalanceConstraint, PSY.ThermalStandard),
+        PSI.ConstraintKey(PostContingencyActivePowerReserveDeploymentVariableLimitsConstraint, PSY.ThermalStandard, "up"),
+        PSI.ConstraintKey(PostContingencyActivePowerReserveDeploymentVariableLimitsConstraint, PSY.ThermalStandard, "dn"),
+        PSI.ConstraintKey(PostContingencyRampConstraint, PSY.ThermalStandard, "up"),
+    ]
+    PTDF_ref = IdDict{System, PTDF}(
+        c_sys5 => PTDF(c_sys5),
+    )
+    test_results = IdDict{System, Vector{Int}}(
+        c_sys5 => [552, 0, 720, 600, 216],
+    )
+    test_obj_values = IdDict{System, Float64}(
+        c_sys5 => 340000.0,
+    )
+    for (ix, sys) in enumerate(systems)
+        
+        components_outages_names = ["Alta"]
+        for component_name in components_outages_names
+            # --- Create Outage Data ---
+            transition_data = GeometricDistributionForcedOutage(;
+                mean_time_to_recovery = 10,  # Units of hours
+                outage_transition_probability = 0.9999,  # Probability for outage per hour
+            )
+            # --- Create Outage Data to a Line ---
+            component = get_component(ThermalStandard, sys, component_name) #Brighton (Infeasible), Solitude (infinite Iteration),  Park City, Alta, Sundance
+            add_supplemental_attribute!(sys, component, transition_data)
+        end
+
+        template = get_thermal_dispatch_template_network(
+            NetworkModel(PTDFPowerModel; PTDF_matrix = PTDF_ref[sys]),
+        )
+        set_service_model!(template,
+            ServiceModel(
+                VariableReserve{ReserveUp},
+                RangeReserveWithDeliverabilityConstraints,
+                "Reserve1",
+            ))
+
+        set_service_model!(template,
+            ServiceModel(
+                VariableReserve{ReserveDown},
+                RangeReserveWithDeliverabilityConstraints,
+                "Reserve2",
+            ))
+
+
+        ps_model = DecisionModel(template, sys; optimizer = HiGHS_optimizer)
+
+        @test build!(ps_model; output_dir = mktempdir(; cleanup = true)) ==
+              PSI.ModelBuildStatus.BUILT
+        psi_constraint_test(ps_model, constraint_keys)
+        moi_tests(
+            ps_model,
+            test_results[sys][1],
+            test_results[sys][2],
+            test_results[sys][3],
+            test_results[sys][4],
+            test_results[sys][5],
+            false,
+        )
+        psi_checkobjfun_test(ps_model, objfuncs[ix])
+        psi_checksolve_test(
+            ps_model,
+            [MOI.OPTIMAL, MOI.ALMOST_OPTIMAL],
+            test_obj_values[sys],
+            10000,
+        )
+    end
+    # PTDF input Error testing
+    ps_model = DecisionModel(template, c_sys5; optimizer = HiGHS_optimizer)
+    @test build!(
+        ps_model;
+        console_level = Logging.AboveMaxLevel,  # Ignore expected errors.
+        output_dir = mktempdir(; cleanup = true),
+    ) == PSI.ModelBuildStatus.FAILED
+end
+
