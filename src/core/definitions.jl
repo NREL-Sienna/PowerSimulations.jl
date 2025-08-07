5502ff4fffc36fe20a13bddfda54a5f24159d69e
Sebastián M <104728742+SebastianManriqueM@users.noreply.github.com>
2025-08-06 21:47:57 -0600

    Minor fix - Add constant for scuc valid outages structs

--- src/core/definitions.jl
+++ src/core/definitions.jl
@@ -40,6 +40,8 @@ const JumpSupportedLiterals =
 
 const TwoTerminalHVDCTypes =
     Union{PSY.TwoTerminalGenericHVDCLine, PSY.TwoTerminalVSCLine, PSY.TwoTerminalLCCLine}
+const OutagesSCUC =
+    Union{PSY.GeometricDistributionForcedOutage, PSY.PlannedOutage}
 const OfferCurveCost = Union{PSY.MarketBidCost, PSY.ImportExportCost}
 # Settings constants
 const UNSET_HORIZON = Dates.Millisecond(0)
