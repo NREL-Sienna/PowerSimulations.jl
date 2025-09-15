947221e7ec68408a46b9b89251c386374179cf2f
Sebastián M <104728742+SebastianManriqueM@users.noreply.github.com>
2025-09-15 17:03:02 -0600

    Fix error related to 3D arrays feature missing for PostContingencyActivePowerReserveDeploymentVariable

--- src/core/variables.jl
+++ src/core/variables.jl
@@ -456,6 +456,7 @@ should_write_resulting_value(::Type{PiecewiseLinearBlockIncrementalOffer}) = fal
 should_write_resulting_value(::Type{PiecewiseLinearBlockDecrementalOffer}) = false
 should_write_resulting_value(::Type{HVDCPiecewiseLossVariable}) = false
 should_write_resulting_value(::Type{HVDCPiecewiseBinaryLossVariable}) = false
+should_write_resulting_value(::Type{PostContingencyActivePowerReserveDeploymentVariable}) = false #TODO: Make true when 3Dimensional is ready
 convert_result_to_natural_units(::Type{ActivePowerVariable}) = true
 convert_result_to_natural_units(::Type{PostContingencyActivePowerChangeVariable}) = true
 convert_result_to_natural_units(::Type{PowerAboveMinimumVariable}) = true
