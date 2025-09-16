dda3223e8ce151166aef10dca904cb325b74a26a
Sebastián M <104728742+SebastianManriqueM@users.noreply.github.com>
2025-09-15 21:21:34 -0600

    formatter

--- src/core/variables.jl
+++ src/core/variables.jl
@@ -456,7 +456,8 @@ should_write_resulting_value(::Type{PiecewiseLinearBlockIncrementalOffer}) = fal
 should_write_resulting_value(::Type{PiecewiseLinearBlockDecrementalOffer}) = false
 should_write_resulting_value(::Type{HVDCPiecewiseLossVariable}) = false
 should_write_resulting_value(::Type{HVDCPiecewiseBinaryLossVariable}) = false
-should_write_resulting_value(::Type{PostContingencyActivePowerReserveDeploymentVariable}) = false #TODO: Make true when 3Dimensional is ready
+should_write_resulting_value(::Type{PostContingencyActivePowerReserveDeploymentVariable}) =
+    false #TODO: Make true when 3Dimensional is ready
 convert_result_to_natural_units(::Type{ActivePowerVariable}) = true
 convert_result_to_natural_units(::Type{PostContingencyActivePowerChangeVariable}) = true
 convert_result_to_natural_units(::Type{PowerAboveMinimumVariable}) = true
