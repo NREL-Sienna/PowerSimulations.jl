e72d304924291852f806aabe62063ae5b359ada8
Sebastián M <104728742+SebastianManriqueM@users.noreply.github.com>
2025-09-26 17:58:05 -0600

    Add test for the case where system have devices that user did not set the device model

--- test/test_static_injection_security_constrained_models.jl
+++ test/test_static_injection_security_constrained_models.jl
@@ -133,6 +133,131 @@
     end
 end
 
+
+@testset "Test if G-n with reserves deliverability constraints builds when there is a device without set_device_model!()" begin
+
+    c_sys5 = PSB.build_system(PSITestSystems, "c_sys5_uc"; add_reserves = true)
+    
+    l4 = get_component(Line, c_sys5, "4")
+    l4_copy = MonitoredLine(;
+        name = PSY.get_name(l4) * "_copy",
+        available = PSY.get_available(l4),
+        active_power_flow = PSY.get_active_power_flow(l4),
+        reactive_power_flow = PSY.get_reactive_power_flow(l4),
+        arc = PSY.get_arc(l4),
+        r = PSY.get_r(l4),
+        x = PSY.get_x(l4),
+        b = PSY.get_b(l4),
+        flow_limits = (from_to = PSY.get_rating(l4), to_from = PSY.get_rating(l4)),
+        rating = PSY.get_rating(l4),
+        angle_limits = PSY.get_angle_limits(l4),
+        rating_b = PSY.get_rating_b(l4),
+        rating_c = PSY.get_rating_c(l4),
+        g = PSY.get_g(l4),
+        services = PSY.get_services(l4),
+        ext = PSY.get_ext(l4))
+    add_component!(c_sys5, l4_copy)
+    remove_component!(c_sys5, l4)
+
+    systems = [c_sys5]
+    objfuncs = [GAEVF, GQEVF, GQEVF]
+    constraint_keys = [
+        PSI.ConstraintKey(
+            ActivePowerVariableLimitsConstraint,
+            PSY.ThermalStandard,
+            "lb",
+        ),
+        PSI.ConstraintKey(
+            ActivePowerVariableLimitsConstraint,
+            PSY.ThermalStandard,
+            "ub",
+        ),
+        PSI.ConstraintKey(RateLimitConstraint, PSY.Line, "lb"),
+        PSI.ConstraintKey(RateLimitConstraint, PSY.Line, "ub"),
+        PSI.ConstraintKey(
+            PostContingencyEmergencyRateLimitConstraint,
+            PSY.VariableReserve{ReserveUp},
+            "Reserve1 -lb",
+        ),
+        PSI.ConstraintKey(
+            PostContingencyEmergencyRateLimitConstraint,
+            PSY.VariableReserve{ReserveUp},
+            "Reserve1 -ub",
+        ), PSI.ConstraintKey(CopperPlateBalanceConstraint, PSY.System),
+        PSI.ConstraintKey(NetworkFlowConstraint, PSY.Line),
+        PSI.ConstraintKey(
+            RequirementConstraint,
+            PSY.VariableReserve{ReserveUp},
+            "Reserve1",
+        ),
+        PSI.ConstraintKey(
+            PostContingencyGenerationBalanceConstraint,
+            PSY.VariableReserve{ReserveUp},
+            "Reserve1",
+        ),
+        PSI.ConstraintKey(
+            PostContingencyActivePowerReserveDeploymentVariableLimitsConstraint,
+            PSY.VariableReserve{ReserveUp},
+            "Reserve1",
+        ),
+        PSI.ConstraintKey(
+            PostContingencyRampConstraint,
+            PSY.VariableReserve{ReserveUp},
+            "Reserve1",
+        ),
+    ]
+    PTDF_ref = IdDict{System, PTDF}(
+        c_sys5 => PTDF(c_sys5),
+    )
+    test_results = IdDict{System, Vector{Int}}(
+        c_sys5 => [504, 0, 552, 432, 192],
+    )
+    test_obj_values = IdDict{System, Float64}(
+        c_sys5 => 329000.0,
+    )
+    components_outages_cases = IdDict{System, Vector{String}}(
+        c_sys5 => ["Alta"],
+    )
+    for (ix, sys) in enumerate(systems)
+        components_outages_names = components_outages_cases[sys]
+        for component_name in components_outages_names
+            # --- Create Outage Data ---
+            transition_data = GeometricDistributionForcedOutage(;
+                mean_time_to_recovery = 10,
+                outage_transition_probability = 0.9999,
+            )
+            # --- Add Outage Supplemental attribute to device and services that should respond ---
+            component = get_component(ThermalStandard, sys, component_name)
+            add_supplemental_attribute!(sys, component, transition_data)
+            reserve_up = get_component(VariableReserve{ReserveUp}, c_sys5, "Reserve1")
+            add_supplemental_attribute!(sys, reserve_up, transition_data)
+        end
+        
+
+        template = ProblemTemplate(NetworkModel(PTDFPowerModel; PTDF_matrix = PTDF_ref[sys]))
+        set_device_model!(template, ThermalStandard, ThermalBasicDispatch)
+        set_device_model!(template, PowerLoad, StaticPowerLoad)
+        #set_device_model!(template, MonitoredLine, StaticBranchBounds)
+        set_device_model!(template, Line, StaticBranch)
+        set_device_model!(template, Transformer2W, StaticBranch)
+        set_device_model!(template, TapTransformer, StaticBranch)
+        set_device_model!(template, TwoTerminalGenericHVDCLine, HVDCTwoTerminalLossless)
+
+        set_service_model!(template,
+            ServiceModel(
+                VariableReserve{ReserveUp},
+                RangeReserveWithDeliverabilityConstraints,
+                "Reserve1",
+            ))
+
+        ps_model = DecisionModel(template, sys; optimizer = HiGHS_optimizer)
+
+        @test build!(ps_model; output_dir = mktempdir(; cleanup = true)) ==
+                PSI.ModelBuildStatus.BUILT
+    end
+
+end
+
 @testset "G-n with reserves deliverability constraints UC allowing 2 reserve products to respond" begin
     c_sys5 = PSB.build_system(PSITestSystems, "c_sys5_uc"; add_reserves = true)
     systems = [c_sys5]
