52dcbd1a311289677cb35d259f6c7622587ebde1
GabrielKS <23368820+GabrielKS@users.noreply.github.com>
2025-11-02 00:05:47 -0600

    Rename `add_import_export_cost.jl` -> `iec_simulation_utils.jl`

--- test/includes.jl
+++ test/includes.jl
@@ -52,7 +52,7 @@ include("test_utils/run_simulation.jl")
 include("test_utils/add_market_bid_cost.jl")
 include("test_utils/mbc_system_utils.jl")
 include("test_utils/mbc_simulation_utils.jl")
-include("test_utils/add_import_export_cost.jl")
+include("test_utils/iec_simulation_utils.jl")
 
 ENV["RUNNING_PSI_TESTS"] = "true"
 ENV["SIENNA_RANDOM_SEED"] = 1234  # Set a fixed seed for reproducibility in tests
