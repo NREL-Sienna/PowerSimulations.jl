e97833f6b685ea09b46a0e12090db588636ce1e3
Sebastián M <104728742+SebastianManriqueM@users.noreply.github.com>
2025-12-23 18:12:07 -0700

    activate and improve DLR tests

--- test/test_network_constructors.jl
+++ test/test_network_constructors.jl
@@ -173,7 +173,6 @@ end
     end
 end
 
-#= Disabled until we merge the SCUC branch
 @testset "Network DC-PF with VirtualPTDF Model and implementing Dynamic Branch Ratings" begin
     line_device_model = DeviceModel(
         Line,
@@ -196,7 +195,6 @@ end
         PSI.ConstraintKey(FlowRateConstraint, PSY.Line, "lb"),
         PSI.ConstraintKey(FlowRateConstraint, PSY.Line, "ub"),
         PSI.ConstraintKey(CopperPlateBalanceConstraint, PSY.System),
-        PSI.ConstraintKey(NetworkFlowConstraint, PSY.Line),
     ]
     PTDF_ref = IdDict{System, VirtualPTDF}(
         c_sys5 => VirtualPTDF(c_sys5),
@@ -210,9 +208,9 @@ end
     )
     dlr_factors = vcat([fill(x, 6) for x in [1.15, 1.05, 1.1, 1]]...)
     test_results = IdDict{System, Vector{Int}}(
-        c_sys5 => [264, 0, 264, 264, 168],
-        c_sys14 => [600, 0, 600, 600, 504],
-        c_sys14_dc => [600, 0, 648, 552, 456],
+        c_sys5 => [120, 0, 264, 264, 24],
+        c_sys14 => [120, 0, 600, 600, 24],
+        c_sys14_dc => [168, 0, 648, 552, 24],
     )
     test_obj_values = IdDict{System, Float64}(
         c_sys5 => 241293.703,
@@ -247,7 +245,7 @@ end
                 dlr_data[ini_time] =
                     TimeArray(
                         data_ts + Day(t - 1),
-                        get_rating(branch) * get_base_power(sys) * dlr_factors,
+                        dlr_factors,
                     )
             end
 
@@ -280,7 +278,100 @@ end
         )
     end
 end
-=#
+
+@testset "Network DC-PF with PTDF Model and implementing Dynamic Branch Ratings with reductions" begin
+    objfuncs = [GAEVF, GQEVF, GQEVF]
+    constraint_keys = Dict(
+        StaticBranch => [
+        PSI.ConstraintKey(FlowRateConstraint, PSY.Line, "lb"),
+        PSI.ConstraintKey(FlowRateConstraint, PSY.Line, "ub"),
+        PSI.ConstraintKey(CopperPlateBalanceConstraint, PSY.System),
+    ]
+    ,
+    StaticBranchBounds => [
+        PSI.ConstraintKey(NetworkFlowConstraint, PSY.Line, ""),
+        PSI.ConstraintKey(CopperPlateBalanceConstraint, PSY.System),
+    ],
+    )
+    branches_dlr =  ["1", "2", "6"]
+    dlr_factors = vcat([fill(x, 6) for x in [1.15, 1.05, 1.1, 1]]...)
+    test_results = Dict(
+        StaticBranch => [120, 0, 264, 264, 24],
+        StaticBranchBounds => [264, 0, 120, 120, 168])
+    test_obj_values = [381341.0, 340475.0, 241293.703]
+    parallel_lines_names_to_add = ["1", "2", "3"]#Add parallel lines in lines with and without DLRs
+    branch_formulations = [StaticBranch, StaticBranchBounds]
+    n_steps = 2
+    for branch_formulation in branch_formulations
+        line_device_model = DeviceModel(
+            Line,
+            branch_formulation;
+            time_series_names = Dict(
+                DynamicBranchRatingTimeSeriesParameter => "dynamic_line_ratings",
+            ))
+        for (ix, add_parallel_line_name) in enumerate(parallel_lines_names_to_add)
+            sys = PSB.build_system(PSITestSystems, "c_sys5")
+            line_to_add_parallel = get_component(Line, sys, add_parallel_line_name)
+            add_equivalent_ac_transmission_with_parallel_circuits!(sys, line_to_add_parallel, PSY.Line)
+            template = get_thermal_dispatch_template_network(
+                NetworkModel(
+                    PTDFPowerModel;
+                    PTDF_matrix = PTDF(sys),
+                ),
+            )
+
+            set_device_model!(template, line_device_model)
+            ps_model = DecisionModel(template, sys; optimizer = HiGHS_optimizer)
+
+            for branch_name in branches_dlr
+                branch = get_component(PSY.ACTransmission, sys, branch_name)
+                dlr_data = SortedDict{Dates.DateTime, TimeSeries.TimeArray}()
+                data_ts = collect(
+                    DateTime("1/1/2024  0:00:00", "d/m/y  H:M:S"):Hour(1):DateTime(
+                        "1/1/2024  23:00:00",
+                        "d/m/y  H:M:S",
+                    ),
+                )
+
+                for t in 1:n_steps
+                    ini_time = data_ts[1] + Day(t - 1)
+                    dlr_data[ini_time] =
+                        TimeArray(
+                            data_ts + Day(t - 1),
+                            PSY.get_rating(branch) * get_base_power(sys) * dlr_factors,
+                        )
+                end
+
+                PSY.add_time_series!(
+                    sys,
+                    branch,
+                    PSY.Deterministic(
+                        "dynamic_line_ratings",
+                        dlr_data;
+                        scaling_factor_multiplier = get_rating,
+                    ),
+                )
+            end
+
+            @test build!(ps_model; output_dir = mktempdir(; cleanup = true)) ==
+                PSI.ModelBuildStatus.BUILT
+            psi_constraint_test(ps_model, constraint_keys[branch_formulation])
+
+            moi_tests(
+                ps_model,
+                test_results[branch_formulation]...,
+                false,
+            )
+            psi_checkobjfun_test(ps_model, objfuncs[1])
+            psi_checksolve_test(
+                ps_model,
+                [MOI.OPTIMAL, MOI.ALMOST_OPTIMAL],
+                test_obj_values[ix],
+                10000,
+            )
+        end
+    end
+end
 
 @testset "Network DC lossless -PF network with PowerModels DCPlosslessForm" begin
     c_sys5 = PSB.build_system(PSITestSystems, "c_sys5")
