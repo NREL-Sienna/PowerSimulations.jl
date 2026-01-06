af0e775be581e785efb6cc7b067f9afc800397f8
rodrigomha <rodrigomha@gmail.com>
2026-01-06 12:20:27 -0800

    update comment

--- src/devices_models/devices/reactivepower_device.jl
+++ src/devices_models/devices/reactivepower_device.jl
@@ -3,7 +3,7 @@
 requires_initialization(::AbstractReactivePowerDeviceFormulation) = false
 get_variable_multiplier(_, ::Type{<:PSY.SynchronousCondenser}, ::AbstractReactivePowerDeviceFormulation) = 1.0
 
-############## ReactivePowerVariable, SynCons ####################
+############## ReactivePowerVariable, SynchronousCondensers ####################
 get_variable_binary(::ReactivePowerVariable, ::Type{PSY.SynchronousCondenser}, ::AbstractReactivePowerDeviceFormulation) = false
 get_variable_warm_start_value(::ReactivePowerVariable, d::PSY.SynchronousCondenser, ::AbstractReactivePowerDeviceFormulation) = PSY.get_reactive_power(d)
 get_variable_lower_bound(::ReactivePowerVariable, d::PSY.SynchronousCondenser, ::AbstractReactivePowerDeviceFormulation) = isnothing(PSY.get_reactive_power_limits(d)) ? nothing : PSY.get_reactive_power_limits(d).min
