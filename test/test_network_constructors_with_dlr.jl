4a4d782bbcf3333efd92d730da74aee747a6c483
SebastianManriqueM <104728742+SebastianManriqueM@users.noreply.github.com>
2026-03-05 18:07:36 -0700

    addind test DLRs with parallel branches of different types

--- test/test_network_constructors_with_dlr.jl
+++ test/test_network_constructors_with_dlr.jl
@@ -97,6 +97,81 @@
     end
 end
 
+@testset "Network DC-PF with PTDF Model and implementing Dynamic Branch Ratings with BranchesParallel of different types" begin
+    objfuncs = [GAEVF, GQEVF, GQEVF]
+    constraint_keys = [
+        PSI.ConstraintKey(FlowRateConstraint, PSY.Line, "lb"),
+        PSI.ConstraintKey(FlowRateConstraint, PSY.Line, "ub"),
+        PSI.ConstraintKey(CopperPlateBalanceConstraint, PSY.System),
+    ]
+    branches_dlr = ["1", "2", "6"]
+    dlr_factors = vcat([fill(x, 6) for x in [0.99, 0.98, 1.0, 0.95]]...)
+
+    test_obj_values = [356577.0, 279735.0, 241293.703]
+    parallel_lines_names_to_add = ["1", "2", "3"]#Add parallel lines in lines with and without DLRs
+    n_steps = 2
+
+    for slack_flag in [false, true]
+        if slack_flag
+            test_results = [408, 0, 264, 264, 24]
+        else
+            test_results = [120, 0, 264, 264, 24]
+        end
+        line_device_model = DeviceModel(
+            Line,
+            StaticBranch;
+            time_series_names = Dict(
+                DynamicBranchRatingTimeSeriesParameter => "dynamic_line_ratings",
+            ),
+            use_slacks = slack_flag,
+        )
+        for (ix, add_parallel_line_name) in enumerate(parallel_lines_names_to_add)
+            sys = PSB.build_system(PSITestSystems, "c_sys5")
+            line_to_add_parallel = get_component(Line, sys, add_parallel_line_name)
+            add_equivalent_ac_transmission_with_parallel_circuits!(
+                sys,
+                line_to_add_parallel,
+                PSY.Line,
+                PSY.MonitoredLine
+            )
+
+            add_dlr_to_system_branches!(
+                sys,
+                branches_dlr,
+                n_steps,
+                dlr_factors;
+                initial_date = "2024-01-01",
+            )
+
+            template = get_thermal_dispatch_template_network(
+                NetworkModel(
+                    PTDFPowerModel;
+                    PTDF_matrix = PTDF(sys),
+                ),
+            )
+            set_device_model!(template, line_device_model)
+            ps_model = DecisionModel(template, sys; optimizer = HiGHS_optimizer)
+
+            @test build!(ps_model; output_dir = mktempdir(; cleanup = true)) ==
+                  PSI.ModelBuildStatus.BUILT
+            psi_constraint_test(ps_model, constraint_keys)
+
+            moi_tests(
+                ps_model,
+                test_results...,
+                false,
+            )
+            psi_checkobjfun_test(ps_model, objfuncs[1])
+            psi_checksolve_test(
+                ps_model,
+                [MOI.OPTIMAL, MOI.ALMOST_OPTIMAL],
+                test_obj_values[ix],
+                10000,
+            )
+        end
+    end
+end
+
 @testset "Network DC-PF with PTDF Model and implementing Dynamic Branch Ratings with BranchesParallel" begin
     objfuncs = [GAEVF, GQEVF, GQEVF]
     constraint_keys = [
