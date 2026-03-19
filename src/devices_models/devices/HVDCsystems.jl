1b45e91f9f4e4e40f0493193bc8f1d3b3405fd76
rodrigomha <rodrigomha@gmail.com>
2026-03-19 12:56:51 -0700

    add fix to ConverterBalance

--- src/devices_models/devices/HVDCsystems.jl
+++ src/devices_models/devices/HVDCsystems.jl
@@ -340,20 +340,30 @@ function add_to_expression!(
     variable = get_variable(container, U(), V)
     expression_dc = get_expression(container, T(), PSY.DCBus)
     expression_ac = get_expression(container, T(), PSY.ACBus)
-    for d in devices, t in get_time_steps(container)
+    sys_expr = get_expression(container, T(), PSY.System)
+    for d in devices
         name = PSY.get_name(d)
+        device_bus = PSY.get_bus(d)
+        bus_number_ac = PSY.get_number(device_bus)
+        ref_bus = get_reference_bus(network_model, device_bus)
         bus_number_dc = PSY.get_number(PSY.get_dc_bus(d))
-        bus_number_ac = PSY.get_number(PSY.get_bus(d))
-        _add_to_jump_expression!(
-            expression_ac[bus_number_ac, t],
-            variable[name, t],
-            1.0,
-        )
-        _add_to_jump_expression!(
-            expression_dc[bus_number_dc, t],
-            variable[name, t],
-            -1.0,
-        )
+        for t in get_time_steps(container)
+            _add_to_jump_expression!(
+                sys_expr[ref_bus, t],
+                variable[name, t],
+                get_variable_multiplier(U(), V, W()),
+            )
+            _add_to_jump_expression!(
+                expression_ac[bus_number_ac, t],
+                variable[name, t],
+                get_variable_multiplier(U(), V, W()),
+            )
+            _add_to_jump_expression!(
+                expression_dc[bus_number_dc, t],
+                variable[name, t],
+                -1.0,
+            )
+        end
     end
     return
 end
